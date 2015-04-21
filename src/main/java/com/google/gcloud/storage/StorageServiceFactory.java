287215f35c72947a218b92930eaf3a5ce38fc761
aozarov <aozarov@gmail.com>
2015-04-21 11:35:32 -0700

    work in progress

--- src/main/java/com/google/gcloud/storage/StorageServiceFactory.java
+++ src/main/java/com/google/gcloud/storage/StorageServiceFactory.java
@@ -21,14 +21,13 @@ package com.google.gcloud.storage;
 public abstract class StorageServiceFactory {
 
   private static final StorageServiceFactory INSTANCE = new StorageServiceFactory() {
-      @Override
-      public StorageService get(StorageServiceOptions options) {
+      @Override public StorageService get(StorageServiceOptions options) {
         return new StorageServiceImpl(options);
       }
     };
 
-  public static StorageService getDefault(StorageServiceOptions options) {
-    return INSTANCE.get(options);
+  public static StorageServiceFactory instance() {
+    return INSTANCE;
   }
 
   public abstract StorageService get(StorageServiceOptions options);
