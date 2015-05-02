a4ee293c3d60e1e21bf18621d45895f26918b300
aozarov <aozarov@gmail.com>
2015-05-01 17:39:00 -0700

    work in progress

--- src/main/java/com/google/gcloud/storage/BatchResponse.java
+++ src/main/java/com/google/gcloud/storage/BatchResponse.java
@@ -16,8 +16,87 @@
 
 package com.google.gcloud.storage;
 
+import com.google.common.collect.ImmutableList;
+
+import java.io.Serializable;
+import java.util.List;
+
 /**
- * Created by ozarov on 4/30/15.
+ * Google Storage batch response.
  */
-public class BatchResponse {
+public class BatchResponse implements Serializable {
+
+  private List<Result<Boolean>> deleteResult;
+  private List<Result<Blob>> updateResult;
+  private List<Result<Blob>> getResult;
+
+  public static class Result<T extends Serializable> implements Serializable {
+
+    private static final long serialVersionUID = -1946539570170529094L;
+
+    private final T value;
+    private final StorageServiceException exception;
+
+    Result(T value) {
+      this.value = value;
+      this.exception = null;
+    }
+
+    Result(StorageServiceException exception) {
+      this.exception = exception;
+      this.value = null;
+    }
+
+
+    /**
+     * Returns the result.
+     *
+     * @throws StorageServiceException if failed
+     */
+    public T result() throws StorageServiceException {
+      return value;
+    }
+
+    /**
+     * Returns the failure or {@code null} if was successful.
+     */
+    public StorageServiceException failure() {
+      return exception;
+    }
+
+    /**
+     * Returns {@code true} if failed, {@code false} otherwise.
+     */
+    public boolean failed() {
+      return exception != null;
+    }
+  }
+
+  BatchResponse(List<Result<Boolean>> deleteResult, List<Result<Blob>> updateResult,
+      List<Result<Blob>> getResult) {
+    this.deleteResult = ImmutableList.copyOf(deleteResult);
+    this.updateResult = ImmutableList.copyOf(updateResult);
+    this.getResult = ImmutableList.copyOf(getResult);
+  }
+
+  /**
+   * Returns the results for the delete operations using the request order.
+   */
+  public List<Result<Boolean>> deletes() {
+    return deleteResult;
+  }
+
+  /**
+   * Returns the results for the update operations using the request order.
+   */
+  public List<Result<Blob>> updates() {
+    return updateResult;
+  }
+
+  /**
+   * Returns the results for the get operations using the request order.
+   */
+  public List<Result<Blob>> gets() {
+    return getResult;
+  }
 }
