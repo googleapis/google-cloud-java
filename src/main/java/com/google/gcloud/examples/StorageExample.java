45bd60290c63586a6d5fa5773f3fd37c79832039
aozarov <aozarov@gmail.com>
2015-05-06 20:27:26 -0700

    remove leftover

--- src/main/java/com/google/gcloud/examples/StorageExample.java
+++ src/main/java/com/google/gcloud/examples/StorageExample.java
@@ -40,7 +40,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 /**
  * An example of using the Google Cloud Storage.
@@ -195,7 +194,6 @@ public class StorageExample {
     @Override
     public void run(StorageService storage, Tuple<Path, Blob> tuple) throws Exception {
       if (Files.size(tuple.x()) > 1024) {
-        Random rnd = new Random();
         try (BlobWriteChannel writer = storage.writer(tuple.y())) {
           byte[] buffer = new byte[1024];
           try (InputStream input = Files.newInputStream(tuple.x())) {
