7af8180a4b29fa9675a95a285e1575a88d3ced78
Ajay Kannan <ajaykannan@google.com>
2015-10-08 15:56:59 -0700

    skip the rerun of tests during deployment

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -30,7 +30,7 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
 
         cd ..
         utilities/update_docs_version.sh # Update version in READMEs
-        mvn deploy --settings target/travis/settings.xml -P sign-deploy
+        mvn deploy -DskipTests=true --settings target/travis/settings.xml -P sign-deploy
     else
         mvn deploy -DskipTests=true -Dgpg.skip=true --settings target/travis/settings.xml
     fi
