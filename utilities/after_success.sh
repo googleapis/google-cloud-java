ac5db6aaf9643e90737ee0c9c8dc4f43488abdc5
Ajay Kannan <ajaykannan@google.com>
2015-10-15 12:26:00 -0700

    clean before deploying

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -32,7 +32,7 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
 
         cd ..
         utilities/update_docs_version.sh # Update version in READMEs
-        mvn deploy -DskipTests=true --settings target/travis/settings.xml -P sign-deploy
+        mvn clean deploy --settings ~/.m2/settings.xml -P sign-deploy
     else
         mvn deploy -DskipTests=true -Dgpg.skip=true --settings target/travis/settings.xml
     fi
