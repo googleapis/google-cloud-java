49f9b3f0341e5624c6a0d43be90205a8d06b0346
Ajay Kannan <ajaykannan@google.com>
2015-10-15 13:22:57 -0700

    clean and test for snapshot builds as well

--- utilities/verify.sh
+++ utilities/verify.sh
@@ -5,10 +5,10 @@
 
 if [ "${TRAVIS_BRANCH}" == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     # Get signing tools and API keyfile
-    openssl aes-256-cbc -K $encrypted_631490ecae8f_key -iv $encrypted_631490ecae8f_iv -in target/travis/signing-tools.tar.enc -out target/travis/signing-tools.tar -d
+    openssl aes-256-cbc -K $encrypted_631490ecae8f_key -iv $encrypted_631490ecae8f_iv -in target/travis/signing-tools.tar.enc -out $TRAVIS_BUILD_DIR/signing-tools.tar -d
     mkdir $TRAVIS_BUILD_DIR/signing-tools
     chmod 700 $TRAVIS_BUILD_DIR/signing-tools
-    tar xvf target/travis/signing-tools.tar -C $TRAVIS_BUILD_DIR/signing-tools
+    tar xvf $TRAVIS_BUILD_DIR/signing-tools.tar -C $TRAVIS_BUILD_DIR/signing-tools
     # Export test env variables
     export GCLOUD_TESTS_PROJECT_ID="gcloud-devel"
     export GCLOUD_TESTS_KEY=$TRAVIS_BUILD_DIR/signing-tools/gcloud-devel-travis.json
