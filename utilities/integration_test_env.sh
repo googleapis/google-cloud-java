c3b6616c98982a201c1dcd6a5b1e7d3210d009d6
Marco Ziccardi <marco.ziccard@gmail.com>
2015-11-09 17:04:23 +0100

    Use GOOGLE_APPLICATION_CREDENTIALS and GCLOUD_PROJECT vars in RemoteGcsHelper

--- utilities/integration_test_env.sh
+++ utilities/integration_test_env.sh
@@ -1,3 +1,3 @@
 # Export test env variables
-export GCLOUD_TESTS_PROJECT_ID="gcloud-devel"
-export GCLOUD_TESTS_KEY=$TRAVIS_BUILD_DIR/signing-tools/gcloud-devel-travis.json
+export GCLOUD_PROJECT="gcloud-devel"
+export GOOGLE_APPLICATION_CREDENTIALS=$TRAVIS_BUILD_DIR/signing-tools/gcloud-devel-travis.json
