06becd885dc0d4c45cd779a06be448b5179e77d1
Martin Derka <derka@google.com>
2016-01-28 15:58:13 -0800

    Added DnsException.

---
+++ gcloud-java-dns/src/main/java/com/google/gcloud/dns/DnsException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gcloud.dns;
+
+import com.google.gcloud.BaseServiceException;
+
+/**
+ * DNS service exception.
+ */
+public class DnsException extends BaseServiceException {
+
+  private static final long serialVersionUID = 490302380416260252L;
+
+  public DnsException(int code, String message, boolean retryable) {
+    super(code, message, retryable);
+  }
+
+  public DnsException(int code, String message, boolean retryable, Exception cause) {
+    super(code, message, retryable, cause);
+  }
+
+  //TODO(mderka) Add translation and retry functionality. Created issue #593.
+}
