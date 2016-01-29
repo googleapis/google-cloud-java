81cedc4edf78e745817c831c1d7cde7653dfc4f8
Martin Derka <derka@google.com>
2016-01-28 16:00:19 -0800

    Added DnsServiceRpc.

---
+++ gcloud-java-dns/src/main/java/com/google/gcloud/spi/DnsServiceRpc.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gcloud.spi;
+
+import java.util.Map;
+
+public interface DnsServiceRpc {
+
+  enum Option {
+    FIELDS("fields"),
+    PAGE_SIZE("maxSize"),
+    PAGE_TOKEN("pageToken"),
+    DNS_NAME("dnsName"),
+    SORTING_KEY("sortBy"),
+    SORTING_ORDER("sortOrder");
+
+    private final String value;
+
+    Option(String value) {
+      this.value = value;
+    }
+
+    public String value() {
+      return value;
+    }
+
+    @SuppressWarnings("unchecked")
+    <T> T get(Map<Option, ?> options) {
+      return (T) options.get(this);
+    }
+
+    String getString(Map<Option, ?> options) {
+      return get(options);
+    }
+
+    Integer getInt(Map<Option, ?> options) {
+      return get(options);
+    }
+  }
+
+  //TODO(mderka) add supported operations. Created issue #594.
+}
