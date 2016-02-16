c2f8952befa41581dcad9a9bd8951837d9c7d39f
Ajay Kannan <ajaykannan@google.com>
2016-02-16 14:26:49 -0800

    Expose consistency parameter to users of LocalGcdHelper

--- gcloud-java-datastore/src/test/java/com/google/gcloud/datastore/DatastoreTest.java
+++ gcloud-java-datastore/src/test/java/com/google/gcloud/datastore/DatastoreTest.java
@@ -118,7 +118,7 @@ public class DatastoreTest {
   @BeforeClass
   public static void beforeClass() throws IOException, InterruptedException {
     if (!LocalGcdHelper.isActive(PROJECT_ID, PORT)) {
-      gcdHelper = LocalGcdHelper.start(PROJECT_ID, PORT);
+      gcdHelper = LocalGcdHelper.start(PROJECT_ID, PORT, 1.0);
     }
   }
 
