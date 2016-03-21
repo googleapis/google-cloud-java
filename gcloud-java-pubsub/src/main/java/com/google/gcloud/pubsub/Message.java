ecacdec0cc454409b88f0621c4e74d2dc18a1bb6
Arie Ozarov <ozarov@google.com>
2016-03-20 22:19:29 -0700

    Adding data types and define API

---
+++ gcloud-java-pubsub/src/main/java/com/google/gcloud/pubsub/Message.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.gcloud.pubsub;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.api.client.util.Strings;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableMap;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Timestamp;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Pub/Sub message.
+ */
+public final class Message implements Serializable {
+
+  private static final long serialVersionUID = -1436515787233340634L;
+  private static final long NANOS_PER_MILLISECOND = 1000000;
+  private static final long MILLIS_PER_SECOND = 1000;
+
+  private final String id;
+  private final ByteString payload;
+  private final ImmutableMap<String, String> attributes;
+  private final Long publishTime;
+
+  /**
+   * Builder for Message.
+   */
+  public static final class Builder {
+
+    private String id;
+    private byte[] payload;
+    private Map<String, String> attributes = new HashMap<>();
+    private Long publishTime;
+
+    private Builder() {}
+
+    private Builder(Message message) {
+      id = message.id;
+      payload = message.payload.toByteArray();
+      attributes = new HashMap<>(message.attributes);
+      publishTime = message.publishTime;
+    }
+
+    Builder id(String id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder payload(byte[] payload) {
+      this.payload = checkNotNull(payload);
+      return this;
+    }
+
+    public Builder addAttribute(String name, String value) {
+      attributes.put(name, value);
+      return this;
+    }
+
+    public Builder removeAttribute(String name) {
+      attributes.remove(name);
+      return this;
+    }
+
+    public Builder clearAttributes() {
+      attributes.clear();
+      return this;
+    }
+
+    Builder publishTime(Long publishTime) {
+      this.publishTime = publishTime;
+      return this;
+    }
+
+    public Message build() {
+      return new Message(this);
+    }
+  }
+
+  private Message(Builder builder) {
+    id = builder.id;
+    payload = ByteString.copyFrom(checkNotNull(builder.payload));
+    attributes = ImmutableMap.copyOf(builder.attributes);
+    publishTime = builder.publishTime;
+  }
+
+  public Long publishTime() {
+    return publishTime;
+  }
+
+  public Map<String, String> attributes() {
+    return attributes;
+  }
+
+  public String id() {
+    return id;
+  }
+
+  public byte[] payload() {
+    return payload.toByteArray();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return Objects.equals(toPb(), ((Message)o).toPb());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(serialVersionUID, id, payload, attributes, publishTime);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("id", id)
+        .add("payload", payload)
+        .add("attributes", attributes)
+        .add("publishTime", publishTime)
+        .toString();
+  }
+
+  com.google.pubsub.v1.PubsubMessage toPb() {
+    com.google.pubsub.v1.PubsubMessage.Builder builder =
+        com.google.pubsub.v1.PubsubMessage.newBuilder();
+    if (id != null) {
+      builder.setMessageId(id);
+    }
+    builder.setData(payload);
+    builder.getAttributes().putAll(attributes);
+    if (publishTime != null) {
+      Timestamp.Builder tsBuilder = Timestamp.newBuilder();
+      tsBuilder.setSeconds(publishTime / MILLIS_PER_SECOND);
+      tsBuilder.setNanos((int) (publishTime % MILLIS_PER_SECOND * NANOS_PER_MILLISECOND));
+      builder.setPublishTime(tsBuilder);
+    }
+    return builder.build();
+  }
+
+  static Message fromPb(com.google.pubsub.v1.PubsubMessage messagePb) {
+    Builder builder = builder();
+    if (messagePb.hasPublishTime()) {
+      Timestamp ts = messagePb.getPublishTime();
+      builder.publishTime(
+          ts.getSeconds() * MILLIS_PER_SECOND + ts.getNanos() / NANOS_PER_MILLISECOND);
+    }
+    if (Strings.isNullOrEmpty(messagePb.getMessageId())) {
+      builder.id(messagePb.getMessageId());
+    }
+    for (Map.Entry<String, String> entry : messagePb.getAttributes().entrySet()) {
+      builder.addAttribute(entry.getKey(), entry.getValue());
+    }
+    builder.payload(messagePb.getData().toByteArray());
+    return builder.build();
+  }
+
+  public Builder toBuilder() {
+    return new Builder(this);
+  }
+
+  public static Message of(byte[] payload) {
+    return builder().payload(payload).build();
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+}
---
+++ gcloud-java-pubsub/src/main/java/com/google/gcloud/pubsub/TopicInfo.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gcloud.pubsub;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.base.MoreObjects;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Pub/Sub Topic information.
+ */
+public class TopicInfo implements Serializable {
+
+  private static final long serialVersionUID = -5907624842808725353L;
+
+  private final String name;
+
+  /**
+   * Builder for TopicInfo.
+   */
+  public abstract static class Builder {
+
+    public abstract Builder name(String name);
+
+    public abstract TopicInfo build();
+  }
+
+  static final class BuilderImpl extends Builder {
+
+    private String name;
+
+    BuilderImpl() {
+    }
+
+    BuilderImpl(TopicInfo topicInfo) {
+      this.name = topicInfo.name;
+    }
+
+    public Builder name(String name) {
+      this.name = checkNotNull(name);
+      return this;
+    }
+
+    public TopicInfo build() {
+      return new TopicInfo(this);
+    }
+  }
+
+  TopicInfo(BuilderImpl builder) {
+    name = builder.name;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    return Objects.equals(toPb(), ((TopicInfo) obj).toPb());
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("name", name)
+        .toString();
+  }
+
+  com.google.pubsub.v1.Topic toPb() {
+    return com.google.pubsub.v1.Topic.newBuilder().setName(name).build();
+  }
+
+  static TopicInfo fromPb(com.google.pubsub.v1.Topic topicPb) {
+    return builder().name(topicPb.getName()).build();
+  }
+
+  public Builder toBuilder() {
+    return new BuilderImpl(this);
+  }
+
+  public static Builder builder() {
+    return new BuilderImpl();
+  }
+}
