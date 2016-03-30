8497772edcbfd0c0b508e840960f789990836b29
Marco Ziccardi <marco.ziccard@gmail.com>
2016-03-30 14:57:00 +0200

    Refactor field selection and helpers - Add FieldSelector.SelectorHelper with static methods to create selectors - Add SelectorHelperTest - Rename dns.AbstractOption to Option, make other Option classes abstract - Add OptionTest class to resource manager and refactor other OptionTest classes

--- java-core/google-cloud-core/src/main/java/com/google/gcloud/FieldSelector.java
+++ java-core/google-cloud-core/src/main/java/com/google/gcloud/FieldSelector.java
@@ -16,9 +16,18 @@
 
 package com.google.gcloud;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Interface for Google Cloud resource's fields. Implementations of this interface can be used to
- * select only desired fields when getting or listing Google Cloud resources.
+ * select only desired fields from a returned Google Cloud resource.
  */
 public interface FieldSelector {
 
@@ -27,4 +36,61 @@ public interface FieldSelector {
    * other field selectors) to specify which resource fields should be returned by an API call.
    */
   String selector();
+
+  /**
+   * A helper class used to build composite selectors given a number of fields. This class is not
+   * supposed to be used directly by users.
+   */
+  class SelectorHelper {
+
+    private SelectorHelper() {}
+
+    private static final Function<FieldSelector, String> FIELD_TO_STRING_FUNCTION =
+        new Function<FieldSelector, String>() {
+          @Override
+          public String apply(FieldSelector fieldSelector) {
+            return fieldSelector.selector();
+          }
+        };
+
+    private static String selector(List<FieldSelector> required, FieldSelector[] others,
+      String... extraResourceFields) {
+      Set<String> fieldStrings = Sets.newHashSetWithExpectedSize(required.size() + others.length);
+      fieldStrings.addAll(Lists.transform(required, FIELD_TO_STRING_FUNCTION));
+      fieldStrings.addAll(Lists.transform(Arrays.asList(others), FIELD_TO_STRING_FUNCTION));
+      fieldStrings.addAll(Arrays.asList(extraResourceFields));
+      return Joiner.on(',').join(fieldStrings);
+    }
+
+    /**
+     * Returns a composite selector given a number of fields. The string selector returned by this
+     * method can be used for field selection in API calls that return a single resource. This
+     * method is not supposed to be used directly by users.
+     */
+    public static String selector(List<FieldSelector> required, FieldSelector... others) {
+      return selector(required, others, new String[]{});
+    }
+
+    /**
+     * Returns a composite selector given a number of fields and a container name. The string
+     * selector returned by this method can be used for field selection in API calls that return a
+     * list of resources. This method is not supposed to be used directly by users.
+     */
+    public static String selector(String containerName, List<FieldSelector> required,
+        FieldSelector... others) {
+      return "nextPageToken," + containerName + '(' + selector(required, others) + ')';
+    }
+
+    /**
+     * Returns a composite selector given a number of fields and a container name. This methods also
+     * takes an {@code extraResourceFields} parameter to specify some extra fields as strings. The
+     * string selector returned by this method can be used for field selection in API calls that
+     * return a list of resources. This method is not supposed to be used directly by users.
+     */
+    public static String selector(String containerName, List<FieldSelector> required,
+        FieldSelector[] others, String... extraResourceFields) {
+      return "nextPageToken," + containerName + '('
+          + selector(required, others, extraResourceFields) + ')';
+    }
+  }
 }
