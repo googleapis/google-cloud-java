a9b294cd31918dbb60ee48cebc11efdb3a804b6c
JP Martin <jpmartin@google.com>
2016-05-03 11:05:57 -0700

    Fix byte counting
    Added licence text, plus some esthetic changes.

--- gcloud-java-examples/src/main/java/com/google/cloud/examples/nio/CountBytes.java
+++ gcloud-java-examples/src/main/java/com/google/cloud/examples/nio/CountBytes.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.examples.nio;
 
 import com.google.common.base.Stopwatch;
@@ -14,7 +30,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * CountBytes will read through the whole file given as input.
  *
- * <p>It's meant for testing that NIO correctly saturates your network link.
+ * <p>It's meant for testing that NIO doesn't read too slowly.
  *
  * <p>See the README for compilation instructions. Run this code with
  * {@code target/appassembler/bin/CountBytes <file>}
@@ -53,16 +69,18 @@ public class CountBytes {
       SeekableByteChannel chan = Files.newByteChannel(path);
       long total = 0;
       int readCalls = 0;
-      for (int read = 1; read > 0; ) {
-        readCalls ++;
-        read = chan.read(buf);
+      while (chan.read(buf) > 0) {
+        readCalls++;
+        total += buf.position();
         buf.flip();
-        total += read;
       }
+      readCalls++; // We must count the last call
       long elapsed = sw.elapsed(TimeUnit.SECONDS);
-      System.out.println("Read all " + total + " bytes in " + elapsed + "s. (" + readCalls +" calls to chan.read)");
+      System.out.println("Read all " + total + " bytes in " + elapsed + "s. " +
+          "(" + readCalls +" calls to chan.read)");
       if (total != size) {
-        System.out.println("Wait, this doesn't match! We saw " + total + " bytes, yet the file size is listed at " + size + " bytes.");
+        System.out.println("Wait, this doesn't match! We saw " + total + " bytes, " +
+            "yet the file size is listed at " + size + " bytes.");
       }
     } catch (Exception ex) {
       System.out.println(fname + ": " + ex.toString());
@@ -72,11 +90,10 @@ public class CountBytes {
   private static void help() {
     String[] help =
         {"The argument is a <path>",
-         "and we show the length of that file.",
+         "and we show the length of that file."
         };
     for (String s : help) {
       System.out.println(s);
     }
   }
-
 }
