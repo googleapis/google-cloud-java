858650685e9bf6ed9040ed434bd6f2b1647280b2
Marco Ziccardi <marco.ziccard@gmail.com>
2016-05-04 09:34:56 +0200

    Add AsyncPage implementation and docs. Add related tests (#974)

--- gcloud-java-core/src/main/java/com/google/cloud/AsyncPage.java
+++ gcloud-java-core/src/main/java/com/google/cloud/AsyncPage.java
@@ -18,6 +18,34 @@ package com.google.cloud;
 
 import java.util.concurrent.Future;
 
+/**
+ * Interface for asynchronously consuming Google Cloud paginated results.
+ *
+ * <p>Use {@code AsyncPage} to iterate through all values (also in next pages):
+ * <pre> {@code
+ * AsyncPage<T> page = ...; // get an AsyncPage<T> instance
+ * Iterator<T> iterator = page.iterateAll();
+ * while (iterator.hasNext()) {
+ *   T value = iterator.next();
+ *   // do something with value
+ * }}</pre>
+ *
+ * <p>Or handle pagination explicitly:
+ * <pre> {@code
+ * AsyncPage<T> page = ...; // get a AsyncPage<T> instance
+ * while (page != null) {
+ *   for (T value : page.values()) {
+ *     // do something with value
+ *   }
+ *   page = page.nextPage().get();
+ * }}</pre>
+ *
+ * @param <T> the value type that the page holds
+ */
 public interface AsyncPage<T> extends Page<T> {
+
+  /**
+   * Returns a {@link Future} object for the next page.
+   */
   Future<AsyncPage<T>> nextPageAsync();
 }
