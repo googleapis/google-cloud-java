ba5a8b1398fdde11d7423be07a968ff31356c5f5
Marco Ziccardi <marco.ziccard@gmail.com>
2016-05-12 08:40:31 +0200

    Add tests and javadoc for Message and ByteArray (#1000)
    * Add tests and javadoc for Message and ByteArray
    
    * Add final on static ByteArray methods back

--- java-core/google-cloud-core/src/main/java/com/google/cloud/ByteArray.java
+++ java-core/google-cloud-core/src/main/java/com/google/cloud/ByteArray.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc. All Rights Reserved.
+ * Copyright 2016 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +20,6 @@ import com.google.common.base.MoreObjects;
 import com.google.common.base.MoreObjects.ToStringHelper;
 import com.google.protobuf.ByteString;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
@@ -35,6 +33,7 @@ import java.util.Iterator;
 public class ByteArray implements Iterable<Byte>, Serializable {
 
   private static final long serialVersionUID = -1908809133893782840L;
+
   private final ByteString byteString;
 
   protected ByteArray(ByteString byteString) {
@@ -75,43 +74,38 @@ public class ByteArray implements Iterable<Byte>, Serializable {
   }
 
   /**
-   * Returns the size of this blob.
+   * Returns the number of bytes in this {@code ByteArray}.
    */
   public final int length() {
     return byteString.size();
   }
 
   /**
-   * Returns a copy as byte array.
+   * Returns a copy of this {@code ByteArray} as an array of bytes.
    */
   public final byte[] toByteArray() {
     return byteString.toByteArray();
   }
 
   /**
-   * Returns the content as {@code UTF-8} string.
+   * Returns a copy of this {@code ByteArray} as an {@code UTF-8} string.
    */
   public final String toStringUtf8() {
     return byteString.toStringUtf8();
   }
 
   /**
-   * Returns a read-only {@link ByteBuffer} for this blob content.
+   * Returns the content of this {@code ByteArray} as a read-only {@link ByteBuffer}.
    */
   public final ByteBuffer asReadOnlyByteBuffer() {
     return byteString.asReadOnlyByteBuffer();
   }
 
   /**
-   * Returns an {@link InputStream} for this blob content.
+   * Returns an {@link InputStream} for this {@code ByteArray} content.
    */
   public final InputStream asInputStream() {
-    final ByteBuffer byteBuffer = asReadOnlyByteBuffer();
-    return new InputStream() {
-      @Override public int read() {
-        return !byteBuffer.hasRemaining() ? -1 : byteBuffer.get() & 0xFF;
-      }
-    };
+    return byteString.newInput();
   }
 
   protected ByteString byteString() {
@@ -119,47 +113,52 @@ public class ByteArray implements Iterable<Byte>, Serializable {
   }
 
   /**
-   * Copies bytes into a ByteBuffer.
+   * Copies the content of this {@code ByteArray} into an existing {@code ByteBuffer}.
    *
    * @throws java.nio.ReadOnlyBufferException if the target is read-only
-   * @throws java.nio.BufferOverflowException if the target's remaining() space is not large
-   *        enough to hold the data
+   * @throws java.nio.BufferOverflowException if the target's {@link ByteBuffer#remaining()} space
+   *     is not large enough to hold the data
    */
   public final void copyTo(ByteBuffer target) {
     byteString.copyTo(target);
   }
 
   /**
-   * Copies bytes into a buffer.
+   * Copies the content of this {@code ByteArray} into an array of bytes.
    *
-   * @throws IndexOutOfBoundsException if an offset or size is negative or too large
+   * @throws IndexOutOfBoundsException if the target is not large enough to hold the data
    */
   public final void copyTo(byte[] target) {
     byteString.copyTo(target, 0, 0, length());
   }
 
-  public static final ByteArray copyFrom(byte[] bytes) {
+  /**
+   * Creates a {@code ByteArray} object given an array of bytes. The bytes are copied.
+   */
+  public final static ByteArray copyFrom(byte[] bytes) {
     return new ByteArray(ByteString.copyFrom(bytes));
   }
 
   /**
-   * Copy the bytes using {@code UTF-8} decoding.
+   * Creates a {@code ByteArray} object given a string. The string is encoded in {@code UTF-8}. The
+   * bytes are copied.
    */
-  public static final ByteArray copyFrom(String string) {
+  public final static ByteArray copyFrom(String string) {
     return new ByteArray(ByteString.copyFrom(string, StandardCharsets.UTF_8));
   }
 
-  public static final ByteArray copyFrom(ByteBuffer bytes) {
+  /**
+   * Creates a {@code ByteArray} object given a {@link ByteBuffer}. The bytes are copied.
+   */
+  public final static ByteArray copyFrom(ByteBuffer bytes) {
     return new ByteArray(ByteString.copyFrom(bytes));
   }
 
-  public static final ByteArray copyFrom(InputStream input) throws IOException {
-    BufferedInputStream bufferedInput = new BufferedInputStream(input);
-    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    int value;
-    while ((value = bufferedInput.read()) != -1) {
-      bytes.write(value);
-    }
-    return copyFrom(bytes.toByteArray());
+  /**
+   * Creates a {@code ByteArray} object given an {@link InputStream}. The stream is read into the
+   * created object.
+   */
+  public final static ByteArray copyFrom(InputStream input) throws IOException {
+    return new ByteArray(ByteString.readFrom(input));
   }
 }
