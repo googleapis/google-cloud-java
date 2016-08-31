f7c7a15757519ea0dd20a3a05eeef3b1ce91d1ce
Marco Ziccardi <marco.ziccard@gmail.com>
2016-08-31 09:42:15 +0200

    Remove crm_iam_policy_check resource descriptor from logging tests

--- google-cloud-logging/src/test/java/com/google/cloud/logging/BaseSystemTest.java
+++ google-cloud-logging/src/test/java/com/google/cloud/logging/BaseSystemTest.java
@@ -62,11 +62,11 @@ public abstract class BaseSystemTest {
 
   private static final Set<String> DESCRIPTOR_TYPES = ImmutableSet.of("gce_instance", "gae_app",
       "cloudsql_database", "api", "gcs_bucket", "global", "dataflow_step", "build",
-      "app_script_function", "dataproc_cluster", "ml_job", "bigquery_resource",
-      "crm_iam_policy_check", "container", "gke_cluster", "cloud_debugger_resource",
-      "http_load_balancer", "aws_ec2_instance", "client_auth_config_brand",
-      "client_auth_config_client", "logging_log", "logging_sink", "metric", "project",
-      "testservice_matrix", "service_account", "deployment", "dns_managed_zone");
+      "app_script_function", "dataproc_cluster", "ml_job", "bigquery_resource", "container",
+      "gke_cluster", "cloud_debugger_resource", "http_load_balancer", "aws_ec2_instance",
+      "client_auth_config_brand", "client_auth_config_client", "logging_log", "logging_sink",
+      "metric", "project", "testservice_matrix", "service_account", "deployment",
+      "dns_managed_zone");
 
   @Rule
   public ExpectedException thrown = ExpectedException.none();
