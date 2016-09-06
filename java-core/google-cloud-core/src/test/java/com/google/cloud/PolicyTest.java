83d86401b7646ea215e884e305f7e2bd8043c156
Marco Ziccardi <marco.ziccard@gmail.com>
2016-09-06 22:36:17 +0200

    Refactor IAM-related classes to be ready to support more services (#1224)
    Refactor IAM-related classes to be ready to support more services
      - Remove resourcemanager's Policy class
      - Rename core's IamPolicy to Policy
      - Add Role class to hold string values for roles
      - Add Policy.Marshaller and Policy.DefaultMarshaller classes to convert Policy to/from gRPC protos
      - Add PolicyMarshaller to resourcemanager to convert Policy to/from resourcemanager's protos
      - Update READMEs, javadoc and examples 
      - Add factory methods for primitive roles, other minor fixes

--- java-core/google-cloud-core/src/test/java/com/google/cloud/PolicyTest.java
+++ java-core/google-cloud-core/src/test/java/com/google/cloud/PolicyTest.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import com.google.cloud.Policy.DefaultMarshaller;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
@@ -33,7 +34,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class IamPolicyTest {
+public class PolicyTest {
 
   private static final Identity ALL_USERS = Identity.allUsers();
   private static final Identity ALL_AUTH_USERS = Identity.allAuthenticatedUsers();
@@ -42,123 +43,101 @@ public class IamPolicyTest {
       Identity.serviceAccount("service-account@gmail.com");
   private static final Identity GROUP = Identity.group("group@gmail.com");
   private static final Identity DOMAIN = Identity.domain("google.com");
-  private static final Map<String, ImmutableSet<Identity>> BINDINGS = ImmutableMap.of(
-      "viewer",
+  private static final Role VIEWER = Role.viewer();
+  private static final Role EDITOR = Role.editor();
+  private static final Role OWNER = Role.owner();
+  private static final Map<Role, ImmutableSet<Identity>> BINDINGS = ImmutableMap.of(
+      VIEWER,
       ImmutableSet.of(USER, SERVICE_ACCOUNT, ALL_USERS),
-      "editor",
+      EDITOR,
       ImmutableSet.of(ALL_AUTH_USERS, GROUP, DOMAIN));
-  private static final PolicyImpl SIMPLE_POLICY = PolicyImpl.builder()
-      .addIdentity("viewer", USER, SERVICE_ACCOUNT, ALL_USERS)
-      .addIdentity("editor", ALL_AUTH_USERS, GROUP, DOMAIN)
+  private static final Policy SIMPLE_POLICY = Policy.builder()
+      .addIdentity(VIEWER, USER, SERVICE_ACCOUNT, ALL_USERS)
+      .addIdentity(EDITOR, ALL_AUTH_USERS, GROUP, DOMAIN)
       .build();
-  private static final PolicyImpl FULL_POLICY =
-      new PolicyImpl.Builder(SIMPLE_POLICY.bindings(), "etag", 1).build();
-
-  static class PolicyImpl extends IamPolicy<String> {
-
-    static class Builder extends IamPolicy.Builder<String, Builder> {
-
-      private Builder() {}
-
-      private Builder(Map<String, Set<Identity>> bindings, String etag, Integer version) {
-        bindings(bindings).etag(etag).version(version);
-      }
-
-      @Override
-      public PolicyImpl build() {
-        return new PolicyImpl(this);
-      }
-    }
-
-    PolicyImpl(Builder builder) {
-      super(builder);
-    }
-
-    @Override
-    public Builder toBuilder() {
-      return new Builder(bindings(), etag(), version());
-    }
-
-    static Builder builder() {
-      return new Builder();
-    }
-  }
+  private static final Policy FULL_POLICY = Policy.builder()
+      .bindings(SIMPLE_POLICY.bindings())
+      .etag("etag")
+      .version(1)
+      .build();
 
   @Test
   public void testBuilder() {
+    assertEquals(BINDINGS, SIMPLE_POLICY.bindings());
+    assertEquals(null, SIMPLE_POLICY.etag());
+    assertEquals(0, SIMPLE_POLICY.version());
     assertEquals(BINDINGS, FULL_POLICY.bindings());
     assertEquals("etag", FULL_POLICY.etag());
-    assertEquals(1, FULL_POLICY.version().intValue());
-    Map<String, Set<Identity>> editorBinding =
-        ImmutableMap.<String, Set<Identity>>builder().put("editor", BINDINGS.get("editor")).build();
-    PolicyImpl policy = FULL_POLICY.toBuilder().bindings(editorBinding).build();
+    assertEquals(1, FULL_POLICY.version());
+    Map<Role, Set<Identity>> editorBinding =
+        ImmutableMap.<Role, Set<Identity>>builder().put(EDITOR, BINDINGS.get(EDITOR)).build();
+    Policy policy = FULL_POLICY.toBuilder().bindings(editorBinding).build();
     assertEquals(editorBinding, policy.bindings());
     assertEquals("etag", policy.etag());
-    assertEquals(1, policy.version().intValue());
-    policy = SIMPLE_POLICY.toBuilder().removeRole("editor").build();
-    assertEquals(ImmutableMap.of("viewer", BINDINGS.get("viewer")), policy.bindings());
+    assertEquals(1, policy.version());
+    policy = SIMPLE_POLICY.toBuilder().removeRole(EDITOR).build();
+    assertEquals(ImmutableMap.of(VIEWER, BINDINGS.get(VIEWER)), policy.bindings());
     assertNull(policy.etag());
-    assertNull(policy.version());
+    assertEquals(0, policy.version());
     policy = policy.toBuilder()
-        .removeIdentity("viewer", USER, ALL_USERS)
-        .addIdentity("viewer", DOMAIN, GROUP)
+        .removeIdentity(VIEWER, USER, ALL_USERS)
+        .addIdentity(VIEWER, DOMAIN, GROUP)
         .build();
-    assertEquals(ImmutableMap.of("viewer", ImmutableSet.of(SERVICE_ACCOUNT, DOMAIN, GROUP)),
+    assertEquals(ImmutableMap.of(VIEWER, ImmutableSet.of(SERVICE_ACCOUNT, DOMAIN, GROUP)),
         policy.bindings());
     assertNull(policy.etag());
-    assertNull(policy.version());
-    policy = PolicyImpl.builder()
-        .removeIdentity("viewer", USER)
-        .addIdentity("owner", USER, SERVICE_ACCOUNT)
-        .addIdentity("editor", GROUP)
-        .removeIdentity("editor", GROUP)
+    assertEquals(0, policy.version());
+    policy = Policy.builder()
+        .removeIdentity(VIEWER, USER)
+        .addIdentity(OWNER, USER, SERVICE_ACCOUNT)
+        .addIdentity(EDITOR, GROUP)
+        .removeIdentity(EDITOR, GROUP)
         .build();
-    assertEquals(
-        ImmutableMap.of("owner", ImmutableSet.of(USER, SERVICE_ACCOUNT)), policy.bindings());
+    assertEquals(ImmutableMap.of(OWNER, ImmutableSet.of(USER, SERVICE_ACCOUNT)), policy.bindings());
     assertNull(policy.etag());
-    assertNull(policy.version());
+    assertEquals(0, policy.version());
   }
 
   @Test
   public void testIllegalPolicies() {
     try {
-      PolicyImpl.builder().addIdentity(null, USER);
+      Policy.builder().addIdentity(null, USER);
       fail("Null role should cause exception.");
     } catch (NullPointerException ex) {
       assertEquals("The role cannot be null.", ex.getMessage());
     }
     try {
-      PolicyImpl.builder().addIdentity("viewer", null, USER);
+      Policy.builder().addIdentity(VIEWER, null, USER);
       fail("Null identity should cause exception.");
     } catch (NullPointerException ex) {
       assertEquals("Null identities are not permitted.", ex.getMessage());
     }
     try {
-      PolicyImpl.builder().addIdentity("viewer", USER, (Identity[]) null);
+      Policy.builder().addIdentity(VIEWER, USER, (Identity[]) null);
       fail("Null identity should cause exception.");
     } catch (NullPointerException ex) {
       assertEquals("Null identities are not permitted.", ex.getMessage());
     }
     try {
-      PolicyImpl.builder().bindings(null);
+      Policy.builder().bindings(null);
       fail("Null bindings map should cause exception.");
     } catch (NullPointerException ex) {
       assertEquals("The provided map of bindings cannot be null.", ex.getMessage());
     }
     try {
-      Map<String, Set<Identity>> bindings = new HashMap<>();
-      bindings.put("viewer", null);
-      PolicyImpl.builder().bindings(bindings);
+      Map<Role, Set<Identity>> bindings = new HashMap<>();
+      bindings.put(VIEWER, null);
+      Policy.builder().bindings(bindings);
       fail("Null set of identities should cause exception.");
     } catch (NullPointerException ex) {
       assertEquals("A role cannot be assigned to a null set of identities.", ex.getMessage());
     }
     try {
-      Map<String, Set<Identity>> bindings = new HashMap<>();
+      Map<Role, Set<Identity>> bindings = new HashMap<>();
       Set<Identity> identities = new HashSet<>();
       identities.add(null);
-      bindings.put("viewer", identities);
-      PolicyImpl.builder().bindings(bindings);
+      bindings.put(VIEWER, identities);
+      Policy.builder().bindings(bindings);
       fail("Null identity should cause exception.");
     } catch (IllegalArgumentException ex) {
       assertEquals("Null identities are not permitted.", ex.getMessage());
@@ -168,20 +147,20 @@ public class IamPolicyTest {
   @Test
   public void testEqualsHashCode() {
     assertNotNull(FULL_POLICY);
-    PolicyImpl emptyPolicy = PolicyImpl.builder().build();
-    AnotherPolicyImpl anotherPolicy = new AnotherPolicyImpl.Builder().build();
-    assertNotEquals(emptyPolicy, anotherPolicy);
-    assertNotEquals(emptyPolicy.hashCode(), anotherPolicy.hashCode());
+    Policy emptyPolicy = Policy.builder().build();
+    Policy anotherPolicy = Policy.builder().build();
+    assertEquals(emptyPolicy, anotherPolicy);
+    assertEquals(emptyPolicy.hashCode(), anotherPolicy.hashCode());
     assertNotEquals(FULL_POLICY, SIMPLE_POLICY);
     assertNotEquals(FULL_POLICY.hashCode(), SIMPLE_POLICY.hashCode());
-    PolicyImpl copy = SIMPLE_POLICY.toBuilder().build();
+    Policy copy = SIMPLE_POLICY.toBuilder().build();
     assertEquals(SIMPLE_POLICY, copy);
     assertEquals(SIMPLE_POLICY.hashCode(), copy.hashCode());
   }
 
   @Test
   public void testBindings() {
-    assertTrue(PolicyImpl.builder().build().bindings().isEmpty());
+    assertTrue(Policy.builder().build().bindings().isEmpty());
     assertEquals(BINDINGS, SIMPLE_POLICY.bindings());
   }
 
@@ -193,29 +172,21 @@ public class IamPolicyTest {
 
   @Test
   public void testVersion() {
-    assertNull(SIMPLE_POLICY.version());
-    assertEquals(1, FULL_POLICY.version().intValue());
+    assertEquals(0, SIMPLE_POLICY.version());
+    assertEquals(1, FULL_POLICY.version());
   }
 
-  static class AnotherPolicyImpl extends IamPolicy<String> {
-
-    static class Builder extends IamPolicy.Builder<String, Builder> {
-
-      private Builder() {}
-
-      @Override
-      public AnotherPolicyImpl build() {
-        return new AnotherPolicyImpl(this);
-      }
-    }
-
-    AnotherPolicyImpl(Builder builder) {
-      super(builder);
-    }
-
-    @Override
-    public Builder toBuilder() {
-      return new Builder();
-    }
+  @Test
+  public void testDefaultMarshaller() {
+    DefaultMarshaller marshaller = new DefaultMarshaller();
+    Policy emptyPolicy = Policy.builder().build();
+    assertEquals(emptyPolicy, marshaller.fromPb(marshaller.toPb(emptyPolicy)));
+    assertEquals(SIMPLE_POLICY, marshaller.fromPb(marshaller.toPb(SIMPLE_POLICY)));
+    assertEquals(FULL_POLICY, marshaller.fromPb(marshaller.toPb(FULL_POLICY)));
+    com.google.iam.v1.Policy policyPb = com.google.iam.v1.Policy.getDefaultInstance();
+    Policy policy = marshaller.fromPb(policyPb);
+    assertTrue(policy.bindings().isEmpty());
+    assertNull(policy.etag());
+    assertEquals(0, policy.version());
   }
 }
