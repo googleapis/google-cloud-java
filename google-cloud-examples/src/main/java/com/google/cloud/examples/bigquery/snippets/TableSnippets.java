cc8de4abcb604f91cd8b2513fbc8bb3592e2b464
Daniel Tang <tangd@google.com>
2016-09-16 11:09:19 -0700

    add bigquery snippets and tests (in progress)

---
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/bigquery/snippets/TableSnippets.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.examples.bigquery.snippets;
+
+import com.google.cloud.Page;
+import com.google.cloud.bigquery.BigQuery.TableDataListOption;
+import com.google.cloud.bigquery.FieldValue;
+import com.google.cloud.bigquery.InsertAllRequest.RowToInsert;
+import com.google.cloud.bigquery.InsertAllResponse;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.Table;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * EDITING INSTRUCTIONS
+ * This file is referenced in Table’s javadoc. Any change to this file should be reflected in
+ * Table’s javadoc.
+ */
+public class TableSnippets {
+  private final Table table;
+
+  public TableSnippets(Table table) {
+    this.table = table;
+  }
+
+  /**
+   * Example of inserting rows into a table.
+   */
+  // [TARGET insert(Iterable<InsertAllRequest.RowToInsert>)]
+  // [VARIABLE "rowId1"]
+  // [VARIABLE "rowId2"]
+  public InsertAllResponse insert(String rowId1, String rowId2) {
+    // [START insert]
+    List<RowToInsert> rows = new ArrayList<>();
+    Map<String, Object> row1 = new HashMap<>();
+    row1.put("stringField", "value1");
+    row1.put("booleanField", true);
+    Map<String, Object> row2 = new HashMap<>();
+    row2.put("stringField", "value2");
+    row2.put("booleanField", false);
+    rows.add(RowToInsert.of(rowId1, row1));
+    rows.add(RowToInsert.of(rowId2, row2));
+    InsertAllResponse response = table.insert(rows);
+    // do something with response
+    // [END insert]
+    return response;
+  }
+
+  /**
+   * Example of inserting rows into a table which ignores invalid rows.
+   */
+  // [TARGET insert(Iterable<InsertAllRequest.RowToInsert>, boolean, boolean)]
+  // [VARIABLE "rowId1"]
+  // [VARIABLE "rowId2"]
+  public InsertAllResponse insertWithParams(String rowId1, String rowId2) {
+    // [START insertWithParams]
+    List<RowToInsert> rows = new ArrayList<>();
+    Map<String, Object> row1 = new HashMap<>();
+    row1.put("stringField", "value1");
+    row1.put("booleanField", true);
+    Map<String, Object> row2 = new HashMap<>();
+    row2.put("stringField", "value2");
+    row2.put("booleanField", false);
+    rows.add(RowToInsert.of(rowId1, row1));
+    rows.add(RowToInsert.of(rowId2, row2));
+    InsertAllResponse response = table.insert(rows, true, true);
+    // do something with response
+    // [END insertWithParams]
+    return response;
+  }
+
+  /**
+   * Example of getting a paginated list of rows in a table.
+   */
+  // [TARGET list(BigQuery.TableDataListOption...)]
+  public Page<List<FieldValue>> list() {
+    // [START list]
+    Page<List<FieldValue>> page = table.list(TableDataListOption.pageSize(100));
+    // do something with page
+    // [END list]
+    return page;
+  }
+
+  /**
+   * Example of copying a table to a destination table and dataset referenced by name.
+   */
+  // [TARGET copy(String, String, BigQuery.JobOption...)]
+  // [VARIABLE "my_dataset"]
+  // [VARIABLE "my_destination_table"]
+  public Job copy(String datasetName, String tableName) {
+    // [START copy]
+    Job job = table.copy(datasetName, tableName);
+    // do something with job
+    // [END copy]
+    return job;
+  }
+}
---
+++ google-cloud-examples/src/test/java/com/google/cloud/examples/bigquery/snippets/ITTableSnippets.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.examples.bigquery.snippets;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Field.Type;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardTableDefinition;
+import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.bigquery.TableInfo;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+/**
+ * Integration tests for {@link TableSnippets}.
+ */
+public class ITTableSnippets {
+  private static final String TABLE_NAME = "table";
+  private static final String DATASET_NAME = "dataset";
+  public static final TableId TABLE_ID = TableId.of(DATASET_NAME, TABLE_NAME);
+  private static final Logger log = Logger.getLogger(ITTableSnippets.class.getName());
+
+  private static BigQuery bq;
+  private static Table table;
+  private static TableSnippets tableSnippets;
+
+  @BeforeClass
+  public static void beforeClass() {
+    bq = BigQueryOptions.defaultInstance().service();
+    StandardTableDefinition.Builder builder = StandardTableDefinition.builder();
+    builder.schema(Schema.of(
+        Field.of("stringField", Type.string()),
+        Field.of("booleanField", Type.bool())));
+    table = bq.create(TableInfo.of(TABLE_ID, builder.build()));
+    tableSnippets = new TableSnippets(table);
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    bq.delete(TABLE_ID);
+  }
+
+  @Test
+  public void testInsert() {
+    tableSnippets.insert("row1", "row2");
+  }
+}
