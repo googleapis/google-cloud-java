0efb3c129fb119855e02d3f3e98c9d25789f55f2
Kevin Deus <kdeus@google.com>
2016-09-16 15:08:48 -0700

    Made table names unique, added sample data to copy table.

--- google-cloud-examples/src/test/java/com/google/cloud/examples/bigquery/snippets/ITTableSnippets.java
+++ google-cloud-examples/src/test/java/com/google/cloud/examples/bigquery/snippets/ITTableSnippets.java
@@ -18,20 +18,25 @@ package com.google.cloud.examples.bigquery.snippets;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
+import com.google.cloud.Page;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.DatasetInfo;
 import com.google.cloud.bigquery.Field;
 import com.google.cloud.bigquery.Field.Type;
+import com.google.cloud.bigquery.InsertAllRequest.RowToInsert;
 import com.google.cloud.bigquery.Job;
 import com.google.cloud.bigquery.FieldValue;
 import com.google.cloud.bigquery.InsertAllResponse;
@@ -40,6 +45,7 @@ import com.google.cloud.bigquery.StandardTableDefinition;
 import com.google.cloud.bigquery.Table;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableInfo;
+import com.google.cloud.bigquery.BigQuery.TableDataListOption;
 import com.google.common.base.Function;
 import com.google.common.base.Objects;
 import com.google.common.collect.FluentIterable;
@@ -50,36 +56,47 @@ import com.google.common.collect.ImmutableSet;
  * Integration tests for {@link TableSnippets}.
  */
 public class ITTableSnippets {
-  private static final String TABLE_NAME = "my_table";
+  private static final String BASE_TABLE_NAME = "my_table";
   private static final String DATASET_NAME = "my_dataset";
   private static final String COPY_DATASET_NAME = "my_copy_dataset";
   private static final Value ROW1 = new Value("value1", true);
   private static final Value ROW2 = new Value("value2", false);
-  private static final TableId TABLE_ID = TableId.of(DATASET_NAME, TABLE_NAME);
-  private static final TableId COPY_TABLE_ID = TableId.of(COPY_DATASET_NAME, TABLE_NAME);
+  // TODO: Make the copy table name unique.
+  private static final TableId COPY_TABLE_ID = TableId.of(COPY_DATASET_NAME, BASE_TABLE_NAME);
   private static final Logger log = Logger.getLogger(ITTableSnippets.class.getName());
 
-  private static Table table;
-  private static Table copyTable;
-  private static TableSnippets tableSnippets;
-  private static BigQuery bigquery;
+  private Table table;
+  private Table copyTable;
+  private TableSnippets tableSnippets;
+  private BigQuery bigquery;
+  
+  private static int nextTableNumber = 0;
+  
+  private String GetTableName() {
+    return BASE_TABLE_NAME + nextTableNumber;
+  }
+  
+  private TableId GetTableId() {
+    return TableId.of(DATASET_NAME, GetTableName());
+  }
 
-  @BeforeClass
-  public static void beforeClass() {
+  @Before
+  public void before() {
+    ++nextTableNumber;
     bigquery = BigQueryOptions.defaultInstance().service();
     bigquery.create(DatasetInfo.builder(DATASET_NAME).build());
     bigquery.create(DatasetInfo.builder(COPY_DATASET_NAME).build());
     StandardTableDefinition.Builder builder = StandardTableDefinition.builder();
     builder.schema(
         Schema.of(Field.of("stringField", Type.string()), Field.of("booleanField", Type.bool())));
-    table = bigquery.create(TableInfo.of(TABLE_ID, builder.build()));
+    table = bigquery.create(TableInfo.of(GetTableId(), builder.build()));
     copyTable = bigquery.create(TableInfo.of(COPY_TABLE_ID, builder.build()));
     tableSnippets = new TableSnippets(table);
   }
 
-  @AfterClass
-  public static void afterClass() {
-    bigquery.delete(TABLE_ID);
+  @After
+  public void after() {
+    bigquery.delete(GetTableId());
     bigquery.delete(COPY_TABLE_ID);
     bigquery.delete(DATASET_NAME);
     bigquery.delete(COPY_DATASET_NAME);
@@ -89,6 +106,8 @@ public class ITTableSnippets {
   public void testInsert() throws InterruptedException {
     InsertAllResponse response = tableSnippets.insert("row1", "row2");
     assertFalse(response.hasErrors());
+    
+    // TODO: Replace the following with VerifyTestRows(table)?
     List<List<FieldValue>> rows = ImmutableList.copyOf(tableSnippets.list().values());
     while (rows.size() != 2) {
       Thread.sleep(500);
@@ -135,22 +154,68 @@ public class ITTableSnippets {
     }
   }
   
+  /**
+   * Inserts some data into the test table.
+   */
+  private void InsertTestRows() {
+    log.info("Inserting test rows...");
+    String rowId1 = "row1";
+    String rowId2 = "row2";
+    List<RowToInsert> rows = new ArrayList<>();
+    Map<String, Object> row1 = new HashMap<>();
+    row1.put("stringField", ROW1.stringField);
+    row1.put("booleanField", ROW1.booleanField);
+    Map<String, Object> row2 = new HashMap<>();
+    row2.put("stringField", ROW2.stringField);
+    row2.put("booleanField", ROW2.booleanField);
+    rows.add(RowToInsert.of(rowId1, row1));
+    rows.add(RowToInsert.of(rowId2, row2));
+    table.insert(rows);
+  }
+  
+  /**
+   * Verifies that the given table has the rows inserted by InsertTestRows().
+   * @param checkTable The table to query.
+   */
+  private void VerifyTestRows(Table checkTable) {
+    List<List<FieldValue>> rows = WaitForTableRows(checkTable, 2);
+    // Verify that the table data matches what it's supposed to.
+    Set<Value> values =
+        FluentIterable.from(rows).transform(new Function<List<FieldValue>, Value>() {
+          @Override
+          public Value apply(List<FieldValue> row) {
+            return new Value(row.get(0).stringValue(), row.get(1).booleanValue());
+          }
+        }).toSet();
+    assertEquals(ImmutableSet.of(ROW2, ROW1), values);
+  }
+
+  private List<List<FieldValue>> WaitForTableRows(Table checkTable, int numRows) {
+    // Wait for the data to appear.
+    Page<List<FieldValue>> page = checkTable.list(TableDataListOption.pageSize(100));
+    List<List<FieldValue>> rows = ImmutableList.copyOf(page.values());
+    int numSleeps = 0;
+    while (rows.size() != numRows) {
+      assertTrue(numSleeps < 10);
+      log.info("Sleeping and waiting for " + numRows + " test rows to appear (currently "
+          + rows.size() + ")...");
+      try {
+        ++numSleeps;
+        Thread.sleep(5000);
+      } catch(InterruptedException e) {
+      }
+      page = checkTable.list(TableDataListOption.pageSize(100));
+      rows = ImmutableList.copyOf(page.values());
+    }
+    return rows;
+  }
+  
   @Test
   public void testCopyTableId() {
-    final String destinationTableName = "copy_destination";
-    StandardTableDefinition.Builder builder = StandardTableDefinition.builder();
-    builder.schema(Schema.of(
-        Field.of("stringField", Type.string()),
-        Field.of("booleanField", Type.bool())));
-    TableId destinationId = TableId.of(DATASET_NAME, destinationTableName);
-    Table destinationTable = bigquery.create(TableInfo.of(destinationId, builder.build()));
+    InsertTestRows();
+    VerifyTestRows(table);
+    log.info("Found test rows in original table.");
 
-    try {
-      Job job = tableSnippets.copyTableId(DATASET_NAME, destinationTableName);
-      
-      // TODO: Test that the destination table has the same contents as the source.
-    } finally {
-      bigquery.delete(destinationId);
-    }
+    tableSnippets.copyTableId(COPY_DATASET_NAME, BASE_TABLE_NAME);
   }
 }
