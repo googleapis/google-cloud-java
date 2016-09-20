4fe00dd9c0dddf9177010eb1c1368b21af2c9a1a
Marco Ziccardi <marco.ziccard@gmail.com>
2016-09-20 13:16:38 +0200

    Add missing snippets to Datastore Transaction, add snippets to Transaction javadoc (#1270)

--- google-cloud-examples/src/main/java/com/google/cloud/examples/datastore/snippets/TransactionSnippets.java
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/datastore/snippets/TransactionSnippets.java
@@ -22,12 +22,19 @@
 
 package com.google.cloud.examples.datastore.snippets;
 
+import com.google.api.client.util.Lists;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreException;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
 import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
 import com.google.cloud.datastore.Transaction;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * This class contains a number of snippets for the {@link Transaction} interface.
@@ -41,6 +48,95 @@ public class TransactionSnippets {
   }
 
   /**
+   * Example of getting an entity for a given key.
+   */
+  // [TARGET get(Key)]
+  // [VARIABLE "my_key_name"]
+  public Entity get(String keyName) {
+    Datastore datastore = transaction.datastore();
+    // [START get]
+    Key key = datastore.newKeyFactory().kind("MyKind").newKey(keyName);
+    Entity entity = transaction.get(key);
+    transaction.commit();
+    // Do something with the entity
+    // [END get]
+    return entity;
+  }
+
+  /**
+   * Example of getting entities for several keys.
+   */
+  // [TARGET get(Key...)]
+  // [VARIABLE "my_first_key_name"]
+  // [VARIABLE "my_second_key_name"]
+  public List<Entity> getMultiple(String firstKeyName, String secondKeyName) {
+    Datastore datastore = transaction.datastore();
+    // TODO change so that it's not necessary to hold the entities in a list for integration testing
+    // [START getMultiple]
+    KeyFactory keyFactory = datastore.newKeyFactory().kind("MyKind");
+    Key firstKey = keyFactory.newKey(firstKeyName);
+    Key secondKey = keyFactory.newKey(secondKeyName);
+    Iterator<Entity> entitiesIterator = transaction.get(firstKey, secondKey);
+    List<Entity> entities = Lists.newArrayList();
+    while (entitiesIterator.hasNext()) {
+      Entity entity = entitiesIterator.next();
+      // do something with the entity
+      entities.add(entity);
+    }
+    transaction.commit();
+    // [END getMultiple]
+    return entities;
+  }
+
+  /**
+   * Example of fetching a list of entities for several keys.
+   */
+  // [TARGET fetch(Key...)]
+  // [VARIABLE "my_first_key_name"]
+  // [VARIABLE "my_second_key_name"]
+  public List<Entity> fetchEntitiesWithKeys(String firstKeyName, String secondKeyName) {
+    Datastore datastore = transaction.datastore();
+    // [START fetchEntitiesWithKeys]
+    KeyFactory keyFactory = datastore.newKeyFactory().kind("MyKind");
+    Key firstKey = keyFactory.newKey(firstKeyName);
+    Key secondKey = keyFactory.newKey(secondKeyName);
+    List<Entity> entities = transaction.fetch(firstKey, secondKey);
+    for (Entity entity : entities) {
+      // do something with the entity
+    }
+    transaction.commit();
+    // [END fetchEntitiesWithKeys]
+    return entities;
+  }
+
+  /**
+   * Example of running a query to find all entities with an ancestor.
+   */
+  // [TARGET run(Query)]
+  // [VARIABLE "my_parent_key_name"]
+  public List<Entity> run(String parentKeyName) {
+    Datastore datastore = transaction.datastore();
+    // [START run]
+    KeyFactory keyFactory = datastore.newKeyFactory().kind("ParentKind");
+    Key parentKey = keyFactory.newKey(parentKeyName);
+    // Build a query
+    Query<Entity> query = Query.entityQueryBuilder()
+        .kind("MyKind")
+        .filter(PropertyFilter.hasAncestor(parentKey))
+        .build();
+    QueryResults<Entity> results = transaction.run(query);
+    List<Entity> entities = Lists.newArrayList();
+    while (results.hasNext()) {
+      Entity result = results.next();
+      // do something with result
+      entities.add(result);
+    }
+    transaction.commit();
+    // [END run]
+    return entities;
+  }
+
+  /**
    * Example of committing a transaction.
    */
   // [TARGET commit()]
@@ -48,9 +144,10 @@ public class TransactionSnippets {
     Datastore datastore = transaction.datastore();
     // [START commit]
     // create an entity
-    KeyFactory keyFactory = datastore.newKeyFactory().kind("someKind");
+    KeyFactory keyFactory = datastore.newKeyFactory().kind("MyKind");
     Key key = datastore.allocateId(keyFactory.newKey());
     Entity entity = Entity.builder(key).set("description", "commit()").build();
+
     // add the entity and commit
     try {
       transaction.put(entity);
@@ -59,6 +156,7 @@ public class TransactionSnippets {
       // handle exception
     }
     // [END commit]
+
     return key;
   }
 
@@ -70,7 +168,7 @@ public class TransactionSnippets {
     Datastore datastore = transaction.datastore();
     // [START rollback]
     // create an entity
-    KeyFactory keyFactory = datastore.newKeyFactory().kind("someKind");
+    KeyFactory keyFactory = datastore.newKeyFactory().kind("MyKind");
     Key key = datastore.allocateId(keyFactory.newKey());
     Entity entity = Entity.builder(key).set("description", "rollback()").build();
 
@@ -90,7 +188,7 @@ public class TransactionSnippets {
     Datastore datastore = transaction.datastore();
     // [START active]
     // create an entity
-    KeyFactory keyFactory = datastore.newKeyFactory().kind("someKind");
+    KeyFactory keyFactory = datastore.newKeyFactory().kind("MyKind");
     Key key = datastore.allocateId(keyFactory.newKey());
     Entity entity = Entity.builder(key).set("description", "active()").build();
     // calling transaction.active() now would return true
