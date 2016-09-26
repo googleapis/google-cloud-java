d8631513f0a222a0d853ab04518bf0e4ba1ec1c5
Marco Ziccardi <marco.ziccard@gmail.com>
2016-09-26 13:35:44 +0200

    Remove workaround for ObjectAccessControl collections bug

--- google-cloud-storage/src/main/java/com/google/cloud/storage/spi/DefaultStorageRpc.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/spi/DefaultStorageRpc.java
@@ -93,32 +93,6 @@ public class DefaultStorageRpc implements StorageRpc {
   private final Storage storage;
 
   private static final long MEGABYTE = 1024L * 1024L;
-  private static final Function<Object, ObjectAccessControl> FROM_OBJECT_TO_ACL_FUNCTION =
-      new Function<Object, ObjectAccessControl>() {
-        @Override
-        @SuppressWarnings("unchecked")
-        public ObjectAccessControl apply(Object obj) {
-          ObjectAccessControl acl = new ObjectAccessControl();
-          Map<String, ?> map = (Map<String, ?>) obj;
-          for (Map.Entry<String, ?> entry : map.entrySet()) {
-            String key = entry.getKey();
-            switch (key) {
-              case "projectTeam":
-                ObjectAccessControl.ProjectTeam projectTeam = new ObjectAccessControl.ProjectTeam();
-                projectTeam.putAll((Map<String, ?>) entry.getValue());
-                acl.set(key, projectTeam);
-                break;
-              case "generation":
-                acl.set(entry.getKey(), Long.parseLong((String) entry.getValue()));
-                break;
-              default:
-                acl.set(entry.getKey(), entry.getValue());
-                break;
-            }
-          }
-          return acl;
-        }
-      };
 
   public DefaultStorageRpc(StorageOptions options) {
     HttpTransport transport = options.httpTransportFactory().create();
@@ -785,11 +759,7 @@ public class DefaultStorageRpc implements StorageRpc {
   @Override
   public List<ObjectAccessControl> listDefaultAcls(String bucket) {
     try {
-      // TODO(mziccard) remove when https://github.com/google/google-api-java-client/issues/1022 is
-      // fixed
-      return Lists.transform(
-          storage.defaultObjectAccessControls().list(bucket).execute().getItems(),
-          FROM_OBJECT_TO_ACL_FUNCTION);
+      return storage.defaultObjectAccessControls().list(bucket).execute().getItems();
     } catch (IOException ex) {
       throw translate(ex);
     }
@@ -852,13 +822,9 @@ public class DefaultStorageRpc implements StorageRpc {
   @Override
   public List<ObjectAccessControl> listAcls(String bucket, String object, Long generation) {
     try {
-      // TODO(mziccard) remove when https://github.com/google/google-api-java-client/issues/1022 is
-      // fixed
-      return Lists.transform(
-          storage.objectAccessControls().list(bucket, object)
-              .setGeneration(generation)
-              .execute().getItems(),
-          FROM_OBJECT_TO_ACL_FUNCTION);
+      return storage.objectAccessControls().list(bucket, object)
+          .setGeneration(generation)
+          .execute().getItems();
     } catch (IOException ex) {
       throw translate(ex);
     }
