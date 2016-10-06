731524ebbc7f1493622e083b1d0ff7c673ae370a
Jason Dobry <jason.dobry@gmail.com>
2016-10-06 16:31:51 -0700

    Add Datastore quickstart sample.

---
+++ samples/snippets/src/main/java/com/example/datastore/QuickstartSample.java
@@ -0,0 +1,46 @@
+/*
+  Copyright 2016, Google, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.example.datastore;
+
+// [START bigquery_quickstart]
+// Imports the Google Cloud client library
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+
+public class QuickstartSample {
+  public static void main(String... args) throws Exception {
+    // Instantiates a client
+    Datastore datastore = DatastoreOptions.defaultInstance().service();
+    // The kind for the new entity
+    String kind = "Task";
+    // The name/ID for the new entity
+    String name = "sampletask1";
+    // The Cloud Datastore key for the new entity
+    Key taskKey = datastore.newKeyFactory().kind(kind).newKey(name);
+    // Prepares the new entity
+    Entity task = Entity.builder(taskKey)
+      .set("description", "Buy milk")
+      .build();
+    // Saves the entity
+    datastore.put(task);
+    System.out.printf("Saved %s: %s%n", task.key().name(), task.getString("description"));
+  }
+}
+// [END bigquery_quickstart]
