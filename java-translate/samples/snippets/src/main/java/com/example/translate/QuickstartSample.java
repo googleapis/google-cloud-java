0efc857c08f2aceb9a5a260af57b6f6225f58efa
Jason Dobry <jason.dobry@gmail.com>
2016-10-07 10:23:52 -0700

    samples: Add Storage quickstart sample.

--- java-translate/samples/snippets/src/main/java/com/example/translate/QuickstartSample.java
+++ java-translate/samples/snippets/src/main/java/com/example/translate/QuickstartSample.java
@@ -27,14 +27,17 @@ public class QuickstartSample {
   public static void main(String... args) throws Exception {
     // Instantiates a client
     Translate translate = TranslateOptions.builder().apiKey("YOUR_API_KEY").build().service();
+
     // The text to translate
     String text = "Hello, world!";
+
     // Translates some text into Russian
     Translation translation = translate.translate(
       text,
       TranslateOption.sourceLanguage("en"),
       TranslateOption.targetLanguage("ru")
     );
+
     System.out.printf("Text: %s%n", text);
     System.out.printf("Translation: %s%n", translation.translatedText());
   }
