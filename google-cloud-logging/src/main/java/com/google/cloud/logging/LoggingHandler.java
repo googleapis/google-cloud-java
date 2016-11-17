9fd9d0e7955c1a45248856546deb1f5d3207c6ba
Greg Wilkins <gregw@webtide.com>
2016-11-18 09:12:14 +1100

    Issue #1329
    Cleanedup imports

--- google-cloud-logging/src/main/java/com/google/cloud/logging/LoggingHandler.java
+++ google-cloud-logging/src/main/java/com/google/cloud/logging/LoggingHandler.java
@@ -26,7 +26,6 @@ import com.google.common.collect.ImmutableSet;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.logging.ErrorManager;
 import java.util.logging.Filter;
