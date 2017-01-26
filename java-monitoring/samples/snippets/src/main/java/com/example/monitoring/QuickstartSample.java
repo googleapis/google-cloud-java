8191453ba3f04f12665651a3f24ecd7149bcd2b3
Jason Dobry <jason.dobry@gmail.com>
2017-01-25 16:44:58 -0800

    samples: Add Monitoring quickstart sample.

---
+++ java-monitoring/samples/snippets/src/main/java/com/example/monitoring/QuickstartSample.java
@@ -0,0 +1,107 @@
+/*
+  Copyright 2017, Google, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.example.monitoring;
+
+// [START monitoring_quickstart]
+import com.google.api.Metric;
+import com.google.api.MonitoredResource;
+
+// Imports the Google Cloud client library
+import com.google.cloud.monitoring.spi.v3.MetricServiceClient;
+
+import com.google.monitoring.v3.CreateTimeSeriesRequest;
+import com.google.monitoring.v3.Point;
+import com.google.monitoring.v3.ProjectName;
+import com.google.monitoring.v3.TimeInterval;
+import com.google.monitoring.v3.TimeSeries;
+import com.google.monitoring.v3.TypedValue;
+import com.google.protobuf.util.Timestamps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class QuickstartSample {
+  public static void main(String... args) throws Exception {
+    // Your Google Cloud Platform project ID
+    String projectId = System.getProperty("projectId");
+
+    if (projectId == null) {
+      System.err.println("Usage: QuickstartSample -DprojectId=YOUR_PROJECT_ID");
+      return;
+    }
+
+    // Instantiates a client
+    MetricServiceClient metricServiceClient = MetricServiceClient.create();
+
+    // Prepares an individual data point
+    TimeInterval interval = TimeInterval.newBuilder()
+        .setEndTime(Timestamps.fromMillis(System.currentTimeMillis()))
+        .build();
+    TypedValue value = TypedValue.newBuilder()
+        .setDoubleValue(123.45)
+        .build();
+    Point point = Point.newBuilder()
+        .setInterval(interval)
+        .setValue(value)
+        .build();
+
+    List<Point> pointList = new ArrayList<>();
+    pointList.add(point);
+
+    ProjectName name = ProjectName.create(projectId);
+
+    // Prepares the metric descriptor
+    Map<String, String> metricLabels = new HashMap<String, String>();
+    metricLabels.put("store_id", "Pittsburg");
+    Metric metric = Metric.newBuilder()
+        .setType("custom.googleapis.com/stores/daily_sales")
+        .putAllLabels(metricLabels)
+        .build();
+
+    // Prepares the monitored resource descriptor
+    Map<String, String> resourceLabels = new HashMap<String, String>();
+    resourceLabels.put("project_id", projectId);
+    MonitoredResource resource = MonitoredResource.newBuilder()
+        .setType("global")
+        .putAllLabels(resourceLabels)
+        .build();
+
+    // Prepares the time series request
+    TimeSeries timeSeries = TimeSeries.newBuilder()
+        .setMetric(metric)
+        .setResource(resource)
+        .addAllPoints(pointList)
+        .build();
+    List<TimeSeries> timeSeriesList = new ArrayList<>();
+    timeSeriesList.add(timeSeries);
+
+    CreateTimeSeriesRequest request = CreateTimeSeriesRequest.newBuilder()
+        .setNameWithProjectName(name)
+        .addAllTimeSeries(timeSeriesList)
+        .build();
+
+    // Writes time series data
+    metricServiceClient.createTimeSeries(request);
+
+    System.out.printf("Done writing time series data.%n");
+
+    metricServiceClient.close();
+  }
+}
+// [END monitoring_quickstart]
---
+++ java-monitoring/samples/snippets/src/test/java/com/example/monitoring/QuickstartSampleIT.java
@@ -0,0 +1,72 @@
+/*
+  Copyright 2016, Google, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.example.monitoring;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Tests for quickstart sample.
+ */
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public class QuickstartSampleIT {
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+  private static final String LEGACY_PROJECT_ENV_NAME = "GCLOUD_PROJECT";
+  private static final String PROJECT_ENV_NAME = "GOOGLE_CLOUD_PROJECT";
+
+  private static String getProjectId() {
+    String projectId = System.getProperty(PROJECT_ENV_NAME, System.getenv(PROJECT_ENV_NAME));
+    if (projectId == null) {
+      projectId = System.getProperty(LEGACY_PROJECT_ENV_NAME,
+          System.getenv(LEGACY_PROJECT_ENV_NAME));
+    }
+    return projectId;
+  }
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    System.setOut(null);
+  }
+
+  @Test
+  public void testQuickstart() throws Exception {
+    // Act
+    System.setProperty("projectId", QuickstartSampleIT.getProjectId());
+    QuickstartSample.main();
+
+    // Assert
+    String got = bout.toString();
+    assertThat(got).contains("Done writing time series data.");
+  }
+}
