8fd7fab6e0a89787f48a20d1a0cfa793f348934b
Michael Darakananda <pongad@google.com>
2017-04-07 15:29:51 +1000

    remove last use of setPort/setServiceAddress (#1880)
    Fixes #1835.

--- java-core/google-cloud-core/src/main/java/com/google/cloud/GrpcTransportOptions.java
+++ java-core/google-cloud-core/src/main/java/com/google/cloud/GrpcTransportOptions.java
@@ -214,9 +214,7 @@ public class GrpcTransportOptions implements TransportOptions {
    */
   public static ChannelProvider setUpChannelProvider(
       InstantiatingChannelProvider.Builder providerBuilder, ServiceOptions<?, ?> serviceOptions) {
-    HostAndPort hostAndPort = HostAndPort.fromString(serviceOptions.getHost());
-    providerBuilder.setServiceAddress(hostAndPort.getHostText())
-        .setPort(hostAndPort.getPort())
+    providerBuilder.setEndpoint(serviceOptions.getHost())
         .setClientLibHeader(ServiceOptions.getGoogApiClientLibName(),
             firstNonNull(ServiceOptions.getLibraryVersion(), ""));
     Credentials scopedCredentials = serviceOptions.getScopedCredentials();
