7c29209e00fc8f343d47d59d1c81a612e160145f
Shin Fan <shinfan@google.com>
2017-04-25 13:48:19 -0700

    Make LocalStorageHelper a utility class

--- google-cloud-contrib/google-cloud-nio/src/main/java/com/google/cloud/storage/contrib/nio/testing/LocalStorageHelper.java
+++ google-cloud-contrib/google-cloud-nio/src/main/java/com/google/cloud/storage/contrib/nio/testing/LocalStorageHelper.java
@@ -24,10 +24,12 @@ import com.google.cloud.storage.StorageOptions;
  * Utility to create an in-memory storage configuration for testing. Storage options can be
  * obtained via the {@link #getOptions()} method. Returned options will point to FakeStorageRpc.
  */
-public class LocalStorageHelper {
+public final class LocalStorageHelper {
 
   // used for testing. Will throw if you pass it an option.
   private static final FakeStorageRpc instance = new FakeStorageRpc(true);
+
+  private LocalStorageHelper() {}
 
   /**
    * Returns a {@link StorageOptions} that use the static FakeStorageRpc instance, and resets it
