6dfd73b4793b8508f4ac59634dc6be2fcc4eb9f4
Mairbek Khadikov <mairbek@google.com>
2017-06-02 14:06:19 -0700

    Typo fix

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/GrpcResultSetTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/GrpcResultSetTest.java
@@ -628,7 +628,7 @@ public class GrpcResultSetTest {
     );
   }
 
-  @Test public void nesetedStructSerialization() throws Exception {
+  @Test public void nestedStructSerialization() throws Exception {
     Type.StructField[] structFields = {
         Type.StructField.of("a", Type.string()),
         Type.StructField.of("b", Type.int64())
