b6202633b0b46c644e3992ea9aafaca17bd060c3
Androbin <robin.richtsfeld@gmail.com>
2017-06-10 13:30:17 +0200

    Prefer read -a to split variable

--- utilities/update_versions.sh
+++ utilities/update_versions.sh
@@ -125,7 +125,7 @@ done
 echo -e "\n${BOLD}Validating explicitly specified modules${NC}"
 declare -A specified_modules_map
 if [ "${MODULES}" != "" ]; then
-  specified_modules_arr=(${MODULES//,/ })
+  IFS="," read -r -a specified_modules_arr <<< "${MODULES}"
 
   for item in ${specified_modules_arr[*]}; do
     if [ "${module_version_map[${item}]}" != "" ]; then
