702f3001153da9beb9eb14b2afc0c1225b4ebae1
Jisha Abubaker <jishaa@google.com>
2017-06-19 16:22:12 -0700

    samples: updating to latest google-cloud-* dependencies (#723)

--- java-language/samples/snippets/src/main/java/com/example/language/QuickstartSample.java
+++ java-language/samples/snippets/src/main/java/com/example/language/QuickstartSample.java
@@ -18,10 +18,9 @@ package com.example.language;
 
 // [START language_quickstart]
 // Imports the Google Cloud client library
-import com.google.cloud.language.spi.v1.LanguageServiceClient;
-
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 
 public class QuickstartSample {
