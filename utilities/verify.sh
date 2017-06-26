fd6cdc9d74672f313ea8ab6dd8ef1b4810e5497e
Thomas Coffee <tcoffee@google.com>
2017-06-26 14:58:48 -0700

    Fix Travis setup order for API key integration test

--- utilities/verify.sh
+++ utilities/verify.sh
@@ -5,13 +5,13 @@
 mvn_opts="-DtrimStackTrace=false -fae --quiet -Djava.util.logging.config.file=logging.properties -P release"
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    source ./utilities/integration_test_env.sh
     # Get signing tools and API keyfile
     openssl aes-256-cbc -K $encrypted_631490ecae8f_key -iv $encrypted_631490ecae8f_iv -in target/travis/signing-tools.tar.enc -out $TRAVIS_BUILD_DIR/signing-tools.tar -d
     mkdir $TRAVIS_BUILD_DIR/signing-tools
     chmod 700 $TRAVIS_BUILD_DIR/signing-tools
     tar xvf $TRAVIS_BUILD_DIR/signing-tools.tar -C $TRAVIS_BUILD_DIR/signing-tools
     echo $api_key_password | gpg --passphrase-fd 0 target/travis/api_key.gpg --output $TRAVIS_BUILD_DIR/signing-tools/api_key
+    source ./utilities/integration_test_env.sh
 else
     # Skip integration tests when testing pull requests.
     mvn_opts+=" -DskipITs"
