7d159fddcbc35d80aa6e78378d12a0992c8708fa
Tim Swast <swast@google.com>
2017-07-26 16:47:42 -0700

    Use HTTPS links and redirects for docs
    Replaces https://github.com/GoogleCloudPlatform/google-cloud-java/pull/2200

--- utilities/create_site.sh
+++ utilities/create_site.sh
@@ -15,7 +15,7 @@ fi
 git clone --branch gh-pages --single-branch https://github.com/GoogleCloudPlatform/google-cloud-java/ tmp_gh-pages
 mkdir -p tmp_gh-pages/$SITE_VERSION_BASE
 mvn site -DskipTests -Djava.util.logging.config.file=logging.properties
-mvn site:stage --quiet -Djava.util.logging.config.file=logging.properties -DtopSiteURL=http://googlecloudplatform.github.io/google-cloud-java/site/${SITE_VERSION_BASE}/
+mvn site:stage --quiet -Djava.util.logging.config.file=logging.properties -DtopSiteURL=https://googlecloudplatform.github.io/google-cloud-java/site/${SITE_VERSION_BASE}/
 cd tmp_gh-pages
 
 for dir in ../target/staging/${SITE_VERSION_BASE}*
@@ -25,7 +25,7 @@ done
 
 sed -i "s/{{SITE_VERSION}}/$SITE_VERSION/g" ${SITE_VERSION_BASE}/index.html # Update "Quickstart with Maven" to reflect version change
 git add $SITE_VERSION_BASE
-echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://GoogleCloudPlatform.github.io/google-cloud-java/${SITE_VERSION_BASE}/index.html'\" /></head><body></body></html>" > index.html
+echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='https://googlecloudplatform.github.io/google-cloud-java/${SITE_VERSION_BASE}/index.html'\" /></head><body></body></html>" > index.html
 git add index.html
 echo "<html><head><script>window.location.replace('/google-cloud-java/${SITE_VERSION_BASE}/apidocs' + location.search)</script></head><body></body></html>" > apidocs/index.html
 git add apidocs/index.html
