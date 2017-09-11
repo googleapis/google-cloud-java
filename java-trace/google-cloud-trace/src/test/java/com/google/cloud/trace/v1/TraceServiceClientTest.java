149d6e102211c994d764608256fa27b4e40a7f30
Shin Fan <shinfan@google.com>
2017-09-11 15:51:32 -0700

    Sync GAPIC clients with latest toolchain (#2429)
    - Update dependencies
      - gax -> 1.8.0
      - google-auth -> 0.8.0
      - grpc -> 1.6.1
      - generated proto -> 0.1.17

--- java-trace/google-cloud-trace/src/test/java/com/google/cloud/trace/v1/TraceServiceClientTest.java
+++ java-trace/google-cloud-trace/src/test/java/com/google/cloud/trace/v1/TraceServiceClientTest.java
@@ -66,7 +66,7 @@ public class TraceServiceClientTest {
   public void setUp() throws IOException {
     serviceHelper.reset();
     TraceServiceSettings settings =
-        TraceServiceSettings.defaultBuilder()
+        TraceServiceSettings.newBuilder()
             .setTransportProvider(
                 GrpcTransportProvider.newBuilder()
                     .setChannelProvider(serviceHelper.createChannelProvider())
