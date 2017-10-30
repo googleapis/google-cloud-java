88a6cde6859b40115ce00fb804bc6dd423e402fc
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2017-10-30 11:19:41 -0700

    Adding google-cloud-dialogflow

---
+++ google-cloud-dialogflow/src/main/java/com/google/cloud/dialogflow/v2beta1/package-info.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2017, Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A client to Dialogflow API.
+ *
+ * <p>The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>============ AgentsClient ============
+ *
+ * <p>Service Description: Manages conversational agents.
+ *
+ * <p>Refer to [documentation](https://api.ai/docs/agents) for more details about # agents.
+ *
+ * <p>Standard methods.
+ *
+ * <p>Sample for AgentsClient:
+ *
+ * <pre>
+ * <code>
+ * try (AgentsClient agentsClient = AgentsClient.of()) {
+ *   ProjectName parent = ProjectName.create("[PROJECT]");
+ *   Agent response = agentsClient.getAgent(parent);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ============== ContextsClient ==============
+ *
+ * <p>Service Description: Manages contexts.
+ *
+ * <p>Refer to [documentation](https://api.ai/docs/contexts) for more details about # contexts.
+ *
+ * <p>Standard methods.
+ *
+ * <p>Sample for ContextsClient:
+ *
+ * <pre>
+ * <code>
+ * try (ContextsClient contextsClient = ContextsClient.of()) {
+ *   ContextName name = ContextName.create("[PROJECT]", "[SESSION]", "[CONTEXT]");
+ *   Context response = contextsClient.getContext(name);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ================= EntityTypesClient =================
+ *
+ * <p>Service Description: Manages agent entity types.
+ *
+ * <p>Refer to [documentation](https://api.ai/docs/entities) for more details about # entity types.
+ *
+ * <p>Standard methods.
+ *
+ * <p>Sample for EntityTypesClient:
+ *
+ * <pre>
+ * <code>
+ * try (EntityTypesClient entityTypesClient = EntityTypesClient.of()) {
+ *   EntityTypeName name = EntityTypeName.create("[PROJECT]", "[ENTITY_TYPE]");
+ *   EntityType response = entityTypesClient.getEntityType(name);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ============= IntentsClient =============
+ *
+ * <p>Service Description: Manages agent intents.
+ *
+ * <p>Refer to [documentation](https://api.ai/docs/intents) for more details about # agent intents.
+ *
+ * <p>Standard methods.
+ *
+ * <p>Sample for IntentsClient:
+ *
+ * <pre>
+ * <code>
+ * try (IntentsClient intentsClient = IntentsClient.of()) {
+ *   IntentName name = IntentName.create("[PROJECT]", "[INTENT]");
+ *   Intent response = intentsClient.getIntent(name);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ======================== SessionEntityTypesClient ========================
+ *
+ * <p>Service Description: Manages session entity types.
+ *
+ * <p>Session entity types can be redefined on a session level, allowing for specific concepts, like
+ * a user's playlists.
+ *
+ * <p>Standard methods.
+ *
+ * <p>Sample for SessionEntityTypesClient:
+ *
+ * <pre>
+ * <code>
+ * try (SessionEntityTypesClient sessionEntityTypesClient = SessionEntityTypesClient.of()) {
+ *   SessionEntityTypeName name = SessionEntityTypeName.create("[PROJECT]", "[SESSION]", "[ENTITY_TYPE]");
+ *   SessionEntityType response = sessionEntityTypesClient.getSessionEntityType(name);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ============== SessionsClient ==============
+ *
+ * <p>Service Description: Manages user sessions.
+ *
+ * <p>Custom methods.
+ *
+ * <p>Sample for SessionsClient:
+ *
+ * <pre>
+ * <code>
+ * try (SessionsClient sessionsClient = SessionsClient.of()) {
+ *   SessionName session = SessionName.create("[PROJECT]", "[SESSION]");
+ *   QueryInput queryInput = QueryInput.newBuilder().build();
+ *   DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+ * }
+ * </code>
+ * </pre>
+ */
+package com.google.cloud.dialogflow.v2beta1;
