2b7391512decd199ba4cb292e9f5086d7766b67a
Andrea Lin <asdf.lina@gmail.com>
2017-11-03 16:59:42 -0700

    remove unused import

--- google-cloud-storage/src/main/java/com/google/cloud/storage/StorageImpl.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/StorageImpl.java
@@ -71,7 +71,6 @@ import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
