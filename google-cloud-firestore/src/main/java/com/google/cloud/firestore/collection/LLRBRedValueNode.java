cbcefca608c5b3e2c66e2c56e63238984ef60ce4
Sebastian Schmidt <mrschmidt@google.com>
2017-11-23 13:46:12 -0500

    Adding License header

--- google-cloud-firestore/src/main/java/com/google/cloud/firestore/collection/LLRBRedValueNode.java
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/collection/LLRBRedValueNode.java
@@ -1,6 +1,20 @@
-package com.google.cloud.firestore.collection;
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static com.google.firebase.database.collection.LLRBNode.Color.RED;
+package com.google.cloud.firestore.collection;
 
 public class LLRBRedValueNode<K, V> extends LLRBValueNode<K, V> {
 
@@ -14,7 +28,7 @@ public class LLRBRedValueNode<K, V> extends LLRBValueNode<K, V> {
 
   @Override
   protected Color getColor() {
-    return RED;
+    return LLRBNode.Color.RED;
   }
 
   @Override
