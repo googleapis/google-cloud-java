5af3b64dc1706ef705d84004957e8b49be16aae3
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2018-01-12 13:52:54 -0800

    Regenerating GAPIC clients (#2774)
    This includes accumulated changes such as:
    - API updates
    - Copyright line updates
    - Toolkit updates

--- java-trace/google-cloud-trace/src/main/java/com/google/cloud/trace/v1/TraceServiceSettings.java
+++ java-trace/google-cloud-trace/src/main/java/com/google/cloud/trace/v1/TraceServiceSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017, Google LLC All rights reserved.
+ * Copyright 2018 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -178,7 +178,7 @@ public class TraceServiceSettings extends ClientSettings<TraceServiceSettings> {
     return new Builder(this);
   }
 
-  private TraceServiceSettings(Builder settingsBuilder) throws IOException {
+  protected TraceServiceSettings(Builder settingsBuilder) throws IOException {
     super(settingsBuilder);
 
     patchTracesSettings = settingsBuilder.patchTracesSettings().build();
@@ -283,11 +283,11 @@ public class TraceServiceSettings extends ClientSettings<TraceServiceSettings> {
       RETRY_PARAM_DEFINITIONS = definitions.build();
     }
 
-    private Builder() {
+    protected Builder() {
       this((ClientContext) null);
     }
 
-    private Builder(ClientContext clientContext) {
+    protected Builder(ClientContext clientContext) {
       super(clientContext);
 
       patchTracesSettings = UnaryCallSettings.newUnaryCallSettingsBuilder();
@@ -332,7 +332,7 @@ public class TraceServiceSettings extends ClientSettings<TraceServiceSettings> {
       return builder;
     }
 
-    private Builder(TraceServiceSettings settings) {
+    protected Builder(TraceServiceSettings settings) {
       super(settings);
 
       patchTracesSettings = settings.patchTracesSettings.toBuilder();
