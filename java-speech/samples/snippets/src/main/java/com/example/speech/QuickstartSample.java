4ba4c999de20d620e180ebce5836fc4884ac01cf
Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
2018-01-18 08:18:34 -0800

    samples: Updated mlengine, monitoring, pubsub, spanner, and speech. (#993)
    * Updated mlengine/online-prediction.
    
    * Updated monitoring/
    
    * Updated pubsub/
    
    * Updated spanner/
    
    * Updated speech/
    
    * Fixed spanner mistakes.

--- java-speech/samples/snippets/src/main/java/com/example/speech/QuickstartSample.java
+++ java-speech/samples/snippets/src/main/java/com/example/speech/QuickstartSample.java
@@ -1,23 +1,24 @@
 /*
-  Copyright 2017, Google Inc.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.speech;
 
 // [START speech_quickstart]
 // Imports the Google Cloud client library
+
 import com.google.cloud.speech.v1.RecognitionAudio;
 import com.google.cloud.speech.v1.RecognitionConfig;
 import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
@@ -26,7 +27,6 @@ import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
 import com.google.cloud.speech.v1.SpeechRecognitionResult;
 import com.google.protobuf.ByteString;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
