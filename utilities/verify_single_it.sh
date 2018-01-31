30497c08eb23396e68eff25b5273e64b57823da9
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2018-01-31 13:50:30 -0800

    CircleCI: Adding speech integration tests (#2826)

--- utilities/verify_single_it.sh
+++ utilities/verify_single_it.sh
@@ -15,13 +15,13 @@ export GCLOUD_PROJECT="gcloud-devel"
 export GOOGLE_APPLICATION_CREDENTIALS=$(realpath .circleci/it-service-account.json)
 
 echo "----- building and installing google-cloud-bom -----"
-mvn -pl google-cloud-bom install -DskipTests
+mvn -B -pl google-cloud-bom install -DskipTests
 
 echo "----- building and installing parent pom -----"
-mvn -N install -DskipTests
+mvn -B -N install -DskipTests
 
 echo "----- building and installing shared modules -----"
-mvn -pl google-cloud-core,google-cloud-core-http,google-cloud-core-grpc install -DskipTests
+mvn -B -pl google-cloud-core,google-cloud-core-http,google-cloud-core-grpc install -DskipTests
 
 echo "----- running integration tests -----"
 mvn -B -pl $MODULE -DtrimStackTrace=false -fae verify
