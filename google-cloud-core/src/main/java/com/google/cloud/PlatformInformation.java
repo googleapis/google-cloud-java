3f0fd629854e69b8deb01e7a88cec71227229088
Michael Darakananda <pongad@google.com>
2018-02-05 13:23:46 +1100

    pr comment

--- google-cloud-core/src/main/java/com/google/cloud/PlatformInformation.java
+++ google-cloud-core/src/main/java/com/google/cloud/PlatformInformation.java
@@ -20,33 +20,15 @@ import com.google.api.core.InternalApi;
 
 @InternalApi("This class should only be used within google-cloud-java")
 public final class PlatformInformation {
-  public static final String SPECIFICATION_VERSION =
-      System.getProperty("java.specification.version");
-  public static final String GAE_RUNTIME_VERSION =
-      System.getProperty("com.google.appengine.runtime.version");
-  public static final String RUNTIME_JETTY_LOGGER =
-      System.getProperty("org.eclipse.jetty.util.log.class");
-  public static final String JETTY_LOGGER_ON_GAE8_PROD =
-      "com.google.apphosting.runtime.jetty9.JettyLogger";
-  public static final String JETTY_LOGGER_ON_GAE8_DEVSERVER =
-      " com.google.appengine.development.jetty9.JettyLogger"; // note the leading space
-  public static final boolean GAE_JAVA7 = "java7".equals(System.getenv("GAE_RUNTIME"));
+  public static final String GAE_RUNTIME = System.getenv("GAE_RUNTIME");
 
   private PlatformInformation() {}
 
-  public static boolean isOnGAE() {
-    return GAE_RUNTIME_VERSION != null;
-  }
-
   public static boolean isOnGAEStandard7() {
-    return GAE_JAVA7;
+    return "java7".equals(GAE_RUNTIME);
   }
 
   public static boolean isOnGAEStandard8() {
-    return isOnGAE()
-        && !SPECIFICATION_VERSION.equals("1.7")
-        && RUNTIME_JETTY_LOGGER != null
-        && (RUNTIME_JETTY_LOGGER.equals(JETTY_LOGGER_ON_GAE8_DEVSERVER)
-            || RUNTIME_JETTY_LOGGER.equals(JETTY_LOGGER_ON_GAE8_PROD));
+    return "java8".equals(GAE_RUNTIME);
   }
 }
