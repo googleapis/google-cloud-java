b26ca3427db7f2fb47b68ca02cf052b9029138a9
Igor Bernstein <igorbernstein@google.com>
2018-03-01 15:31:27 -0500

    fix grammar

--- google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStub.java
+++ google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStub.java
@@ -133,7 +133,8 @@ public class EnhancedBigtableStub implements AutoCloseable {
         filtering.withDefaultCallContext(clientContext.getDefaultCallContext());
 
     // NOTE: Ideally `withDefaultCallContext` should be the outer-most callable, however the
-    // ReadRowsUserCallable overrides the first() method which be lost. At some point in the future,
+    // ReadRowsUserCallable overrides the first() method. This override would be lost if
+    // ReadRowsUserCallable is wrapped by another callable.  At some point in the future,
     // gax-java should allow preserving these kind of overrides through callable chains, at which
     // point this should be re-ordered.
     return new ReadRowsUserCallable<>(withContext, requestContext);
