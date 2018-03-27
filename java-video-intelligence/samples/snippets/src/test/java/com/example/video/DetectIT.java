173830292e4e8c8139889390439fd8ab7c3fb234
Noah Negrey <nnegrey@users.noreply.github.com>
2018-03-26 17:59:31 -0700

    samples: Add video v1p1beta samples for face detection and video transcription (#1070)
    * Add video v1p1beta samples for face detection and video transcription
    
    * Update based on Feedback
    
    * Clean up READMEs
    
    * Add timeout for tests

---
+++ java-video-intelligence/samples/snippets/src/test/java/com/example/video/DetectIT.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.video;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for video analysis sample. */
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public class DetectIT {
+
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+
+  static final String FACES_FILE_LOCATION =
+      "gs://java-docs-samples-testing/video/googlework_short.mp4";
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    System.setOut(null);
+  }
+
+  @Test
+  public void testFacesBoundingBoxes() throws Exception {
+    String[] args = {"faces-bounding-boxes", FACES_FILE_LOCATION};
+    Detect.argsHelper(args);
+    String got = bout.toString();
+
+    assertThat(got).contains("Top:");
+  }
+
+  @Test
+  public void testFacesEmotions() throws Exception {
+    String[] args = {"faces-emotions", FACES_FILE_LOCATION};
+    Detect.argsHelper(args);
+    String got = bout.toString();
+
+    assertThat(got).contains("CONCENTRATION");
+  }
+
+  @Test
+  public void testSpeechTranscription() throws Exception {
+    String[] args = {"speech-transcription", FACES_FILE_LOCATION};
+    Detect.argsHelper(args);
+    String got = bout.toString();
+
+    assertThat(got).contains("cultural");
+  }
+}
