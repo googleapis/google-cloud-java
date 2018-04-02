ca73b1e1202237cf95f79472dfd440a0ab044d46
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2018-04-02 09:35:14 -0700

    Bumping proto/grpc deps and regenerating clients (#3111)

--- java-speech/google-cloud-speech/src/test/java/com/google/cloud/speech/v1p1beta1/SpeechSmokeTest.java
+++ java-speech/google-cloud-speech/src/test/java/com/google/cloud/speech/v1p1beta1/SpeechSmokeTest.java
@@ -19,11 +19,17 @@ import com.google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding;
 import com.google.common.base.Preconditions;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.junit.Test;
 
 @javax.annotation.Generated("by GAPIC")
 public class SpeechSmokeTest {
   private static final String PROJECT_ENV_NAME = "GOOGLE_CLOUD_PROJECT";
   private static final String LEGACY_PROJECT_ENV_NAME = "GCLOUD_PROJECT";
+
+  @Test
+  public void run() {
+    main(null);
+  }
 
   public static void main(String args[]) {
     Logger.getLogger("").setLevel(Level.WARNING);
