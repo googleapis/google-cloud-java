f7a20e6f0a532d3c8ac3a1eb9d77a979594064c0
Vadym Matsishevskyi <25311427+vam-google@users.noreply.github.com>
2018-05-10 10:54:50 -0700

    Move grpc and proto artifacts to google-cloud-java from api-client-staging (second part) (#3251)
    1) Radically change the structure of the repo. Now the root `pom.xml` file is not deployed to maven and is not a parent pom for any of the other modules, but is simply an aggregator pom, which aggregates other mostly independent of each other modules.
    2) Update proto and grpc artifact to the latest generated versions as of time of this PR.
    3) Add `cloudiot-v1` and `websecurityscanner-v1alpha` proto and grp artifacts (not released yet even in api-client-staging). Note, gapic clients for this apis are not added yet.
    4) Rename `google-cloud-pom` parent artifact (for manual and gapic clients) to `google-cloud-clients`.
    5) Move all manual and gapic clients from root directory to `google-cloud-clients` sub directory.
    6) Make `google-cloud-bom` not a child of `google-cloud-clients` (former `google-cloud-pom`) anymore, keep it on root level so it becomes a sibling of `google-cloud-clients` (module which used to be its parent).
    7) Similarly make `google-cloud-examples`, `google-cloud-testing` and `google-cloud-util` not children of `google-cloud-clients` and keep them on root level. Make these three modules also excluded from maven deployment (they will not be published to maven anymore).
    
    After this PR is done, additional work is required to fix circleci individual IT tests runs (should be trivial). Also deployment and documentation scripts must be modified accordingly (will be done right after pushing this PR).

---
+++ java-errorreporting/google-cloud-errorreporting/src/main/java/com/google/cloud/errorreporting/v1beta1/package-info.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A client to Stackdriver Error Reporting API.
+ *
+ * <p>The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>======================= ErrorGroupServiceClient =======================
+ *
+ * <p>Service Description: Service for retrieving and updating individual error groups.
+ *
+ * <p>Sample for ErrorGroupServiceClient:
+ *
+ * <pre>
+ * <code>
+ * try (ErrorGroupServiceClient errorGroupServiceClient = ErrorGroupServiceClient.create()) {
+ *   GroupName groupName = GroupName.of("[PROJECT]", "[GROUP]");
+ *   ErrorGroup response = errorGroupServiceClient.getGroup(groupName);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ======================= ErrorStatsServiceClient =======================
+ *
+ * <p>Service Description: An API for retrieving and managing error statistics as well as data for
+ * individual events.
+ *
+ * <p>Sample for ErrorStatsServiceClient:
+ *
+ * <pre>
+ * <code>
+ * try (ErrorStatsServiceClient errorStatsServiceClient = ErrorStatsServiceClient.create()) {
+ *   ProjectName projectName = ProjectName.of("[PROJECT]");
+ *   DeleteEventsResponse response = errorStatsServiceClient.deleteEvents(projectName);
+ * }
+ * </code>
+ * </pre>
+ *
+ * ========================= ReportErrorsServiceClient =========================
+ *
+ * <p>Service Description: An API for reporting error events.
+ *
+ * <p>Sample for ReportErrorsServiceClient:
+ *
+ * <pre>
+ * <code>
+ * try (ReportErrorsServiceClient reportErrorsServiceClient = ReportErrorsServiceClient.create()) {
+ *   ProjectName projectName = ProjectName.of("[PROJECT]");
+ *   ReportedErrorEvent event = ReportedErrorEvent.newBuilder().build();
+ *   ReportErrorEventResponse response = reportErrorsServiceClient.reportErrorEvent(projectName, event);
+ * }
+ * </code>
+ * </pre>
+ */
+package com.google.cloud.errorreporting.v1beta1;
