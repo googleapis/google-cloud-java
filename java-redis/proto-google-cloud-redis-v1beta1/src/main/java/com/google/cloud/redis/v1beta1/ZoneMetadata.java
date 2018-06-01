cf3f68fa7937bad6fde3dc99456261eedf925096
Andrea Lin <andrealin@google.com>
2018-06-01 13:15:13 -0700

    Update gax-java, api-common dependecies to latest (#3335)
    * update gax deps to latest
    
    * remove ResourceNameType, update api-common to 1.6, update jsr to 3.0.2
    
    * regenerate grpc/proto packages using new protoc-gapic-p:

--- java-redis/proto-google-cloud-redis-v1beta1/src/main/java/com/google/cloud/redis/v1beta1/ZoneMetadata.java
+++ java-redis/proto-google-cloud-redis-v1beta1/src/main/java/com/google/cloud/redis/v1beta1/ZoneMetadata.java
@@ -33,9 +33,6 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
-    if (extensionRegistry == null) {
-      throw new java.lang.NullPointerException();
-    }
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
     try {
@@ -377,7 +374,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-      return new ZoneMetadata(input, extensionRegistry);
+        return new ZoneMetadata(input, extensionRegistry);
     }
   };
 
