417b5f09e46471ed382102fe6a92e9c6181dc1c1
Andrea Lin <andrealin@google.com>
2018-06-01 13:15:13 -0700

    Update gax-java, api-common dependecies to latest (#3335)
    * update gax deps to latest
    
    * remove ResourceNameType, update api-common to 1.6, update jsr to 3.0.2
    
    * regenerate grpc/proto packages using new protoc-gapic-p:

--- java-speech/proto-google-cloud-speech-v1/src/main/java/com/google/cloud/speech/v1/StreamingRecognizeResponse.java
+++ java-speech/proto-google-cloud-speech-v1/src/main/java/com/google/cloud/speech/v1/StreamingRecognizeResponse.java
@@ -69,9 +69,6 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
-    if (extensionRegistry == null) {
-      throw new java.lang.NullPointerException();
-    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -1437,7 +1434,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-      return new StreamingRecognizeResponse(input, extensionRegistry);
+        return new StreamingRecognizeResponse(input, extensionRegistry);
     }
   };
 
