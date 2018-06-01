c6d36a17de1096f62566d5b2db1b2b37709842d7
Andrea Lin <andrealin@google.com>
2018-06-01 13:15:13 -0700

    Update gax-java, api-common dependecies to latest (#3335)
    * update gax deps to latest
    
    * remove ResourceNameType, update api-common to 1.6, update jsr to 3.0.2
    
    * regenerate grpc/proto packages using new protoc-gapic-p:

--- java-container/proto-google-cloud-container-v1/src/main/java/com/google/container/v1/NodePool.java
+++ java-container/proto-google-cloud-container-v1/src/main/java/com/google/container/v1/NodePool.java
@@ -44,9 +44,6 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
-    if (extensionRegistry == null) {
-      throw new java.lang.NullPointerException();
-    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -2379,7 +2376,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-      return new NodePool(input, extensionRegistry);
+        return new NodePool(input, extensionRegistry);
     }
   };
 
