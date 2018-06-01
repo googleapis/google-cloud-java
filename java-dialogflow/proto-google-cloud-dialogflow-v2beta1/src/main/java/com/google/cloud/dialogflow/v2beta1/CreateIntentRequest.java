cbee1907c08e74e5734d01ce1d873d588c8c4274
Andrea Lin <andrealin@google.com>
2018-06-01 13:15:13 -0700

    Update gax-java, api-common dependecies to latest (#3335)
    * update gax deps to latest
    
    * remove ResourceNameType, update api-common to 1.6, update jsr to 3.0.2
    
    * regenerate grpc/proto packages using new protoc-gapic-p:

--- java-dialogflow/proto-google-cloud-dialogflow-v2beta1/src/main/java/com/google/cloud/dialogflow/v2beta1/CreateIntentRequest.java
+++ java-dialogflow/proto-google-cloud-dialogflow-v2beta1/src/main/java/com/google/cloud/dialogflow/v2beta1/CreateIntentRequest.java
@@ -35,9 +35,6 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
-    if (extensionRegistry == null) {
-      throw new java.lang.NullPointerException();
-    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -1061,7 +1058,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-      return new CreateIntentRequest(input, extensionRegistry);
+        return new CreateIntentRequest(input, extensionRegistry);
     }
   };
 
