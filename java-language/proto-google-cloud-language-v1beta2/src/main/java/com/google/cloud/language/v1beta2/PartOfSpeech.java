d682ba8713daf935c5a22ad346664078331438be
Michael Darakananda <pongad@google.com>
2018-06-07 12:32:25 -0700

    regenerate clients (#3354)

--- java-language/proto-google-cloud-language-v1beta2/src/main/java/com/google/cloud/language/v1beta2/PartOfSpeech.java
+++ java-language/proto-google-cloud-language-v1beta2/src/main/java/com/google/cloud/language/v1beta2/PartOfSpeech.java
@@ -44,6 +44,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -4115,7 +4118,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new PartOfSpeech(input, extensionRegistry);
+      return new PartOfSpeech(input, extensionRegistry);
     }
   };
 
