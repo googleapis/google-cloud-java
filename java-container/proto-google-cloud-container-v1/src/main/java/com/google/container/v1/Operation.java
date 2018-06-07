8d8fb4afe68cebb6e03ae14ff2756ba8921c402f
Michael Darakananda <pongad@google.com>
2018-06-07 12:32:25 -0700

    regenerate clients (#3354)

--- java-container/proto-google-cloud-container-v1/src/main/java/com/google/container/v1/Operation.java
+++ java-container/proto-google-cloud-container-v1/src/main/java/com/google/container/v1/Operation.java
@@ -43,6 +43,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -2407,7 +2410,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new Operation(input, extensionRegistry);
+      return new Operation(input, extensionRegistry);
     }
   };
 
--- java-container/proto-google-cloud-container-v1/src/main/java/com/google/container/v1/SetNetworkPolicyRequest.java
+++ java-container/proto-google-cloud-container-v1/src/main/java/com/google/container/v1/SetNetworkPolicyRequest.java
@@ -35,6 +35,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -1088,7 +1091,7 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new SetNetworkPolicyRequest(input, extensionRegistry);
+      return new SetNetworkPolicyRequest(input, extensionRegistry);
     }
   };
 
