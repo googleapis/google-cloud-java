22f11991238f079495f4df94454ab00d623cbcf6
Tim Swast <swast@google.com>
2018-06-08 09:40:24 -0700

    Remove unused Query import

--- google-cloud-examples/src/main/java/com/google/cloud/examples/bigquery/snippets/CloudSnippets.java
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/bigquery/snippets/CloudSnippets.java
@@ -32,7 +32,6 @@ import com.google.cloud.bigquery.TableId;
 import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
-import com.google.datastore.v1.Query;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Instant;
