12dbd0ed79bd56229a341ef877eef723b9c43a2b
Andrea Lin <asdf.lina@gmail.com>
2018-06-15 12:05:57 -0700

    googleJavaFormat on ComputeExample.javag

--- google-cloud-examples/src/main/java/com/google/cloud/examples/compute/v1/ComputeExample.java
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/compute/v1/ComputeExample.java
@@ -4,7 +4,6 @@ import com.google.api.core.ApiFuture;
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
-
 import com.google.cloud.compute.v1.Address;
 import com.google.cloud.compute.v1.AddressClient;
 import com.google.cloud.compute.v1.AddressSettings;
@@ -17,17 +16,16 @@ import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Working example code to make live calls on Addresses resources in a GCP Compute project.
- */
+/** Working example code to make live calls on Addresses resources in a GCP Compute project. */
 public class ComputeExample {
 
   // Replace the following String values with your Project and Region ids.
   private static String PROJECT_NAME = "my-project-id";
   private static String REGION = "us-central1";
 
-  /** List addresses, Insert an address, and then delete the address.
-   *  Use ResourceNames in the request objects.
+  /**
+   * List addresses, Insert an address, and then delete the address. Use ResourceNames in the
+   * request objects.
    */
   public static void main(String[] args) throws IOException {
     AddressClient client = createCredentialedClient();
@@ -45,8 +43,8 @@ public class ComputeExample {
     listAddresses(client);
 
     System.out.println("Deleting address:");
-    Operation deleteResponse = client.deleteAddress(
-        ProjectRegionAddressName.of(newAddressName, PROJECT_NAME, REGION));
+    Operation deleteResponse =
+        client.deleteAddress(ProjectRegionAddressName.of(newAddressName, PROJECT_NAME, REGION));
     System.out.format("Result of delete: %s\n", deleteResponse.toString());
     int sleepTimeInSeconds = 3;
     System.out.format("Waiting %d seconds for server to update...\n", sleepTimeInSeconds);
@@ -68,7 +66,9 @@ public class ComputeExample {
             .setCredentialsProvider(FixedCredentialsProvider.create(myCredentials))
             .setTransportChannelProvider(
                 AddressSettings.defaultHttpJsonTransportProviderBuilder()
-                   .setEndpoint(myEndpoint).build()).build();
+                    .setEndpoint(myEndpoint)
+                    .build())
+            .build();
     return AddressClient.create(addressSettings);
   }
 
@@ -87,10 +87,11 @@ public class ComputeExample {
     // Begin samplegen code for insertAddress().
     ProjectRegionName region = ProjectRegionName.of(PROJECT_NAME, REGION);
     Address address = Address.newBuilder().build();
-    InsertAddressHttpRequest request = InsertAddressHttpRequest.newBuilder()
-        .setRegion(region.toString())
-        .setAddressResource(address)
-        .build();
+    InsertAddressHttpRequest request =
+        InsertAddressHttpRequest.newBuilder()
+            .setRegion(region.toString())
+            .setAddressResource(address)
+            .build();
     // Do something
     Operation response = client.insertAddress(request);
 
@@ -104,10 +105,11 @@ public class ComputeExample {
     // Begin samplegen code for insertAddress().
     ProjectRegionName region = ProjectRegionName.of(PROJECT_NAME, REGION);
     Address address = Address.newBuilder().build();
-    InsertAddressHttpRequest request = InsertAddressHttpRequest.newBuilder()
-        .setRegion(region.toString())
-        .setAddressResource(address)
-        .build();
+    InsertAddressHttpRequest request =
+        InsertAddressHttpRequest.newBuilder()
+            .setRegion(region.toString())
+            .setAddressResource(address)
+            .build();
     ApiFuture<Operation> future = client.insertAddressCallable().futureCall(request);
     // Do something
     Operation response = future.get();
@@ -119,10 +121,10 @@ public class ComputeExample {
   /** List Addresses in the under the project PROJECT_NAME and region REGION. */
   private static AddressClient.ListAddressesPagedResponse listAddresses(AddressClient client) {
     System.out.println("Listing addresses:");
-    ProjectRegionName regionName = ProjectRegionName.newBuilder().setRegion(REGION).setProject(PROJECT_NAME).build();
-    ListAddressesHttpRequest listRequest = ListAddressesHttpRequest.newBuilder()
-        .setRegion(regionName.toString())
-        .build();
+    ProjectRegionName regionName =
+        ProjectRegionName.newBuilder().setRegion(REGION).setProject(PROJECT_NAME).build();
+    ListAddressesHttpRequest listRequest =
+        ListAddressesHttpRequest.newBuilder().setRegion(regionName.toString()).build();
     AddressClient.ListAddressesPagedResponse response = client.listAddresses(listRequest);
     for (Address address : response.iterateAll()) {
       System.out.println("\t - " + address.toString());
@@ -130,11 +132,12 @@ public class ComputeExample {
     return response;
   }
 
-  private static void verifyListAddressWithGets(AddressClient client, AddressClient.ListAddressesPagedResponse listResponse) {
+  private static void verifyListAddressWithGets(
+      AddressClient client, AddressClient.ListAddressesPagedResponse listResponse) {
     for (Address address : listResponse.iterateAll()) {
       System.out.format("Making get request for address \"%s\"...\n", address.getName());
-      Address fetchedAddress = client.getAddress(
-          ProjectRegionAddressName.of(address.getName(), PROJECT_NAME, REGION));
+      Address fetchedAddress =
+          client.getAddress(ProjectRegionAddressName.of(address.getName(), PROJECT_NAME, REGION));
       System.out.format("addresses.get returns \n\t%s\n\n", fetchedAddress.toString());
     }
   }
