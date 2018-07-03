cb21b7d24b057af9433240bf4ce875f47ff31f34
Andrea Lin <asdf.lina@gmail.com>
2018-07-03 14:23:02 -0700

    update proto and grpc packages for logging

--- google-api-grpc/proto-google-cloud-logging-v2/src/main/java/com/google/logging/type/HttpRequest.java
+++ google-api-grpc/proto-google-cloud-logging-v2/src/main/java/com/google/logging/type/HttpRequest.java
@@ -62,13 +62,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 10: {
             java.lang.String s = input.readStringRequireUtf8();
 
@@ -159,6 +152,13 @@ private static final long serialVersionUID = 0L;
             protocol_ = s;
             break;
           }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
+            break;
+          }
         }
       }
     } catch (com.google.protobuf.InvalidProtocolBufferException e) {
@@ -176,6 +176,7 @@ private static final long serialVersionUID = 0L;
     return com.google.logging.type.HttpRequestProto.internal_static_google_logging_type_HttpRequest_descriptor;
   }
 
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.logging.type.HttpRequestProto.internal_static_google_logging_type_HttpRequest_fieldAccessorTable
@@ -624,6 +625,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -633,6 +635,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     if (!getRequestMethodBytes().isEmpty()) {
@@ -683,6 +686,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -914,6 +918,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -921,6 +926,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.logging.type.HttpRequest prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -950,6 +956,7 @@ private static final long serialVersionUID = 0L;
       return com.google.logging.type.HttpRequestProto.internal_static_google_logging_type_HttpRequest_descriptor;
     }
 
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.logging.type.HttpRequestProto.internal_static_google_logging_type_HttpRequest_fieldAccessorTable
@@ -972,6 +979,7 @@ private static final long serialVersionUID = 0L;
               .alwaysUseFieldBuilders) {
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       requestMethod_ = "";
@@ -1011,15 +1019,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.logging.type.HttpRequestProto.internal_static_google_logging_type_HttpRequest_descriptor;
     }
 
+    @java.lang.Override
     public com.google.logging.type.HttpRequest getDefaultInstanceForType() {
       return com.google.logging.type.HttpRequest.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.logging.type.HttpRequest build() {
       com.google.logging.type.HttpRequest result = buildPartial();
       if (!result.isInitialized()) {
@@ -1028,6 +1039,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.logging.type.HttpRequest buildPartial() {
       com.google.logging.type.HttpRequest result = new com.google.logging.type.HttpRequest(this);
       result.requestMethod_ = requestMethod_;
@@ -1053,32 +1065,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.logging.type.HttpRequest) {
         return mergeFrom((com.google.logging.type.HttpRequest)other);
@@ -1147,10 +1166,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -2270,11 +2291,13 @@ private static final long serialVersionUID = 0L;
       onChanged();
       return this;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -2296,6 +2319,7 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<HttpRequest>
       PARSER = new com.google.protobuf.AbstractParser<HttpRequest>() {
+    @java.lang.Override
     public HttpRequest parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -2313,6 +2337,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.logging.type.HttpRequest getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
--- google-api-grpc/proto-google-cloud-logging-v2/src/main/java/com/google/logging/v2/LogEntry.java
+++ google-api-grpc/proto-google-cloud-logging-v2/src/main/java/com/google/logging/v2/LogEntry.java
@@ -51,13 +51,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 18: {
             com.google.protobuf.Any.Builder subBuilder = null;
             if (payloadCase_ == 2) {
@@ -226,6 +219,13 @@ private static final long serialVersionUID = 0L;
             spanId_ = s;
             break;
           }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
+            break;
+          }
         }
       }
     } catch (com.google.protobuf.InvalidProtocolBufferException e) {
@@ -244,6 +244,7 @@ private static final long serialVersionUID = 0L;
   }
 
   @SuppressWarnings({"rawtypes"})
+  @java.lang.Override
   protected com.google.protobuf.MapField internalGetMapField(
       int number) {
     switch (number) {
@@ -254,6 +255,7 @@ private static final long serialVersionUID = 0L;
             "Invalid map field number: " + number);
     }
   }
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.logging.v2.LogEntryProto.internal_static_google_logging_v2_LogEntry_fieldAccessorTable
@@ -677,6 +679,7 @@ private static final long serialVersionUID = 0L;
    * <code>.google.logging.type.LogSeverity severity = 10;</code>
    */
   public com.google.logging.type.LogSeverity getSeverity() {
+    @SuppressWarnings("deprecation")
     com.google.logging.type.LogSeverity result = com.google.logging.type.LogSeverity.valueOf(severity_);
     return result == null ? com.google.logging.type.LogSeverity.UNRECOGNIZED : result;
   }
@@ -1073,6 +1076,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -1082,6 +1086,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     if (payloadCase_ == 2) {
@@ -1138,6 +1143,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -1433,6 +1439,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -1440,6 +1447,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.logging.v2.LogEntry prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -1489,6 +1497,7 @@ private static final long serialVersionUID = 0L;
               "Invalid map field number: " + number);
       }
     }
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.logging.v2.LogEntryProto.internal_static_google_logging_v2_LogEntry_fieldAccessorTable
@@ -1511,6 +1520,7 @@ private static final long serialVersionUID = 0L;
               .alwaysUseFieldBuilders) {
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       logName_ = "";
@@ -1571,15 +1581,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.logging.v2.LogEntryProto.internal_static_google_logging_v2_LogEntry_descriptor;
     }
 
+    @java.lang.Override
     public com.google.logging.v2.LogEntry getDefaultInstanceForType() {
       return com.google.logging.v2.LogEntry.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.logging.v2.LogEntry build() {
       com.google.logging.v2.LogEntry result = buildPartial();
       if (!result.isInitialized()) {
@@ -1588,6 +1601,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.logging.v2.LogEntry buildPartial() {
       com.google.logging.v2.LogEntry result = new com.google.logging.v2.LogEntry(this);
       int from_bitField0_ = bitField0_;
@@ -1657,32 +1671,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.logging.v2.LogEntry) {
         return mergeFrom((com.google.logging.v2.LogEntry)other);
@@ -1760,10 +1781,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -3062,6 +3085,7 @@ private static final long serialVersionUID = 0L;
      * <code>.google.logging.type.LogSeverity severity = 10;</code>
      */
     public com.google.logging.type.LogSeverity getSeverity() {
+      @SuppressWarnings("deprecation")
       com.google.logging.type.LogSeverity result = com.google.logging.type.LogSeverity.valueOf(severity_);
       return result == null ? com.google.logging.type.LogSeverity.UNRECOGNIZED : result;
     }
@@ -4233,11 +4257,13 @@ private static final long serialVersionUID = 0L;
       }
       return sourceLocationBuilder_;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -4259,6 +4285,7 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<LogEntry>
       PARSER = new com.google.protobuf.AbstractParser<LogEntry>() {
+    @java.lang.Override
     public LogEntry parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -4276,6 +4303,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.logging.v2.LogEntry getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
