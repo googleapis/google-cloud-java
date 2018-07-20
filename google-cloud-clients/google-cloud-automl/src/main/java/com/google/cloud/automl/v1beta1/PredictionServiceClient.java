59aac73e2692ea56bc2636d7d268ddda5df619a7
Michael Darakananda <pongad@google.com>
2018-07-19 17:21:24 -0700

    add vision/v1p3beta1 and automl clients (#3484)

---
+++ google-cloud-clients/google-cloud-automl/src/main/java/com/google/cloud/automl/v1beta1/PredictionServiceClient.java
@@ -0,0 +1,301 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.automl.v1beta1;
+
+import com.google.api.core.BetaApi;
+import com.google.api.gax.core.BackgroundResource;
+import com.google.api.gax.rpc.UnaryCallable;
+import com.google.cloud.automl.v1beta1.stub.PredictionServiceStub;
+import com.google.cloud.automl.v1beta1.stub.PredictionServiceStubSettings;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Generated;
+
+// AUTO-GENERATED DOCUMENTATION AND SERVICE
+/**
+ * Service Description: AutoML Prediction API.
+ *
+ * <p>This class provides the ability to make remote calls to the backing service through method
+ * calls that map to API methods. Sample code to get started:
+ *
+ * <pre>
+ * <code>
+ * try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create()) {
+ *   ModelName name = ModelName.of("[PROJECT]", "[LOCATION]", "[MODEL]");
+ *   ExamplePayload payload = ExamplePayload.newBuilder().build();
+ *   Map&lt;String, String&gt; params = new HashMap&lt;&gt;();
+ *   PredictResponse response = predictionServiceClient.predict(name, payload, params);
+ * }
+ * </code>
+ * </pre>
+ *
+ * <p>Note: close() needs to be called on the predictionServiceClient object to clean up resources
+ * such as threads. In the example above, try-with-resources is used, which automatically calls
+ * close().
+ *
+ * <p>The surface of this class includes several types of Java methods for each of the API's
+ * methods:
+ *
+ * <ol>
+ *   <li> A "flattened" method. With this type of method, the fields of the request type have been
+ *       converted into function parameters. It may be the case that not all fields are available as
+ *       parameters, and not every API method will have a flattened method entry point.
+ *   <li> A "request object" method. This type of method only takes one parameter, a request object,
+ *       which must be constructed before the call. Not every API method will have a request object
+ *       method.
+ *   <li> A "callable" method. This type of method takes no parameters and returns an immutable API
+ *       callable object, which can be used to initiate calls to the service.
+ * </ol>
+ *
+ * <p>See the individual methods for example code.
+ *
+ * <p>Many parameters require resource names to be formatted in a particular way. To assist with
+ * these names, this class includes a format method for each type of name, and additionally a parse
+ * method to extract the individual identifiers contained within names that are returned.
+ *
+ * <p>This class can be customized by passing in a custom instance of PredictionServiceSettings to
+ * create(). For example:
+ *
+ * <p>To customize credentials:
+ *
+ * <pre>
+ * <code>
+ * PredictionServiceSettings predictionServiceSettings =
+ *     PredictionServiceSettings.newBuilder()
+ *         .setCredentialsProvider(FixedCredentialsProvider.create(myCredentials))
+ *         .build();
+ * PredictionServiceClient predictionServiceClient =
+ *     PredictionServiceClient.create(predictionServiceSettings);
+ * </code>
+ * </pre>
+ *
+ * To customize the endpoint:
+ *
+ * <pre>
+ * <code>
+ * PredictionServiceSettings predictionServiceSettings =
+ *     PredictionServiceSettings.newBuilder().setEndpoint(myEndpoint).build();
+ * PredictionServiceClient predictionServiceClient =
+ *     PredictionServiceClient.create(predictionServiceSettings);
+ * </code>
+ * </pre>
+ */
+@Generated("by gapic-generator")
+@BetaApi
+public class PredictionServiceClient implements BackgroundResource {
+  private final PredictionServiceSettings settings;
+  private final PredictionServiceStub stub;
+
+  /** Constructs an instance of PredictionServiceClient with default settings. */
+  public static final PredictionServiceClient create() throws IOException {
+    return create(PredictionServiceSettings.newBuilder().build());
+  }
+
+  /**
+   * Constructs an instance of PredictionServiceClient, using the given settings. The channels are
+   * created based on the settings passed in, or defaults for any settings that are not set.
+   */
+  public static final PredictionServiceClient create(PredictionServiceSettings settings)
+      throws IOException {
+    return new PredictionServiceClient(settings);
+  }
+
+  /**
+   * Constructs an instance of PredictionServiceClient, using the given stub for making calls. This
+   * is for advanced usage - prefer to use PredictionServiceSettings}.
+   */
+  @BetaApi("A restructuring of stub classes is planned, so this may break in the future")
+  public static final PredictionServiceClient create(PredictionServiceStub stub) {
+    return new PredictionServiceClient(stub);
+  }
+
+  /**
+   * Constructs an instance of PredictionServiceClient, using the given settings. This is protected
+   * so that it is easy to make a subclass, but otherwise, the static factory methods should be
+   * preferred.
+   */
+  protected PredictionServiceClient(PredictionServiceSettings settings) throws IOException {
+    this.settings = settings;
+    this.stub = ((PredictionServiceStubSettings) settings.getStubSettings()).createStub();
+  }
+
+  @BetaApi("A restructuring of stub classes is planned, so this may break in the future")
+  protected PredictionServiceClient(PredictionServiceStub stub) {
+    this.settings = null;
+    this.stub = stub;
+  }
+
+  public final PredictionServiceSettings getSettings() {
+    return settings;
+  }
+
+  @BetaApi("A restructuring of stub classes is planned, so this may break in the future")
+  public PredictionServiceStub getStub() {
+    return stub;
+  }
+
+  // AUTO-GENERATED DOCUMENTATION AND METHOD
+  /**
+   * Perform a prediction.
+   *
+   * <p>Sample code:
+   *
+   * <pre><code>
+   * try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create()) {
+   *   ModelName name = ModelName.of("[PROJECT]", "[LOCATION]", "[MODEL]");
+   *   ExamplePayload payload = ExamplePayload.newBuilder().build();
+   *   Map&lt;String, String&gt; params = new HashMap&lt;&gt;();
+   *   PredictResponse response = predictionServiceClient.predict(name, payload, params);
+   * }
+   * </code></pre>
+   *
+   * @param name Name of the model requested to serve the prediction.
+   * @param payload Required. Payload to perform a prediction on. The payload must match the problem
+   *     type that the model was trained to solve.
+   * @param params Additional domain-specific parameters, any string must be up to 25000 characters
+   *     long.
+   *     <p>&#42; For Image Classification:
+   *     <p>`score_threshold` - (float) A value from 0.0 to 1.0. When the model makes predictions
+   *     for an image, it will only produce results that have at least this confidence score
+   *     threshold. The default is 0.5.
+   * @throws com.google.api.gax.rpc.ApiException if the remote call fails
+   */
+  public final PredictResponse predict(
+      ModelName name, ExamplePayload payload, Map<String, String> params) {
+
+    PredictRequest request =
+        PredictRequest.newBuilder()
+            .setName(name == null ? null : name.toString())
+            .setPayload(payload)
+            .putAllParams(params)
+            .build();
+    return predict(request);
+  }
+
+  // AUTO-GENERATED DOCUMENTATION AND METHOD
+  /**
+   * Perform a prediction.
+   *
+   * <p>Sample code:
+   *
+   * <pre><code>
+   * try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create()) {
+   *   ModelName name = ModelName.of("[PROJECT]", "[LOCATION]", "[MODEL]");
+   *   ExamplePayload payload = ExamplePayload.newBuilder().build();
+   *   Map&lt;String, String&gt; params = new HashMap&lt;&gt;();
+   *   PredictResponse response = predictionServiceClient.predict(name.toString(), payload, params);
+   * }
+   * </code></pre>
+   *
+   * @param name Name of the model requested to serve the prediction.
+   * @param payload Required. Payload to perform a prediction on. The payload must match the problem
+   *     type that the model was trained to solve.
+   * @param params Additional domain-specific parameters, any string must be up to 25000 characters
+   *     long.
+   *     <p>&#42; For Image Classification:
+   *     <p>`score_threshold` - (float) A value from 0.0 to 1.0. When the model makes predictions
+   *     for an image, it will only produce results that have at least this confidence score
+   *     threshold. The default is 0.5.
+   * @throws com.google.api.gax.rpc.ApiException if the remote call fails
+   */
+  public final PredictResponse predict(
+      String name, ExamplePayload payload, Map<String, String> params) {
+
+    PredictRequest request =
+        PredictRequest.newBuilder().setName(name).setPayload(payload).putAllParams(params).build();
+    return predict(request);
+  }
+
+  // AUTO-GENERATED DOCUMENTATION AND METHOD
+  /**
+   * Perform a prediction.
+   *
+   * <p>Sample code:
+   *
+   * <pre><code>
+   * try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create()) {
+   *   ModelName name = ModelName.of("[PROJECT]", "[LOCATION]", "[MODEL]");
+   *   ExamplePayload payload = ExamplePayload.newBuilder().build();
+   *   PredictRequest request = PredictRequest.newBuilder()
+   *     .setName(name.toString())
+   *     .setPayload(payload)
+   *     .build();
+   *   PredictResponse response = predictionServiceClient.predict(request);
+   * }
+   * </code></pre>
+   *
+   * @param request The request object containing all of the parameters for the API call.
+   * @throws com.google.api.gax.rpc.ApiException if the remote call fails
+   */
+  public final PredictResponse predict(PredictRequest request) {
+    return predictCallable().call(request);
+  }
+
+  // AUTO-GENERATED DOCUMENTATION AND METHOD
+  /**
+   * Perform a prediction.
+   *
+   * <p>Sample code:
+   *
+   * <pre><code>
+   * try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create()) {
+   *   ModelName name = ModelName.of("[PROJECT]", "[LOCATION]", "[MODEL]");
+   *   ExamplePayload payload = ExamplePayload.newBuilder().build();
+   *   PredictRequest request = PredictRequest.newBuilder()
+   *     .setName(name.toString())
+   *     .setPayload(payload)
+   *     .build();
+   *   ApiFuture&lt;PredictResponse&gt; future = predictionServiceClient.predictCallable().futureCall(request);
+   *   // Do something
+   *   PredictResponse response = future.get();
+   * }
+   * </code></pre>
+   */
+  public final UnaryCallable<PredictRequest, PredictResponse> predictCallable() {
+    return stub.predictCallable();
+  }
+
+  @Override
+  public final void close() {
+    stub.close();
+  }
+
+  @Override
+  public void shutdown() {
+    stub.shutdown();
+  }
+
+  @Override
+  public boolean isShutdown() {
+    return stub.isShutdown();
+  }
+
+  @Override
+  public boolean isTerminated() {
+    return stub.isTerminated();
+  }
+
+  @Override
+  public void shutdownNow() {
+    stub.shutdownNow();
+  }
+
+  @Override
+  public boolean awaitTermination(long duration, TimeUnit unit) throws InterruptedException {
+    return stub.awaitTermination(duration, unit);
+  }
+}
