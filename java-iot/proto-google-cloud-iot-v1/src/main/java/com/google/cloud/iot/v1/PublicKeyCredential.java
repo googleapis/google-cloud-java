8b988204336ef8710f1407c8de78b1d1b7e6f571
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2018-07-25 10:47:31 -0700

    Regenerating with protoc 3.6.0 (#3506)

--- java-iot/proto-google-cloud-iot-v1/src/main/java/com/google/cloud/iot/v1/PublicKeyCredential.java
+++ java-iot/proto-google-cloud-iot-v1/src/main/java/com/google/cloud/iot/v1/PublicKeyCredential.java
@@ -48,13 +48,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 8: {
             int rawValue = input.readEnum();
 
@@ -65,6 +58,13 @@ private static final long serialVersionUID = 0L;
             java.lang.String s = input.readStringRequireUtf8();
 
             key_ = s;
+            break;
+          }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
             break;
           }
         }
@@ -84,6 +84,7 @@ private static final long serialVersionUID = 0L;
     return com.google.cloud.iot.v1.ResourcesProto.internal_static_google_cloud_iot_v1_PublicKeyCredential_descriptor;
   }
 
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.cloud.iot.v1.ResourcesProto.internal_static_google_cloud_iot_v1_PublicKeyCredential_fieldAccessorTable
@@ -111,6 +112,7 @@ private static final long serialVersionUID = 0L;
    * <code>.google.cloud.iot.v1.PublicKeyFormat format = 1;</code>
    */
   public com.google.cloud.iot.v1.PublicKeyFormat getFormat() {
+    @SuppressWarnings("deprecation")
     com.google.cloud.iot.v1.PublicKeyFormat result = com.google.cloud.iot.v1.PublicKeyFormat.valueOf(format_);
     return result == null ? com.google.cloud.iot.v1.PublicKeyFormat.UNRECOGNIZED : result;
   }
@@ -158,6 +160,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -167,6 +170,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     if (format_ != com.google.cloud.iot.v1.PublicKeyFormat.UNSPECIFIED_PUBLIC_KEY_FORMAT.getNumber()) {
@@ -178,6 +182,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -299,6 +304,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -306,6 +312,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.cloud.iot.v1.PublicKeyCredential prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -333,6 +340,7 @@ private static final long serialVersionUID = 0L;
       return com.google.cloud.iot.v1.ResourcesProto.internal_static_google_cloud_iot_v1_PublicKeyCredential_descriptor;
     }
 
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.cloud.iot.v1.ResourcesProto.internal_static_google_cloud_iot_v1_PublicKeyCredential_fieldAccessorTable
@@ -355,6 +363,7 @@ private static final long serialVersionUID = 0L;
               .alwaysUseFieldBuilders) {
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       format_ = 0;
@@ -364,15 +373,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.cloud.iot.v1.ResourcesProto.internal_static_google_cloud_iot_v1_PublicKeyCredential_descriptor;
     }
 
+    @java.lang.Override
     public com.google.cloud.iot.v1.PublicKeyCredential getDefaultInstanceForType() {
       return com.google.cloud.iot.v1.PublicKeyCredential.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.cloud.iot.v1.PublicKeyCredential build() {
       com.google.cloud.iot.v1.PublicKeyCredential result = buildPartial();
       if (!result.isInitialized()) {
@@ -381,6 +393,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.cloud.iot.v1.PublicKeyCredential buildPartial() {
       com.google.cloud.iot.v1.PublicKeyCredential result = new com.google.cloud.iot.v1.PublicKeyCredential(this);
       result.format_ = format_;
@@ -389,32 +402,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.cloud.iot.v1.PublicKeyCredential) {
         return mergeFrom((com.google.cloud.iot.v1.PublicKeyCredential)other);
@@ -438,10 +458,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -491,6 +513,7 @@ private static final long serialVersionUID = 0L;
      * <code>.google.cloud.iot.v1.PublicKeyFormat format = 1;</code>
      */
     public com.google.cloud.iot.v1.PublicKeyFormat getFormat() {
+      @SuppressWarnings("deprecation")
       com.google.cloud.iot.v1.PublicKeyFormat result = com.google.cloud.iot.v1.PublicKeyFormat.valueOf(format_);
       return result == null ? com.google.cloud.iot.v1.PublicKeyFormat.UNRECOGNIZED : result;
     }
@@ -612,11 +635,13 @@ private static final long serialVersionUID = 0L;
       onChanged();
       return this;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -638,6 +663,7 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<PublicKeyCredential>
       PARSER = new com.google.protobuf.AbstractParser<PublicKeyCredential>() {
+    @java.lang.Override
     public PublicKeyCredential parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -655,6 +681,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.cloud.iot.v1.PublicKeyCredential getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
