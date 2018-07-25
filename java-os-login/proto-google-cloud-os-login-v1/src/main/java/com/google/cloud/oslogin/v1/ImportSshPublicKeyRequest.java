db67f8a3b4b4beda6bc6f03fa00f94947589c875
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2018-07-25 10:47:31 -0700

    Regenerating with protoc 3.6.0 (#3506)

--- java-os-login/proto-google-cloud-os-login-v1/src/main/java/com/google/cloud/oslogin/v1/ImportSshPublicKeyRequest.java
+++ java-os-login/proto-google-cloud-os-login-v1/src/main/java/com/google/cloud/oslogin/v1/ImportSshPublicKeyRequest.java
@@ -48,13 +48,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 10: {
             java.lang.String s = input.readStringRequireUtf8();
 
@@ -80,6 +73,13 @@ private static final long serialVersionUID = 0L;
             projectId_ = s;
             break;
           }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
+            break;
+          }
         }
       }
     } catch (com.google.protobuf.InvalidProtocolBufferException e) {
@@ -97,6 +97,7 @@ private static final long serialVersionUID = 0L;
     return com.google.cloud.oslogin.v1.OsLoginProto.internal_static_google_cloud_oslogin_v1_ImportSshPublicKeyRequest_descriptor;
   }
 
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.cloud.oslogin.v1.OsLoginProto.internal_static_google_cloud_oslogin_v1_ImportSshPublicKeyRequest_fieldAccessorTable
@@ -222,6 +223,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -231,6 +233,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     if (!getParentBytes().isEmpty()) {
@@ -245,6 +248,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -379,6 +383,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -386,6 +391,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -413,6 +419,7 @@ private static final long serialVersionUID = 0L;
       return com.google.cloud.oslogin.v1.OsLoginProto.internal_static_google_cloud_oslogin_v1_ImportSshPublicKeyRequest_descriptor;
     }
 
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.cloud.oslogin.v1.OsLoginProto.internal_static_google_cloud_oslogin_v1_ImportSshPublicKeyRequest_fieldAccessorTable
@@ -435,6 +442,7 @@ private static final long serialVersionUID = 0L;
               .alwaysUseFieldBuilders) {
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       parent_ = "";
@@ -450,15 +458,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.cloud.oslogin.v1.OsLoginProto.internal_static_google_cloud_oslogin_v1_ImportSshPublicKeyRequest_descriptor;
     }
 
+    @java.lang.Override
     public com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest getDefaultInstanceForType() {
       return com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest build() {
       com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest result = buildPartial();
       if (!result.isInitialized()) {
@@ -467,6 +478,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest buildPartial() {
       com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest result = new com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest(this);
       result.parent_ = parent_;
@@ -480,32 +492,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest) {
         return mergeFrom((com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest)other);
@@ -533,10 +552,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -885,11 +906,13 @@ private static final long serialVersionUID = 0L;
       onChanged();
       return this;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -911,6 +934,7 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<ImportSshPublicKeyRequest>
       PARSER = new com.google.protobuf.AbstractParser<ImportSshPublicKeyRequest>() {
+    @java.lang.Override
     public ImportSshPublicKeyRequest parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -928,6 +952,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.cloud.oslogin.v1.ImportSshPublicKeyRequest getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
