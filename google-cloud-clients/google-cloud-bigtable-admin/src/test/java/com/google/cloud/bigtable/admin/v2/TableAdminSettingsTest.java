10ae878a7470e468cf2929f8c796071fed185dd0
Igor Bernstein <igorbernstein@google.com>
2018-08-07 18:44:17 -0400

    Bigtable: Decouple TableAdminClient from BigtableTableAdminSettings. (#3512)
    This is in preparation for renaming the auto generated GAPIC clients &
    settings to be prefixed with 'Base'. This will make it easier to
    understand the layout of the code: the GAPIC generated client/settings
    will be called BaseBigtableTableAdmin{Client,Settings}, while the
    handwritten overlay will be called BigtableTableAdmin{Client,Settings}.

---
+++ google-cloud-clients/google-cloud-bigtable-admin/src/test/java/com/google/cloud/bigtable/admin/v2/TableAdminSettingsTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.admin.v2;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.api.gax.rpc.StatusCode.Code;
+import com.google.bigtable.admin.v2.InstanceName;
+import java.io.IOException;
+import org.junit.Test;
+
+public class TableAdminSettingsTest {
+
+  @Test
+  public void testInstanceName() throws IOException {
+    InstanceName instanceName = InstanceName.of("my-project", "my-instance");
+
+    TableAdminSettings.Builder builder = TableAdminSettings.newBuilder()
+        .setInstanceName(instanceName);
+
+    assertThat(builder.getInstanceName()).isEqualTo(instanceName);
+    assertThat(builder.build().getInstanceName()).isEqualTo(instanceName);
+    assertThat(builder.build().toBuilder().getInstanceName()).isEqualTo(instanceName);
+  }
+
+  @Test
+  public void testMissingInstanceName() {
+    Exception actualException = null;
+
+    try {
+      TableAdminSettings.newBuilder().build();
+    } catch (Exception e) {
+      actualException = e;
+    }
+
+    assertThat(actualException).isInstanceOf(NullPointerException.class);
+  }
+
+  @Test
+  public void testStubSettings() throws IOException {
+    InstanceName instanceName = InstanceName.of("my-project", "my-instance");
+
+    TableAdminSettings.Builder builder = TableAdminSettings.newBuilder()
+        .setInstanceName(instanceName);
+
+    builder.stubSettings().createTableSettings()
+        .setRetryableCodes(Code.INVALID_ARGUMENT);
+
+    assertThat(builder.build().getStubSettings().createTableSettings().getRetryableCodes())
+        .containsExactly(Code.INVALID_ARGUMENT);
+
+    assertThat(builder.build().toBuilder().build().getStubSettings().createTableSettings()
+        .getRetryableCodes())
+        .containsExactly(Code.INVALID_ARGUMENT);
+  }
+}
