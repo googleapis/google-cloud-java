0dd9c9b60dda544964ee9a7849a1c1387fd59e6f
Jonas Wiklund <jwiklund@gmail.com>
2018-08-23 14:25:54 +0200

    Force a review rebuild.
    CLA should be signed.

--- google-cloud-clients/google-cloud-pubsub/src/main/java/com/google/cloud/pubsub/v1/Subscriber.java
+++ google-cloud-clients/google-cloud-pubsub/src/main/java/com/google/cloud/pubsub/v1/Subscriber.java
@@ -583,7 +583,7 @@ public class Subscriber extends AbstractApiService {
     }
 
     /**
-     * Sets the number of pullers used to pull messages from the subscription. Defaults to 1.
+     * Sets the number of pullers used to pull messages from the subscription. Defaults to one.
      */
     public Builder setParallelPullCount(int parallelPullCount) {
       this.parallelPullCount = parallelPullCount;
