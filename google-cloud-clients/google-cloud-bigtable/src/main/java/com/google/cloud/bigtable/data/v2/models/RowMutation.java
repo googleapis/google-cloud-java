8b5b037d194d2ae190e8c7b7dc4f4d2197d8af05
Ajay <ajay@qlogic.io>
2018-09-06 13:36:44 -0400

    fix typo

--- google-cloud-clients/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/models/RowMutation.java
+++ google-cloud-clients/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/models/RowMutation.java
@@ -54,7 +54,7 @@ public final class RowMutation implements MutationApi<RowMutation>, Serializable
   }
 
   /**
-   * Creates new instance of mutation builder by wrapping existing existing set of row mutations.
+   * Creates new instance of mutation builder by wrapping existing set of row mutations.
    * The builder will be owned by this RowMutation and should not be used by the caller after this call.
    * This functionality is intended for advanced usage.
    *
@@ -71,7 +71,7 @@ public final class RowMutation implements MutationApi<RowMutation>, Serializable
   }
 
   /**
-   * Creates new instance of mutation builder by wrapping existing existing set of row mutations.
+   * Creates new instance of mutation builder by wrapping existing set of row mutations.
    * The builder will be owned by this RowMutation and should not be used by the caller after this call.
    * This functionality is intended for advanced usage.
    *
