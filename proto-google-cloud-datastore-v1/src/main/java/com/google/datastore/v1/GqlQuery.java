981bdf5a6643786d019eb0b80403ad678074b08a
Andrea Lin <andrealin@google.com>
2018-09-12 15:43:27 -0700

    Regenerate proto/grpc files with protoc 3.6.0 (#3672)

--- proto-google-cloud-datastore-v1/src/main/java/com/google/datastore/v1/GqlQuery.java
+++ proto-google-cloud-datastore-v1/src/main/java/com/google/datastore/v1/GqlQuery.java
@@ -35,6 +35,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -46,13 +49,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 10: {
             java.lang.String s = input.readStringRequireUtf8();
 
@@ -86,6 +82,13 @@ private static final long serialVersionUID = 0L;
                 namedBindings__.getKey(), namedBindings__.getValue());
             break;
           }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
+            break;
+          }
         }
       }
     } catch (com.google.protobuf.InvalidProtocolBufferException e) {
@@ -107,6 +110,7 @@ private static final long serialVersionUID = 0L;
   }
 
   @SuppressWarnings({"rawtypes"})
+  @java.lang.Override
   protected com.google.protobuf.MapField internalGetMapField(
       int number) {
     switch (number) {
@@ -117,6 +121,7 @@ private static final long serialVersionUID = 0L;
             "Invalid map field number: " + number);
     }
   }
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.datastore.v1.QueryProto.internal_static_google_datastore_v1_GqlQuery_fieldAccessorTable
@@ -360,6 +365,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -369,6 +375,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     if (!getQueryStringBytes().isEmpty()) {
@@ -389,6 +396,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -538,6 +546,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -545,6 +554,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.datastore.v1.GqlQuery prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -594,6 +604,7 @@ private static final long serialVersionUID = 0L;
               "Invalid map field number: " + number);
       }
     }
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.datastore.v1.QueryProto.internal_static_google_datastore_v1_GqlQuery_fieldAccessorTable
@@ -617,6 +628,7 @@ private static final long serialVersionUID = 0L;
         getPositionalBindingsFieldBuilder();
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       queryString_ = "";
@@ -633,15 +645,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.datastore.v1.QueryProto.internal_static_google_datastore_v1_GqlQuery_descriptor;
     }
 
+    @java.lang.Override
     public com.google.datastore.v1.GqlQuery getDefaultInstanceForType() {
       return com.google.datastore.v1.GqlQuery.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.datastore.v1.GqlQuery build() {
       com.google.datastore.v1.GqlQuery result = buildPartial();
       if (!result.isInitialized()) {
@@ -650,6 +665,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.datastore.v1.GqlQuery buildPartial() {
       com.google.datastore.v1.GqlQuery result = new com.google.datastore.v1.GqlQuery(this);
       int from_bitField0_ = bitField0_;
@@ -672,32 +688,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.datastore.v1.GqlQuery) {
         return mergeFrom((com.google.datastore.v1.GqlQuery)other);
@@ -749,10 +772,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -1450,11 +1475,13 @@ private static final long serialVersionUID = 0L;
       }
       return positionalBindingsBuilder_;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -1476,11 +1503,12 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<GqlQuery>
       PARSER = new com.google.protobuf.AbstractParser<GqlQuery>() {
+    @java.lang.Override
     public GqlQuery parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new GqlQuery(input, extensionRegistry);
+      return new GqlQuery(input, extensionRegistry);
     }
   };
 
@@ -1493,6 +1521,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.datastore.v1.GqlQuery getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
--- proto-google-cloud-datastore-v1/src/main/java/com/google/datastore/v1/LookupResponse.java
+++ proto-google-cloud-datastore-v1/src/main/java/com/google/datastore/v1/LookupResponse.java
@@ -35,6 +35,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -46,13 +49,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 10: {
             if (!((mutable_bitField0_ & 0x00000001) == 0x00000001)) {
               found_ = new java.util.ArrayList<com.google.datastore.v1.EntityResult>();
@@ -78,6 +74,13 @@ private static final long serialVersionUID = 0L;
             }
             deferred_.add(
                 input.readMessage(com.google.datastore.v1.Key.parser(), extensionRegistry));
+            break;
+          }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
             break;
           }
         }
@@ -106,6 +109,7 @@ private static final long serialVersionUID = 0L;
     return com.google.datastore.v1.DatastoreProto.internal_static_google_datastore_v1_LookupResponse_descriptor;
   }
 
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.datastore.v1.DatastoreProto.internal_static_google_datastore_v1_LookupResponse_fieldAccessorTable
@@ -309,6 +313,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -318,6 +323,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     for (int i = 0; i < found_.size(); i++) {
@@ -332,6 +338,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -469,6 +476,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -476,6 +484,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.datastore.v1.LookupResponse prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -503,6 +512,7 @@ private static final long serialVersionUID = 0L;
       return com.google.datastore.v1.DatastoreProto.internal_static_google_datastore_v1_LookupResponse_descriptor;
     }
 
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.datastore.v1.DatastoreProto.internal_static_google_datastore_v1_LookupResponse_fieldAccessorTable
@@ -528,6 +538,7 @@ private static final long serialVersionUID = 0L;
         getDeferredFieldBuilder();
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       if (foundBuilder_ == null) {
@@ -551,15 +562,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.datastore.v1.DatastoreProto.internal_static_google_datastore_v1_LookupResponse_descriptor;
     }
 
+    @java.lang.Override
     public com.google.datastore.v1.LookupResponse getDefaultInstanceForType() {
       return com.google.datastore.v1.LookupResponse.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.datastore.v1.LookupResponse build() {
       com.google.datastore.v1.LookupResponse result = buildPartial();
       if (!result.isInitialized()) {
@@ -568,6 +582,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.datastore.v1.LookupResponse buildPartial() {
       com.google.datastore.v1.LookupResponse result = new com.google.datastore.v1.LookupResponse(this);
       int from_bitField0_ = bitField0_;
@@ -602,32 +617,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.datastore.v1.LookupResponse) {
         return mergeFrom((com.google.datastore.v1.LookupResponse)other);
@@ -722,10 +744,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -1788,11 +1812,13 @@ private static final long serialVersionUID = 0L;
       }
       return deferredBuilder_;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -1814,11 +1840,12 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<LookupResponse>
       PARSER = new com.google.protobuf.AbstractParser<LookupResponse>() {
+    @java.lang.Override
     public LookupResponse parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new LookupResponse(input, extensionRegistry);
+      return new LookupResponse(input, extensionRegistry);
     }
   };
 
@@ -1831,6 +1858,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.datastore.v1.LookupResponse getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
