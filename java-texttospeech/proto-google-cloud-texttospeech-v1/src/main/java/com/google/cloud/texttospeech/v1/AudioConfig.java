b16a85aa67019125a9ce3519893d2c8ac400d38a
Andrea Lin <andrealin@google.com>
2018-09-12 15:43:27 -0700

    Regenerate proto/grpc files with protoc 3.6.0 (#3672)

--- java-texttospeech/proto-google-cloud-texttospeech-v1/src/main/java/com/google/cloud/texttospeech/v1/AudioConfig.java
+++ java-texttospeech/proto-google-cloud-texttospeech-v1/src/main/java/com/google/cloud/texttospeech/v1/AudioConfig.java
@@ -37,6 +37,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -48,13 +51,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 8: {
             int rawValue = input.readEnum();
 
@@ -81,6 +77,13 @@ private static final long serialVersionUID = 0L;
             sampleRateHertz_ = input.readInt32();
             break;
           }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
+            break;
+          }
         }
       }
     } catch (com.google.protobuf.InvalidProtocolBufferException e) {
@@ -98,6 +101,7 @@ private static final long serialVersionUID = 0L;
     return com.google.cloud.texttospeech.v1.TextToSpeechProto.internal_static_google_cloud_texttospeech_v1_AudioConfig_descriptor;
   }
 
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.cloud.texttospeech.v1.TextToSpeechProto.internal_static_google_cloud_texttospeech_v1_AudioConfig_fieldAccessorTable
@@ -125,6 +129,7 @@ private static final long serialVersionUID = 0L;
    * <code>.google.cloud.texttospeech.v1.AudioEncoding audio_encoding = 1;</code>
    */
   public com.google.cloud.texttospeech.v1.AudioEncoding getAudioEncoding() {
+    @SuppressWarnings("deprecation")
     com.google.cloud.texttospeech.v1.AudioEncoding result = com.google.cloud.texttospeech.v1.AudioEncoding.valueOf(audioEncoding_);
     return result == null ? com.google.cloud.texttospeech.v1.AudioEncoding.UNRECOGNIZED : result;
   }
@@ -199,6 +204,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -208,6 +214,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     if (audioEncoding_ != com.google.cloud.texttospeech.v1.AudioEncoding.AUDIO_ENCODING_UNSPECIFIED.getNumber()) {
@@ -228,6 +235,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -383,6 +391,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -390,6 +399,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.cloud.texttospeech.v1.AudioConfig prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -417,6 +427,7 @@ private static final long serialVersionUID = 0L;
       return com.google.cloud.texttospeech.v1.TextToSpeechProto.internal_static_google_cloud_texttospeech_v1_AudioConfig_descriptor;
     }
 
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.cloud.texttospeech.v1.TextToSpeechProto.internal_static_google_cloud_texttospeech_v1_AudioConfig_fieldAccessorTable
@@ -439,6 +450,7 @@ private static final long serialVersionUID = 0L;
               .alwaysUseFieldBuilders) {
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       audioEncoding_ = 0;
@@ -454,15 +466,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.cloud.texttospeech.v1.TextToSpeechProto.internal_static_google_cloud_texttospeech_v1_AudioConfig_descriptor;
     }
 
+    @java.lang.Override
     public com.google.cloud.texttospeech.v1.AudioConfig getDefaultInstanceForType() {
       return com.google.cloud.texttospeech.v1.AudioConfig.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.cloud.texttospeech.v1.AudioConfig build() {
       com.google.cloud.texttospeech.v1.AudioConfig result = buildPartial();
       if (!result.isInitialized()) {
@@ -471,6 +486,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.cloud.texttospeech.v1.AudioConfig buildPartial() {
       com.google.cloud.texttospeech.v1.AudioConfig result = new com.google.cloud.texttospeech.v1.AudioConfig(this);
       result.audioEncoding_ = audioEncoding_;
@@ -482,32 +498,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.cloud.texttospeech.v1.AudioConfig) {
         return mergeFrom((com.google.cloud.texttospeech.v1.AudioConfig)other);
@@ -539,10 +562,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -592,6 +617,7 @@ private static final long serialVersionUID = 0L;
      * <code>.google.cloud.texttospeech.v1.AudioEncoding audio_encoding = 1;</code>
      */
     public com.google.cloud.texttospeech.v1.AudioEncoding getAudioEncoding() {
+      @SuppressWarnings("deprecation")
       com.google.cloud.texttospeech.v1.AudioEncoding result = com.google.cloud.texttospeech.v1.AudioEncoding.valueOf(audioEncoding_);
       return result == null ? com.google.cloud.texttospeech.v1.AudioEncoding.UNRECOGNIZED : result;
     }
@@ -827,11 +853,13 @@ private static final long serialVersionUID = 0L;
       onChanged();
       return this;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -853,11 +881,12 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<AudioConfig>
       PARSER = new com.google.protobuf.AbstractParser<AudioConfig>() {
+    @java.lang.Override
     public AudioConfig parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new AudioConfig(input, extensionRegistry);
+      return new AudioConfig(input, extensionRegistry);
     }
   };
 
@@ -870,6 +899,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.cloud.texttospeech.v1.AudioConfig getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
