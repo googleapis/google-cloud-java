f7e240b549a2c3d0bfe6e5f698adcfa53a86ecb6
Andrea Lin <andrealin@google.com>
2018-09-12 15:43:27 -0700

    Regenerate proto/grpc files with protoc 3.6.0 (#3672)

--- java-iot/proto-google-cloud-iot-v1/src/main/java/com/google/cloud/iot/v1/ListDeviceStatesResponse.java
+++ java-iot/proto-google-cloud-iot-v1/src/main/java/com/google/cloud/iot/v1/ListDeviceStatesResponse.java
@@ -33,6 +33,9 @@ private static final long serialVersionUID = 0L;
       com.google.protobuf.ExtensionRegistryLite extensionRegistry)
       throws com.google.protobuf.InvalidProtocolBufferException {
     this();
+    if (extensionRegistry == null) {
+      throw new java.lang.NullPointerException();
+    }
     int mutable_bitField0_ = 0;
     com.google.protobuf.UnknownFieldSet.Builder unknownFields =
         com.google.protobuf.UnknownFieldSet.newBuilder();
@@ -44,13 +47,6 @@ private static final long serialVersionUID = 0L;
           case 0:
             done = true;
             break;
-          default: {
-            if (!parseUnknownFieldProto3(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
           case 10: {
             if (!((mutable_bitField0_ & 0x00000001) == 0x00000001)) {
               deviceStates_ = new java.util.ArrayList<com.google.cloud.iot.v1.DeviceState>();
@@ -58,6 +54,13 @@ private static final long serialVersionUID = 0L;
             }
             deviceStates_.add(
                 input.readMessage(com.google.cloud.iot.v1.DeviceState.parser(), extensionRegistry));
+            break;
+          }
+          default: {
+            if (!parseUnknownFieldProto3(
+                input, unknownFields, extensionRegistry, tag)) {
+              done = true;
+            }
             break;
           }
         }
@@ -80,6 +83,7 @@ private static final long serialVersionUID = 0L;
     return com.google.cloud.iot.v1.DeviceManagerProto.internal_static_google_cloud_iot_v1_ListDeviceStatesResponse_descriptor;
   }
 
+  @java.lang.Override
   protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internalGetFieldAccessorTable() {
     return com.google.cloud.iot.v1.DeviceManagerProto.internal_static_google_cloud_iot_v1_ListDeviceStatesResponse_fieldAccessorTable
@@ -148,6 +152,7 @@ private static final long serialVersionUID = 0L;
   }
 
   private byte memoizedIsInitialized = -1;
+  @java.lang.Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
     if (isInitialized == 1) return true;
@@ -157,6 +162,7 @@ private static final long serialVersionUID = 0L;
     return true;
   }
 
+  @java.lang.Override
   public void writeTo(com.google.protobuf.CodedOutputStream output)
                       throws java.io.IOException {
     for (int i = 0; i < deviceStates_.size(); i++) {
@@ -165,6 +171,7 @@ private static final long serialVersionUID = 0L;
     unknownFields.writeTo(output);
   }
 
+  @java.lang.Override
   public int getSerializedSize() {
     int size = memoizedSize;
     if (size != -1) return size;
@@ -282,6 +289,7 @@ private static final long serialVersionUID = 0L;
         .parseWithIOException(PARSER, input, extensionRegistry);
   }
 
+  @java.lang.Override
   public Builder newBuilderForType() { return newBuilder(); }
   public static Builder newBuilder() {
     return DEFAULT_INSTANCE.toBuilder();
@@ -289,6 +297,7 @@ private static final long serialVersionUID = 0L;
   public static Builder newBuilder(com.google.cloud.iot.v1.ListDeviceStatesResponse prototype) {
     return DEFAULT_INSTANCE.toBuilder().mergeFrom(prototype);
   }
+  @java.lang.Override
   public Builder toBuilder() {
     return this == DEFAULT_INSTANCE
         ? new Builder() : new Builder().mergeFrom(this);
@@ -316,6 +325,7 @@ private static final long serialVersionUID = 0L;
       return com.google.cloud.iot.v1.DeviceManagerProto.internal_static_google_cloud_iot_v1_ListDeviceStatesResponse_descriptor;
     }
 
+    @java.lang.Override
     protected com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
         internalGetFieldAccessorTable() {
       return com.google.cloud.iot.v1.DeviceManagerProto.internal_static_google_cloud_iot_v1_ListDeviceStatesResponse_fieldAccessorTable
@@ -339,6 +349,7 @@ private static final long serialVersionUID = 0L;
         getDeviceStatesFieldBuilder();
       }
     }
+    @java.lang.Override
     public Builder clear() {
       super.clear();
       if (deviceStatesBuilder_ == null) {
@@ -350,15 +361,18 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public com.google.protobuf.Descriptors.Descriptor
         getDescriptorForType() {
       return com.google.cloud.iot.v1.DeviceManagerProto.internal_static_google_cloud_iot_v1_ListDeviceStatesResponse_descriptor;
     }
 
+    @java.lang.Override
     public com.google.cloud.iot.v1.ListDeviceStatesResponse getDefaultInstanceForType() {
       return com.google.cloud.iot.v1.ListDeviceStatesResponse.getDefaultInstance();
     }
 
+    @java.lang.Override
     public com.google.cloud.iot.v1.ListDeviceStatesResponse build() {
       com.google.cloud.iot.v1.ListDeviceStatesResponse result = buildPartial();
       if (!result.isInitialized()) {
@@ -367,6 +381,7 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public com.google.cloud.iot.v1.ListDeviceStatesResponse buildPartial() {
       com.google.cloud.iot.v1.ListDeviceStatesResponse result = new com.google.cloud.iot.v1.ListDeviceStatesResponse(this);
       int from_bitField0_ = bitField0_;
@@ -383,32 +398,39 @@ private static final long serialVersionUID = 0L;
       return result;
     }
 
+    @java.lang.Override
     public Builder clone() {
       return (Builder) super.clone();
     }
+    @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.setField(field, value);
     }
+    @java.lang.Override
     public Builder clearField(
         com.google.protobuf.Descriptors.FieldDescriptor field) {
       return (Builder) super.clearField(field);
     }
+    @java.lang.Override
     public Builder clearOneof(
         com.google.protobuf.Descriptors.OneofDescriptor oneof) {
       return (Builder) super.clearOneof(oneof);
     }
+    @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         int index, java.lang.Object value) {
       return (Builder) super.setRepeatedField(field, index, value);
     }
+    @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field,
         java.lang.Object value) {
       return (Builder) super.addRepeatedField(field, value);
     }
+    @java.lang.Override
     public Builder mergeFrom(com.google.protobuf.Message other) {
       if (other instanceof com.google.cloud.iot.v1.ListDeviceStatesResponse) {
         return mergeFrom((com.google.cloud.iot.v1.ListDeviceStatesResponse)other);
@@ -451,10 +473,12 @@ private static final long serialVersionUID = 0L;
       return this;
     }
 
+    @java.lang.Override
     public final boolean isInitialized() {
       return true;
     }
 
+    @java.lang.Override
     public Builder mergeFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
@@ -803,11 +827,13 @@ private static final long serialVersionUID = 0L;
       }
       return deviceStatesBuilder_;
     }
+    @java.lang.Override
     public final Builder setUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.setUnknownFieldsProto3(unknownFields);
     }
 
+    @java.lang.Override
     public final Builder mergeUnknownFields(
         final com.google.protobuf.UnknownFieldSet unknownFields) {
       return super.mergeUnknownFields(unknownFields);
@@ -829,11 +855,12 @@ private static final long serialVersionUID = 0L;
 
   private static final com.google.protobuf.Parser<ListDeviceStatesResponse>
       PARSER = new com.google.protobuf.AbstractParser<ListDeviceStatesResponse>() {
+    @java.lang.Override
     public ListDeviceStatesResponse parsePartialFrom(
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-        return new ListDeviceStatesResponse(input, extensionRegistry);
+      return new ListDeviceStatesResponse(input, extensionRegistry);
     }
   };
 
@@ -846,6 +873,7 @@ private static final long serialVersionUID = 0L;
     return PARSER;
   }
 
+  @java.lang.Override
   public com.google.cloud.iot.v1.ListDeviceStatesResponse getDefaultInstanceForType() {
     return DEFAULT_INSTANCE;
   }
