a745c13069c637120b9eefde9460e32a44879b9f
Jeff Ching <chingor@google.com>
2018-10-05 11:20:33 -0700

    Add synthtool scripts (#3765)
    * Add assets synth script
    
    * Add automl synth script
    
    * Add BigQueryDataTransfer synth
    
    * Add BigTable synth
    
    * Add BigTableAdmin synth
    
    * Add container synth
    
    * Add containeranalysis synth
    
    * Add dataproc synth
    
    * Add datastore synth
    
    * Add dialogflow synth
    
    * Add dlp synth
    
    * Add errorreporting synth
    
    * Add firestore synth
    
    * Add iot synth
    
    * Add kms synth
    
    * Add language synth
    
    * Add logging synth
    
    * Add monitoring synth
    
    * Add oslogin synth
    
    * Add pubsub synth
    
    * Add redis synth
    
    * Add spanner synth
    
    * Add speech synth
    
    * Add tasks synth
    
    * Add texttospeech synth
    
    * Add trace synth
    
    * Add videointelligence synth
    
    * Add vision synth
    
    * Add websecurityscanner synth
    
    * Add compute synth
    
    * Remove unnecessary import
    
    * Remove compute synth and remove excess logging statements

---
+++ java-bigtable/google-cloud-bigtable/synth.py
@@ -0,0 +1,31 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool as s
+import synthtool.gcp as gcp
+
+gapic = gcp.GAPICGenerator()
+common_templates = gcp.CommonTemplates()
+
+library = gapic.java_library(
+    service='bigtable',
+    version='v2',
+    config_path='/google/bigtable/artman_bigtable.yaml',
+    artman_output_name='')
+
+s.copy(library / 'gapic-google-cloud-bigtable-v2/src', 'src')
+s.copy(library / 'grpc-google-cloud-bigtable-v2/src', '../../google-api-grpc/grpc-google-cloud-bigtable-v2/src')
+s.copy(library / 'proto-google-cloud-bigtable-v2/src', '../../google-api-grpc/proto-google-cloud-bigtable-v2/src')
