949b8c5032055918177a0e89867af3b2b6342926
Igor Bernstein <igorbernstein@google.com>
2018-11-02 13:45:16 -0400

    oops

--- google-cloud-testing/google-cloud-bigtable-emulator/src/test/java/com/google/cloud/bigtable/emulator/v2/BigtableEmulatorRuleTest.java
+++ google-cloud-testing/google-cloud-bigtable-emulator/src/test/java/com/google/cloud/bigtable/emulator/v2/BigtableEmulatorRuleTest.java
@@ -39,9 +39,8 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class BigtableEmulatorRuleTest {
-
   @Rule
-  public BigtableEmulatorRule bigtableRule = new BigtableEmulatorRule();
+  public BigtableEmulatorRule bigtableRule = BigtableEmulatorRule.create();
   private BigtableTableAdminBlockingStub tableAdminStub;
   private BigtableBlockingStub dataStub;
 
