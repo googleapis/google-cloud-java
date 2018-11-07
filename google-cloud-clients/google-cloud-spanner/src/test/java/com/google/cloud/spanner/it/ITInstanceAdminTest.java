28fab80ed42dcdf56bac22f67b6e262e3ad54cd2
Hanzhen Yi <33737743+hzyi-google@users.noreply.github.com>
2018-11-06 16:38:35 -0800

    Spanner gapic migration (#3881)
    * Add GapicSpannerRpc stub (#3016)
    
    * spanner: implement partitionRead (#3022)
    
    * spanner: implement LRO methods using GAPIC stub (#3039)
    
    * Add implementation to:listInstances, listInstanceConfigs, listDatabases, getInstance, getInstanceConfigs, getDatabase, dropDatabase, deleteInstance, getDatabaseDdl  (#3043) 
    
    * spanner: move admin clients to GAPIC stub (#3067)
    
    * Spanner: migrate all unary call methods to gapic and inject headers (#3112)
    
    * Spanner: migrate streaming methods to gapic (#3139)
    
    * Spanner gapic migration lro methods (#3166)
    
    Migrate longrunning methods to GAPIC including: createDatabase, updateDatabaseDdl, createInstance, and updateInstance.
    
    * Make ChannelPool work (#3258)
    
    * Spanner gapic migration error augmentation with interceptor (#3304)
    
    * Add interceptors (#3346)
    
    * Clean up Spanner before merging to master (#3362)
    
    * Spanner Gapic Migration: fix updateDatabaseDdl (#3403)

--- google-cloud-clients/google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITInstanceAdminTest.java
+++ google-cloud-clients/google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITInstanceAdminTest.java
@@ -18,13 +18,13 @@ package com.google.cloud.spanner.it;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.api.gax.longrunning.OperationFuture;
 import com.google.cloud.spanner.Instance;
 import com.google.cloud.spanner.InstanceAdminClient;
 import com.google.cloud.spanner.InstanceConfig;
 import com.google.cloud.spanner.InstanceInfo;
 import com.google.cloud.spanner.IntegrationTest;
 import com.google.cloud.spanner.IntegrationTestEnv;
-import com.google.cloud.spanner.Operation;
 import com.google.cloud.spanner.Options;
 import com.google.common.collect.Iterators;
 import com.google.spanner.admin.instance.v1.UpdateInstanceMetadata;
@@ -90,9 +90,9 @@ public class ITInstanceAdminTest {
             .setNodeCount(instance.getNodeCount() + 1)
             .build();
     // Only update display name
-    Operation<Instance, UpdateInstanceMetadata> op =
+    OperationFuture<Instance, UpdateInstanceMetadata> op =
         instanceClient.updateInstance(toUpdate, InstanceInfo.InstanceField.DISPLAY_NAME);
-    Instance newInstance = op.waitFor().getResult();
+    Instance newInstance = op.get();
     assertThat(newInstance.getNodeCount()).isEqualTo(instance.getNodeCount());
     assertThat(newInstance.getDisplayName()).isEqualTo(newDisplayName);
 
@@ -102,7 +102,7 @@ public class ITInstanceAdminTest {
 
     toUpdate =
         InstanceInfo.newBuilder(instance.getId()).setDisplayName(instance.getDisplayName()).build();
-    instanceClient.updateInstance(toUpdate, InstanceInfo.InstanceField.DISPLAY_NAME).waitFor();
+    instanceClient.updateInstance(toUpdate, InstanceInfo.InstanceField.DISPLAY_NAME).get();
   }
 
   @Test
@@ -118,9 +118,9 @@ public class ITInstanceAdminTest {
             .setNodeCount(instance.getNodeCount() + 1)
             .build();
     // Only update display name
-    Operation<Instance, UpdateInstanceMetadata> op =
+    OperationFuture<Instance, UpdateInstanceMetadata> op =
         toUpdate.update(InstanceInfo.InstanceField.DISPLAY_NAME);
-    Instance newInstance = op.waitFor().getResult();
+    Instance newInstance = op.get();
     assertThat(newInstance.getNodeCount()).isEqualTo(instance.getNodeCount());
     assertThat(newInstance.getDisplayName()).isEqualTo(newDisplayName);
 
@@ -128,6 +128,6 @@ public class ITInstanceAdminTest {
     assertThat(newInstanceFromGet).isEqualTo(newInstance);
 
     toUpdate = newInstance.toBuilder().setDisplayName(instance.getDisplayName()).build();
-    toUpdate.update(InstanceInfo.InstanceField.DISPLAY_NAME).waitFor();
+    toUpdate.update(InstanceInfo.InstanceField.DISPLAY_NAME).get();
   }
 }
