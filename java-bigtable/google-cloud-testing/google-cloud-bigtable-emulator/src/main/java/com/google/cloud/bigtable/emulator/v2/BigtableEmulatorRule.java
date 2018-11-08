49d4114177af6b4a506bffb5e67830c4d8b70d47
Igor Bernstein <igorbernstein@google.com>
2018-11-08 15:05:39 -0500

    bigtable emulator (#3840)
    This allows customers to easily interact with the bigtable emulator. Currently the Bigtable emulator is implemented in go. It is a fairly complex bit of code that would be very difficult to replicate in java. This makes it very difficult for users to interact with it in a java project. This PR attempts to make things easier for the user by wrapping the golang emulator in a jar and providing a JUnit rule to simplify lifecycle management of the emulator.
    
    It's implemented as 2 artifacts:
    - google-cloud-gcloud-maven-plugin: a maven plugin that can download artifacts from the gcloud sdk repo and add them as resources to a jar. This avoids the need to check in binaries into google-cloud-java and makes sure that the binaries are up to date.
    - google-cloud-bigtable-emulator: This uses the maven plugin to bundle the bigtable emulator in a jar and adds a java wrapper around the binary. The wrapper was extracted from the existing bigtable client integration tests: https://github.com/googleapis/google-cloud-java/blob/master/google-cloud-clients/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/it/env/Emulator.java

---
+++ java-bigtable/google-cloud-testing/google-cloud-bigtable-emulator/src/main/java/com/google/cloud/bigtable/emulator/v2/BigtableEmulatorRule.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.emulator.v2;
+
+import com.google.api.core.BetaApi;
+import io.grpc.ManagedChannel;
+import org.junit.rules.ExternalResource;
+
+/**
+ * The BigtableEmulatorRule manages the lifecycle of the Bigtable {@link Emulator}. Before the
+ * start of a test, the emulator will be started on a random port and will be shutdown after the
+ * test finishes.
+ *
+ * <p>Example usage:
+ *
+ * <code>{@code
+ *   {@literal @RunWith(JUnit4.class)}
+ *   public class MyTest {
+ *     {@literal @Rule}
+ *     public final BigtableEmulatorRule bigtableEmulator = BigtableEmulatorRule.create();
+ *
+ *     {@literal @Test}
+ *     public void testUsingEmulator() {
+ *        ManagedChannel adminChannel = bigtableEmulator.getAdminChannel();
+ *        // Do something with channel
+ *     }
+ *   }
+ * }</code>
+ */
+@BetaApi("Surface for Bigtable emulator is not yet stable")
+public class BigtableEmulatorRule extends ExternalResource {
+  private Emulator emulator;
+
+  public static BigtableEmulatorRule create() {
+    return new BigtableEmulatorRule();
+  }
+
+  private BigtableEmulatorRule() { }
+
+  /** Initializes the Bigtable emulator before a test runs. */
+  @Override
+  protected void before() throws Throwable {
+    emulator = Emulator.createBundled();
+    emulator.start();
+  }
+
+  /** Stops the Bigtable emulator after a test finishes. */
+  @Override
+  protected void after() {
+    emulator.stop();
+    emulator = null;
+  }
+
+  /**
+   * Gets a {@link ManagedChannel} connected to the Emulator. The channel is configured for data
+   * operations.
+   */
+  public ManagedChannel getDataChannel() {
+    return emulator.getDataChannel();
+  }
+
+  /**
+   * Gets a {@link ManagedChannel} connected to the Emulator. This channel should be used for admin
+   * operations.
+   */
+  public ManagedChannel getAdminChannel() {
+    return emulator.getAdminChannel();
+  }
+
+  /**
+   * Gets the port of the emulator, allowing the caller to create their own {@link ManagedChannel}.
+   */
+  public int getPort() {
+    return emulator.getPort();
+  }
+}
