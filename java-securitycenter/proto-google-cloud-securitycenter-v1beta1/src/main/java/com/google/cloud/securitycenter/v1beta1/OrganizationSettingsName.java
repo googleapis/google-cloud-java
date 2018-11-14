994aceb6a4177a3d2b20c4224348b936816f2baa
Jeff Ching <chingor@google.com>
2018-11-14 11:33:57 -0800

    New Client: Cloud Security Command Center (#3885)
    * Add synth.py script and generate files
    
    * Add versions for release management, add README.md, pom.xml
    
    * Add google-cloud-security center as module for google-cloud-clients
    
    * Add securitycenter to google-api-grpc modules
    
    * Add securitycenter artifacts to the bom
    
    * security center depends on iam protos
    
    * Regenerate Security Center models/stubs
    
    * Fix artifact versions
    
    * Fix artifact versions
    
    * Fix artifact versions

---
+++ java-securitycenter/proto-google-cloud-securitycenter-v1beta1/src/main/java/com/google/cloud/securitycenter/v1beta1/OrganizationSettingsName.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.securitycenter.v1beta1;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.api.pathtemplate.PathTemplate;
+import com.google.api.resourcenames.ResourceName;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+// AUTO-GENERATED DOCUMENTATION AND CLASS
+@javax.annotation.Generated("by GAPIC protoc plugin")
+public class OrganizationSettingsName implements ResourceName {
+
+  private static final PathTemplate PATH_TEMPLATE =
+      PathTemplate.createWithoutUrlEncoding("organizations/{organization}/organizationSettings");
+
+  private volatile Map<String, String> fieldValuesMap;
+
+  private final String organization;
+
+  public String getOrganization() {
+    return organization;
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+
+  public Builder toBuilder() {
+    return new Builder(this);
+  }
+
+  private OrganizationSettingsName(Builder builder) {
+    organization = Preconditions.checkNotNull(builder.getOrganization());
+  }
+
+  public static OrganizationSettingsName of(String organization) {
+    return newBuilder()
+      .setOrganization(organization)
+      .build();
+  }
+
+  public static String format(String organization) {
+    return newBuilder()
+      .setOrganization(organization)
+      .build()
+      .toString();
+  }
+
+  public static OrganizationSettingsName parse(String formattedString) {
+    if (formattedString.isEmpty()) {
+      return null;
+    }
+    Map<String, String> matchMap =
+        PATH_TEMPLATE.validatedMatch(formattedString, "OrganizationSettingsName.parse: formattedString not in valid format");
+    return of(matchMap.get("organization"));
+  }
+
+  public static List<OrganizationSettingsName> parseList(List<String> formattedStrings) {
+    List<OrganizationSettingsName> list = new ArrayList<>(formattedStrings.size());
+    for (String formattedString : formattedStrings) {
+      list.add(parse(formattedString));
+    }
+    return list;
+  }
+
+  public static List<String> toStringList(List<OrganizationSettingsName> values) {
+    List<String> list = new ArrayList<String>(values.size());
+    for (OrganizationSettingsName value : values) {
+      if (value == null) {
+        list.add("");
+      } else {
+        list.add(value.toString());
+      }
+    }
+    return list;
+  }
+
+  public static boolean isParsableFrom(String formattedString) {
+    return PATH_TEMPLATE.matches(formattedString);
+  }
+
+  public Map<String, String> getFieldValuesMap() {
+    if (fieldValuesMap == null) {
+      synchronized (this) {
+        if (fieldValuesMap == null) {
+          ImmutableMap.Builder<String, String> fieldMapBuilder = ImmutableMap.builder();
+          fieldMapBuilder.put("organization", organization);
+          fieldValuesMap = fieldMapBuilder.build();
+        }
+      }
+    }
+    return fieldValuesMap;
+  }
+
+  public String getFieldValue(String fieldName) {
+    return getFieldValuesMap().get(fieldName);
+  }
+
+  @Override
+  public String toString() {
+    return PATH_TEMPLATE.instantiate("organization", organization);
+  }
+
+  /** Builder for OrganizationSettingsName. */
+  public static class Builder {
+
+    private String organization;
+
+    public String getOrganization() {
+      return organization;
+    }
+
+    public Builder setOrganization(String organization) {
+      this.organization = organization;
+      return this;
+    }
+
+    private Builder() {
+    }
+
+    private Builder(OrganizationSettingsName organizationSettingsName) {
+      organization = organizationSettingsName.organization;
+    }
+
+    public OrganizationSettingsName build() {
+      return new OrganizationSettingsName(this);
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (o instanceof OrganizationSettingsName) {
+      OrganizationSettingsName that = (OrganizationSettingsName) o;
+      return (this.organization.equals(that.organization));
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    int h = 1;
+    h *= 1000003;
+    h ^= organization.hashCode();
+    return h;
+  }
+}
+
