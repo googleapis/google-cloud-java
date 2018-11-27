7f64209dc21abc3f7d26e0b9a2098a876e6d524f
Kenneth Jung <kmj@google.com>
2018-11-27 12:27:14 -0800

    BigQuery Storage: Add shim layer settings and configuration (#4022)
    * Create a shim layer for the BigQuery Storage API.
    
    This change adds a simple shim layer to the BigQuery Storage API client.
    It is modeled after the shim layer in the Cloud Bigtable client, but
    unlike that implementation, it does not create chained instances of
    unary and server-streaming callables to perform translation; instead,
    its parameters are simple pass-throughs to the underlying gRPC client
    stub.
    
    The shim currently does nothing but read default values from the base
    client layer and pass them back through.
    
    * Set the default gRPC inbound message size.
    
    This change sets the default value for the maximum inbound message size
    in the BigQuery Storage API client. A ReadRowsResponse object can
    contain serialized row block of up to 10 MB in size if the underlying
    row(s) are sufficiently large.
    
    * Update the default timeout value for ReadRows.
    
    This change modifies the default client-side timeout value for the
    ReadRows API. ReadRows calls can last for long periods in the case of
    large tables. The default timeout value is set to a full day, which is
    the duration for which a read session is valid.
    
    * Add a resumption strategy for ReadRows.
    
    This change modifies the configuration for the ReadRows API to add a
    resumption strategy, allowing the connection to be resumed transparently
    in the case of transient errors.

---
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta1/stub/readrows/ReadRowsResumptionStrategy.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery.storage.v1beta1.stub.readrows;
+
+import com.google.api.core.InternalApi;
+import com.google.api.gax.retrying.StreamResumptionStrategy;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadRowsRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadRowsResponse;
+
+/**
+ * An implementation of a {@link StreamResumptionStrategy} for the ReadRows API. This class tracks
+ * the offset of the last row received and, upon retry, attempts to resume the stream at the next
+ * offset.
+ *
+ * <p>This class is considered an internal implementation detail and not meant to be used by
+ * applications.
+ */
+@InternalApi
+public class ReadRowsResumptionStrategy
+    implements StreamResumptionStrategy<ReadRowsRequest, ReadRowsResponse> {
+
+  // Number of rows processed.
+  private long rowsProcessed = 0;
+
+  @Override
+  public StreamResumptionStrategy<ReadRowsRequest, ReadRowsResponse> createNew() {
+    return new ReadRowsResumptionStrategy();
+  }
+
+  @Override
+  public ReadRowsResponse processResponse(ReadRowsResponse response) {
+    rowsProcessed += response.getAvroRows().getRowCount();
+    return response;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * Given the initial/original request, this implementation generates a request that will yield a
+   * new stream whose first response would come right after the last response received by
+   * processResponse. It takes into account the offset from the original request.
+   */
+  @Override
+  public ReadRowsRequest getResumeRequest(ReadRowsRequest originalRequest) {
+    ReadRowsRequest.Builder resumeRequestBuilder = originalRequest.toBuilder();
+
+    resumeRequestBuilder.getReadPositionBuilder()
+        .setOffset(originalRequest.getReadPosition().getOffset() + rowsProcessed);
+
+    return resumeRequestBuilder.build();
+  }
+
+  @Override
+  public boolean canResume() {
+    return true;
+  }
+}
---
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta1/stub/EnhancedBigQueryStorageStubSettingsTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery.storage.v1beta1.stub;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.api.gax.retrying.RetrySettings;
+import com.google.api.gax.rpc.ServerStreamingCallSettings;
+import com.google.api.gax.rpc.StatusCode.Code;
+import com.google.api.gax.rpc.UnaryCallSettings;
+import com.google.api.gax.rpc.WatchdogProvider;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.BatchCreateReadSessionStreamsRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.BatchCreateReadSessionStreamsResponse;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.CreateReadSessionRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.FinalizeStreamRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadRowsRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadRowsResponse;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadSession;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.SplitReadStreamRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.SplitReadStreamResponse;
+import com.google.protobuf.Empty;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+import org.threeten.bp.Duration;
+
+@RunWith(JUnit4.class)
+public class EnhancedBigQueryStorageStubSettingsTest {
+
+  private static final int MAX_INBOUND_MESSAGE_SIZE = 1024 * 1024 * 11;
+
+  @Test
+  public void testSettingsArePreserved() {
+    String endpoint = "some.other.host:123";
+    CredentialsProvider credentialsProvider = Mockito.mock(CredentialsProvider.class);
+    Duration watchdogInterval = Duration.ofSeconds(12);
+    WatchdogProvider watchdogProvider = Mockito.mock(WatchdogProvider.class);
+
+    EnhancedBigQueryStorageStubSettings.Builder builder =
+        EnhancedBigQueryStorageStubSettings.newBuilder()
+            .setEndpoint(endpoint)
+            .setCredentialsProvider(credentialsProvider)
+            .setStreamWatchdogCheckInterval(watchdogInterval)
+            .setStreamWatchdogProvider(watchdogProvider);
+
+    verifyBuilder(
+        builder,
+        endpoint,
+        credentialsProvider,
+        watchdogInterval,
+        watchdogProvider);
+
+    verifySettings(
+        builder.build(),
+        endpoint,
+        credentialsProvider,
+        watchdogInterval,
+        watchdogProvider);
+
+    verifyBuilder(
+        builder.build().toBuilder(),
+        endpoint,
+        credentialsProvider,
+        watchdogInterval,
+        watchdogProvider);
+  }
+
+  private void verifyBuilder(
+      EnhancedBigQueryStorageStubSettings.Builder builder,
+      String endpoint,
+      CredentialsProvider credentialsProvider,
+      Duration watchdogInterval,
+      WatchdogProvider watchdogProvider) {
+    assertThat(builder.getEndpoint()).isEqualTo(endpoint);
+    assertThat(builder.getCredentialsProvider()).isEqualTo(credentialsProvider);
+    assertThat(builder.getStreamWatchdogCheckInterval()).isEqualTo(watchdogInterval);
+    assertThat(builder.getStreamWatchdogProvider()).isEqualTo(watchdogProvider);
+
+    InstantiatingGrpcChannelProvider channelProvider =
+        (InstantiatingGrpcChannelProvider) builder.getTransportChannelProvider();
+    assertThat(channelProvider.toBuilder().getMaxInboundMessageSize())
+        .isEqualTo(MAX_INBOUND_MESSAGE_SIZE);
+    assertThat(channelProvider.toBuilder().getPoolSize()).isGreaterThan(1);
+  }
+
+  private void verifySettings(
+      EnhancedBigQueryStorageStubSettings settings,
+      String endpoint,
+      CredentialsProvider credentialsProvider,
+      Duration watchdogInterval,
+      WatchdogProvider watchdogProvider) {
+    assertThat(settings.getEndpoint()).isEqualTo(endpoint);
+    assertThat(settings.getCredentialsProvider()).isEqualTo(credentialsProvider);
+    assertThat(settings.getStreamWatchdogCheckInterval()).isEqualTo(watchdogInterval);
+    assertThat(settings.getStreamWatchdogProvider()).isEqualTo(watchdogProvider);
+
+    InstantiatingGrpcChannelProvider channelProvider =
+        (InstantiatingGrpcChannelProvider) settings.getTransportChannelProvider();
+    assertThat(channelProvider.toBuilder().getMaxInboundMessageSize())
+        .isEqualTo(MAX_INBOUND_MESSAGE_SIZE);
+    assertThat(channelProvider.toBuilder().getPoolSize()).isGreaterThan(1);
+  }
+
+  @Test
+  public void testCreateReadSessionSettings() {
+    UnaryCallSettings.Builder<CreateReadSessionRequest, ReadSession> builder =
+        EnhancedBigQueryStorageStubSettings.newBuilder().createReadSessionSettings();
+    verifyRetrySettings(builder.getRetryableCodes(), builder.getRetrySettings());
+  }
+
+  @Test
+  public void testReadRowsSettings() {
+    ServerStreamingCallSettings.Builder<ReadRowsRequest, ReadRowsResponse> builder =
+        EnhancedBigQueryStorageStubSettings.newBuilder().readRowsSettings();
+    assertThat(builder.getRetryableCodes()).containsAllOf(Code.DEADLINE_EXCEEDED, Code.UNAVAILABLE);
+    RetrySettings retrySettings = builder.getRetrySettings();
+    assertThat(retrySettings.getInitialRetryDelay()).isEqualTo(Duration.ofMillis(100L));
+    assertThat(retrySettings.getRetryDelayMultiplier()).isWithin(1e-6).of(1.3);
+    assertThat(retrySettings.getMaxRetryDelay()).isEqualTo(Duration.ofMinutes(1L));
+    assertThat(retrySettings.getInitialRpcTimeout()).isEqualTo(Duration.ofDays(1L));
+    assertThat(retrySettings.getRpcTimeoutMultiplier()).isWithin(1e-6).of(1.0);
+    assertThat(retrySettings.getMaxRpcTimeout()).isEqualTo(Duration.ofDays(1L));
+    assertThat(retrySettings.getTotalTimeout()).isEqualTo(Duration.ofDays(1L));
+    assertThat(builder.getIdleTimeout()).isEqualTo(Duration.ZERO);
+  }
+
+  @Test
+  public void testBatchCreateReadSessionStreamsSettings() {
+    UnaryCallSettings.Builder<
+            BatchCreateReadSessionStreamsRequest, BatchCreateReadSessionStreamsResponse>
+        builder =
+        EnhancedBigQueryStorageStubSettings.newBuilder().batchCreateReadSessionStreamsSettings();
+    verifyRetrySettings(builder.getRetryableCodes(), builder.getRetrySettings());
+  }
+
+  @Test
+  public void testFinalizeStreamSettings() {
+    UnaryCallSettings.Builder<FinalizeStreamRequest, Empty> builder =
+        EnhancedBigQueryStorageStubSettings.newBuilder().finalizeStreamSettings();
+    verifyRetrySettings(builder.getRetryableCodes(), builder.getRetrySettings());
+  }
+
+  @Test
+  public void testSplitReadStreamSettings() {
+    UnaryCallSettings.Builder<SplitReadStreamRequest, SplitReadStreamResponse> builder =
+        EnhancedBigQueryStorageStubSettings.newBuilder().splitReadStreamSettings();
+    verifyRetrySettings(builder.getRetryableCodes(), builder.getRetrySettings());
+  }
+
+  private void verifyRetrySettings(Set<Code> retryCodes, RetrySettings retrySettings) {
+    assertThat(retryCodes).containsAllOf(Code.DEADLINE_EXCEEDED, Code.UNAVAILABLE);
+    assertThat(retrySettings.getTotalTimeout()).isGreaterThan(Duration.ZERO);
+    assertThat(retrySettings.getInitialRetryDelay()).isGreaterThan(Duration.ZERO);
+    assertThat(retrySettings.getRetryDelayMultiplier()).isAtLeast(1.0);
+    assertThat(retrySettings.getMaxRetryDelay()).isGreaterThan(Duration.ZERO);
+    assertThat(retrySettings.getInitialRpcTimeout()).isGreaterThan(Duration.ZERO);
+    assertThat(retrySettings.getRpcTimeoutMultiplier()).isAtLeast(1.0);
+    assertThat(retrySettings.getMaxRpcTimeout()).isGreaterThan(Duration.ZERO);
+  }
+}
