3120f64f50182421112a2ace1021d7fff3fb77da
Jeff Ching <chingor@google.com>
2018-11-29 16:42:00 -0800

    Add lint test and format generated code (#4114)
    * Add lint Kokoro tests
    
    * synthtool will now run the java formatter after generating the files

--- java-bigtable/google-cloud-bigtable/synth.py
+++ java-bigtable/google-cloud-bigtable/synth.py
@@ -16,16 +16,25 @@
 
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.java as java
 
 gapic = gcp.GAPICGenerator()
-common_templates = gcp.CommonTemplates()
 
-library = gapic.java_library(
-    service='bigtable',
-    version='v2',
-    config_path='/google/bigtable/artman_bigtable.yaml',
-    artman_output_name='')
+service = 'bigtable'
+versions = ['v2']
+config_pattern = '/google/bigtable/artman_bigtable.yaml'
 
-s.copy(library / 'gapic-google-cloud-bigtable-v2/src', 'src')
-s.copy(library / 'grpc-google-cloud-bigtable-v2/src', '../../google-api-grpc/grpc-google-cloud-bigtable-v2/src')
-s.copy(library / 'proto-google-cloud-bigtable-v2/src', '../../google-api-grpc/proto-google-cloud-bigtable-v2/src')
+for version in versions:
+  library = gapic.java_library(
+      service=service,
+      version=version,
+      config_path=config_pattern.format(version=version),
+      artman_output_name='')
+
+  s.copy(library / f'gapic-google-cloud-{service}-{version}/src', 'src')
+  s.copy(library / f'grpc-google-cloud-{service}-{version}/src', f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
+  s.copy(library / f'proto-google-cloud-{service}-{version}/src', f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
+
+  java.format_code('./src')
+  java.format_code(f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
+  java.format_code(f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
