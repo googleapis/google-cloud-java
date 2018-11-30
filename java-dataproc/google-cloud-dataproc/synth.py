74093fb4bbdfa247d1278f8059649ac814bd8cdc
Jeff Ching <chingor@google.com>
2018-11-29 16:42:00 -0800

    Add lint test and format generated code (#4114)
    * Add lint Kokoro tests
    
    * synthtool will now run the java formatter after generating the files

--- java-dataproc/google-cloud-dataproc/synth.py
+++ java-dataproc/google-cloud-dataproc/synth.py
@@ -16,17 +16,25 @@
 
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.java as java
 
 gapic = gcp.GAPICGenerator()
-common_templates = gcp.CommonTemplates()
 
-for version in ["v1", "v1beta2"]:
+service = 'dataproc'
+versions = ['v1', 'v1beta2']
+config_pattern = '/google/cloud/dataproc/artman_dataproc_{version}.yaml'
+
+for version in versions:
     library = gapic.java_library(
-        service='dataproc',
+        service=service,
         version=version,
-        config_path=f'/google/cloud/dataproc/artman_dataproc_{version}.yaml',
+        config_path=config_pattern.format(version=version),
         artman_output_name='')
 
-    s.copy(library / f'gapic-google-cloud-dataproc-{version}/src', 'src')
-    s.copy(library / f'grpc-google-cloud-dataproc-{version}/src', f'../../google-api-grpc/grpc-google-cloud-dataproc-{version}/src')
-    s.copy(library / f'proto-google-cloud-dataproc-{version}/src', f'../../google-api-grpc/proto-google-cloud-dataproc-{version}/src')
+    s.copy(library / f'gapic-google-cloud-{service}-{version}/src', 'src')
+    s.copy(library / f'grpc-google-cloud-{service}-{version}/src', f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
+    s.copy(library / f'proto-google-cloud-{service}-{version}/src', f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
+
+    java.format_code('./src')
+    java.format_code(f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
+    java.format_code(f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
