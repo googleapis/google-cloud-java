f5d76bc654c483abf1ff7eaf7929aaa0dd1bac00
Jeff Ching <chingor@google.com>
2018-11-30 11:29:29 -0800

    Run google-java-format on all artifacts (#4121)
    * Run google-java-format on all artifats
    
    * Format some missed files
    
    * Add more missed files
    
    * Fix formatting issues from the merge

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta1/BigQueryStorageClient.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta1/BigQueryStorageClient.java
@@ -62,13 +62,13 @@ import java.util.concurrent.TimeUnit;
  * methods:
  *
  * <ol>
- *   <li> A "flattened" method. With this type of method, the fields of the request type have been
+ *   <li>A "flattened" method. With this type of method, the fields of the request type have been
  *       converted into function parameters. It may be the case that not all fields are available as
  *       parameters, and not every API method will have a flattened method entry point.
- *   <li> A "request object" method. This type of method only takes one parameter, a request object,
+ *   <li>A "request object" method. This type of method only takes one parameter, a request object,
  *       which must be constructed before the call. Not every API method will have a request object
  *       method.
- *   <li> A "callable" method. This type of method takes no parameters and returns an immutable API
+ *   <li>A "callable" method. This type of method takes no parameters and returns an immutable API
  *       callable object, which can be used to initiate calls to the service.
  * </ol>
  *
@@ -382,8 +382,8 @@ public class BigQueryStorageClient implements BackgroundResource {
    * </code></pre>
    */
   public final UnaryCallable<
-      BatchCreateReadSessionStreamsRequest, BatchCreateReadSessionStreamsResponse>
-  batchCreateReadSessionStreamsCallable() {
+          BatchCreateReadSessionStreamsRequest, BatchCreateReadSessionStreamsResponse>
+      batchCreateReadSessionStreamsCallable() {
     return stub.batchCreateReadSessionStreamsCallable();
   }
 
@@ -577,7 +577,7 @@ public class BigQueryStorageClient implements BackgroundResource {
    * </code></pre>
    */
   public final UnaryCallable<SplitReadStreamRequest, SplitReadStreamResponse>
-  splitReadStreamCallable() {
+      splitReadStreamCallable() {
     return stub.splitReadStreamCallable();
   }
 
