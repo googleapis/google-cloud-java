3ee34f5e5efd522bef4a0625f5c9ec3666857b43
Jeff Ching <chingor@google.com>
2018-12-12 14:58:47 -0800

    Synth script should format java code

--- google-cloud-clients/google-cloud-iamcredentials/synth.py
+++ google-cloud-clients/google-cloud-iamcredentials/synth.py
@@ -16,20 +16,25 @@
 
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.java as java
 
 gapic = gcp.GAPICGenerator()
-common_templates = gcp.CommonTemplates()
 
-versions = ['v1']
 service = 'iamcredentials'
+versions = ['v1']
+config_pattern = '/google/iam/credentials/artman_iamcredentials_{version}.yaml'
 
 for version in versions:
     library = gapic.java_library(
         service=service,
         version=version,
-        config_path=f'/google/iam/credentials/artman_iamcredentials_v1.yaml',
+        config_path=config_pattern.format(version=version),
         artman_output_name='')
 
     s.copy(library / f'gapic-google-cloud-{service}-{version}/src', 'src')
     s.copy(library / f'grpc-google-cloud-{service}-{version}/src', f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
     s.copy(library / f'proto-google-cloud-{service}-{version}/src', f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
+
+    java.format_code('./src')
+    java.format_code(f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
+    java.format_code(f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
