ae3b35e539d2fe7cf5ccda15b148173c8bf8a778
Charles Li <charles@qlogic.io>
2018-12-28 15:49:36 -0500

    update format for storage/Option.java

--- google-cloud-clients/google-cloud-storage/src/main/java/com/google/cloud/storage/Option.java
+++ google-cloud-clients/google-cloud-storage/src/main/java/com/google/cloud/storage/Option.java
@@ -24,9 +24,7 @@ import com.google.common.base.MoreObjects;
 import java.io.Serializable;
 import java.util.Objects;
 
-/**
- * Base class for Storage operation option.
- */
+/** Base class for Storage operation option. */
 public abstract class Option implements Serializable {
 
   private static final long serialVersionUID = -73199088766477208L;
