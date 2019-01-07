aac172680b559088669bba3bb5dd966671de9da8
elisheva-qlogic <elisheva@qlogic.io>
2019-01-07 10:43:40 -0500

    instanceAdmin sample and tests

---
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/bigtable/InstanceAdmin.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2019 Google LLC.  All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.examples.bigtable;
+
+import com.google.api.gax.rpc.AlreadyExistsException;
+import com.google.api.gax.rpc.NotFoundException;
+import com.google.bigtable.admin.v2.ProjectName;
+import com.google.cloud.bigtable.admin.v2.BigtableInstanceAdminClient;
+import com.google.cloud.bigtable.admin.v2.BigtableInstanceAdminSettings;
+import com.google.cloud.bigtable.admin.v2.models.Cluster;
+import com.google.cloud.bigtable.admin.v2.models.CreateClusterRequest;
+import com.google.cloud.bigtable.admin.v2.models.CreateInstanceRequest;
+import com.google.cloud.bigtable.admin.v2.models.Instance;
+import com.google.cloud.bigtable.admin.v2.models.Instance.Type;
+import com.google.cloud.bigtable.admin.v2.models.PartialListInstancesException;
+import com.google.cloud.bigtable.admin.v2.models.StorageType;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class InstanceAdmin {
+
+  private static final String CLUSTER = "cluster" + System.currentTimeMillis();
+  private final String clusterId;
+  private final String instanceId;
+  private final BigtableInstanceAdminClient adminClient;
+
+  public static void main(String[] args) throws IOException {
+
+    if (args.length != 1) {
+      System.out.println("Missing required project id");
+      return;
+    }
+    String projectId = args[0];
+
+    InstanceAdmin instanceAdmin = new InstanceAdmin(projectId, "ssd-instance", "ssd-cluster");
+    instanceAdmin.run();
+  }
+
+  public InstanceAdmin(String projectId, String instanceId, String clusterId) throws IOException {
+    this.instanceId = instanceId;
+    this.clusterId = clusterId;
+
+    // [START connecting_to_bigtable]
+    // Create the settings to configure a bigtable instance admin client
+    BigtableInstanceAdminSettings instanceAdminSettings =
+        BigtableInstanceAdminSettings.newBuilder()
+            .setProjectName(ProjectName.of(projectId))
+            .build();
+
+    // Create bigtable instance admin client
+    adminClient = BigtableInstanceAdminClient.create(instanceAdminSettings);
+    // [END connecting_to_bigtable]
+  }
+
+  public void run() {
+    createProdInstance();
+    listInstances();
+    getInstance();
+    listClusters();
+    addCluster();
+    deleteCluster();
+    deleteInstance();
+    adminClient.close();
+  }
+
+  public void createProdInstance() {
+    // Check if instance exists, create instance if does not exists
+    if (!adminClient.exists(instanceId)) {
+      System.out.println("Instance does not exist, creating a PRODUCTION instance");
+      // [START bigtable_create_prod_instance]
+      // Creates a Production Instance with the ID "ssd-instance"
+      // cluster id "ssd-cluster", 3 nodes and location us-central1-f
+      CreateInstanceRequest createInstanceRequest =
+          CreateInstanceRequest.of(instanceId)
+              .addCluster(clusterId, "us-central1-f", 3, StorageType.SSD)
+              .setType(Type.PRODUCTION)
+              .addLabel("example", "instance_admin");
+      // Creates production instance with given request
+      try {
+        Instance instance = adminClient.createInstance(createInstanceRequest);
+        System.out.printf("PRODUCTION type instance %s created successfully%n", instance.getId());
+      } catch (AlreadyExistsException e) {
+        System.err.println("Failed to create instance, already exists: " + e.getMessage());
+        System.exit(0);
+      }
+      // [END bigtable_create_prod_instance]
+    }
+  }
+
+  public void listInstances() {
+    System.out.println("\nListing Instances");
+    // [START bigtable_list_instances]
+    try {
+      List<Instance> instances = adminClient.listInstances();
+      for (Instance instance : instances) {
+        System.out.println(instance.getId());
+      }
+    } catch (PartialListInstancesException e) {
+      System.out.println("Failed to list instances: " + e.getMessage());
+      System.out.println("The following zones are unavailable: " + e.getUnavailableZones());
+      System.out.println("But the following instances are reachable: " + e.getInstances());
+    }
+    // [END bigtable_list_instances]
+  }
+
+  public Instance getInstance() {
+    System.out.println("\nGet Instance");
+    // [START bigtable_get_instance]
+    Instance instance = null;
+    try {
+      instance = adminClient.getInstance(instanceId);
+      System.out.println("Instance ID: " + instance.getId());
+      System.out.println("Display Name: " + instance.getDisplayName());
+      System.out.print("Labels: ");
+      Map<String, String> labels = instance.getLabels();
+      for (String key : labels.keySet()) {
+        System.out.printf("%s - %s", key, labels.get(key));
+      }
+      System.out.println("\nState: " + instance.getState());
+      System.out.println("Type: " + instance.getType());
+    } catch (NotFoundException e) {
+      System.err.println("Failed to get non-existent instance: " + e.getMessage());
+    }
+    // [END bigtable_get_instance]
+    return instance;
+  }
+
+  public void listClusters() {
+    System.out.println("\nListing Clusters");
+    // [START bigtable_get_clusters]
+    try {
+      List<Cluster> clusters = adminClient.listClusters(instanceId);
+      for (Cluster cluster : clusters) {
+        System.out.println(cluster.getId());
+      }
+    } catch (NotFoundException e) {
+      System.err.println("Failed to list clusters from a non-existent instance: " + e.getMessage());
+    }
+    // [END bigtable_get_clusters]
+  }
+
+  public void deleteInstance() {
+    System.out.println("\nDeleting Instance");
+    // [START bigtable_delete_instance]
+    try {
+      adminClient.deleteInstance(instanceId);
+      System.out.println("Instance deleted: " + instanceId);
+    } catch (NotFoundException e) {
+      System.err.println("Failed to delete non-existent instance: " + e.getMessage());
+    }
+    // [END bigtable_delete_instance]
+  }
+
+  public void addCluster() {
+    System.out.printf("%nAdding cluster %s to instance: %s%n", CLUSTER, instanceId);
+    // [START bigtable_create_cluster]
+    try {
+      adminClient.createCluster(
+          CreateClusterRequest.of(instanceId, CLUSTER)
+              .setZone("us-central1-c")
+              .setServeNodes(3)
+              .setStorageType(StorageType.SSD));
+      System.out.printf("Cluster: %s created successfully%n", CLUSTER);
+    } catch (AlreadyExistsException e) {
+      System.err.println("Failed to add cluster, already exists: " + e.getMessage());
+    }
+    // [END bigtable_create_cluster]
+  }
+
+  public void deleteCluster() {
+    System.out.printf("%nDeleting cluster %s from instance: %s%n", CLUSTER, instanceId);
+    // [START bigtable_delete_cluster]
+    try {
+      adminClient.deleteCluster(instanceId, CLUSTER);
+      adminClient.listClusters(instanceId);
+      System.out.printf("Cluster: %s deleted successfully%n", CLUSTER);
+    } catch (NotFoundException e) {
+      System.err.println("Failed to delete a non-existent cluster: " + e.getMessage());
+    }
+    // [END bigtable_delete_cluster]
+  }
+}
---
+++ google-cloud-examples/src/test/java/com/google/cloud/examples/bigtable/ITInstanceAdmin.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2019 Google LLC.  All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.examples.bigtable;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.api.gax.rpc.NotFoundException;
+import com.google.bigtable.admin.v2.InstanceName;
+import com.google.bigtable.admin.v2.ProjectName;
+import com.google.cloud.bigtable.admin.v2.BigtableInstanceAdminClient;
+import com.google.cloud.bigtable.admin.v2.BigtableInstanceAdminSettings;
+import com.google.cloud.bigtable.admin.v2.models.Cluster;
+import com.google.cloud.bigtable.admin.v2.models.CreateInstanceRequest;
+import com.google.cloud.bigtable.admin.v2.models.Instance;
+import com.google.cloud.bigtable.admin.v2.models.Instance.Type;
+import com.google.cloud.bigtable.admin.v2.models.StorageType;
+import java.io.IOException;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.AssumptionViolatedException;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ITInstanceAdmin {
+
+  private static final String INSTANCE_PROPERTY_NAME = "bigtable.instance";
+  private static final String INSTANCE_PREFIX = "instanceadmin";
+  private static final String CLUSTER = "cluster" + System.currentTimeMillis();
+  private static String clusterId;
+  private static String instanceId;
+  private static BigtableInstanceAdminClient adminClient;
+  private static InstanceAdmin instanceAdmin;
+  private static ProjectName projectName;
+
+  @BeforeClass
+  public static void beforeClass() throws IOException {
+    String targetProject = System.getProperty(INSTANCE_PROPERTY_NAME);
+    if (targetProject == null) {
+      adminClient = null;
+      return;
+    }
+    projectName = ProjectName.of(InstanceName.parse(targetProject).getProject());
+    BigtableInstanceAdminSettings instanceAdminSettings =
+        BigtableInstanceAdminSettings.newBuilder().setProjectName(projectName).build();
+    adminClient = BigtableInstanceAdminClient.create(instanceAdminSettings);
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    garbageCollect();
+    adminClient.close();
+  }
+
+  @Before
+  public void setup() throws IOException {
+    if (adminClient == null) {
+      throw new AssumptionViolatedException(
+          INSTANCE_PROPERTY_NAME + " property is not set, skipping integration tests.");
+    }
+    instanceId = generateId();
+    clusterId = generateId();
+    instanceAdmin = new InstanceAdmin(projectName.getProject(), instanceId, clusterId);
+    adminClient.createInstance(
+        CreateInstanceRequest.of(instanceId)
+            .addCluster(clusterId, "us-central1-f", 3, StorageType.SSD)
+            .setType(Type.PRODUCTION)
+            .addLabel("example", "instance_admin"));
+  }
+
+  @After
+  public void after() {
+    if (adminClient.exists(instanceId)) {
+      adminClient.deleteInstance(instanceId);
+    }
+  }
+
+  @Test
+  public void testCreateAndDeleteInstance() throws IOException {
+    // Create instance
+    String fakeInstance = generateId();
+    String fakeCluster = generateId();
+    InstanceAdmin testInstanceAdmin =
+        new InstanceAdmin(projectName.getProject(), fakeInstance, fakeCluster);
+    testInstanceAdmin.createProdInstance();
+    assertTrue(adminClient.exists(fakeInstance));
+
+    // Delete instance
+    testInstanceAdmin.deleteInstance();
+    assertTrue(!adminClient.exists(fakeInstance));
+  }
+
+  @Test
+  public void testGetInstance() {
+    // Get instance
+    Instance instance = instanceAdmin.getInstance();
+    assertNotNull(instance);
+  }
+
+  @Test(expected = NotFoundException.class)
+  public void testAddAndDeleteCluster() {
+    // Add cluster
+    instanceAdmin.addCluster();
+    Cluster cluster = adminClient.getCluster(instanceId, CLUSTER);
+    assertNotNull(cluster);
+
+    // Delete cluster
+    instanceAdmin.deleteCluster();
+    adminClient.getCluster(instanceId, CLUSTER);
+  }
+
+  // TODO: add test for instanceAdmin.listInstances()
+  // TODO: and test for instanceAdmin.listClusters()
+
+  @Test
+  public void testRunDoesNotFail() {
+    instanceAdmin.run();
+  }
+
+  private static String generateId() {
+    return String.format("%s-%x", INSTANCE_PREFIX, new Random().nextInt());
+  }
+
+  private static void garbageCollect() {
+    Pattern timestampPattern = Pattern.compile(INSTANCE_PREFIX + "-([0-9a-f]+)");
+    System.out.println();
+    for (Instance instance : adminClient.listInstances()) {
+      Matcher matcher = timestampPattern.matcher(instance.getId());
+      if (!matcher.matches()) {
+        continue;
+      }
+      System.out.println("Garbage collecting orphaned table: " + instance);
+      adminClient.deleteInstance(instance.getId());
+    }
+  }
+}
