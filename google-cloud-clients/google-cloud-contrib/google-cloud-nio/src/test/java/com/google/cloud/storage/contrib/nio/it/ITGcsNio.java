8b082e01068cab3d4c2204361c4fc37d6d8fcb33
romlogic <45241349+romlogic@users.noreply.github.com>
2019-01-08 08:59:13 -0600

    Revert "good to have edits"
    This reverts commit 5da4c6039a4c752087a9896743a6e8b18323f3ca.

--- google-cloud-clients/google-cloud-contrib/google-cloud-nio/src/test/java/com/google/cloud/storage/contrib/nio/it/ITGcsNio.java
+++ google-cloud-clients/google-cloud-contrib/google-cloud-nio/src/test/java/com/google/cloud/storage/contrib/nio/it/ITGcsNio.java
@@ -414,7 +414,7 @@ public class ITGcsNio {
   public void testWrite() throws IOException {
     CloudStorageFileSystem testBucket = getTestBucket();
     Path path = testBucket.getPath(PREFIX + randomSuffix());
-    // The file shouldn't exist initially. If it does, it's either because it's a leftover
+    // file shouldn't exist initially. If it does it's either because it's a leftover
     // from a previous run (so we should delete the file)
     // or because we're misconfigured and pointing to an actually important file
     // (so we should absolutely not delete it).
@@ -703,7 +703,7 @@ public class ITGcsNio {
   }
 
   /**
-   * Deletes the given directory and all of its contents if non-empty.
+   * Delete the given directory and all of its contents if non-empty.
    * @param directory the directory to delete
    * @throws IOException
    */
