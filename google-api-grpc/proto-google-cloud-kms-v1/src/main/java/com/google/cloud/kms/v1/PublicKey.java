cc5eeb6587a7e1f555d7018be812e49dc91e1642
yoshi-automation <yoshi-automation@google.com>
2019-02-05 00:40:07 -0800

    Regenerate kms client

--- google-api-grpc/proto-google-cloud-kms-v1/src/main/java/com/google/cloud/kms/v1/PublicKey.java
+++ google-api-grpc/proto-google-cloud-kms-v1/src/main/java/com/google/cloud/kms/v1/PublicKey.java
@@ -204,8 +204,7 @@ public final class PublicKey extends com.google.protobuf.GeneratedMessageV3
     }
     if (algorithm_
         != com.google.cloud.kms.v1.CryptoKeyVersion.CryptoKeyVersionAlgorithm
-            .CRYPTO_KEY_VERSION_ALGORITHM_UNSPECIFIED
-            .getNumber()) {
+            .CRYPTO_KEY_VERSION_ALGORITHM_UNSPECIFIED.getNumber()) {
       output.writeEnum(2, algorithm_);
     }
     unknownFields.writeTo(output);
@@ -222,8 +221,7 @@ public final class PublicKey extends com.google.protobuf.GeneratedMessageV3
     }
     if (algorithm_
         != com.google.cloud.kms.v1.CryptoKeyVersion.CryptoKeyVersionAlgorithm
-            .CRYPTO_KEY_VERSION_ALGORITHM_UNSPECIFIED
-            .getNumber()) {
+            .CRYPTO_KEY_VERSION_ALGORITHM_UNSPECIFIED.getNumber()) {
       size += com.google.protobuf.CodedOutputStream.computeEnumSize(2, algorithm_);
     }
     size += unknownFields.getSerializedSize();
