8951d3bd9b387040f312562e94b31f8f091633ac
Solomon Duskis <sduskis@google.com>
2019-02-07 18:48:52 -0500

    adding a warning to `fromProtoUnsafe`

--- google-cloud-clients/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/models/Mutation.java
+++ google-cloud-clients/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/models/Mutation.java
@@ -73,7 +73,12 @@ public final class Mutation implements MutationApi<Mutation>, Serializable {
     return new Mutation(true);
   }
 
-  /** Wraps the List of protobuf {@link com.google.bigtable.v2.Mutation}. */
+  /**
+   * Wraps the List of protobuf {@link com.google.bigtable.v2.Mutation}. This methods, like {@link
+   * #createUnsafe()}, allows setCell operation to use server side timestamp. This is dangerous
+   * because mutations will no longer be idempotent, which might cause multiple duplicate values to
+   * be stored in Bigtable. This option should only be used for advanced usecases with extreme care.
+   */
   @BetaApi
   public static Mutation fromProtoUnsafe(List<com.google.bigtable.v2.Mutation> protos) {
     Mutation mutation = new Mutation(true);
