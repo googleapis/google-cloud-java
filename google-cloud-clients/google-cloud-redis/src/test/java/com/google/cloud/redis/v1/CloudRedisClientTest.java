6843857119c21b303572d747c255c8bb44c6a7f9
yoshi-automation <yoshi-automation@google.com>
2019-03-15 00:48:56 -0700

    Regenerate redis client

--- google-cloud-clients/google-cloud-redis/src/test/java/com/google/cloud/redis/v1/CloudRedisClientTest.java
+++ google-cloud-clients/google-cloud-redis/src/test/java/com/google/cloud/redis/v1/CloudRedisClientTest.java
@@ -144,7 +144,6 @@ public class CloudRedisClientTest {
     String statusMessage = "statusMessage-239442758";
     int memorySizeGb = 34199707;
     String authorizedNetwork = "authorizedNetwork-1733809270";
-    String persistenceIamIdentity = "persistenceIamIdentity1061944584";
     Instance expectedResponse =
         Instance.newBuilder()
             .setName(name2)
@@ -159,7 +158,6 @@ public class CloudRedisClientTest {
             .setStatusMessage(statusMessage)
             .setMemorySizeGb(memorySizeGb)
             .setAuthorizedNetwork(authorizedNetwork)
-            .setPersistenceIamIdentity(persistenceIamIdentity)
             .build();
     mockCloudRedis.addResponse(expectedResponse);
 
@@ -210,7 +208,6 @@ public class CloudRedisClientTest {
     String statusMessage = "statusMessage-239442758";
     int memorySizeGb2 = 1493816946;
     String authorizedNetwork = "authorizedNetwork-1733809270";
-    String persistenceIamIdentity = "persistenceIamIdentity1061944584";
     Instance expectedResponse =
         Instance.newBuilder()
             .setName(name)
@@ -225,7 +222,6 @@ public class CloudRedisClientTest {
             .setStatusMessage(statusMessage)
             .setMemorySizeGb(memorySizeGb2)
             .setAuthorizedNetwork(authorizedNetwork)
-            .setPersistenceIamIdentity(persistenceIamIdentity)
             .build();
     Operation resultOperation =
         Operation.newBuilder()
@@ -294,7 +290,6 @@ public class CloudRedisClientTest {
     String statusMessage = "statusMessage-239442758";
     int memorySizeGb2 = 1493816946;
     String authorizedNetwork = "authorizedNetwork-1733809270";
-    String persistenceIamIdentity = "persistenceIamIdentity1061944584";
     Instance expectedResponse =
         Instance.newBuilder()
             .setName(name)
@@ -309,7 +304,6 @@ public class CloudRedisClientTest {
             .setStatusMessage(statusMessage)
             .setMemorySizeGb(memorySizeGb2)
             .setAuthorizedNetwork(authorizedNetwork)
-            .setPersistenceIamIdentity(persistenceIamIdentity)
             .build();
     Operation resultOperation =
         Operation.newBuilder()
@@ -416,160 +410,6 @@ public class CloudRedisClientTest {
 
   @Test
   @SuppressWarnings("all")
-  public void importInstanceTest() throws Exception {
-    String name2 = "name2-1052831874";
-    String displayName = "displayName1615086568";
-    String locationId = "locationId552319461";
-    String alternativeLocationId = "alternativeLocationId-718920621";
-    String redisVersion = "redisVersion-685310444";
-    String reservedIpRange = "reservedIpRange-1082940580";
-    String host = "host3208616";
-    int port = 3446913;
-    String currentLocationId = "currentLocationId1312712735";
-    String statusMessage = "statusMessage-239442758";
-    int memorySizeGb = 34199707;
-    String authorizedNetwork = "authorizedNetwork-1733809270";
-    String persistenceIamIdentity = "persistenceIamIdentity1061944584";
-    Instance expectedResponse =
-        Instance.newBuilder()
-            .setName(name2)
-            .setDisplayName(displayName)
-            .setLocationId(locationId)
-            .setAlternativeLocationId(alternativeLocationId)
-            .setRedisVersion(redisVersion)
-            .setReservedIpRange(reservedIpRange)
-            .setHost(host)
-            .setPort(port)
-            .setCurrentLocationId(currentLocationId)
-            .setStatusMessage(statusMessage)
-            .setMemorySizeGb(memorySizeGb)
-            .setAuthorizedNetwork(authorizedNetwork)
-            .setPersistenceIamIdentity(persistenceIamIdentity)
-            .build();
-    Operation resultOperation =
-        Operation.newBuilder()
-            .setName("importInstanceTest")
-            .setDone(true)
-            .setResponse(Any.pack(expectedResponse))
-            .build();
-    mockCloudRedis.addResponse(resultOperation);
-
-    String formattedName = InstanceName.format("[PROJECT]", "[LOCATION]", "[INSTANCE]");
-    InputConfig inputConfig = InputConfig.newBuilder().build();
-
-    Instance actualResponse = client.importInstanceAsync(formattedName, inputConfig).get();
-    Assert.assertEquals(expectedResponse, actualResponse);
-
-    List<GeneratedMessageV3> actualRequests = mockCloudRedis.getRequests();
-    Assert.assertEquals(1, actualRequests.size());
-    ImportInstanceRequest actualRequest = (ImportInstanceRequest) actualRequests.get(0);
-
-    Assert.assertEquals(formattedName, actualRequest.getName());
-    Assert.assertEquals(inputConfig, actualRequest.getInputConfig());
-    Assert.assertTrue(
-        channelProvider.isHeaderSent(
-            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
-            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
-  }
-
-  @Test
-  @SuppressWarnings("all")
-  public void importInstanceExceptionTest() throws Exception {
-    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
-    mockCloudRedis.addException(exception);
-
-    try {
-      String formattedName = InstanceName.format("[PROJECT]", "[LOCATION]", "[INSTANCE]");
-      InputConfig inputConfig = InputConfig.newBuilder().build();
-
-      client.importInstanceAsync(formattedName, inputConfig).get();
-      Assert.fail("No exception raised");
-    } catch (ExecutionException e) {
-      Assert.assertEquals(InvalidArgumentException.class, e.getCause().getClass());
-      InvalidArgumentException apiException = (InvalidArgumentException) e.getCause();
-      Assert.assertEquals(StatusCode.Code.INVALID_ARGUMENT, apiException.getStatusCode().getCode());
-    }
-  }
-
-  @Test
-  @SuppressWarnings("all")
-  public void exportInstanceTest() throws Exception {
-    String name2 = "name2-1052831874";
-    String displayName = "displayName1615086568";
-    String locationId = "locationId552319461";
-    String alternativeLocationId = "alternativeLocationId-718920621";
-    String redisVersion = "redisVersion-685310444";
-    String reservedIpRange = "reservedIpRange-1082940580";
-    String host = "host3208616";
-    int port = 3446913;
-    String currentLocationId = "currentLocationId1312712735";
-    String statusMessage = "statusMessage-239442758";
-    int memorySizeGb = 34199707;
-    String authorizedNetwork = "authorizedNetwork-1733809270";
-    String persistenceIamIdentity = "persistenceIamIdentity1061944584";
-    Instance expectedResponse =
-        Instance.newBuilder()
-            .setName(name2)
-            .setDisplayName(displayName)
-            .setLocationId(locationId)
-            .setAlternativeLocationId(alternativeLocationId)
-            .setRedisVersion(redisVersion)
-            .setReservedIpRange(reservedIpRange)
-            .setHost(host)
-            .setPort(port)
-            .setCurrentLocationId(currentLocationId)
-            .setStatusMessage(statusMessage)
-            .setMemorySizeGb(memorySizeGb)
-            .setAuthorizedNetwork(authorizedNetwork)
-            .setPersistenceIamIdentity(persistenceIamIdentity)
-            .build();
-    Operation resultOperation =
-        Operation.newBuilder()
-            .setName("exportInstanceTest")
-            .setDone(true)
-            .setResponse(Any.pack(expectedResponse))
-            .build();
-    mockCloudRedis.addResponse(resultOperation);
-
-    String formattedName = InstanceName.format("[PROJECT]", "[LOCATION]", "[INSTANCE]");
-    OutputConfig outputConfig = OutputConfig.newBuilder().build();
-
-    Instance actualResponse = client.exportInstanceAsync(formattedName, outputConfig).get();
-    Assert.assertEquals(expectedResponse, actualResponse);
-
-    List<GeneratedMessageV3> actualRequests = mockCloudRedis.getRequests();
-    Assert.assertEquals(1, actualRequests.size());
-    ExportInstanceRequest actualRequest = (ExportInstanceRequest) actualRequests.get(0);
-
-    Assert.assertEquals(formattedName, actualRequest.getName());
-    Assert.assertEquals(outputConfig, actualRequest.getOutputConfig());
-    Assert.assertTrue(
-        channelProvider.isHeaderSent(
-            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
-            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
-  }
-
-  @Test
-  @SuppressWarnings("all")
-  public void exportInstanceExceptionTest() throws Exception {
-    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
-    mockCloudRedis.addException(exception);
-
-    try {
-      String formattedName = InstanceName.format("[PROJECT]", "[LOCATION]", "[INSTANCE]");
-      OutputConfig outputConfig = OutputConfig.newBuilder().build();
-
-      client.exportInstanceAsync(formattedName, outputConfig).get();
-      Assert.fail("No exception raised");
-    } catch (ExecutionException e) {
-      Assert.assertEquals(InvalidArgumentException.class, e.getCause().getClass());
-      InvalidArgumentException apiException = (InvalidArgumentException) e.getCause();
-      Assert.assertEquals(StatusCode.Code.INVALID_ARGUMENT, apiException.getStatusCode().getCode());
-    }
-  }
-
-  @Test
-  @SuppressWarnings("all")
   public void failoverInstanceTest() throws Exception {
     String name2 = "name2-1052831874";
     String displayName = "displayName1615086568";
@@ -583,7 +423,6 @@ public class CloudRedisClientTest {
     String statusMessage = "statusMessage-239442758";
     int memorySizeGb = 34199707;
     String authorizedNetwork = "authorizedNetwork-1733809270";
-    String persistenceIamIdentity = "persistenceIamIdentity1061944584";
     Instance expectedResponse =
         Instance.newBuilder()
             .setName(name2)
@@ -598,7 +437,6 @@ public class CloudRedisClientTest {
             .setStatusMessage(statusMessage)
             .setMemorySizeGb(memorySizeGb)
             .setAuthorizedNetwork(authorizedNetwork)
-            .setPersistenceIamIdentity(persistenceIamIdentity)
             .build();
     Operation resultOperation =
         Operation.newBuilder()
