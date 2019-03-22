670380953bc37e2b637b54fe503bbb624ef5d44a
Micah Kornfield <micahk@google.com>
2019-03-22 14:01:03 -0700

    Updates per rubrics

--- google-cloud-examples/src/main/java/com/google/cloud/examples/securitycenter/snippets/AssetSnippets.java
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/securitycenter/snippets/AssetSnippets.java
@@ -5,149 +5,164 @@ import com.google.cloud.securitycenter.v1beta1.ListAssetsResponse.ListAssetsResu
 import com.google.cloud.securitycenter.v1beta1.OrganizationName;
 import com.google.cloud.securitycenter.v1beta1.SecurityCenterClient;
 import com.google.cloud.securitycenter.v1beta1.SecurityCenterClient.ListAssetsPagedResponse;
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import org.threeten.bp.Duration;
 import org.threeten.bp.Instant;
-import org.threeten.bp.LocalDateTime;
-import org.threeten.bp.ZoneId;
-import org.threeten.bp.temporal.ChronoUnit;
 
 /** Snippets for how to work with Assets in Cloud Security Command Center. */
 public class AssetSnippets {
+  private AssetSnippets() {}
 
-  private final SecurityCenterClient securityCenterClient;
-  private final OrganizationName organizationName;
-
-  /** Filter that returns all projects in the organization */
-  // [START asset_resource_project_filter]
-  public static final String PROJECT_ASSET_FILTERS =
-      "security_center_properties.resource_type=\"google.cloud.resourcemanager.Project\"";
   // [END asset_resource_project_filter]
 
   /**
-   * Create a new AssetSnippets object.
+   * Lists all assets for an organization.
    *
-   * @param client The client to use for contacting the service.
-   * @param organizationId The organization ID (this should be a numeric value, not the display name
-   *     of the organization).
+   * @param organizationName The organization to list assets for.
    */
-  public AssetSnippets(SecurityCenterClient client, String organizationId) {
-    this.securityCenterClient = client;
-    // [START name_from_id]
-    this.organizationName = OrganizationName.of(organizationId);
-    // [END name_from_id]
+  // [START list_all_assets]
+  static ImmutableList<ListAssetsResult> listAssets(OrganizationName organizationName) {
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
+      // Start setting up a request for to search for all assets in an organization.
+      // OrganizationName organizationName = OrganizationName.of("123234324");
+      ListAssetsRequest.Builder request =
+          ListAssetsRequest.newBuilder().setParent(organizationName.toString());
+
+      // Call the API.
+      ListAssetsPagedResponse response = client.listAssets(request.build());
+
+      // This creates one list for all assets.  If your organization has a large number of assets
+      // this can cause out of memory issues.  You can process them batches by returning
+      // the Iterable returned response.iterateAll() directly.
+      ImmutableList<ListAssetsResult> results = ImmutableList.copyOf(response.iterateAll());
+      System.out.println("All assets:");
+      System.out.println(results);
+      return results;
+    } catch (IOException e) {
+      throw new RuntimeException("Couldn't create client.", e);
+    }
   }
+  // [END list_all_assets]
 
   /**
-   * Lists assets for an organization given meeting <code>filter</code> as of a specific instant in
-   * time.
+   * Lists all project assets for an organization.
    *
-   * @param filter The filter that assets must meet (e.g. {@link #PROJECT_ASSET_FILTERS}). If null,
-   *     all assets in the organization are returned.
-   * @param asOf The instant in time to query for. If null, current time is assumed
+   * @param organizationName The organization to list assets for.
    */
+  // [START list_assets_with_filter]
+  static ImmutableList<ListAssetsResult> listAssetsWithFilter(OrganizationName organizationName) {
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
+      // Start setting up a request for to search for all assets in an organization.
+      // OrganizationName organizationName = OrganizationName.of("123234324");
+      ListAssetsRequest request =
+          ListAssetsRequest.newBuilder()
+              .setParent(organizationName.toString())
+              .setFilter(
+                  "security_center_properties.resource_type=\"google.cloud.resourcemanager.Project\"")
+              .build();
 
-  // [START list_assets]
-  public ImmutableList<ListAssetsResult> listAssets(String filter, Instant asOf) {
-    // Start setting up a request for to search for all assets in an organization.
-    // OrganizationName organizationName = OrganizationName.of("123234324");
-    ListAssetsRequest.Builder request =
-        ListAssetsRequest.newBuilder().setParent(organizationName.toString());
-    // Default for API is to return all assets.
-    if (filter != null) {
-      request.setFilter(filter);
+      // Call the API.
+      ListAssetsPagedResponse response = client.listAssets(request);
+
+      // This creates one list for all assets.  If your organization has a large number of assets
+      // this can cause out of memory issues.  You can process them batches by returning
+      // the Iterable returned response.iterateAll() directly.
+      ImmutableList<ListAssetsResult> results = ImmutableList.copyOf(response.iterateAll());
+      System.out.println("Projects:");
+      System.out.println(results);
+      return results;
+    } catch (IOException e) {
+      throw new RuntimeException("Couldn't create client.", e);
     }
-    // Limits assets returned to a particular point in time.
-    if (asOf != null) {
+  }
+  // [END list_assets_with_filter]
+
+  /**
+   * Lists all project assets for an organization at a given point in time.
+   *
+   * @param organizationName The organization to list assets for.
+   * @param asOf The snapshot time to query for assets. If null defaults to one day ago.
+   */
+  // [START list_assets_as_of_time]
+  static ImmutableList<ListAssetsResult> listAssetsAsOfYesterday(
+      OrganizationName organizationName, Instant asOf) {
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
+      // Start setting up a request for to search for all assets in an organization.
+      // OrganizationName organizationName = OrganizationName.of("123234324");
+
+      // Initialize the builder with the organization and filter
+      ListAssetsRequest.Builder request =
+          ListAssetsRequest.newBuilder()
+              .setParent(organizationName.toString())
+              .setFilter(
+                  "security_center_properties.resource_type=\"google.cloud.resourcemanager.Project\"");
+
+      // Set read time to either the instant passed in or one day ago.
+      asOf = MoreObjects.firstNonNull(asOf, Instant.now().minus(Duration.ofDays(1)));
       request.getReadTimeBuilder().setSeconds(asOf.getEpochSecond()).setNanos(asOf.getNano());
+
+      // Call the API.
+      ListAssetsPagedResponse response = client.listAssets(request.build());
+
+      // This creates one list for all assets.  If your organization has a large number of assets
+      // this can cause out of memory issues.  You can process them batches by returning
+      // the Iterable returned response.iterateAll() directly.
+      ImmutableList<ListAssetsResult> results = ImmutableList.copyOf(response.iterateAll());
+      System.out.println("Projects:");
+      System.out.println(results);
+      return results;
+    } catch (IOException e) {
+      throw new RuntimeException("Couldn't create client.", e);
     }
-    ListAssetsPagedResponse response = securityCenterClient.listAssets(request.build());
-
-    // This creates one list for all assets.  If your organization has a large number of assets
-    // this can cause out of memory issues.  You can process them batches by returning
-    // the Iterable returned response.iterateAll() directly.
-    return ImmutableList.copyOf(response.iterateAll());
   }
-  // [END list_assets]
-
-  /** Run and print results from common queries. */
-  void demoListAssets() {
-
-    // Query for all currently existing assets
-    // [START demo_list_all_assets]
-    System.out.println("All Assets: " + listAssets(null, null));
-    // [END demo_list_all_assets]
-    // Query for all projects as of now.
-    // [START demo_list_assets_with_filter]
-    System.out.println("Project Assets (now): " + listAssets(PROJECT_ASSET_FILTERS, null));
-    // [END demo_list_assets_with_filter]
-    // Query for all projects as of a day ago.
-    // [START demo_list_assets_with_filter_and_time]
-    System.out.println(
-        "Project Assets (1 day ago): "
-            + listAssets(PROJECT_ASSET_FILTERS, Instant.now().minus(Duration.ofDays(1))));
-    // [END demo_list_assets_with_filter_and_time]
-  }
+  // [END list_assets_as_of_time]
 
   /**
    * Returns Assets and metadata about assets activity (e.g. added, removed, no change) between
    * between <code>asOf.minus(timespan)</code> and <code>asOf</code>.
    *
    * @param timeSpan The time-range to compare assets over.
-   * @param filter The filter that assets must meet (e.g. {@link #PROJECT_ASSET_FILTERS}). If null,
-   *     all assets in the organization are returned.
    * @param asOf The instant in time to query for. If null, current time is assumed.
    */
-  // [START list_asset_changes]
-  public ImmutableList<ListAssetsResult> listAssetAndStatusChanges(
-      Duration timeSpan, String filter, Instant asOf) {
+  // [START list_asset_changes_status_changes]
+  static ImmutableList<ListAssetsResult> listAssetAndStatusChanges(
+      OrganizationName organizationName, Duration timeSpan, Instant asOf) {
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
 
-    // Start setting up a request for to search for all assets in an organization.
-    // OrganizationName organizationName = OrganizationName.of("123234324");
-    ListAssetsRequest.Builder request =
-        ListAssetsRequest.newBuilder().setParent(organizationName.toString());
-    request
-        .getCompareDurationBuilder()
-        .setSeconds(timeSpan.getSeconds())
-        .setNanos(timeSpan.getNano());
-    // Default for API is to return all assets.
-    if (filter != null) {
-      request.setFilter(filter);
-    }
-    // Limits assets returned to a particular point in time.
-    if (asOf != null) {
+      // Start setting up a request for to search for all assets in an organization.
+      // OrganizationName organizationName = OrganizationName.of("123234324");
+      ListAssetsRequest.Builder request =
+          ListAssetsRequest.newBuilder()
+              .setParent(organizationName.toString())
+              .setFilter(
+                  "security_center_properties.resource_type=\"google.cloud.resourcemanager.Project\"");
+      request
+          .getCompareDurationBuilder()
+          .setSeconds(timeSpan.getSeconds())
+          .setNanos(timeSpan.getNano());
+
+      // Set read time to either the instant passed in or now.
+      asOf = MoreObjects.firstNonNull(asOf, Instant.now());
       request.getReadTimeBuilder().setSeconds(asOf.getEpochSecond()).setNanos(asOf.getNano());
+
+      // Call the API.
+      ListAssetsPagedResponse response = client.listAssets(request.build());
+
+      // This creates one list for all assets.  If your organization has a large number of assets
+      // this can cause out of memory issues.  You can process them batches by returning
+      // the Iterable returned response.iterateAll() directly.
+      ImmutableList<ListAssetsResult> results = ImmutableList.copyOf(response.iterateAll());
+      System.out.println("Projects:");
+      System.out.println(results);
+      return results;
+    } catch (IOException e) {
+      throw new RuntimeException("Couldn't create client.", e);
     }
-
-    ListAssetsPagedResponse response = securityCenterClient.listAssets(request.build());
-
-    // This creates one list for all assets.  If your organization has a large number of assets
-    // this can cause out of memory issues.  You can process them batches by returning
-    // the Iterable returned response.iterateAll() directly.
-    return ImmutableList.copyOf(response.iterateAll());
   }
-  // [END list_asset_changes]
-
-  /**
-   * Run and print demo outputs of different parameters for {@link
-   * #listAssetAndStatusChanges(Duration, String, Instant)}.
-   */
-  void demoListAssetAndStatusChanges() {
-    // [START demo_list_asset_changes]
-    final LocalDateTime march = LocalDateTime.of(2019, 3, 18, 0, 0);
-    final LocalDateTime feb = LocalDateTime.of(2019, 2, 18, 0, 0);
-    final Duration lastMonth = Duration.ofDays(ChronoUnit.DAYS.between(feb, march));
-    // Query projects and their state changes over between February 18, 2019 and March 18, 2019.
-    System.out.println(
-        "Project Changes over a month: "
-            + listAssetAndStatusChanges(
-                lastMonth,
-                PROJECT_ASSET_FILTERS,
-                jan1.atZone(ZoneId.of("Europe/Paris")).toInstant()));
-    // [END demo_list_asset_changes]
-  }
+  // [END list_asset_changes_status_changes]
 
   public static void main(String... args) throws IOException {
     try (SecurityCenterClient client = SecurityCenterClient.create()) {
@@ -161,12 +176,8 @@ public class AssetSnippets {
             "Organization ID must either be set in the environment variable \"ORGANIZATION_ID\" or passed"
                 + " as the first parameter to the program.");
       }
-      AssetSnippets snippets = new AssetSnippets(client, org_id);
-      System.out.println("Project Assets:" + snippets.listAssets(PROJECT_ASSET_FILTERS, null));
-      System.out.println(
-          "Project Assets (changes as of a day ago): "
-              + snippets.listAssetAndStatusChanges(
-                  Duration.ofDays(1), PROJECT_ASSET_FILTERS, null));
+
+      listAssetsWithFilter(OrganizationName.of(org_id));
     }
   }
 }
--- google-cloud-examples/src/test/java/com/google/cloud/examples/securitycenter/snippets/ITAssetSnippets.java
+++ google-cloud-examples/src/test/java/com/google/cloud/examples/securitycenter/snippets/ITAssetSnippets.java
@@ -1,13 +1,13 @@
 package com.google.cloud.examples.securitycenter.snippets;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import com.google.cloud.securitycenter.v1beta1.ListAssetsResponse.ListAssetsResult;
-import com.google.cloud.securitycenter.v1beta1.SecurityCenterClient;
+import com.google.cloud.securitycenter.v1beta1.ListAssetsResponse.ListAssetsResult.State;
+import com.google.cloud.securitycenter.v1beta1.OrganizationName;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.threeten.bp.Duration;
 import org.threeten.bp.Instant;
@@ -17,67 +17,44 @@ import org.threeten.bp.ZoneOffset;
 /** Smoke tests for {@link com.google.cloud.examples.securitycenter.snippets.AssetSnippets} */
 public class ITAssetSnippets {
 
-  static SecurityCenterClient client;
-  static AssetSnippets snippets;
-
-  public static final Instant NOTHING_INSTANCE =
+  private static final Instant NOTHING_INSTANCE =
       LocalDateTime.of(2019, 1, 1, 0, 0).toInstant(ZoneOffset.UTC);
-  public static final Instant SOMETHING_INSTANCE =
+  private static final Instant SOMETHING_INSTANCE =
       LocalDateTime.of(2019, 3, 14, 8, 0).toInstant(ZoneOffset.ofHours((-8)));
-
-  @BeforeClass
-  public static void beforeClass() throws IOException {
-    client = SecurityCenterClient.create();
-    snippets = new AssetSnippets(client, getOrganizationId());
-  }
 
   @Test
   public void mainRuns() throws IOException {
-    AssetSnippets.main(getOrganizationId());
-  }
-
-  @Test
-  public void demosRun() throws IOException {
-    snippets.demoListAssets();
-    snippets.demoListAssetAndStatusChanges();
-  }
-
-  @Test
-  public void testAllAssetsReturned() {
-    assertTrue(0 < snippets.listAssets(null, null).size());
+    AssetSnippets.main(getOrganizationId().getOrganization());
   }
 
   @Test
   public void testBeforeDateNoAssetsReturned() {
-    assertTrue(snippets.listAssets(null, NOTHING_INSTANCE).isEmpty());
+    assertTrue(
+        AssetSnippets.listAssetsAsOfYesterday(getOrganizationId(), NOTHING_INSTANCE).isEmpty());
   }
 
   @Test
   public void testListAssetsNoFilterOrDate() {
-    assertTrue(59 >= snippets.listAssets(null, null).size());
+    assertTrue(59 >= AssetSnippets.listAssets(getOrganizationId()).size());
   }
 
   @Test
   public void testListAssetsWithFilterAndInstance() {
     assertTrue(
-        3 >= snippets.listAssets(AssetSnippets.PROJECT_ASSET_FILTERS, SOMETHING_INSTANCE).size());
+        3 >= AssetSnippets.listAssetsAsOfYesterday(getOrganizationId(), SOMETHING_INSTANCE).size());
   }
 
   @Test
   public void testChangesReturnsValues() {
     ImmutableList<ListAssetsResult> result =
-        snippets.listAssetAndStatusChanges(
-            Duration.ofDays(3), AssetSnippets.PROJECT_ASSET_FILTERS, SOMETHING_INSTANCE);
+        AssetSnippets.listAssetAndStatusChanges(
+            getOrganizationId(), Duration.ofDays(3), SOMETHING_INSTANCE);
     assertTrue("Result: " + result.toString(), result.toString().contains("ADDED"));
     assertTrue(3 >= result.size());
+    assertEquals(result.get(0).getState(), State.ADDED);
   }
 
-  @AfterClass
-  public static void tearDown() {
-    client.close();
-  }
-
-  private static String getOrganizationId() {
-    return System.getenv("GCLOUD_ORGANIZATION");
+  private static OrganizationName getOrganizationId() {
+    return OrganizationName.of(System.getenv("GCLOUD_ORGANIZATION"));
   }
 }
