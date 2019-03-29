6573f5801804bc6794021c39feead092bc9ca9a1
yoshi-automation <yoshi-automation@google.com>
2019-03-29 00:51:49 -0700

    Regenerate pubsub client

--- google-api-grpc/proto-google-cloud-pubsub-v1/src/main/java/com/google/pubsub/v1/DeleteTopicRequest.java
+++ google-api-grpc/proto-google-cloud-pubsub-v1/src/main/java/com/google/pubsub/v1/DeleteTopicRequest.java
@@ -59,7 +59,7 @@ public final class DeleteTopicRequest extends com.google.protobuf.GeneratedMessa
             }
           default:
             {
-              if (!parseUnknownFieldProto3(input, unknownFields, extensionRegistry, tag)) {
+              if (!parseUnknownField(input, unknownFields, extensionRegistry, tag)) {
                 done = true;
               }
               break;
@@ -180,10 +180,9 @@ public final class DeleteTopicRequest extends com.google.protobuf.GeneratedMessa
     }
     com.google.pubsub.v1.DeleteTopicRequest other = (com.google.pubsub.v1.DeleteTopicRequest) obj;
 
-    boolean result = true;
-    result = result && getTopic().equals(other.getTopic());
-    result = result && unknownFields.equals(other.unknownFields);
-    return result;
+    if (!getTopic().equals(other.getTopic())) return false;
+    if (!unknownFields.equals(other.unknownFields)) return false;
+    return true;
   }
 
   @java.lang.Override
@@ -376,35 +375,35 @@ public final class DeleteTopicRequest extends com.google.protobuf.GeneratedMessa
 
     @java.lang.Override
     public Builder clone() {
-      return (Builder) super.clone();
+      return super.clone();
     }
 
     @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.setField(field, value);
+      return super.setField(field, value);
     }
 
     @java.lang.Override
     public Builder clearField(com.google.protobuf.Descriptors.FieldDescriptor field) {
-      return (Builder) super.clearField(field);
+      return super.clearField(field);
     }
 
     @java.lang.Override
     public Builder clearOneof(com.google.protobuf.Descriptors.OneofDescriptor oneof) {
-      return (Builder) super.clearOneof(oneof);
+      return super.clearOneof(oneof);
     }
 
     @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, int index, java.lang.Object value) {
-      return (Builder) super.setRepeatedField(field, index, value);
+      return super.setRepeatedField(field, index, value);
     }
 
     @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.addRepeatedField(field, value);
+      return super.addRepeatedField(field, value);
     }
 
     @java.lang.Override
@@ -553,7 +552,7 @@ public final class DeleteTopicRequest extends com.google.protobuf.GeneratedMessa
 
     @java.lang.Override
     public final Builder setUnknownFields(final com.google.protobuf.UnknownFieldSet unknownFields) {
-      return super.setUnknownFieldsProto3(unknownFields);
+      return super.setUnknownFields(unknownFields);
     }
 
     @java.lang.Override
--- google-api-grpc/proto-google-cloud-pubsub-v1/src/main/java/com/google/pubsub/v1/UpdateSubscriptionRequest.java
+++ google-api-grpc/proto-google-cloud-pubsub-v1/src/main/java/com/google/pubsub/v1/UpdateSubscriptionRequest.java
@@ -80,7 +80,7 @@ public final class UpdateSubscriptionRequest extends com.google.protobuf.Generat
             }
           default:
             {
-              if (!parseUnknownFieldProto3(input, unknownFields, extensionRegistry, tag)) {
+              if (!parseUnknownField(input, unknownFields, extensionRegistry, tag)) {
                 done = true;
               }
               break;
@@ -246,17 +246,16 @@ public final class UpdateSubscriptionRequest extends com.google.protobuf.Generat
     com.google.pubsub.v1.UpdateSubscriptionRequest other =
         (com.google.pubsub.v1.UpdateSubscriptionRequest) obj;
 
-    boolean result = true;
-    result = result && (hasSubscription() == other.hasSubscription());
+    if (hasSubscription() != other.hasSubscription()) return false;
     if (hasSubscription()) {
-      result = result && getSubscription().equals(other.getSubscription());
+      if (!getSubscription().equals(other.getSubscription())) return false;
     }
-    result = result && (hasUpdateMask() == other.hasUpdateMask());
+    if (hasUpdateMask() != other.hasUpdateMask()) return false;
     if (hasUpdateMask()) {
-      result = result && getUpdateMask().equals(other.getUpdateMask());
+      if (!getUpdateMask().equals(other.getUpdateMask())) return false;
     }
-    result = result && unknownFields.equals(other.unknownFields);
-    return result;
+    if (!unknownFields.equals(other.unknownFields)) return false;
+    return true;
   }
 
   @java.lang.Override
@@ -474,35 +473,35 @@ public final class UpdateSubscriptionRequest extends com.google.protobuf.Generat
 
     @java.lang.Override
     public Builder clone() {
-      return (Builder) super.clone();
+      return super.clone();
     }
 
     @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.setField(field, value);
+      return super.setField(field, value);
     }
 
     @java.lang.Override
     public Builder clearField(com.google.protobuf.Descriptors.FieldDescriptor field) {
-      return (Builder) super.clearField(field);
+      return super.clearField(field);
     }
 
     @java.lang.Override
     public Builder clearOneof(com.google.protobuf.Descriptors.OneofDescriptor oneof) {
-      return (Builder) super.clearOneof(oneof);
+      return super.clearOneof(oneof);
     }
 
     @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, int index, java.lang.Object value) {
-      return (Builder) super.setRepeatedField(field, index, value);
+      return super.setRepeatedField(field, index, value);
     }
 
     @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.addRepeatedField(field, value);
+      return super.addRepeatedField(field, value);
     }
 
     @java.lang.Override
@@ -552,7 +551,7 @@ public final class UpdateSubscriptionRequest extends com.google.protobuf.Generat
       return this;
     }
 
-    private com.google.pubsub.v1.Subscription subscription_ = null;
+    private com.google.pubsub.v1.Subscription subscription_;
     private com.google.protobuf.SingleFieldBuilderV3<
             com.google.pubsub.v1.Subscription,
             com.google.pubsub.v1.Subscription.Builder,
@@ -733,7 +732,7 @@ public final class UpdateSubscriptionRequest extends com.google.protobuf.Generat
       return subscriptionBuilder_;
     }
 
-    private com.google.protobuf.FieldMask updateMask_ = null;
+    private com.google.protobuf.FieldMask updateMask_;
     private com.google.protobuf.SingleFieldBuilderV3<
             com.google.protobuf.FieldMask,
             com.google.protobuf.FieldMask.Builder,
@@ -923,7 +922,7 @@ public final class UpdateSubscriptionRequest extends com.google.protobuf.Generat
 
     @java.lang.Override
     public final Builder setUnknownFields(final com.google.protobuf.UnknownFieldSet unknownFields) {
-      return super.setUnknownFieldsProto3(unknownFields);
+      return super.setUnknownFields(unknownFields);
     }
 
     @java.lang.Override
