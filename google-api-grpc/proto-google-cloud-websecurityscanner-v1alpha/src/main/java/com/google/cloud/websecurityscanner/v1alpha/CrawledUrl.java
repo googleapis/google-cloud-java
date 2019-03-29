c79a90266dfd3605e5797863a77105252ac7e70c
yoshi-automation <yoshi-automation@google.com>
2019-03-29 01:06:07 -0700

    Regenerate websecurityscanner client

--- google-api-grpc/proto-google-cloud-websecurityscanner-v1alpha/src/main/java/com/google/cloud/websecurityscanner/v1alpha/CrawledUrl.java
+++ google-api-grpc/proto-google-cloud-websecurityscanner-v1alpha/src/main/java/com/google/cloud/websecurityscanner/v1alpha/CrawledUrl.java
@@ -77,7 +77,7 @@ public final class CrawledUrl extends com.google.protobuf.GeneratedMessageV3
             }
           default:
             {
-              if (!parseUnknownFieldProto3(input, unknownFields, extensionRegistry, tag)) {
+              if (!parseUnknownField(input, unknownFields, extensionRegistry, tag)) {
                 done = true;
               }
               break;
@@ -303,12 +303,11 @@ public final class CrawledUrl extends com.google.protobuf.GeneratedMessageV3
     com.google.cloud.websecurityscanner.v1alpha.CrawledUrl other =
         (com.google.cloud.websecurityscanner.v1alpha.CrawledUrl) obj;
 
-    boolean result = true;
-    result = result && getHttpMethod().equals(other.getHttpMethod());
-    result = result && getUrl().equals(other.getUrl());
-    result = result && getBody().equals(other.getBody());
-    result = result && unknownFields.equals(other.unknownFields);
-    return result;
+    if (!getHttpMethod().equals(other.getHttpMethod())) return false;
+    if (!getUrl().equals(other.getUrl())) return false;
+    if (!getBody().equals(other.getBody())) return false;
+    if (!unknownFields.equals(other.unknownFields)) return false;
+    return true;
   }
 
   @java.lang.Override
@@ -514,35 +513,35 @@ public final class CrawledUrl extends com.google.protobuf.GeneratedMessageV3
 
     @java.lang.Override
     public Builder clone() {
-      return (Builder) super.clone();
+      return super.clone();
     }
 
     @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.setField(field, value);
+      return super.setField(field, value);
     }
 
     @java.lang.Override
     public Builder clearField(com.google.protobuf.Descriptors.FieldDescriptor field) {
-      return (Builder) super.clearField(field);
+      return super.clearField(field);
     }
 
     @java.lang.Override
     public Builder clearOneof(com.google.protobuf.Descriptors.OneofDescriptor oneof) {
-      return (Builder) super.clearOneof(oneof);
+      return super.clearOneof(oneof);
     }
 
     @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, int index, java.lang.Object value) {
-      return (Builder) super.setRepeatedField(field, index, value);
+      return super.setRepeatedField(field, index, value);
     }
 
     @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.addRepeatedField(field, value);
+      return super.addRepeatedField(field, value);
     }
 
     @java.lang.Override
@@ -904,7 +903,7 @@ public final class CrawledUrl extends com.google.protobuf.GeneratedMessageV3
 
     @java.lang.Override
     public final Builder setUnknownFields(final com.google.protobuf.UnknownFieldSet unknownFields) {
-      return super.setUnknownFieldsProto3(unknownFields);
+      return super.setUnknownFields(unknownFields);
     }
 
     @java.lang.Override
