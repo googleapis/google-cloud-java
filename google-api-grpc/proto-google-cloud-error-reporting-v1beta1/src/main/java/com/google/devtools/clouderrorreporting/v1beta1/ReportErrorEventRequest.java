400f10a8b5164f0953d9987763672c5248678f6f
yoshi-automation <yoshi-automation@google.com>
2019-03-29 00:44:24 -0700

    Regenerate errorreporting client

--- google-api-grpc/proto-google-cloud-error-reporting-v1beta1/src/main/java/com/google/devtools/clouderrorreporting/v1beta1/ReportErrorEventRequest.java
+++ google-api-grpc/proto-google-cloud-error-reporting-v1beta1/src/main/java/com/google/devtools/clouderrorreporting/v1beta1/ReportErrorEventRequest.java
@@ -77,7 +77,7 @@ public final class ReportErrorEventRequest extends com.google.protobuf.Generated
             }
           default:
             {
-              if (!parseUnknownFieldProto3(input, unknownFields, extensionRegistry, tag)) {
+              if (!parseUnknownField(input, unknownFields, extensionRegistry, tag)) {
                 done = true;
               }
               break;
@@ -253,14 +253,13 @@ public final class ReportErrorEventRequest extends com.google.protobuf.Generated
     com.google.devtools.clouderrorreporting.v1beta1.ReportErrorEventRequest other =
         (com.google.devtools.clouderrorreporting.v1beta1.ReportErrorEventRequest) obj;
 
-    boolean result = true;
-    result = result && getProjectName().equals(other.getProjectName());
-    result = result && (hasEvent() == other.hasEvent());
+    if (!getProjectName().equals(other.getProjectName())) return false;
+    if (hasEvent() != other.hasEvent()) return false;
     if (hasEvent()) {
-      result = result && getEvent().equals(other.getEvent());
+      if (!getEvent().equals(other.getEvent())) return false;
     }
-    result = result && unknownFields.equals(other.unknownFields);
-    return result;
+    if (!unknownFields.equals(other.unknownFields)) return false;
+    return true;
   }
 
   @java.lang.Override
@@ -475,35 +474,35 @@ public final class ReportErrorEventRequest extends com.google.protobuf.Generated
 
     @java.lang.Override
     public Builder clone() {
-      return (Builder) super.clone();
+      return super.clone();
     }
 
     @java.lang.Override
     public Builder setField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.setField(field, value);
+      return super.setField(field, value);
     }
 
     @java.lang.Override
     public Builder clearField(com.google.protobuf.Descriptors.FieldDescriptor field) {
-      return (Builder) super.clearField(field);
+      return super.clearField(field);
     }
 
     @java.lang.Override
     public Builder clearOneof(com.google.protobuf.Descriptors.OneofDescriptor oneof) {
-      return (Builder) super.clearOneof(oneof);
+      return super.clearOneof(oneof);
     }
 
     @java.lang.Override
     public Builder setRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, int index, java.lang.Object value) {
-      return (Builder) super.setRepeatedField(field, index, value);
+      return super.setRepeatedField(field, index, value);
     }
 
     @java.lang.Override
     public Builder addRepeatedField(
         com.google.protobuf.Descriptors.FieldDescriptor field, java.lang.Object value) {
-      return (Builder) super.addRepeatedField(field, value);
+      return super.addRepeatedField(field, value);
     }
 
     @java.lang.Override
@@ -675,7 +674,7 @@ public final class ReportErrorEventRequest extends com.google.protobuf.Generated
       return this;
     }
 
-    private com.google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent event_ = null;
+    private com.google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent event_;
     private com.google.protobuf.SingleFieldBuilderV3<
             com.google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent,
             com.google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent.Builder,
@@ -866,7 +865,7 @@ public final class ReportErrorEventRequest extends com.google.protobuf.Generated
 
     @java.lang.Override
     public final Builder setUnknownFields(final com.google.protobuf.UnknownFieldSet unknownFields) {
-      return super.setUnknownFieldsProto3(unknownFields);
+      return super.setUnknownFields(unknownFields);
     }
 
     @java.lang.Override
