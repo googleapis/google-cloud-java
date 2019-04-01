f4df1f1700cb8d40a9eab5a7fd6ca7bad5350cae
kolea2 <45548808+kolea2@users.noreply.github.com>
2019-04-01 19:13:20 -0400

    Upgrade gax to 1.43.0. (#4836)
    * Upgrade gax to 1.43.0.
    
    * code format

--- java-talent/google-cloud-talent/src/test/java/com/google/cloud/talent/v4beta1/MockCompletionImpl.java
+++ java-talent/google-cloud-talent/src/test/java/com/google/cloud/talent/v4beta1/MockCompletionImpl.java
@@ -17,7 +17,7 @@ package com.google.cloud.talent.v4beta1;
 
 import com.google.api.core.BetaApi;
 import com.google.cloud.talent.v4beta1.CompletionGrpc.CompletionImplBase;
-import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.AbstractMessage;
 import io.grpc.stub.StreamObserver;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -27,7 +27,7 @@ import java.util.Queue;
 @javax.annotation.Generated("by GAPIC")
 @BetaApi
 public class MockCompletionImpl extends CompletionImplBase {
-  private ArrayList<GeneratedMessageV3> requests;
+  private ArrayList<AbstractMessage> requests;
   private Queue<Object> responses;
 
   public MockCompletionImpl() {
@@ -35,15 +35,15 @@ public class MockCompletionImpl extends CompletionImplBase {
     responses = new LinkedList<>();
   }
 
-  public List<GeneratedMessageV3> getRequests() {
+  public List<AbstractMessage> getRequests() {
     return requests;
   }
 
-  public void addResponse(GeneratedMessageV3 response) {
+  public void addResponse(AbstractMessage response) {
     responses.add(response);
   }
 
-  public void setResponses(List<GeneratedMessageV3> responses) {
+  public void setResponses(List<AbstractMessage> responses) {
     this.responses = new LinkedList<Object>(responses);
   }
 
