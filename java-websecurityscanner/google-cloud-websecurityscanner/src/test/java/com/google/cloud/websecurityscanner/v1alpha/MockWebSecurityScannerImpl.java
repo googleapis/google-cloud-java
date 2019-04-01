e40f179c353eb9d8d1562a6a87872ea0fabc6b84
kolea2 <45548808+kolea2@users.noreply.github.com>
2019-04-01 19:13:20 -0400

    Upgrade gax to 1.43.0. (#4836)
    * Upgrade gax to 1.43.0.
    
    * code format

--- java-websecurityscanner/google-cloud-websecurityscanner/src/test/java/com/google/cloud/websecurityscanner/v1alpha/MockWebSecurityScannerImpl.java
+++ java-websecurityscanner/google-cloud-websecurityscanner/src/test/java/com/google/cloud/websecurityscanner/v1alpha/MockWebSecurityScannerImpl.java
@@ -17,8 +17,8 @@ package com.google.cloud.websecurityscanner.v1alpha;
 
 import com.google.api.core.BetaApi;
 import com.google.cloud.websecurityscanner.v1alpha.WebSecurityScannerGrpc.WebSecurityScannerImplBase;
+import com.google.protobuf.AbstractMessage;
 import com.google.protobuf.Empty;
-import com.google.protobuf.GeneratedMessageV3;
 import io.grpc.stub.StreamObserver;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -28,7 +28,7 @@ import java.util.Queue;
 @javax.annotation.Generated("by GAPIC")
 @BetaApi
 public class MockWebSecurityScannerImpl extends WebSecurityScannerImplBase {
-  private ArrayList<GeneratedMessageV3> requests;
+  private ArrayList<AbstractMessage> requests;
   private Queue<Object> responses;
 
   public MockWebSecurityScannerImpl() {
@@ -36,15 +36,15 @@ public class MockWebSecurityScannerImpl extends WebSecurityScannerImplBase {
     responses = new LinkedList<>();
   }
 
-  public List<GeneratedMessageV3> getRequests() {
+  public List<AbstractMessage> getRequests() {
     return requests;
   }
 
-  public void addResponse(GeneratedMessageV3 response) {
+  public void addResponse(AbstractMessage response) {
     responses.add(response);
   }
 
-  public void setResponses(List<GeneratedMessageV3> responses) {
+  public void setResponses(List<AbstractMessage> responses) {
     this.responses = new LinkedList<Object>(responses);
   }
 
