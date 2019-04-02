3f176c20b55dfaaa8fc32f28d82b31784b93e636
yoshi-automation <yoshi-automation@google.com>
2019-04-02 00:44:16 -0700

    Regenerate errorreporting client

--- google-cloud-clients/google-cloud-errorreporting/src/test/java/com/google/cloud/errorreporting/v1beta1/MockReportErrorsServiceImpl.java
+++ google-cloud-clients/google-cloud-errorreporting/src/test/java/com/google/cloud/errorreporting/v1beta1/MockReportErrorsServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.Queue;
 @javax.annotation.Generated("by GAPIC")
 @BetaApi
 public class MockReportErrorsServiceImpl extends ReportErrorsServiceImplBase {
-  private ArrayList<AbstractMessage> requests;
+  private List<AbstractMessage> requests;
   private Queue<Object> responses;
 
   public MockReportErrorsServiceImpl() {
