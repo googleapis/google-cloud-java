9e5429f45cf9face9fed585d0233534993e36b58
yoshi-automation <yoshi-automation@google.com>
2019-04-02 00:36:18 -0700

    Regenerate bigtable client

--- google-cloud-clients/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/MockBigtableTableAdminImpl.java
+++ google-cloud-clients/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/MockBigtableTableAdminImpl.java
@@ -48,7 +48,7 @@ import java.util.Queue;
 @javax.annotation.Generated("by GAPIC")
 @BetaApi
 public class MockBigtableTableAdminImpl extends BigtableTableAdminImplBase {
-  private ArrayList<AbstractMessage> requests;
+  private List<AbstractMessage> requests;
   private Queue<Object> responses;
 
   public MockBigtableTableAdminImpl() {
