34ca53d04a9a11fad373602d6a38fddce8433ec7
Solomon Duskis <sduskis@google.com>
2019-04-17 10:17:36 -0400

    Initial Generation of data catalog (#4957)
    * Initial Generation of data catalog
    
    * Fixing poms, adding README and synt.py

---
+++ google-cloud-clients/google-cloud-datacatalog/src/test/java/com/google/cloud/datacatalog/v1beta1/MockDataCatalogImpl.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.datacatalog.v1beta1;
+
+import com.google.api.core.BetaApi;
+import com.google.cloud.datacatalog.DataCatalogGrpc.DataCatalogImplBase;
+import com.google.cloud.datacatalog.Entry;
+import com.google.cloud.datacatalog.LookupEntryRequest;
+import com.google.protobuf.AbstractMessage;
+import io.grpc.stub.StreamObserver;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+@javax.annotation.Generated("by GAPIC")
+@BetaApi
+public class MockDataCatalogImpl extends DataCatalogImplBase {
+  private List<AbstractMessage> requests;
+  private Queue<Object> responses;
+
+  public MockDataCatalogImpl() {
+    requests = new ArrayList<>();
+    responses = new LinkedList<>();
+  }
+
+  public List<AbstractMessage> getRequests() {
+    return requests;
+  }
+
+  public void addResponse(AbstractMessage response) {
+    responses.add(response);
+  }
+
+  public void setResponses(List<AbstractMessage> responses) {
+    this.responses = new LinkedList<Object>(responses);
+  }
+
+  public void addException(Exception exception) {
+    responses.add(exception);
+  }
+
+  public void reset() {
+    requests = new ArrayList<>();
+    responses = new LinkedList<>();
+  }
+
+  @Override
+  public void lookupEntry(LookupEntryRequest request, StreamObserver<Entry> responseObserver) {
+    Object response = responses.remove();
+    if (response instanceof Entry) {
+      requests.add(request);
+      responseObserver.onNext((Entry) response);
+      responseObserver.onCompleted();
+    } else if (response instanceof Exception) {
+      responseObserver.onError((Exception) response);
+    } else {
+      responseObserver.onError(new IllegalArgumentException("Unrecognized response type"));
+    }
+  }
+}
