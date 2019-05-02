7d4e1c92e49da656e61dde98428046beaf20a4d7
Seth Hollyman <shollyman@google.com>
2019-05-02 11:43:33 -0700

    bigquery:  deprecate usage of delete in BigQuery client.
    Now that datasets may contain resources other than just tables,
    deleting a resource using only the string identifiers without
    type information is ambiguous.

--- google-cloud-clients/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQuery.java
+++ google-cloud-clients/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQuery.java
@@ -673,22 +673,14 @@ public interface BigQuery extends Service<BigQueryOptions> {
   /**
    * Deletes the requested table.
    *
-   * <p>Example of deleting a table.
-   *
-   * <pre>{@code
-   * String datasetName = "my_dataset_name";
-   * String tableName = "my_table_name";
-   * boolean deleted = bigquery.delete(datasetName, tableName);
-   * if (deleted) {
-   *   // the table was deleted
-   * } else {
-   *   // the table was not found
-   * }
-   * }</pre>
+   * @deprecated Now that BigQuery datasets contain multiple resource types, this
+   *   invocation is ambiguous.  Please use more strongly typed version of {@code #delete}
+   *   that leverages an non-ambiguous resource type identifier such as {@code TableId}.
    *
    * @return {@code true} if table was deleted, {@code false} if it was not found
    * @throws BigQueryException upon failure
    */
+  @Deprecated
   boolean delete(String datasetId, String tableId);
 
   /**
