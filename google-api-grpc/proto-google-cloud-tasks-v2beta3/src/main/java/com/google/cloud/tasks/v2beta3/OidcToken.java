bd87c02bca9e9b86f303ff4fcd251da880b5fba8
yoshi-automation <yoshi-automation@google.com>
2019-05-11 00:53:28 -0700

    Regenerate tasks client

--- google-api-grpc/proto-google-cloud-tasks-v2beta3/src/main/java/com/google/cloud/tasks/v2beta3/OidcToken.java
+++ google-api-grpc/proto-google-cloud-tasks-v2beta3/src/main/java/com/google/cloud/tasks/v2beta3/OidcToken.java
@@ -11,7 +11,7 @@ package com.google.cloud.tasks.v2beta3;
  * [OpenID Connect
  * token](https://developers.google.com/identity/protocols/OpenIDConnect). This
  * type of authorization should be used when sending requests to third party
- * endpoints.
+ * endpoints or Cloud Run.
  * </pre>
  *
  * Protobuf type {@code google.cloud.tasks.v2beta3.OidcToken}
@@ -374,7 +374,7 @@ public final class OidcToken extends com.google.protobuf.GeneratedMessageV3
    * [OpenID Connect
    * token](https://developers.google.com/identity/protocols/OpenIDConnect). This
    * type of authorization should be used when sending requests to third party
-   * endpoints.
+   * endpoints or Cloud Run.
    * </pre>
    *
    * Protobuf type {@code google.cloud.tasks.v2beta3.OidcToken}
