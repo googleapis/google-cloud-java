f4b6f2ce2bf4fb2c1459b91e3288fe50a78990d1
yoshi-automation <yoshi-automation@google.com>
2019-06-04 12:43:46 -0700

    Regenerate phishingprotection client

--- google-api-grpc/proto-google-cloud-phishingprotection-v1beta1/src/main/java/com/google/phishingprotection/v1beta1/PhishingProtectionProto.java
+++ google-api-grpc/proto-google-cloud-phishingprotection-v1beta1/src/main/java/com/google/phishingprotection/v1beta1/PhishingProtectionProto.java
@@ -48,20 +48,12 @@ public final class PhishingProtectionProto {
           + "d.PhishingProtection.V1Beta1\312\002\'Google\\Cl"
           + "oud\\PhishingProtection\\V1beta1b\006proto3"
     };
-    com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
-        new com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner() {
-          public com.google.protobuf.ExtensionRegistry assignDescriptors(
-              com.google.protobuf.Descriptors.FileDescriptor root) {
-            descriptor = root;
-            return null;
-          }
-        };
-    com.google.protobuf.Descriptors.FileDescriptor.internalBuildGeneratedFileFrom(
-        descriptorData,
-        new com.google.protobuf.Descriptors.FileDescriptor[] {
-          com.google.api.AnnotationsProto.getDescriptor(),
-        },
-        assigner);
+    descriptor =
+        com.google.protobuf.Descriptors.FileDescriptor.internalBuildGeneratedFileFrom(
+            descriptorData,
+            new com.google.protobuf.Descriptors.FileDescriptor[] {
+              com.google.api.AnnotationsProto.getDescriptor(),
+            });
     internal_static_google_cloud_phishingprotection_v1beta1_ReportPhishingRequest_descriptor =
         getDescriptor().getMessageTypes().get(0);
     internal_static_google_cloud_phishingprotection_v1beta1_ReportPhishingRequest_fieldAccessorTable =
