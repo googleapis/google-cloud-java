597807dfdf5ba8a9df037680d63fa6ae59107d5a
yoshi-automation <yoshi-automation@google.com>
2019-06-05 00:53:33 -0700

    Regenerate phishingprotection client

--- google-api-grpc/proto-google-cloud-phishingprotection-v1beta1/src/main/java/com/google/phishingprotection/v1beta1/ReportPhishingResponse.java
+++ google-api-grpc/proto-google-cloud-phishingprotection-v1beta1/src/main/java/com/google/phishingprotection/v1beta1/ReportPhishingResponse.java
@@ -25,6 +25,12 @@ public final class ReportPhishingResponse extends com.google.protobuf.GeneratedM
   private ReportPhishingResponse() {}
 
   @java.lang.Override
+  @SuppressWarnings({"unused"})
+  protected java.lang.Object newInstance(UnusedPrivateParameter unused) {
+    return new ReportPhishingResponse();
+  }
+
+  @java.lang.Override
   public final com.google.protobuf.UnknownFieldSet getUnknownFields() {
     return this.unknownFields;
   }
