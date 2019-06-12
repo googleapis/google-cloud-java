b1c78bd50847afc4291083a08527ce38c9a84137
Gonzalo Gasca Meza <gogasca@google.com>
2019-06-12 08:30:16 -0700

    samples: Update Recognize.java (#1460)
    Update comment to match correct audio encoding

--- java-speech/samples/snippets/src/main/java/com/example/speech/Recognize.java
+++ java-speech/samples/snippets/src/main/java/com/example/speech/Recognize.java
@@ -287,7 +287,7 @@ public class Recognize {
     // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
     try (SpeechClient speech = SpeechClient.create()) {
 
-      // Configure remote file request for Linear16
+      // Configure remote file request for FLAC
       RecognitionConfig config =
           RecognitionConfig.newBuilder()
               .setEncoding(AudioEncoding.FLAC)
@@ -336,7 +336,7 @@ public class Recognize {
     // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
     try (SpeechClient speech = SpeechClient.create()) {
 
-      // Configure remote file request for Linear16
+      // Configure remote file request for FLAC
       RecognitionConfig config =
           RecognitionConfig.newBuilder()
               .setEncoding(AudioEncoding.FLAC)
