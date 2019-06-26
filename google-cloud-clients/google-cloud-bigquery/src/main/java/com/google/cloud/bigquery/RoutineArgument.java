e5f492d382167262360769e73a09fd719f357ee8
Seth Hollyman <shollyman@google.com>
2019-06-26 14:25:41 -0700

    RoutineArgument and test

---
+++ google-cloud-clients/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/RoutineArgument.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery;
+
+import com.google.api.services.bigquery.model.Argument;
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Function;
+
+import javax.annotation.Nullable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@AutoValue
+public abstract class RoutineArgument {
+
+    static final Function<Argument, RoutineArgument> FROM_PB_FUNCTION =
+            new Function<Argument, RoutineArgument>() {
+                @Override
+                public RoutineArgument apply(Argument pb) {
+                    return RoutineArgument.fromPb(pb);
+                }
+            };
+    static final Function<RoutineArgument, Argument> TO_PB_FUNCTION =
+            new Function<RoutineArgument, Argument>() {
+                @Override
+                public Argument apply(RoutineArgument argument) {
+                    return argument.toPb();
+                }
+            };
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder setName(String name);
+        public abstract Builder setKind(String kind);
+        public abstract Builder setMode(String mode);
+        public abstract Builder setDataType(StandardSQLDataType dataType);
+        public abstract RoutineArgument build();
+    }
+
+    @Nullable
+    public abstract String getName();
+
+    @Nullable
+    public abstract String getKind();
+
+    @Nullable
+    public abstract String getMode();
+
+    @Nullable
+    public abstract StandardSQLDataType getDataType();
+
+    public abstract Builder toBuilder();
+
+    static Builder newBuilder() { return new AutoValue_RoutineArgument.Builder(); }
+
+    Argument toPb() {
+        Argument argumentPb = new Argument()
+                .setName(getName())
+                .setArgumentKind(getKind())
+                .setMode(getMode());
+        if (getDataType() != null) {
+            argumentPb.setDataType(getDataType().toPb());
+        }
+        return argumentPb;
+    }
+
+    static RoutineArgument fromPb(Argument argumentPb) {
+        Builder builder = newBuilder();
+        builder.setName(argumentPb.getName());
+        builder.setKind(argumentPb.getArgumentKind());
+        builder.setMode(argumentPb.getMode());
+        if (argumentPb.getDataType() != null) {
+            builder.setDataType(StandardSQLDataType.fromPb(argumentPb.getDataType()));
+        }
+        return builder.build();
+    }
+
+}
---
+++ google-cloud-clients/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/RoutineInfo.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery;
+
+import com.google.api.services.bigquery.model.Routine;
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+@AutoValue
+public abstract class RoutineInfo {
+
+    static final Function<Routine, RoutineInfo> FROM_PB_FUNCTION =
+            new Function<Routine, RoutineInfo>() {
+                @Override
+                public RoutineInfo apply(Routine pb) {
+                    return RoutineInfo.fromPb(pb);
+                }
+            };
+    static final Function<RoutineInfo, Routine> TO_PB_FUNCTION =
+            new Function<RoutineInfo, Routine>() {
+                @Override
+                public Routine apply(RoutineInfo routineInfo) {
+                    return routineInfo.toPb();
+                }
+            };
+
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+         abstract Builder setEtag(String etag);
+         public abstract Builder setRoutineType(String routineType);
+         abstract Builder setCreationTime(Long creationMillis);
+         abstract Builder setLastModifiedTime(Long lastModifiedMillis);
+         public abstract Builder setArguments(List<RoutineArgument> arguments);
+         public abstract Builder setReturnType(StandardSQLDataType returnType);
+         public abstract Builder setImportedLibraries(List<String> libraries);
+         public abstract Builder setBody(String body);
+         public abstract RoutineInfo build();
+    }
+
+    @Nullable
+    public abstract String getEtag();
+
+    @Nullable
+    public abstract String getRoutineType();
+
+    @Nullable
+    public abstract Long getCreationTime();
+
+    @Nullable
+    public abstract Long getLastModifiedTime();
+
+    public abstract List<RoutineArgument> getArguments();
+
+    public abstract StandardSQLDataType getReturnType();
+
+    public abstract List<String> getImportedLibraries();
+
+    @Nullable
+    public abstract String getBody();
+
+    public abstract Builder toBuilder();
+
+    static Builder newBuilder() { return new AutoValue_RoutineInfo.Builder(); }
+
+    Routine toPb() {
+        Routine routinePb = new Routine()
+                .setEtag(getEtag())
+                .setRoutineType(getRoutineType())
+                .setCreationTime(getCreationTime())
+                .setLastModifiedTime(getLastModifiedTime());
+        if (getArguments() != null) {
+            routinePb.setArguments(Lists.transform(getArguments(), RoutineArgument.TO_PB_FUNCTION));
+        }
+        return routinePb;
+    }
+
+    static RoutineInfo fromPb(Routine routinePb) {
+            Builder builder = newBuilder();
+            builder.setEtag(routinePb.getEtag());
+            builder.setRoutineType(routinePb.getRoutineType());
+            builder.setCreationTime(routinePb.getCreationTime());
+            builder.setLastModifiedTime(routinePb.getLastModifiedTime());
+            if (routinePb.getArguments() != null) {
+                builder.setArguments(Lists.transform(routinePb.getArguments(), RoutineArgument.FROM_PB_FUNCTION));
+            }
+            if (routinePb.getReturnType() != null) {
+                builder.setReturnType(StandardSQLDataType.fromPb(routinePb.getReturnType()));
+            }
+            if (routinePb.getImportedLibraries() != null) {
+                builder.setImportedLibraries(ImmutableList.copyOf(routinePb.getImportedLibraries()));
+            }
+            builder.setBody(routinePb.getDefinitionBody());
+            return builder.build();
+    }
+
+}
