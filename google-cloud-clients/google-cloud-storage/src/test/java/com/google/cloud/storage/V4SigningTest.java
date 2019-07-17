004cfcb35b580ef9d6bf268f2c0588f0439fb031
BenWhitehead <BenWhitehead@users.noreply.github.com>
2019-07-17 19:04:16 -0400

    Update Storage conformance tests
    * Refactor V4SigningTest to use JUnits Parameterized Test runner.
      This allows running each of the tests as an individual test thereby
      providing success/ignore/failure information per test.
    * Refactor V4SigningTest to use the generated Java Protobuf classes
      from the google-cloud-conformance-test module. The format read from is
      still json.
    * Update to use the latest revision of the conformance-tests data.
      * One of the updated tests `Headers should be trimmed` does not
        currently pass and has been marked as ignore until there is a
        resolution to b/136171758 relating to the specification of how
        whitespace is to be handled when signing URLs.
      * I choose to move forward with the update, to reduce the drift
        between the test definitions in conformance-tests where other
        automation is being introduced to hopefully reduce the maintenance
        burden of conformance tests.

--- google-cloud-clients/google-cloud-storage/src/test/java/com/google/cloud/storage/V4SigningTest.java
+++ google-cloud-clients/google-cloud-storage/src/test/java/com/google/cloud/storage/V4SigningTest.java
@@ -16,33 +16,51 @@
 
 package com.google.cloud.storage;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assume.assumeThat;
 
 import com.google.api.core.ApiClock;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.conformance.storage.v1.SigningV4Test;
+import com.google.cloud.conformance.storage.v1.TestFile;
+import com.google.cloud.storage.Storage.SignUrlOption;
 import com.google.cloud.storage.testing.RemoteStorageHelper;
-import com.google.common.io.CharStreams;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.protobuf.Timestamp;
+import com.google.protobuf.util.JsonFormat;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class V4SigningTest {
+
+  private static final String SERVICE_ACCOUNT_JSON_RESOURCE =
+      "com/google/cloud/conformance/storage/v1/test_service_account.not-a-test.json";
+  private static final String TEST_DATA_JSON_RESOURCE =
+      "com/google/cloud/conformance/storage/v1/v4_signatures.json";
 
   private static class FakeClock implements ApiClock {
     private final AtomicLong currentNanoTime;
 
-    public FakeClock(long initialNanoTime) {
-      this.currentNanoTime = new AtomicLong(initialNanoTime);
+    public FakeClock(Timestamp timestamp) {
+      this.currentNanoTime =
+          new AtomicLong(
+              TimeUnit.NANOSECONDS.convert(timestamp.getSeconds(), TimeUnit.SECONDS)
+                  + timestamp.getNanos());
     }
 
     public long nanoTime() {
@@ -54,77 +72,89 @@ public class V4SigningTest {
     }
   }
 
-  public class TestCase {
-    String description;
-    String bucket;
-    String object;
-    String method;
-    String expiration;
-    String timestamp;
-    String expectedUrl;
-    JsonObject headers;
+  @Rule public TestName testName = new TestName();
 
-    public String toString() {
-      return description;
-    }
+  private final SigningV4Test testData;
+  private final ServiceAccountCredentials serviceAccountCredentials;
+
+  /**
+   * @param testData The serialized test data representing the test case.
+   * @param serviceAccountCredentials The credentials to use in this test.
+   * @param description Not used by the test, but used by the parameterized test runner as the name
+   *     of the test.
+   */
+  public V4SigningTest(
+      SigningV4Test testData,
+      ServiceAccountCredentials serviceAccountCredentials,
+      @SuppressWarnings("unused") String description) {
+    this.testData = testData;
+    this.serviceAccountCredentials = serviceAccountCredentials;
   }
 
   @Test
-  public void testV4UrlSigning() throws Exception {
+  public void test() {
+    assumeThat(
+        "Test skipped until b/136171758 is resolved",
+        testName.getMethodName(),
+        is(not("test[Headers should be trimmed]")));
+
     Storage storage =
         RemoteStorageHelper.create()
             .getOptions()
             .toBuilder()
-            .setCredentials(
-                ServiceAccountCredentials.fromStream(
-                    getClass().getResourceAsStream("/UrlSignerV4TestAccount.json")))
+            .setCredentials(serviceAccountCredentials)
+            .setClock(new FakeClock(testData.getTimestamp()))
             .build()
             .getService();
-    Gson gson = new GsonBuilder().create();
 
-    String testCaseJson =
-        CharStreams.toString(
-            new InputStreamReader(getClass().getResourceAsStream("/UrlSignerV4TestData.json")));
+    BlobInfo blob = BlobInfo.newBuilder(testData.getBucket(), testData.getObject()).build();
 
-    JsonArray testCases = gson.fromJson(testCaseJson, JsonArray.class);
+    final String signedUrl =
+        storage
+            .signUrl(
+                blob,
+                testData.getExpiration(),
+                TimeUnit.SECONDS,
+                SignUrlOption.httpMethod(HttpMethod.valueOf(testData.getMethod())),
+                SignUrlOption.withExtHeaders(testData.getHeadersMap()),
+                SignUrlOption.withV4Signature())
+            .toString();
+    assertEquals(testData.getExpectedUrl(), signedUrl);
+  }
 
-    SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd'T'hhmmss'Z'");
-    format.setTimeZone(TimeZone.getTimeZone("UTC"));
+  /**
+   * Attempt to load all of the tests and return a {@code Collection<Object[]>} representing the set
+   * of tests. Each entry in the returned collection is the set of parameters to the constructor of
+   * this test class.
+   *
+   * <p>The results of this method will then be ran by JUnit's Parameterized test runner
+   */
+  @Parameters(name = "{2}")
+  public static Collection<Object[]> testCases() throws IOException {
+    final ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
-    for (JsonElement testCaseElement : testCases) {
-      TestCase testCase = gson.fromJson(testCaseElement, TestCase.class);
+    final InputStream credentialsStream = cl.getResourceAsStream(SERVICE_ACCOUNT_JSON_RESOURCE);
+    assertNotNull(
+        String.format("Unable to load service account json: %s", SERVICE_ACCOUNT_JSON_RESOURCE),
+        credentialsStream);
 
-      storage =
-          storage
-              .getOptions()
-              .toBuilder()
-              .setClock(
-                  new FakeClock(
-                      TimeUnit.NANOSECONDS.convert(
-                          format.parse(testCase.timestamp).getTime(), TimeUnit.MILLISECONDS)))
-              .build()
-              .getService();
+    final InputStream dataJson = cl.getResourceAsStream(TEST_DATA_JSON_RESOURCE);
+    assertNotNull(
+        String.format("Unable to load test definition: %s", TEST_DATA_JSON_RESOURCE), dataJson);
 
-      BlobInfo blob = BlobInfo.newBuilder(testCase.bucket, testCase.object).build();
+    final ServiceAccountCredentials serviceAccountCredentials =
+        ServiceAccountCredentials.fromStream(credentialsStream);
 
-      Map<String, String> headers = new HashMap<>();
-      if (testCase.headers != null) {
-        for (Map.Entry<String, JsonElement> entry : testCase.headers.entrySet()) {
-          headers.put(entry.getKey(), entry.getValue().getAsString());
-        }
-      }
+    final InputStreamReader reader = new InputStreamReader(dataJson);
+    final TestFile.Builder testBuilder = TestFile.newBuilder();
+    JsonFormat.parser().merge(reader, testBuilder);
+    final TestFile testFile = testBuilder.build();
 
-      assertEquals(
-          testCase.expectedUrl,
-          storage
-              .signUrl(
-                  blob,
-                  Long.valueOf(testCase.expiration),
-                  TimeUnit.SECONDS,
-                  Storage.SignUrlOption.httpMethod(HttpMethod.valueOf(testCase.method)),
-                  Storage.SignUrlOption.withExtHeaders(headers),
-                  Storage.SignUrlOption.withV4Signature())
-              .toString());
+    final List<SigningV4Test> tests = testFile.getSigningV4TestsList();
+    final ArrayList<Object[]> data = new ArrayList<>(tests.size());
+    for (SigningV4Test test : tests) {
+      data.add(new Object[] {test, serviceAccountCredentials, test.getDescription()});
     }
+    return data;
   }
 }
