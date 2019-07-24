380de670ac65bce2bbde3275196dc0a993de4aaf
Jeff Ching <chingor@google.com>
2019-07-24 09:57:49 -0700

    Bulk client regen - builder documentation and Generated annotation (#5850)
    * Regenerate asset client
    
    * Regenerate bigquerystorage client
    
    * Regenerate bigtable client
    
    * Regenerate compute client
    
    * Regenerate container client
    
    * Regenerate containeranalysis client
    
    * Regenerate dataproc client
    
    * Regenerate dlp client
    
    * Regenerate errorreporting client
    
    * Regenerate firestore client
    
    * Regenerate iamcredentials client
    
    * Regenerate iot client
    
    * Regenerate kms client
    
    * Regenerate language client
    
    * Regenerate logging client
    
    * Regenerate monitoring client
    
    * Regenerate os-login client
    
    * Regenerate phishingprotection client
    
    * Regenerate pubsub client
    
    * Regenerate recaptchaenterprise client
    
    * Regenerate scheduler client
    
    * Regenerate talent client
    
    * Regenerate tasks client
    
    * Regenerate texttospeech client
    
    * Regenerate trace client
    
    * Regenerate webrisk client
    
    * Regenerate websecurityscanner client
    
    * Regenerate grafeas client

--- java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableTableAdminStubSettings.java
+++ java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableTableAdminStubSettings.java
@@ -89,8 +89,9 @@ import org.threeten.bp.Duration;
  * </ul>
  *
  * <p>The builder of this class is recursive, so contained classes are themselves builders. When
- * build() is called, the tree of builders is called to create the complete settings object. For
- * example, to set the total timeout of createTable to 30 seconds:
+ * build() is called, the tree of builders is called to create the complete settings object.
+ *
+ * <p>For example, to set the total timeout of createTable to 30 seconds:
  *
  * <pre>
  * <code>
