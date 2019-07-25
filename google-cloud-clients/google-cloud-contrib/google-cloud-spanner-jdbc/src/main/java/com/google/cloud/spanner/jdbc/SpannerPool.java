b1eaa8d860415256deb1b35abb523f8b9b8dcaa7
Olav Loite <koloite@gmail.com>
2019-07-25 18:54:01 +0200

    add ConnectionOptions

---
+++ google-cloud-clients/google-cloud-contrib/google-cloud-spanner-jdbc/src/main/java/com/google/cloud/spanner/jdbc/SpannerPool.java
@@ -0,0 +1,11 @@
+package com.google.cloud.spanner.jdbc;
+
+/**
+ * This is just a stub class to prevent compile errors. This class will be replaced with the actual
+ * class in a separate PR.
+ */
+class SpannerPool {
+  static final SpannerPool INSTANCE = new SpannerPool();
+
+  void checkAndCloseSpanners() {}
+}
