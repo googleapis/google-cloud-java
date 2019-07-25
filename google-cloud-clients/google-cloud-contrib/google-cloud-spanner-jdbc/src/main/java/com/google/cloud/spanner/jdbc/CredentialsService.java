7dd92644710328e3fa9f5846452969ac8230be8f
Olav Loite <koloite@gmail.com>
2019-07-25 13:47:31 +0200

    add CredentialService

---
+++ google-cloud-clients/google-cloud-contrib/google-cloud-spanner-jdbc/src/main/java/com/google/cloud/spanner/jdbc/CredentialsService.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spanner.jdbc;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.auth.http.HttpTransportFactory;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.spanner.ErrorCode;
+import com.google.cloud.spanner.SpannerException;
+import com.google.cloud.spanner.SpannerExceptionFactory;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+
+/**
+ * Internal class for the Spanner Connection API. This class and its methods have public visibility
+ * only for allowing integration tests to access it. This may change in future releases.
+ *
+ * <p>Service class for getting credentials from key files stored locally or on cloud storage.
+ */
+@VisibleForTesting
+public class CredentialsService {
+  private static final String GOOGLE_CLOUD_STORAGE_PREFIX = "gs://";
+
+  @VisibleForTesting public static final CredentialsService INSTANCE = new CredentialsService();
+
+  /** Util for creating a default http transport */
+  static class CloudSpannerOAuthUtil {
+    static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    static final HttpTransportFactory HTTP_TRANSPORT_FACTORY = new DefaultHttpTransportFactory();
+
+    static class DefaultHttpTransportFactory implements HttpTransportFactory {
+      @Override
+      public HttpTransport create() {
+        return HTTP_TRANSPORT;
+      }
+    }
+
+    private CloudSpannerOAuthUtil() {}
+  }
+
+  CredentialsService() {}
+
+  /**
+   * Create credentials from the given URL pointing to a credentials json file. This may be a local
+   * file or a file on Google Cloud Storage. Credentials on Google Cloud Storage can only be used if
+   * the application is running in an environment where application default credentials have been
+   * set.
+   *
+   * @param credentialsUrl The URL of the credentials file to read. If <code>null</code>, then this
+   *     method will return the application default credentials of the environment.
+   * @return the {@link GoogleCredentials} object pointed to by the URL.
+   * @throws SpannerException If the URL does not point to a valid credentials file, or if the file
+   *     cannot be accessed.
+   */
+  @VisibleForTesting
+  public GoogleCredentials createCredentials(String credentialsUrl) {
+    try {
+      if (credentialsUrl == null) {
+        return GoogleCredentials.getApplicationDefault();
+      } else {
+        return internalGetCredentialsFromUrl(credentialsUrl);
+      }
+    } catch (IOException e) {
+      throw SpannerExceptionFactory.newSpannerException(
+          ErrorCode.INVALID_ARGUMENT, "Invalid credentials path specified", e);
+    }
+  }
+
+  @VisibleForTesting
+  GoogleCredentials internalGetCredentialsFromUrl(String credentialsUrl) throws IOException {
+    Preconditions.checkNotNull(credentialsUrl);
+    Preconditions.checkArgument(
+        credentialsUrl.length() > 0, "credentialsUrl may not be an empty string");
+    if (credentialsUrl.startsWith(GOOGLE_CLOUD_STORAGE_PREFIX)) {
+      return internalGetCredentialsFromCloudStorage(credentialsUrl);
+    } else {
+      return internalGetCredentialsFromLocalFile(credentialsUrl);
+    }
+  }
+
+  @VisibleForTesting
+  GoogleCredentials internalGetCredentialsFromLocalFile(String filePath) throws IOException {
+    File credentialsFile = new File(filePath);
+    if (!credentialsFile.isFile()) {
+      throw new IOException(
+          String.format("Error reading credential file %s: File does not exist", filePath));
+    }
+    try (InputStream credentialsStream = new FileInputStream(credentialsFile)) {
+      return GoogleCredentials.fromStream(
+          credentialsStream, CloudSpannerOAuthUtil.HTTP_TRANSPORT_FACTORY);
+    }
+  }
+
+  @VisibleForTesting
+  GoogleCredentials internalGetCredentialsFromCloudStorage(String credentialsUrl)
+      throws IOException {
+    Preconditions.checkArgument(credentialsUrl.startsWith(GOOGLE_CLOUD_STORAGE_PREFIX));
+    try {
+      Storage storage = internalCreateStorage();
+      String bucketName = internalGetBucket(credentialsUrl);
+      String blobName = internalGetBlob(credentialsUrl);
+      Blob blob = storage.get(bucketName, blobName);
+      return GoogleCredentials.fromStream(
+          internalCreateInputStream(blob), CloudSpannerOAuthUtil.HTTP_TRANSPORT_FACTORY);
+    } catch (Exception e) {
+      throw new IOException(
+          String.format(
+              "Could not retrieve credentials from %s. "
+                  + "Credentials on Google Cloud Storage can only be used when the application is running "
+                  + "in an environment that already has default credentials to access Google Cloud Storage",
+              credentialsUrl),
+          e);
+    }
+  }
+
+  @VisibleForTesting
+  Storage internalCreateStorage() {
+    return StorageOptions.newBuilder().build().getService();
+  }
+
+  @VisibleForTesting
+  InputStream internalCreateInputStream(Blob blob) {
+    return Channels.newInputStream(blob.reader());
+  }
+
+  @VisibleForTesting
+  String internalGetBucket(String storageUrl) {
+    Preconditions.checkArgument(
+        storageUrl.startsWith(GOOGLE_CLOUD_STORAGE_PREFIX),
+        String.format("Storage URL must start with %s", GOOGLE_CLOUD_STORAGE_PREFIX));
+    Preconditions.checkArgument(
+        storageUrl.substring(5).contains("/"), "Storage URL must contain a blob name");
+    return storageUrl.substring(5, storageUrl.indexOf('/', 5));
+  }
+
+  @VisibleForTesting
+  String internalGetBlob(String storageUrl) {
+    Preconditions.checkArgument(
+        storageUrl.startsWith(GOOGLE_CLOUD_STORAGE_PREFIX),
+        String.format("Storage URL must start with %s", GOOGLE_CLOUD_STORAGE_PREFIX));
+    Preconditions.checkArgument(
+        storageUrl.substring(5).contains("/"), "Storage URL must contain a blob name");
+    return storageUrl.substring(storageUrl.indexOf('/', 5) + 1);
+  }
+}
---
+++ google-cloud-clients/google-cloud-contrib/google-cloud-spanner-jdbc/src/test/java/com/google/cloud/spanner/jdbc/CredentialsServiceTest.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spanner.jdbc;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.spanner.SpannerException;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
+import com.google.rpc.Code;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for reading and parsing key files and getting service accounts. The key files that are used
+ * for this test class contain random private keys generated with the following command: <code>
+ * openssl genpkey -out rsakey.pem -algorithm RSA -pkeyopt rsa_keygen_bits:2048</code> These keys
+ * are not in use for anything else except these test classes.
+ */
+@RunWith(JUnit4.class)
+public class CredentialsServiceTest {
+  static final String FILE_TEST_PATH =
+      CredentialsServiceTest.class.getResource("test-key.json").getFile();
+  static final String CLOUD_FILE_TEST_PATH =
+      CredentialsServiceTest.class.getResource("test-key-cloud-storage.json").getFile();
+  static final String GS_TEST_PATH = "gs://test-bucket/test-key-cloud-storage.json";
+  static final String GS_INVALID_TEST_PATH = "gs://test-bucket/non-existing-key.json";
+
+  private final CredentialsService service =
+      new CredentialsService() {
+        @Override
+        protected Storage internalCreateStorage() {
+          Storage storage = mock(Storage.class);
+          when(storage.get("test-bucket", "test-key-cloud-storage.json"))
+              .thenReturn(mock(Blob.class));
+          when(storage.get("test-bucket", "non-existing-key.json"))
+              .thenThrow(new StorageException(Code.NOT_FOUND_VALUE, "Unknown blob"));
+          return storage;
+        }
+
+        @Override
+        protected InputStream internalCreateInputStream(Blob blob) {
+          try {
+            return new FileInputStream(CLOUD_FILE_TEST_PATH);
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        }
+      };
+
+  @Test
+  public void testCreateCredentials_UseDefault_NoDefaultSet() throws IOException {
+    Credentials defaultCredentials = null;
+    try {
+      defaultCredentials = GoogleCredentials.getApplicationDefault();
+    } catch (Exception e) {
+      // ignore
+    }
+    Credentials credentials = null;
+    try {
+      credentials = service.createCredentials(null);
+    } catch (Exception e) {
+      // ignore
+    }
+    assertThat(credentials, is(equalTo(defaultCredentials)));
+  }
+
+  @Test
+  public void testCreateCredentials_UseDefault_WithDefaultSet() throws Exception {
+    injectEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", FILE_TEST_PATH);
+    try {
+      Credentials defaultCredentials = GoogleCredentials.getApplicationDefault();
+      Credentials credentials = service.createCredentials(null);
+      assertThat(credentials, is(notNullValue()));
+      assertThat(credentials, is(equalTo(defaultCredentials)));
+    } finally {
+      injectEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", null);
+    }
+  }
+
+  @Test
+  public void testCreateCredentialsFile() throws IOException {
+    Credentials credentialsFromFile = service.internalGetCredentialsFromLocalFile(FILE_TEST_PATH);
+    Credentials credentials = service.createCredentials(FILE_TEST_PATH);
+    assertThat(credentials, is(equalTo(credentialsFromFile)));
+  }
+
+  @Test
+  public void testCreateCredentialsCloudStorage() throws IOException {
+    Credentials expectedCredentials =
+        service.internalGetCredentialsFromLocalFile(CLOUD_FILE_TEST_PATH);
+    Credentials credentials = service.createCredentials(GS_TEST_PATH);
+    assertThat(credentials, is(equalTo(expectedCredentials)));
+  }
+
+  @Test(expected = SpannerException.class)
+  public void testCreateCredentialsInvalidFile() {
+    service.createCredentials("invalid_file_path.json");
+  }
+
+  @Test(expected = SpannerException.class)
+  public void testCreateCredentialsInvalidCloudStorage() {
+    service.createCredentials(GS_INVALID_TEST_PATH);
+  }
+
+  @Test
+  public void testGetCredentialsFromFile() throws IOException {
+    GoogleCredentials credentials = service.internalGetCredentialsFromLocalFile(FILE_TEST_PATH);
+    assertThat(credentials, is(notNullValue()));
+  }
+
+  @Test
+  public void testGetCredentialsFromCloudStorage() throws IOException {
+    GoogleCredentials expectedCredentials =
+        service.internalGetCredentialsFromLocalFile(CLOUD_FILE_TEST_PATH);
+    assertThat(
+        service.internalGetCredentialsFromCloudStorage(GS_TEST_PATH),
+        is(equalTo(expectedCredentials)));
+  }
+
+  @Test
+  public void testGetBlob() {
+    assertThat(service.internalGetBlob("gs://test-bucket/test-blob"), is(equalTo("test-blob")));
+    assertThat(service.internalGetBlob("gs://test-bucket/blob-test"), is(equalTo("blob-test")));
+  }
+
+  @Test
+  public void testGetBucket() {
+    assertThat(service.internalGetBucket("gs://test-bucket/test-blob"), is(equalTo("test-bucket")));
+    assertThat(service.internalGetBucket("gs://bucket-test/blob-test"), is(equalTo("bucket-test")));
+  }
+
+  @SuppressWarnings("unchecked")
+  private static void injectEnvironmentVariable(String key, String value) throws Exception {
+    Class<?> processEnvironment = Class.forName("java.lang.ProcessEnvironment");
+    Field unmodifiableMapField =
+        getAccessibleField(processEnvironment, "theUnmodifiableEnvironment");
+    Object unmodifiableMap = unmodifiableMapField.get(null);
+    injectIntoUnmodifiableMap(key, value, unmodifiableMap);
+
+    Field mapField = getAccessibleField(processEnvironment, "theEnvironment");
+    Map<String, String> map = (Map<String, String>) mapField.get(null);
+    map.put(key, value);
+  }
+
+  private static Field getAccessibleField(Class<?> clazz, String fieldName)
+      throws NoSuchFieldException {
+    Field field = clazz.getDeclaredField(fieldName);
+    field.setAccessible(true);
+    return field;
+  }
+
+  @SuppressWarnings("unchecked")
+  private static void injectIntoUnmodifiableMap(String key, String value, Object map)
+      throws ReflectiveOperationException {
+    Class<?> unmodifiableMap = Class.forName("java.util.Collections$UnmodifiableMap");
+    Field field = getAccessibleField(unmodifiableMap, "m");
+    Object obj = field.get(map);
+    if (value == null) {
+      ((Map<String, String>) obj).remove(key);
+    } else {
+      ((Map<String, String>) obj).put(key, value);
+    }
+  }
+}
