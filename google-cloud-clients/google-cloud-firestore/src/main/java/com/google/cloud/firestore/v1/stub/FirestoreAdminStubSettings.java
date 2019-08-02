8242b78f8b190b2835635f2e1a906126a0bfdca2
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:47:42 -0700

    Regenerate firestore client

--- google-cloud-clients/google-cloud-firestore/src/main/java/com/google/cloud/firestore/v1/stub/FirestoreAdminStubSettings.java
+++ google-cloud-clients/google-cloud-firestore/src/main/java/com/google/cloud/firestore/v1/stub/FirestoreAdminStubSettings.java
@@ -194,7 +194,8 @@ public class FirestoreAdminStubSettings extends StubSettings<FirestoreAdminStubS
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
