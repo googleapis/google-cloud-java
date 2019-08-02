aca29142ff41bff8f2ecd8439f7bf0492a976d34
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:58:54 -0700

    Regenerate talent client

--- google-cloud-clients/google-cloud-talent/src/main/java/com/google/cloud/talent/v4beta1/stub/ApplicationServiceStubSettings.java
+++ google-cloud-clients/google-cloud-talent/src/main/java/com/google/cloud/talent/v4beta1/stub/ApplicationServiceStubSettings.java
@@ -162,7 +162,8 @@ public class ApplicationServiceStubSettings extends StubSettings<ApplicationServ
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
