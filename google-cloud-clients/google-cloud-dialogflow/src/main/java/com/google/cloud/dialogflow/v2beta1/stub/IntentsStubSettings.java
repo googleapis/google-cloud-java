fff14aea33aa7d64d59c9dcfd48e2885c04774e7
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:44:36 -0700

    Regenerate dialogflow client

--- google-cloud-clients/google-cloud-dialogflow/src/main/java/com/google/cloud/dialogflow/v2beta1/stub/IntentsStubSettings.java
+++ google-cloud-clients/google-cloud-dialogflow/src/main/java/com/google/cloud/dialogflow/v2beta1/stub/IntentsStubSettings.java
@@ -199,7 +199,8 @@ public class IntentsStubSettings extends StubSettings<IntentsStubSettings> {
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
