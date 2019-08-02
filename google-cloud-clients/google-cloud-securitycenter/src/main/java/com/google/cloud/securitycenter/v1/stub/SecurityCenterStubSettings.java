7e73193f58565ba34e985f17911c4fdff85c8e2b
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:57:05 -0700

    Regenerate securitycenter client

--- google-cloud-clients/google-cloud-securitycenter/src/main/java/com/google/cloud/securitycenter/v1/stub/SecurityCenterStubSettings.java
+++ google-cloud-clients/google-cloud-securitycenter/src/main/java/com/google/cloud/securitycenter/v1/stub/SecurityCenterStubSettings.java
@@ -296,7 +296,8 @@ public class SecurityCenterStubSettings extends StubSettings<SecurityCenterStubS
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
