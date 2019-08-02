e5896908011d54fb726ebaccfb40730e38577131
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:38:47 -0700

    Regenerate container client

--- google-cloud-clients/google-cloud-container/src/main/java/com/google/cloud/container/v1/stub/ClusterManagerStubSettings.java
+++ google-cloud-clients/google-cloud-container/src/main/java/com/google/cloud/container/v1/stub/ClusterManagerStubSettings.java
@@ -336,7 +336,8 @@ public class ClusterManagerStubSettings extends StubSettings<ClusterManagerStubS
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
