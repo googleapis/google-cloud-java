8ba683e5c0cd868fc8a882f4bac25c971b574670
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:33:59 -0700

    Regenerate automl client

--- google-cloud-clients/google-cloud-automl/src/main/java/com/google/cloud/automl/v1beta1/stub/AutoMlStubSettings.java
+++ google-cloud-clients/google-cloud-automl/src/main/java/com/google/cloud/automl/v1beta1/stub/AutoMlStubSettings.java
@@ -414,7 +414,8 @@ public class AutoMlStubSettings extends StubSettings<AutoMlStubSettings> {
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
