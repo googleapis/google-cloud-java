01959380eb77853c9a672f075422414893e9b7d9
yoshi-automation <yoshi-automation@google.com>
2019-08-02 01:06:22 -0700

    Regenerate grafeas client

--- google-cloud-clients/grafeas/src/main/java/io/grafeas/v1/stub/GrafeasStubSettings.java
+++ google-cloud-clients/grafeas/src/main/java/io/grafeas/v1/stub/GrafeasStubSettings.java
@@ -237,7 +237,8 @@ public class GrafeasStubSettings extends StubSettings<GrafeasStubSettings> {
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
