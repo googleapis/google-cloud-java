60cc5b6514274cc1b4964bfb2db340385291a1ae
yoshi-automation <yoshi-automation@google.com>
2019-08-02 00:52:32 -0700

    Regenerate os-login client

--- google-cloud-clients/google-cloud-os-login/src/main/java/com/google/cloud/oslogin/v1/stub/OsLoginServiceStubSettings.java
+++ google-cloud-clients/google-cloud-os-login/src/main/java/com/google/cloud/oslogin/v1/stub/OsLoginServiceStubSettings.java
@@ -162,7 +162,8 @@ public class OsLoginServiceStubSettings extends StubSettings<OsLoginServiceStubS
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
