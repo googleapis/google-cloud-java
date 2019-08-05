7ab4e38ca87ae9541bcd0de78520d9c1a7159bdc
Jeff Ching <chingor@google.com>
2019-08-05 14:17:23 -0700

    Bulk client regen: set default maxInboundMessageSize to max int (#6022)
    * Regenerate errorreporting client
    
    * Regenerate firestore client
    
    * Regenerate gameservices client
    
    * Regenerate iamcredentials client
    
    * Regenerate iot client
    
    * Regenerate kms client
    
    * Regenerate language client
    
    * Regenerate logging client
    
    * Regenerate os-login client
    
    * Regenerate phishingprotection client
    
    * Regenerate recaptchaenterprise client
    
    * Regenerate redis client
    
    * Regenerate scheduler client
    
    * Regenerate securitycenter client
    
    * Regenerate speech client
    
    * Regenerate talent client
    
    * Regenerate tasks client
    
    * Regenerate trace client
    
    * Regenerate translate client
    
    * Regenerate vision client
    
    * Regenerate webrisk client
    
    * Regenerate websecurityscanner client
    
    * Regenerate grafeas client

--- java-game-servers/google-cloud-gameservices/src/main/java/com/google/cloud/gaming/v1alpha/stub/ScalingPoliciesServiceStubSettings.java
+++ java-game-servers/google-cloud-gameservices/src/main/java/com/google/cloud/gaming/v1alpha/stub/ScalingPoliciesServiceStubSettings.java
@@ -195,7 +195,8 @@ public class ScalingPoliciesServiceStubSettings
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
