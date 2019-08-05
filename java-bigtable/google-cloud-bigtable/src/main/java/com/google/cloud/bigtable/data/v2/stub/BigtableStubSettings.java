362f52da72a2a657dcfec164cf020f0b2e3a6b5e
Jeff Ching <chingor@google.com>
2019-08-05 14:17:41 -0700

    Bulk client regen: set default maxInboundMessageSize to max int (part 2) (#6023)
    * Regenerate asset client
    
    * Regenerate automl client
    
    * Regenerate bigquerydatatransfer client
    
    * Regenerate bigquerystorage client
    
    * Regenerate bigtable client
    
    * Regenerate container client
    
    * Regenerate containeranalysis client
    
    * Regenerate datacatalog client
    
    * Regenerate datalabeling client
    
    * Regenerate dataproc client
    
    * Regenerate dialogflow client
    
    * Regenerate dlp client

--- java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/BigtableStubSettings.java
+++ java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/BigtableStubSettings.java
@@ -147,7 +147,8 @@ public class BigtableStubSettings extends StubSettings<BigtableStubSettings> {
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
