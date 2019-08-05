7200909898065e2f7e7fc3958b2b85e9ac8760a6
Jeff Ching <chingor@google.com>
2019-08-05 14:17:23 -0700

    Bulk client regen: set default maxInboundMessageSize to max int (#6022)
    * Regenerate errorreporting client
    
    * Regenerate firestore client
    
    * Regenerate gameservices client
    
    * Regenerate iamcredentials client
    
    * Regenerate iot client
    
    * Regenerate kms client
    
    * Regenerate language client
    
    * Regenerate logging client
    
    * Regenerate os-login client
    
    * Regenerate phishingprotection client
    
    * Regenerate recaptchaenterprise client
    
    * Regenerate redis client
    
    * Regenerate scheduler client
    
    * Regenerate securitycenter client
    
    * Regenerate speech client
    
    * Regenerate talent client
    
    * Regenerate tasks client
    
    * Regenerate trace client
    
    * Regenerate translate client
    
    * Regenerate vision client
    
    * Regenerate webrisk client
    
    * Regenerate websecurityscanner client
    
    * Regenerate grafeas client

--- java-talent/google-cloud-talent/src/main/java/com/google/cloud/talent/v4beta1/stub/JobServiceStubSettings.java
+++ java-talent/google-cloud-talent/src/main/java/com/google/cloud/talent/v4beta1/stub/JobServiceStubSettings.java
@@ -229,7 +229,8 @@ public class JobServiceStubSettings extends StubSettings<JobServiceStubSettings>
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
