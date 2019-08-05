5ac06cef4258b1863c19e43218e019da3231d0fb
Jeff Ching <chingor@google.com>
2019-08-05 14:17:41 -0700

    Bulk client regen: set default maxInboundMessageSize to max int (part 2) (#6023)
    * Regenerate asset client
    
    * Regenerate automl client
    
    * Regenerate bigquerydatatransfer client
    
    * Regenerate bigquerystorage client
    
    * Regenerate bigtable client
    
    * Regenerate container client
    
    * Regenerate containeranalysis client
    
    * Regenerate datacatalog client
    
    * Regenerate datalabeling client
    
    * Regenerate dataproc client
    
    * Regenerate dialogflow client
    
    * Regenerate dlp client

--- java-dataproc/google-cloud-dataproc/src/main/java/com/google/cloud/dataproc/v1beta2/stub/JobControllerStubSettings.java
+++ java-dataproc/google-cloud-dataproc/src/main/java/com/google/cloud/dataproc/v1beta2/stub/JobControllerStubSettings.java
@@ -164,7 +164,8 @@ public class JobControllerStubSettings extends StubSettings<JobControllerStubSet
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
