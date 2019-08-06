c29e0884e5354d084c62f0958b2ea5612efe0971
yoshi-automation <yoshi-automation@google.com>
2019-08-06 00:50:42 -0700

    Regenerate monitoring client

--- google-cloud-clients/google-cloud-monitoring/src/main/java/com/google/cloud/monitoring/v3/stub/GroupServiceStubSettings.java
+++ google-cloud-clients/google-cloud-monitoring/src/main/java/com/google/cloud/monitoring/v3/stub/GroupServiceStubSettings.java
@@ -175,7 +175,8 @@ public class GroupServiceStubSettings extends StubSettings<GroupServiceStubSetti
 
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
-    return InstantiatingGrpcChannelProvider.newBuilder();
+    return InstantiatingGrpcChannelProvider.newBuilder()
+        .setMaxInboundMessageSize(Integer.MAX_VALUE);
   }
 
   public static TransportChannelProvider defaultTransportChannelProvider() {
