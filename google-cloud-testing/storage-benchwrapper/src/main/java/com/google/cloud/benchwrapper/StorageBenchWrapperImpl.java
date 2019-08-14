db88471f2b84cd4bd16e9e389f7daaedbca31945
Jean de Klerk <deklerk@google.com>
2019-08-14 14:41:40 -0600

    storage: add benchwrapper
    This adds a small gRPC server around the google-cloud-storage client library,
    which allows g3 benchmarking code to prod at the library and record
    measurements without needing to be written in Java / be in the same repository.
    
    Since g3 requires TLS on all connections, the server is TLS-ed with a dummy
    cert. These certs aren't intended to be private: the kokoro environment
    is the only place this gets run. (never in prod, etc)

---
+++ google-cloud-testing/storage-benchwrapper/src/main/java/com/google/cloud/benchwrapper/StorageBenchWrapperImpl.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.benchwrapper;
+
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import com.google.cloud.benchwrapper.StorageBenchWrapperGrpc.StorageBenchWrapperImplBase;
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.StorageOptions;
+
+class StorageBenchWrapperImpl extends StorageBenchWrapperImplBase {
+  private Storage client;
+
+  public StorageBenchWrapperImpl(String storageEmulatorHost) {
+    client = StorageOptions.newBuilder()
+        .setHost("http://" + storageEmulatorHost)
+        .build()
+        .getService();
+  }
+
+  public void write(ObjectWrite request, StreamObserver<EmptyResponse> responseObserver) {
+    System.out.println("write has been called");
+    EmptyResponse reply = EmptyResponse.newBuilder().build();
+    responseObserver.onNext(reply);
+    responseObserver.onCompleted();
+  }
+
+  public void read(ObjectRead request, StreamObserver<EmptyResponse> responseObserver) {
+    System.out.println("read has been called");
+
+    Blob blob = client.get(BlobId.of(request.getBucketName(), request.getObjectName()));
+
+    try (ReadChannel reader = blob.reader()) {
+      ByteBuffer bytes = ByteBuffer.allocate(64 * 1024);
+      while (reader.read(bytes) > 0) {
+        bytes.flip();
+        // do nothing with bytes
+        bytes.clear();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    EmptyResponse reply = EmptyResponse.newBuilder().build();
+    responseObserver.onNext(reply);
+    responseObserver.onCompleted();
+  }
+}
