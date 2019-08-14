ab193fa8e7803a5aaaaaaa3e3b218717a286967b
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2019-08-14 14:19:40 -0400

    Fix performance regression on Cloud Pub/Sub publish (#6076)
    * Mark ordering keys APIs public
    
    * Go back to calling publish on the exectuor, which is what we did before the updates to support ordering keys. Without it, we end up with a lot of publishes hanging for long periods of time.

--- google-cloud-clients/google-cloud-pubsub/src/main/java/com/google/cloud/pubsub/v1/Publisher.java
+++ google-cloud-clients/google-cloud-pubsub/src/main/java/com/google/cloud/pubsub/v1/Publisher.java
@@ -256,7 +256,13 @@ public class Publisher {
     if (!batchesToSend.isEmpty() && orderingKey.isEmpty()) {
       for (final OutstandingBatch batch : batchesToSend) {
         logger.log(Level.FINER, "Scheduling a batch for immediate sending.");
-        publishOutstandingBatch(batch);
+        executor.execute(
+            new Runnable() {
+              @Override
+              public void run() {
+                publishOutstandingBatch(batch);
+              }
+            });
       }
     }
 
