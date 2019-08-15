f7c4f12c33352932014bb277523056b2513ab4c2
Sebastian Schmidt <mrschmidt@google.com>
2019-08-15 11:25:36 -0700

    Expose Object serialization for internal consumers

---
+++ google-cloud-clients/google-cloud-firestore/src/main/java/com/google/cloud/firestore/Internal.java
@@ -0,0 +1,40 @@
+package com.google.cloud.firestore;
+
+import static com.google.cloud.firestore.UserDataConverter.NO_DELETES;
+
+import com.google.api.core.InternalApi;
+import com.google.cloud.Timestamp;
+import com.google.common.base.Preconditions;
+import com.google.firestore.v1.Document;
+import java.util.Map;
+
+@InternalApi
+public class Internal {
+  private FirestoreImpl firestore;
+
+  @InternalApi
+  public Internal(FirestoreImpl firestore) {
+    this.firestore = firestore;
+  }
+
+  @InternalApi
+  public DocumentSnapshot snapshotFromObject(String documentPath, Object pojo) {
+    DocumentReference documentReference = firestore.document(documentPath);
+    Object data = CustomClassMapper.convertToPlainJavaTypes(pojo);
+    Preconditions.checkArgument(
+        data instanceof Map, "Can't create a document from an array or primitive");
+    return DocumentSnapshot.fromObject(
+        firestore, documentReference, (Map<String, Object>) data, NO_DELETES);
+  }
+
+  @InternalApi
+  public DocumentSnapshot snapshotFromMap(String documentPath, Map<String, Object> data) {
+    DocumentReference documentReference = firestore.document(documentPath);
+    return DocumentSnapshot.fromObject(firestore, documentReference, data, NO_DELETES);
+  }
+
+  @InternalApi
+  public DocumentSnapshot snapshotFromProto(Timestamp readTime, Document document) {
+    return DocumentSnapshot.fromDocument(firestore, readTime, document);
+  }
+}
