ae8f739333aac902f89352c6413d7772155463be
BenWhitehead <BenWhitehead@users.noreply.github.com>
2019-08-26 13:42:45 -0400

    Review fixes

--- google-cloud-clients/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/readrows/ReadRowsMergingAcceptanceTest.java
+++ google-cloud-clients/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/readrows/ReadRowsMergingAcceptanceTest.java
@@ -16,7 +16,7 @@
 package com.google.cloud.bigtable.data.v2.stub.readrows;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertNotNull;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import com.google.api.gax.rpc.ServerStream;
 import com.google.api.gax.rpc.ServerStreamingCallable;
@@ -28,7 +28,6 @@ import com.google.cloud.bigtable.data.v2.models.Row;
 import com.google.cloud.bigtable.data.v2.models.RowCell;
 import com.google.cloud.bigtable.gaxx.testing.FakeStreamingApi.ServerStreamingStashCallable;
 import com.google.cloud.conformance.bigtable.v2.TestDefinition.ReadRowsTest;
-import com.google.cloud.conformance.bigtable.v2.TestDefinition.ReadRowsTest.Result;
 import com.google.cloud.conformance.bigtable.v2.TestDefinition.TestFile;
 import com.google.common.base.CaseFormat;
 import com.google.common.collect.Lists;
@@ -62,22 +61,23 @@ public class ReadRowsMergingAcceptanceTest {
     this.testCase = testData;
   }
 
+  // Each tuple consists of [testData: ReadRowsTest, junitName: String]
   @Parameters(name = "{1}")
   public static Collection<Object[]> data() throws IOException {
-    final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-    final InputStream dataJson = cl.getResourceAsStream(TEST_DATA_JSON_RESOURCE);
-    assertNotNull(
-        String.format("Unable to load test definition: %s", TEST_DATA_JSON_RESOURCE), dataJson);
+    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    InputStream dataJson = cl.getResourceAsStream(TEST_DATA_JSON_RESOURCE);
+    assertWithMessage("Unable to load test definition: %s", TEST_DATA_JSON_RESOURCE)
+        .that(dataJson).isNotNull();
 
-    final InputStreamReader reader = new InputStreamReader(dataJson);
-    final TestFile.Builder testBuilder = TestFile.newBuilder();
+    InputStreamReader reader = new InputStreamReader(dataJson);
+    TestFile.Builder testBuilder = TestFile.newBuilder();
     JsonFormat.parser().merge(reader, testBuilder);
-    final TestFile testDefinition = testBuilder.build();
+    TestFile testDefinition = testBuilder.build();
 
-    final List<ReadRowsTest> tests = testDefinition.getReadRowsTestsList();
-    final ArrayList<Object[]> data = new ArrayList<>(tests.size());
+    List<ReadRowsTest> tests = testDefinition.getReadRowsTestsList();
+    ArrayList<Object[]> data = new ArrayList<>(tests.size());
     for (ReadRowsTest test : tests) {
-      final String junitName =
+      String junitName =
           CaseFormat.LOWER_HYPHEN.to(
               CaseFormat.LOWER_CAMEL, test.getDescription().replace(" ", "-"));
       data.add(new Object[] {test, junitName});
@@ -106,14 +106,14 @@ public class ReadRowsMergingAcceptanceTest {
     ServerStream<Row> stream = mergingCallable.call(ReadRowsRequest.getDefaultInstance());
 
     // Read all of the rows and transform them into logical cells
-    List<Result> actualResults = Lists.newArrayList();
+    List<ReadRowsTest.Result> actualResults = Lists.newArrayList();
     Exception error = null;
 
     try {
       for (Row row : stream) {
         for (RowCell cell : row.getCells()) {
           actualResults.add(
-              Result.newBuilder()
+              ReadRowsTest.Result.newBuilder()
                   .setRowKeyBytes(row.getKey())
                   .setFamilyName(cell.getFamily())
                   .setQualifierBytes(cell.getQualifier())
@@ -140,16 +140,16 @@ public class ReadRowsMergingAcceptanceTest {
   }
 
   private static boolean expectsError(ReadRowsTest rrt) {
-    final List<Result> results = rrt.getResultsList();
+    List<ReadRowsTest.Result> results = rrt.getResultsList();
     return results != null && !results.isEmpty() && results.get(results.size() - 1).getError();
   }
 
-  private static List<Result> getNonExceptionResults(ReadRowsTest rrt) {
-    final List<Result> results = rrt.getResultsList();
+  private static List<ReadRowsTest.Result> getNonExceptionResults(ReadRowsTest rrt) {
+    List<ReadRowsTest.Result> results = rrt.getResultsList();
 
-    List<Result> response = new ArrayList<>();
+    List<ReadRowsTest.Result> response = new ArrayList<>();
     if (results != null) {
-      for (Result result : results) {
+      for (ReadRowsTest.Result result : results) {
         if (!result.getError()) {
           response.add(result);
         }
