040255008ceb507a5a8618fd540aa77253ec9abe
Frank Natividad <franknatividad@google.com>
2019-08-28 15:16:11 -0700

    Fix bpo test

--- google-cloud-clients/google-cloud-storage/src/main/java/com/google/cloud/storage/spi/v1/HttpStorageRpc.java
+++ google-cloud-clients/google-cloud-storage/src/main/java/com/google/cloud/storage/spi/v1/HttpStorageRpc.java
@@ -453,9 +453,10 @@ public class HttpStorageRpc implements StorageRpc {
     Scope scope = tracer.withSpan(span);
     try {
       String projection = Option.PROJECTION.getString(options);
-
+      System.out.println(bucket.getIamConfiguration());
       if (bucket.getIamConfiguration() != null
           && bucket.getIamConfiguration().getBucketPolicyOnly() != null
+          && bucket.getIamConfiguration().getBucketPolicyOnly().getEnabled() != null
           && bucket.getIamConfiguration().getBucketPolicyOnly().getEnabled()) {
         // If BucketPolicyOnly is enabled, patch calls will fail if ACL information is included in
         // the request
