0d8b8af527495703c5177390454576f57ee4aa5e
cwxie-google <51139172+cwxie-google@users.noreply.github.com>
2019-08-29 11:18:13 -0700

    Add quickstart for Real Time Feed API V1P2Beta1 (#1563)
    * asset: add quickstart for Real Time Feed API V1P2Beta1
    
    * address review comments
    
    * refine test
    
    * address comments
    
    * updated based on comments
    
    * fix presubmit error

---
+++ java-asset/samples/snippets/src/main/java/com/example/asset/CreateFeedExample.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.asset;
+
+// [START asset_quickstart_create_feed]
+import com.google.cloud.asset.v1.ProjectName;
+import com.google.cloud.asset.v1p2beta1.AssetServiceClient;
+import com.google.cloud.asset.v1p2beta1.CreateFeedRequest;
+import com.google.cloud.asset.v1p2beta1.Feed;
+import com.google.cloud.asset.v1p2beta1.FeedOutputConfig;
+import com.google.cloud.asset.v1p2beta1.PubsubDestination;
+import java.util.Arrays;
+
+public class CreateFeedExample {
+  // Create a feed
+  public static void createFeed(
+      String[] assetNames, String feedId, String topic, String projectId) throws Exception {
+    // String[] assetNames = {"MY_ASSET_NAME"}
+    // String FeedId = "MY_FEED_ID"
+    // String topic = "projects/[PROJECT_ID]/topics/[TOPIC_NAME]"
+    // String projectID = "MY_PROJECT_ID"
+    Feed feed = Feed.newBuilder()
+        .addAllAssetNames(Arrays.asList(assetNames))
+        .setFeedOutputConfig(
+          FeedOutputConfig.newBuilder().setPubsubDestination(
+              PubsubDestination.newBuilder().setTopic(topic).build()).build()).build();
+    CreateFeedRequest request = CreateFeedRequest.newBuilder()
+        .setParent(String.format(ProjectName.of(projectId).toString()))
+        .setFeedId(feedId)
+        .setFeed(feed)
+        .build();
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (AssetServiceClient client = AssetServiceClient.create()) {
+      Feed response = client.createFeed(request);
+      System.out.println("Feed created successfully: " + response.getName());
+    } catch (Exception e) {
+      System.out.println("Error during CreateFeed: \n" + e.toString());
+    }
+  }
+}
+// [END asset_quickstart_create_feed]
