b7b7aec49fc244592d63b3e56d595f5b7675a727
Seth Hollyman <shollyman@google.com>
2019-09-05 16:59:32 -0700

    run formatter to deal with lint

--- google-cloud-clients/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/InsertAllRequest.java
+++ google-cloud-clients/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/InsertAllRequest.java
@@ -52,10 +52,9 @@ public final class InsertAllRequest implements Serializable {
    * A Google Big Query row to be inserted into a table. Each {@code RowToInsert} has an associated
    * id used by BigQuery to detect duplicate insertion requests on a best-effort basis.
    *
-   * To ensure proper serialization of numeric data, it is recommended to supply values using a
-   * string-typed representation.  Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
+   * <p>To ensure proper serialization of numeric data, it is recommended to supply values using a
+   * string-typed representation. Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
    * must be provided as a base64 encoded string.
-
    *
    * <p>Example usage of creating a row to insert:
    *
@@ -130,8 +129,8 @@ public final class InsertAllRequest implements Serializable {
     /**
      * Creates a row to be inserted with associated id.
      *
-     * To ensure proper serialization of numeric data, it is recommended to supply values using a
-     * string-typed representation.  Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
+     * <p>To ensure proper serialization of numeric data, it is recommended to supply values using a
+     * string-typed representation. Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
      * must be provided as a base64 encoded string.
      *
      * @param id id of the row, used to identify duplicates
@@ -144,8 +143,8 @@ public final class InsertAllRequest implements Serializable {
     /**
      * Creates a row to be inserted without associated id.
      *
-     * To ensure proper serialization of numeric data, it is recommended to supply values using a
-     * string-typed representation.  Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
+     * <p>To ensure proper serialization of numeric data, it is recommended to supply values using a
+     * string-typed representation. Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
      * must be provided as a base64 encoded string.
      *
      * @param content the actual content of the row
@@ -190,8 +189,8 @@ public final class InsertAllRequest implements Serializable {
     /**
      * Adds a row to be inserted with associated id.
      *
-     * To ensure proper serialization of numeric data, it is recommended to supply values using a
-     * string-typed representation.  Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
+     * <p>To ensure proper serialization of numeric data, it is recommended to supply values using a
+     * string-typed representation. Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
      * must be provided as a base64 encoded string.
      *
      * <p>Example usage of adding a row with associated id:
@@ -218,8 +217,8 @@ public final class InsertAllRequest implements Serializable {
     /**
      * Adds a row to be inserted without an associated id.
      *
-     * To ensure proper serialization of numeric data, it is recommended to supply values using a
-     * string-typed representation.  Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
+     * <p>To ensure proper serialization of numeric data, it is recommended to supply values using a
+     * string-typed representation. Additionally, data for fields of {@link LegacySQLTypeName#Bytes}
      * must be provided as a base64 encoded string.
      *
      * <p>Example usage of adding a row without an associated id:
