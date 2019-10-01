5fefc317575cd162a4fd84ee522a8eaa6077a53f
Jeff Ching <chingor@google.com>
2019-10-01 14:05:55 -0700

    chore: initial commit

---
+++ java-recommender/README.md
@@ -0,0 +1,109 @@
+# Google Cloud Java Client for Google Cloud Recommender
+
+Java idiomatic client for [Google Cloud Recommender][api-reference].
+
+[![Kokoro CI][kokoro-badge-image]][kokoro-badge-link]
+[![Maven][maven-version-image]][maven-version-link]
+
+- [Product Documentation][product-docs]
+- [Client Library Documentation][javadocs]
+
+> Note: This client is a work-in-progress, and may occasionally
+> make backwards-incompatible changes.
+
+## Quickstart
+
+[//]: # ({x-version-update-start:google-cloud-recommender:released})
+If you are using Maven, add this to your pom.xml file
+```xml
+<dependency>
+  <groupId>com.google.cloud</groupId>
+  <artifactId>google-cloud-recommender</artifactId>
+  <version>0.102.0-beta</version>
+</dependency>
+```
+If you are using Gradle, add this to your dependencies
+```Groovy
+compile 'com.google.cloud:google-cloud-recommender:0.102.0-beta'
+```
+If you are using SBT, add this to your dependencies
+```Scala
+libraryDependencies += "com.google.cloud" % "google-cloud-recommender" % "0.102.0-beta"
+```
+[//]: # ({x-version-update-end})
+
+## Authentication
+
+See the [Authentication][authentication] section in the base directory's README.
+
+## About Google Cloud Recommender
+
+[Google Cloud Recommender][api-reference] is a suite of Machine Learning products.
+
+See the [Google Cloud Recommender client library docs][javadocs] to learn how to
+use this Google Cloud Recommender Client Library.
+
+## Getting Started
+
+### Prerequisites
+
+You will need a [Google Developers Console][developer-console] project with the
+Google Cloud Recommender API enabled. [Follow these instructions][create-project] to get your
+project set up. You will also need to set up the local development environment by
+[installing the Google Cloud SDK][cloud-sdk] and running the following commands in command line:
+`gcloud auth login` and `gcloud config set project [YOUR PROJECT ID]`.
+
+### Installation and setup
+
+You'll need to obtain the `google-cloud-recommender` library.  See the [Quickstart](#quickstart) section
+to add `google-cloud-recommender` as a dependency in your code.
+
+## Troubleshooting
+
+To get help, follow the instructions in the [shared Troubleshooting document][troubleshooting].
+
+## Transport
+
+Google Cloud Recommender uses gRPC for the transport layer.
+
+## Java Versions
+
+Java 7 or above is required for using this client.
+
+## Versioning
+
+This library follows [Semantic Versioning](http://semver.org/).
+
+It is currently in major version zero (``0.y.z``), which means that anything may change at any time
+and the public API should not be considered stable.
+
+## Contributing
+
+Contributions to this library are always welcome and highly encouraged.
+
+See [CONTRIBUTING.md][contributing] documentation for more information on how to get started.
+
+Please note that this project is released with a Contributor Code of Conduct. By participating in
+this project you agree to abide by its terms. See [Code of Conduct][code-of-conduct] for more
+information.
+
+## License
+
+Apache 2.0 - See [LICENSE][license] for more information.
+
+[api-reference]: https://cloud.google.com/recommendations-ai/docs/apis
+[product-docs]: https://cloud.google.com/recommendations/docs
+[javadocs]: https://googleapis.dev/java/java-recommender/latest
+[kokoro-badge-image]: http://storage.googleapis.com/cloud-devrel-public/java/badges/java-recommender/master.svg
+[kokoro-badge-link]: http://storage.googleapis.com/cloud-devrel-public/java/badges/java-recommender/master.html
+[maven-version-image]: https://img.shields.io/maven-central/v/com.google.cloud/google-cloud-recommender.svg
+[maven-version-link]: https://search.maven.org/search?q=g:com.google.cloud%20AND%20a:google-cloud-recommender&core=gav
+[authentication]: https://github.com/googleapis/google-cloud-java#authentication
+[developer-console]: https://console.developers.google.com/
+[create-project]: https://cloud.google.com/resource-manager/docs/creating-managing-projects
+[cloud-sdk]: https://cloud.google.com/sdk/
+[troubleshooting]: https://github.com/googleapis/google-cloud-common/blob/master/troubleshooting/readme.md#troubleshooting
+
+[contributing]: https://github.com/googleapis/java-recommender/blob/master/CONTRIBUTING.md
+[code-of-conduct]: https://github.com/googleapis/java-recommender/blob/master/CODE_OF_CONDUCT.md#contributor-code-of-conduct
+[license]: https://github.com/googleapis/java-recommender/blob/master/LICENSE
---
+++ java-recommender/google-cloud-recommender/src/test/java/com/google/cloud/recommender/v1beta1/RecommenderClientTest.java
@@ -0,0 +1,348 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.recommender.v1beta1;
+
+import static com.google.cloud.recommender.v1beta1.RecommenderClient.ListRecommendationsPagedResponse;
+
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GaxGrpcProperties;
+import com.google.api.gax.grpc.testing.LocalChannelProvider;
+import com.google.api.gax.grpc.testing.MockGrpcService;
+import com.google.api.gax.grpc.testing.MockServiceHelper;
+import com.google.api.gax.rpc.ApiClientHeaderProvider;
+import com.google.api.gax.rpc.InvalidArgumentException;
+import com.google.common.collect.Lists;
+import com.google.protobuf.AbstractMessage;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@javax.annotation.Generated("by GAPIC")
+public class RecommenderClientTest {
+  private static MockRecommender mockRecommender;
+  private static MockServiceHelper serviceHelper;
+  private RecommenderClient client;
+  private LocalChannelProvider channelProvider;
+
+  @BeforeClass
+  public static void startStaticServer() {
+    mockRecommender = new MockRecommender();
+    serviceHelper =
+        new MockServiceHelper("in-process-1", Arrays.<MockGrpcService>asList(mockRecommender));
+    serviceHelper.start();
+  }
+
+  @AfterClass
+  public static void stopServer() {
+    serviceHelper.stop();
+  }
+
+  @Before
+  public void setUp() throws IOException {
+    serviceHelper.reset();
+    channelProvider = serviceHelper.createChannelProvider();
+    RecommenderSettings settings =
+        RecommenderSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .build();
+    client = RecommenderClient.create(settings);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    client.close();
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void listRecommendationsTest() {
+    String nextPageToken = "";
+    Recommendation recommendationsElement = Recommendation.newBuilder().build();
+    List<Recommendation> recommendations = Arrays.asList(recommendationsElement);
+    ListRecommendationsResponse expectedResponse =
+        ListRecommendationsResponse.newBuilder()
+            .setNextPageToken(nextPageToken)
+            .addAllRecommendations(recommendations)
+            .build();
+    mockRecommender.addResponse(expectedResponse);
+
+    String formattedParent =
+        RecommenderClient.formatRecommenderName("[PROJECT]", "[LOCATION]", "[RECOMMENDER]");
+
+    ListRecommendationsPagedResponse pagedListResponse =
+        client.listRecommendations(formattedParent);
+
+    List<Recommendation> resources = Lists.newArrayList(pagedListResponse.iterateAll());
+    Assert.assertEquals(1, resources.size());
+    Assert.assertEquals(expectedResponse.getRecommendationsList().get(0), resources.get(0));
+
+    List<AbstractMessage> actualRequests = mockRecommender.getRequests();
+    Assert.assertEquals(1, actualRequests.size());
+    ListRecommendationsRequest actualRequest = (ListRecommendationsRequest) actualRequests.get(0);
+
+    Assert.assertEquals(formattedParent, actualRequest.getParent());
+    Assert.assertTrue(
+        channelProvider.isHeaderSent(
+            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
+            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void listRecommendationsExceptionTest() throws Exception {
+    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
+    mockRecommender.addException(exception);
+
+    try {
+      String formattedParent =
+          RecommenderClient.formatRecommenderName("[PROJECT]", "[LOCATION]", "[RECOMMENDER]");
+
+      client.listRecommendations(formattedParent);
+      Assert.fail("No exception raised");
+    } catch (InvalidArgumentException e) {
+      // Expected exception
+    }
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void getRecommendationTest() {
+    String name2 = "name2-1052831874";
+    String description = "description-1724546052";
+    String recommenderSubtype = "recommenderSubtype-1488504412";
+    String etag = "etag3123477";
+    Recommendation expectedResponse =
+        Recommendation.newBuilder()
+            .setName(name2)
+            .setDescription(description)
+            .setRecommenderSubtype(recommenderSubtype)
+            .setEtag(etag)
+            .build();
+    mockRecommender.addResponse(expectedResponse);
+
+    String formattedName =
+        RecommenderClient.formatRecommendationName(
+            "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+
+    Recommendation actualResponse = client.getRecommendation(formattedName);
+    Assert.assertEquals(expectedResponse, actualResponse);
+
+    List<AbstractMessage> actualRequests = mockRecommender.getRequests();
+    Assert.assertEquals(1, actualRequests.size());
+    GetRecommendationRequest actualRequest = (GetRecommendationRequest) actualRequests.get(0);
+
+    Assert.assertEquals(formattedName, actualRequest.getName());
+    Assert.assertTrue(
+        channelProvider.isHeaderSent(
+            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
+            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void getRecommendationExceptionTest() throws Exception {
+    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
+    mockRecommender.addException(exception);
+
+    try {
+      String formattedName =
+          RecommenderClient.formatRecommendationName(
+              "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+
+      client.getRecommendation(formattedName);
+      Assert.fail("No exception raised");
+    } catch (InvalidArgumentException e) {
+      // Expected exception
+    }
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void markRecommendationClaimedTest() {
+    String name2 = "name2-1052831874";
+    String description = "description-1724546052";
+    String recommenderSubtype = "recommenderSubtype-1488504412";
+    String etag2 = "etag2-1293302904";
+    Recommendation expectedResponse =
+        Recommendation.newBuilder()
+            .setName(name2)
+            .setDescription(description)
+            .setRecommenderSubtype(recommenderSubtype)
+            .setEtag(etag2)
+            .build();
+    mockRecommender.addResponse(expectedResponse);
+
+    String formattedName =
+        RecommenderClient.formatRecommendationName(
+            "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+    String etag = "etag3123477";
+
+    Recommendation actualResponse = client.markRecommendationClaimed(formattedName, etag);
+    Assert.assertEquals(expectedResponse, actualResponse);
+
+    List<AbstractMessage> actualRequests = mockRecommender.getRequests();
+    Assert.assertEquals(1, actualRequests.size());
+    MarkRecommendationClaimedRequest actualRequest =
+        (MarkRecommendationClaimedRequest) actualRequests.get(0);
+
+    Assert.assertEquals(formattedName, actualRequest.getName());
+    Assert.assertEquals(etag, actualRequest.getEtag());
+    Assert.assertTrue(
+        channelProvider.isHeaderSent(
+            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
+            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void markRecommendationClaimedExceptionTest() throws Exception {
+    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
+    mockRecommender.addException(exception);
+
+    try {
+      String formattedName =
+          RecommenderClient.formatRecommendationName(
+              "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+      String etag = "etag3123477";
+
+      client.markRecommendationClaimed(formattedName, etag);
+      Assert.fail("No exception raised");
+    } catch (InvalidArgumentException e) {
+      // Expected exception
+    }
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void markRecommendationSucceededTest() {
+    String name2 = "name2-1052831874";
+    String description = "description-1724546052";
+    String recommenderSubtype = "recommenderSubtype-1488504412";
+    String etag2 = "etag2-1293302904";
+    Recommendation expectedResponse =
+        Recommendation.newBuilder()
+            .setName(name2)
+            .setDescription(description)
+            .setRecommenderSubtype(recommenderSubtype)
+            .setEtag(etag2)
+            .build();
+    mockRecommender.addResponse(expectedResponse);
+
+    String formattedName =
+        RecommenderClient.formatRecommendationName(
+            "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+    String etag = "etag3123477";
+
+    Recommendation actualResponse = client.markRecommendationSucceeded(formattedName, etag);
+    Assert.assertEquals(expectedResponse, actualResponse);
+
+    List<AbstractMessage> actualRequests = mockRecommender.getRequests();
+    Assert.assertEquals(1, actualRequests.size());
+    MarkRecommendationSucceededRequest actualRequest =
+        (MarkRecommendationSucceededRequest) actualRequests.get(0);
+
+    Assert.assertEquals(formattedName, actualRequest.getName());
+    Assert.assertEquals(etag, actualRequest.getEtag());
+    Assert.assertTrue(
+        channelProvider.isHeaderSent(
+            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
+            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void markRecommendationSucceededExceptionTest() throws Exception {
+    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
+    mockRecommender.addException(exception);
+
+    try {
+      String formattedName =
+          RecommenderClient.formatRecommendationName(
+              "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+      String etag = "etag3123477";
+
+      client.markRecommendationSucceeded(formattedName, etag);
+      Assert.fail("No exception raised");
+    } catch (InvalidArgumentException e) {
+      // Expected exception
+    }
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void markRecommendationFailedTest() {
+    String name2 = "name2-1052831874";
+    String description = "description-1724546052";
+    String recommenderSubtype = "recommenderSubtype-1488504412";
+    String etag2 = "etag2-1293302904";
+    Recommendation expectedResponse =
+        Recommendation.newBuilder()
+            .setName(name2)
+            .setDescription(description)
+            .setRecommenderSubtype(recommenderSubtype)
+            .setEtag(etag2)
+            .build();
+    mockRecommender.addResponse(expectedResponse);
+
+    String formattedName =
+        RecommenderClient.formatRecommendationName(
+            "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+    String etag = "etag3123477";
+
+    Recommendation actualResponse = client.markRecommendationFailed(formattedName, etag);
+    Assert.assertEquals(expectedResponse, actualResponse);
+
+    List<AbstractMessage> actualRequests = mockRecommender.getRequests();
+    Assert.assertEquals(1, actualRequests.size());
+    MarkRecommendationFailedRequest actualRequest =
+        (MarkRecommendationFailedRequest) actualRequests.get(0);
+
+    Assert.assertEquals(formattedName, actualRequest.getName());
+    Assert.assertEquals(etag, actualRequest.getEtag());
+    Assert.assertTrue(
+        channelProvider.isHeaderSent(
+            ApiClientHeaderProvider.getDefaultApiClientHeaderKey(),
+            GaxGrpcProperties.getDefaultApiClientHeaderPattern()));
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void markRecommendationFailedExceptionTest() throws Exception {
+    StatusRuntimeException exception = new StatusRuntimeException(Status.INVALID_ARGUMENT);
+    mockRecommender.addException(exception);
+
+    try {
+      String formattedName =
+          RecommenderClient.formatRecommendationName(
+              "[PROJECT]", "[LOCATION]", "[RECOMMENDER]", "[RECOMMENDATION]");
+      String etag = "etag3123477";
+
+      client.markRecommendationFailed(formattedName, etag);
+      Assert.fail("No exception raised");
+    } catch (InvalidArgumentException e) {
+      // Expected exception
+    }
+  }
+}
