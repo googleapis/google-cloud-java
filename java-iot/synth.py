06949d9939537d6759c568627e8bbf4e9e0b1786
Jeff Ching <chingor@google.com>
2019-10-15 15:21:28 -0700

    feat: make repo releasable, add parent/bom (#1)
    * feat: make repo releasable, add parent/bom
    
    * fix: restore client changes
    
    * fix: dependency declaration

--- java-iot/synth.py
+++ java-iot/synth.py
@@ -25,16 +25,15 @@ versions = ['v1']
 config_pattern = '/google/cloud/iot/artman_cloudiot.yaml'
 
 for version in versions:
-  library = gapic.java_library(
-      service=service,
-      version=version,
-      config_path=config_pattern.format(version=version),
-      artman_output_name='')
+    java.gapic_library(
+        service=service,
+        version=version,
+        config_pattern=config_pattern,
+        gapic=gapic,
+    )
 
-  s.copy(library / f'gapic-google-cloud-{service}-{version}/src', 'src')
-  s.copy(library / f'grpc-google-cloud-{service}-{version}/src', f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
-  s.copy(library / f'proto-google-cloud-{service}-{version}/src', f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
-
-  java.format_code('./src')
-  java.format_code(f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
-  java.format_code(f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
+common_templates = gcp.CommonTemplates()
+templates = common_templates.java_library()
+s.copy(templates, excludes=[
+  'README.md',
+])
