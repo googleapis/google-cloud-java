b3d4d0763130c57fbc786228436f0bf754c298ed
Noah Negrey <nnegrey@users.noreply.github.com>
2020-01-07 11:49:42 -0700

    samples: automl: use fake model ids for deployment testing so that tests do no… (#1929)
    * automl: use fake model ids for deployment testing so that tests do not take a long time to run
    
    * remove bom from automl until bom is released with v1 of client library
    
    * Fix typo
    
    * Remove score threshold

--- java-automl/samples/snippets/src/test/java/com/example/automl/DeployModelTest.java
+++ java-automl/samples/snippets/src/test/java/com/example/automl/DeployModelTest.java
@@ -27,32 +27,27 @@ import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-// Tests for Automl natural language sentiment analysis models.
 @RunWith(JUnit4.class)
-@Ignore
-public class LanguageSentimentAnalysisModelManagementIT {
+public class DeployModelTest {
   private static final String PROJECT_ID = System.getenv("AUTOML_PROJECT_ID");
-  private static final String MODEL_ID = System.getenv("SENTIMENT_ANALYSIS_MODEL_ID");
+  private static final String MODEL_ID = "TEN0000000000000000000";
   private ByteArrayOutputStream bout;
   private PrintStream out;
 
   private static void requireEnvVar(String varName) {
     assertNotNull(
-            System.getenv(varName),
-            "Environment variable '%s' is required to perform these tests.".format(varName)
-    );
+        System.getenv(varName),
+        "Environment variable '%s' is required to perform these tests.".format(varName));
   }
 
   @BeforeClass
   public static void checkRequirements() {
     requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
     requireEnvVar("AUTOML_PROJECT_ID");
-    requireEnvVar("SENTIMENT_ANALYSIS_MODEL_ID");
   }
 
   @Before
@@ -68,14 +63,16 @@ public class LanguageSentimentAnalysisModelManagementIT {
   }
 
   @Test
-  public void testDeployUndeployModel()
-      throws IOException, ExecutionException, InterruptedException {
-    UndeployModel.undeployModel(PROJECT_ID, MODEL_ID);
-    String got = bout.toString();
-    assertThat(got).contains("Model undeployment finished");
-
-    DeployModel.deployModel(PROJECT_ID, MODEL_ID);
-    got = bout.toString();
-    assertThat(got).contains("Model deployment finished");
+  public void testDeployModel() {
+    // As model deployment can take a long time, instead try to deploy a
+    // nonexistent model and confirm that the model was not found, but other
+    // elements of the request were valid.
+    try {
+      DeployModel.deployModel(PROJECT_ID, MODEL_ID);
+      String got = bout.toString();
+      assertThat(got).contains("The model does not exist");
+    } catch (IOException | ExecutionException | InterruptedException e) {
+      assertThat(e.getMessage()).contains("The model does not exist");
+    }
   }
 }
--- java-automl/samples/snippets/src/test/java/com/example/automl/VisionObjectDetectionDeployModelNodeCountTest.java
+++ java-automl/samples/snippets/src/test/java/com/example/automl/VisionObjectDetectionDeployModelNodeCountTest.java
@@ -27,32 +27,27 @@ import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-// Tests for Automl natural language entity extraction models.
 @RunWith(JUnit4.class)
-@Ignore
-public class LanguageEntityExtractionModelManagementIT {
+public class VisionObjectDetectionDeployModelNodeCountTest {
   private static final String PROJECT_ID = System.getenv("AUTOML_PROJECT_ID");
-  private static final String MODEL_ID = System.getenv("ENTITY_EXTRACTION_MODEL_ID");
+  private static final String MODEL_ID = "0000000000000000000000";
   private ByteArrayOutputStream bout;
   private PrintStream out;
 
   private static void requireEnvVar(String varName) {
     assertNotNull(
-            System.getenv(varName),
-            "Environment variable '%s' is required to perform these tests.".format(varName)
-    );
+        System.getenv(varName),
+        "Environment variable '%s' is required to perform these tests.".format(varName));
   }
 
   @BeforeClass
   public static void checkRequirements() {
     requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
     requireEnvVar("AUTOML_PROJECT_ID");
-    requireEnvVar("ENTITY_EXTRACTION_MODEL_ID");
   }
 
   @Before
@@ -68,14 +63,17 @@ public class LanguageEntityExtractionModelManagementIT {
   }
 
   @Test
-  public void testDeployUndeployModel()
-      throws IOException, ExecutionException, InterruptedException {
-    UndeployModel.undeployModel(PROJECT_ID, MODEL_ID);
-    String got = bout.toString();
-    assertThat(got).contains("Model undeployment finished");
-
-    DeployModel.deployModel(PROJECT_ID, MODEL_ID);
-    got = bout.toString();
-    assertThat(got).contains("Model deployment finished");
+  public void testDeployModelWithNodeCount() {
+    // As model deployment can take a long time, instead try to deploy a
+    // nonexistent model and confirm that the model was not found, but other
+    // elements of the request were valid.
+    try {
+      VisionObjectDetectionDeployModelNodeCount.visionObjectDetectionDeployModelNodeCount(
+          PROJECT_ID, MODEL_ID);
+      String got = bout.toString();
+      assertThat(got).contains("The model does not exist");
+    } catch (IOException | ExecutionException | InterruptedException e) {
+      assertThat(e.getMessage()).contains("The model does not exist");
+    }
   }
 }
