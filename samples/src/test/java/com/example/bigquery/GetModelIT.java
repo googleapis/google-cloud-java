a03670b3ba19c7805712f09754435e422228abd6
Stephanie Wang <stephaniewang526@users.noreply.github.com>
2020-01-27 15:32:56 -0500

    feat: new sample - Get Model (#124)
    * feat: add new sample - Tables: Relax column query append
    
    * nit
    
    * update based on comments
    
    * update exception handling
    
    * refactor
    
    * update base on comments
    
    * code refactoring
    
    * fix build errors
    
    * add debugging statements
    
    * remove unused exception
    
    * feat: new sample - Models: Get a model resource
    
    * feat: new sample - Models: Get a model resource
    
    * fix build error

---
+++ samples/src/test/java/com/example/bigquery/GetModelIT.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquery;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GetModelIT {
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+
+  private static final String BIGQUERY_DATASET_NAME = System.getenv("BIGQUERY_DATASET_NAME");
+  private static final String BIGQUERY_MODEL_NAME = System.getenv("BIGQUERY_MODEL_NAME");
+
+  private static void requireEnvVar(String varName) {
+    assertNotNull(
+        "Environment variable " + varName + " is required to perform these tests.",
+        System.getenv(varName));
+  }
+
+  @BeforeClass
+  public static void checkRequirements() {
+    requireEnvVar("BIGQUERY_DATASET_NAME");
+    requireEnvVar("BIGQUERY_MODEL_NAME");
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    System.setOut(null);
+  }
+
+  @Test
+  public void getModel() {
+    GetModel.getModel(BIGQUERY_DATASET_NAME, BIGQUERY_MODEL_NAME);
+    assertThat(bout.toString()).contains("Successfully retrieved model");
+  }
+}
