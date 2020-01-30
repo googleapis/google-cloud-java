949615823c5bb12ef749639d7337f6341973ddbf
Martin Mladenovski <mmladenovski@users.noreply.github.com>
2020-01-30 13:02:43 -0800

    feat: add an enhanced layer for BigQuery Storage v1beta2 client (#48)
    * Create a shim layer for the BigQuery Storage API v1beta2.
    
    This change adds a simple shim layer to the BigQuery Read API client.
    
    It is modeled after the shim layer in the BigQuery Storage API v1beta1, where the parameters are simple pass-throughs to the underlying gRPC client stub.
    
    The shim currently does nothing but read default values from the base
    client layer and pass them back through.
    
    * Add a resumption strategy for v1beta2 ReadRows.
    
    This change modifies the configuration for the v1beta2 ReadRows API to add a resumption strategy. The resumption strategy will allow the connection to be resumed transparently in the case of transient errors.
    
    * Address feedback about "pass-through" comments.
    
    * Formatting files with mvn com.coveo:fmt-maven-plugin:format

---
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta2/stub/EnhancedBigQueryReadStub.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery.storage.v1beta2.stub;
+
+import com.google.api.core.InternalApi;
+import com.google.api.gax.core.BackgroundResource;
+import com.google.api.gax.rpc.ClientContext;
+import com.google.api.gax.rpc.ServerStreamingCallable;
+import com.google.api.gax.rpc.UnaryCallable;
+import com.google.cloud.bigquery.storage.v1beta2.CreateReadSessionRequest;
+import com.google.cloud.bigquery.storage.v1beta2.ReadRowsRequest;
+import com.google.cloud.bigquery.storage.v1beta2.ReadRowsResponse;
+import com.google.cloud.bigquery.storage.v1beta2.ReadSession;
+import com.google.cloud.bigquery.storage.v1beta2.SplitReadStreamRequest;
+import com.google.cloud.bigquery.storage.v1beta2.SplitReadStreamResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Enhanced stub class for BigQuery Storage API.
+ *
+ * <p>This class is for advanced usage and reflects the underlying API directly.
+ */
+public class EnhancedBigQueryReadStub implements BackgroundResource {
+  private final GrpcBigQueryReadStub stub;
+
+  public static EnhancedBigQueryReadStub create(EnhancedBigQueryReadStubSettings settings)
+      throws IOException {
+    // Configure the base settings.
+    BigQueryReadStubSettings.Builder baseSettingsBuilder =
+        BigQueryReadStubSettings.newBuilder()
+            .setTransportChannelProvider(settings.getTransportChannelProvider())
+            .setEndpoint(settings.getEndpoint())
+            .setHeaderProvider(settings.getHeaderProvider())
+            .setCredentialsProvider(settings.getCredentialsProvider())
+            .setStreamWatchdogCheckInterval(settings.getStreamWatchdogCheckInterval())
+            .setStreamWatchdogProvider(settings.getStreamWatchdogProvider());
+
+    baseSettingsBuilder
+        .createReadSessionSettings()
+        .setRetryableCodes(settings.createReadSessionSettings().getRetryableCodes())
+        .setRetrySettings(settings.createReadSessionSettings().getRetrySettings());
+
+    baseSettingsBuilder
+        .readRowsSettings()
+        .setRetryableCodes(settings.readRowsSettings().getRetryableCodes())
+        .setRetrySettings(settings.readRowsSettings().getRetrySettings())
+        .setResumptionStrategy(settings.readRowsSettings().getResumptionStrategy())
+        .setIdleTimeout(settings.readRowsSettings().getIdleTimeout());
+
+    baseSettingsBuilder
+        .splitReadStreamSettings()
+        .setRetryableCodes(settings.splitReadStreamSettings().getRetryableCodes())
+        .setRetrySettings(settings.splitReadStreamSettings().getRetrySettings());
+
+    BigQueryReadStubSettings baseSettings = baseSettingsBuilder.build();
+    ClientContext clientContext = ClientContext.create(baseSettings);
+    GrpcBigQueryReadStub stub = new GrpcBigQueryReadStub(baseSettings, clientContext);
+    return new EnhancedBigQueryReadStub(stub);
+  }
+
+  @InternalApi("Visible for testing")
+  EnhancedBigQueryReadStub(GrpcBigQueryReadStub stub) {
+    this.stub = stub;
+  }
+
+  public UnaryCallable<CreateReadSessionRequest, ReadSession> createReadSessionCallable() {
+    return stub.createReadSessionCallable();
+  }
+
+  public ServerStreamingCallable<ReadRowsRequest, ReadRowsResponse> readRowsCallable() {
+    return stub.readRowsCallable();
+  }
+
+  public UnaryCallable<SplitReadStreamRequest, SplitReadStreamResponse> splitReadStreamCallable() {
+    return stub.splitReadStreamCallable();
+  }
+
+  @Override
+  public void close() {
+    stub.close();
+  }
+
+  @Override
+  public void shutdown() {
+    stub.shutdown();
+  }
+
+  @Override
+  public boolean isShutdown() {
+    return stub.isShutdown();
+  }
+
+  @Override
+  public boolean isTerminated() {
+    return stub.isTerminated();
+  }
+
+  @Override
+  public void shutdownNow() {
+    stub.shutdownNow();
+  }
+
+  @Override
+  public boolean awaitTermination(long duration, TimeUnit unit) throws InterruptedException {
+    return stub.awaitTermination(duration, unit);
+  }
+}
