1291b567476e979228ec6d5da568bf22f6ef7326
Jeff Ching <chingor@google.com>
2020-02-04 09:17:28 -0800

    chore: update repo-metadata and regenerate README from templates (#67)
    * chore: update repo-metadata and regenerate README from templates
    
    * fix javadoc url

--- java-cloudbuild/synth.py
+++ java-cloudbuild/synth.py
@@ -43,9 +43,4 @@ for version in versions:
     java.format_code(f'grpc-google-cloud-build-{version}/src')
     java.format_code(f'proto-google-cloud-build-{version}/src')
 
-common_templates = gcp.CommonTemplates()
-templates = common_templates.java_library()
-s.copy(templates, excludes=[
-  'README.md',
-])
-
+java.common_templates()
