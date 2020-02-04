252f68e371d887fa5533e5f1bfb46b982857d306
Jeff Ching <chingor@google.com>
2020-02-04 09:13:23 -0800

    chore: update repo-metadata and regenerate README from templates (#54)

--- java-securitycenter/synth.py
+++ java-securitycenter/synth.py
@@ -25,15 +25,11 @@ versions = ['v1beta1', 'v1']
 config_pattern = '/google/cloud/securitycenter/artman_securitycenter_{version}.yaml'
 
 for version in versions:
-    java.gapic_library(
-        service=service,
-        version=version,
-        config_pattern=config_pattern,
-        gapic=gapic,
-    )
+  java.gapic_library(
+    service=service,
+    version=version,
+    config_pattern=config_pattern,
+    gapic=gapic,
+  )
 
-common_templates = gcp.CommonTemplates()
-templates = common_templates.java_library()
-s.copy(templates, excludes=[
-  'README.md',
-])
+java.common_templates()
