28c61b7aa8ccd9c843d342ba2b71d7261a23f773
Tim Hostetler <thostet@google.com>
2020-02-14 13:26:20 -0800

    docs: Add tests for securitycenter Notification snippets
    Topic name is exposed as I'm unsure how to set environment variables
    here. However, these variables are already publically available in other github
    repos.

---
+++ google-cloud-examples/src/test/java/com/google/cloud/examples/securitycenter/snippets/v1p1beta1/ITNotificationConfigSnippets.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.examples.securitycenter.snippets.v1p1beta1;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/** Smoke tests for {@link NotificationConfigSnippets} */
+public class ITNotificationConfigSnippets {
+
+  @Test
+  public void testCreateNotificationConfig() {
+    assertNotNull(
+        NotificationConfigSnippets.createNotificationConfig(
+            getOrganizationId(), getPubsubTopic(), "java-notification-config-create"));
+
+    deleteConfig("java-notification-config-create");
+  }
+
+  @Test
+  public void testDeleteNotificationConfig() {
+    createConfig("java-notification-config-delete");
+
+    assertTrue(
+        NotificationConfigSnippets.deleteNotificationConfig(
+            getOrganizationId(), "java-notification-config-delete"));
+  }
+
+  @Test
+  public void testListNotificationConfig() {
+    createConfig("java-notification-config-list");
+
+    assertNotNull(NotificationConfigSnippets.listNotificationConfigs(getOrganizationId()));
+
+    deleteConfig("java-notification-config-list");
+  }
+
+  @Test
+  public void testGetNotificationConfig() {
+    createConfig("java-notification-config-get");
+
+    assertNotNull(
+        NotificationConfigSnippets.getNotificationConfig(
+            getOrganizationId(), "java-notification-config-get"));
+
+    deleteConfig("java-notification-config-get");
+  }
+
+  @Test
+  public void testUpdateNotificationConfig() {
+    createConfig("java-notification-config-update");
+
+    assertNotNull(
+        NotificationConfigSnippets.updateNotificationConfig(
+            getOrganizationId(), "java-notification-config-update", getPubsubTopic()));
+
+    deleteConfig("java-notification-config-update");
+  }
+
+  private static void createConfig(String configId) {
+    NotificationConfigSnippets.createNotificationConfig(
+        getOrganizationId(), getPubsubTopic(), configId);
+  }
+
+  private static void deleteConfig(String configId) {
+    assertTrue(NotificationConfigSnippets.deleteNotificationConfig(getOrganizationId(), configId));
+  }
+
+  private static String getOrganizationId() {
+    return System.getenv("GCLOUD_ORGANIZATION");
+  }
+
+  private static String getPubsubTopic() {
+    return "projects/project-a-id/topics/notifications-sample-topic";
+  }
+}
