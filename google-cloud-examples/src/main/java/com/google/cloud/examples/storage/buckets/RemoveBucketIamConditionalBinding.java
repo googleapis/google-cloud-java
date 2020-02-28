0bab6a1cb748b3e627358a262a44b75ac3f9c78c
Frank Natividad <franknatividad@google.com>
2020-02-28 14:18:33 -0800

    format

--- google-cloud-examples/src/main/java/com/google/cloud/examples/storage/buckets/RemoveBucketIamConditionalBinding.java
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/storage/buckets/RemoveBucketIamConditionalBinding.java
@@ -6,49 +6,49 @@ import com.google.cloud.Condition;
 import com.google.cloud.Policy;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class RemoveBucketIamConditionalBinding {
-    /** Example of removing a conditional binding to the Bucket-level IAM */
-    public static void removeBucketIamConditionalBinding(String projectId, String bucketName) {
-        // The ID of your GCP project
-        // String projectId = "your-project-id";
+  /** Example of removing a conditional binding to the Bucket-level IAM */
+  public static void removeBucketIamConditionalBinding(String projectId, String bucketName) {
+    // The ID of your GCP project
+    // String projectId = "your-project-id";
 
-        // The ID of your GCS bucket
-        // String bucketName = "your-unique-bucket-name";
+    // The ID of your GCS bucket
+    // String bucketName = "your-unique-bucket-name";
 
-        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-        Policy originalPolicy =
-                storage.getIamPolicy(bucketName, Storage.BucketSourceOption.requestedPolicyVersion(3));
+    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    Policy originalPolicy =
+        storage.getIamPolicy(bucketName, Storage.BucketSourceOption.requestedPolicyVersion(3));
 
-        String role = "roles/storage.objectViewer";
+    String role = "roles/storage.objectViewer";
 
-        List<Binding> bindings = new ArrayList(originalPolicy.getBindingsList());
-        Condition.Builder conditionBuilder = Condition.newBuilder();
-        conditionBuilder.setTitle("Title");
-        conditionBuilder.setDescription("Description");
-        conditionBuilder.setExpression(
-                "resource.name.startsWith(\"projects/_/buckets/bucket-name/objects/prefix-a-\")");
+    List<Binding> bindings = new ArrayList(originalPolicy.getBindingsList());
+    Condition.Builder conditionBuilder = Condition.newBuilder();
+    conditionBuilder.setTitle("Title");
+    conditionBuilder.setDescription("Description");
+    conditionBuilder.setExpression(
+        "resource.name.startsWith(\"projects/_/buckets/bucket-name/objects/prefix-a-\")");
 
-        Iterator iterator = bindings.iterator();
-        while (iterator.hasNext()) {
-            Binding binding = (Binding)iterator.next();
-            if (binding.getRole().equals(role) && binding.getCondition().equals(conditionBuilder.build())) {
-                iterator.remove();
-                break;
-            }
-        }
-
-        Policy updatedPolicy =
-                storage.setIamPolicy(
-                        bucketName, originalPolicy.toBuilder().setBindings(bindings).setVersion(3).build());
-
-        if(bindings.size() > updatedPolicy.getBindingsList().size()) {
-            System.out.println("Conditional Binding was removed.");
-        }
+    Iterator iterator = bindings.iterator();
+    while (iterator.hasNext()) {
+      Binding binding = (Binding) iterator.next();
+      if (binding.getRole().equals(role)
+          && binding.getCondition().equals(conditionBuilder.build())) {
+        iterator.remove();
+        break;
+      }
     }
+
+    Policy updatedPolicy =
+        storage.setIamPolicy(
+            bucketName, originalPolicy.toBuilder().setBindings(bindings).setVersion(3).build());
+
+    if (bindings.size() > updatedPolicy.getBindingsList().size()) {
+      System.out.println("Conditional Binding was removed.");
+    }
+  }
 }
 // [END storage_remove_bucket_conditional_iam_binding]
