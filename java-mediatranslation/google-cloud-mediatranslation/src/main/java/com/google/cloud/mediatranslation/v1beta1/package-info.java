fca818de53d8352cfac69567ca76d0747812c3cc
Jeff Ching <chingor@google.com>
2020-03-18 07:38:25 -0700

    feat: initial generation

---
+++ java-mediatranslation/google-cloud-mediatranslation/src/main/java/com/google/cloud/mediatranslation/v1beta1/package-info.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A client to Media Translation API.
+ *
+ * <p>The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>============================== SpeechTranslationServiceClient ==============================
+ *
+ * <p>Service Description: Provides translation from/to media types.
+ *
+ * <p>Sample for SpeechTranslationServiceClient:
+ *
+ * <pre>
+ * <code>
+ * try (SpeechTranslationServiceClient speechTranslationServiceClient = SpeechTranslationServiceClient.create()) {
+ *   StreamingTranslateSpeechRequest request = StreamingTranslateSpeechRequest.newBuilder().build();
+ *   StreamingTranslateSpeechResponse response = speechTranslationServiceClient.streamingTranslateSpeechCallable(request);
+ * }
+ * </code>
+ * </pre>
+ */
+@Generated("by gapic-generator")
+package com.google.cloud.mediatranslation.v1beta1;
+
+import javax.annotation.Generated;
