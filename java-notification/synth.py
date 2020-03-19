720ee8f3cfa5da33331447453d8ea1f725407c7a
Jeff Ching <chingor@google.com>
2020-03-19 08:34:14 -0700

    chore: use java helper for templates (#51)

--- java-notification/synth.py
+++ java-notification/synth.py
@@ -14,11 +14,8 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import synthtool as s
-import synthtool.gcp as gcp
+import synthtool.languages.java as java
 
-common_templates = gcp.CommonTemplates()
-templates = common_templates.java_library()
-s.copy(templates, excludes=[
+java.common_templates(excludes=[
     'README.md',
 ])
