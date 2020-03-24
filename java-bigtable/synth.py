2dba5da5410cf8a16d15c5fd06a260bad6278efc
kolea2 <45548808+kolea2@users.noreply.github.com>
2020-03-24 10:56:43 -0400

    chore: clean up synth.py script with newer tooling (#221)
    * chore: clean up synth.py script with newer tooling
    
    * chore: clean up synth.py script with newer tooling
    
    * update based on review comments

--- java-bigtable/synth.py
+++ java-bigtable/synth.py
@@ -27,27 +27,8 @@ AUTOSYNTH_MULTIPLE_COMMITS = True
 def main():
   gapic = gcp.GAPICGenerator()
 
-  # License header
-  license = """
-  /*
-   * Copyright 2019 Google LLC
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   *     https://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
-  """
-
-  generate_data_api(gapic, license)
-  generate_admin_api(gapic, license)
+  generate_data_api(gapic)
+  generate_admin_api(gapic)
 
   java.format_code(f'./google-cloud-bigtable/src')
 
@@ -60,10 +41,10 @@ def main():
     'CONTRIBUTING.md',
   ])
 
-def generate_data_api(gapic, license):
+def generate_data_api(gapic):
   library = gapic.java_library(
-    service='bigtable',
-    version='v2',
+      service='bigtable',
+      version='v2',
     config_path='/google/bigtable/artman_bigtable.yaml',
     artman_output_name='')
 
@@ -88,16 +69,10 @@ def generate_data_api(gapic, license):
     "google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/GrpcBigtableCallableFactory.java",
   ]
 
-  package_name = f'com.google.cloud.bigtable.v2'
+  package_name = f'com.google.bigtable.v2'
   java.fix_proto_headers(library / f'proto-google-cloud-bigtable-v2')
-  # java.fix_grpc_headers(library / f'grpc-google-cloud-bigtable-v2', package_name)
+  java.fix_grpc_headers(library / f'grpc-google-cloud-bigtable-v2', package_name)
 
-  ##todo replace with fix_grpc_headers
-  s.replace(
-      library / f'grpc-google-cloud-bigtable-v2/src/**/*.java',
-      f"package com.google.bigtable.v2;",
-      f"{license}package com.google.bigtable.v2;"
-  )
   s.copy(library / f'gapic-google-cloud-bigtable-v2', 'google-cloud-bigtable/', excludes=excludes)
   s.copy(library / f'grpc-google-cloud-bigtable-v2/src', f'grpc-google-cloud-bigtable-v2/src')
   s.copy(library / f'proto-google-cloud-bigtable-v2/src', f'proto-google-cloud-bigtable-v2/src')
@@ -107,11 +82,10 @@ def generate_data_api(gapic, license):
   java.format_code(f'./grpc-google-cloud-bigtable-v2/src')
   java.format_code(f'./proto-google-cloud-bigtable-v2/src')
 
-
-def generate_admin_api(gapic, license):
+def generate_admin_api(gapic):
   library = gapic.java_library(
-      service='bigtable-admin',
-      version='v2',
+        service='bigtable-admin',
+        version='v2',
       config_path='/google/bigtable/admin/artman_bigtableadmin.yaml',
       artman_output_name='')
 
@@ -136,14 +110,7 @@ def generate_admin_api(gapic, license):
 
   package_name = f'com.google.cloud.bigtable.admin.v2'
   java.fix_proto_headers(library / f'proto-google-cloud-bigtable-admin-v2')
-  # java.fix_grpc_headers(library / f'grpc-google-cloud-bigtable-admin-v2', package_name)
-
-  ##todo replace with fix_grpc_headers
-  s.replace(
-      library / f'grpc-google-cloud-bigtable-admin-v2/src/**/*.java',
-      f"package com.google.bigtable.admin.v2;",
-      f"{license}package com.google.bigtable.admin.v2;"
-  )
+  java.fix_grpc_headers(library / f'grpc-google-cloud-bigtable-admin-v2', package_name)
 
   s.copy(library / f'gapic-google-cloud-bigtable-admin-v2/src', 'google-cloud-bigtable/src', excludes=excludes)
   s.copy(library / f'grpc-google-cloud-bigtable-admin-v2/src', f'grpc-google-cloud-bigtable-admin-v2/src')
@@ -153,7 +120,6 @@ def generate_admin_api(gapic, license):
 
   java.format_code(f'./grpc-google-cloud-bigtable-admin-v2/src')
   java.format_code(f'./proto-google-cloud-bigtable-admin-v2/src')
-
 
 def make_internal_only(sources):
   """Annotates all toplevel classes with `@InternalOnly`"""
@@ -166,7 +132,6 @@ def make_internal_only(sources):
   # Replace javadoc and annotations before every public class with InternalApi
   # javadoc and annotation
   s.replace(sources,
-
             before=r'/\*\*.+?\*/\n(?:^@[^\n]*\n)*(?=public [a-zA-B ]*class)',
             after='/** For internal use only. */\n@Generated("by gapic-generator")\n@InternalApi\n',
             flags=re.MULTILINE | re.DOTALL)
