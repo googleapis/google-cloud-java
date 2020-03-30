124d8b69739a446047f4acd69ea8e041714ed43c
Stephanie Wang <stephaniewang526@users.noreply.github.com>
2020-03-30 18:41:01 -0400

    samples: add new sample bigquery_query_params_structs (#204)
    * samples: add new sample bigquery_query_params_structs
    
    * update sample
    
    * inline boolean val
    
    Co-Authored-By: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * inline vars
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>

---
+++ samples/snippets/src/main/java/com/example/bigquery/QueryWithStructsParameters.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquery;
+
+// [START bigquery_query_params_structs]
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryException;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.QueryParameterValue;
+import com.google.cloud.bigquery.TableResult;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QueryWithStructsParameters {
+
+  public static void queryWithStructsParameters() {
+    try {
+      // Initialize client that will be used to send requests. This client only needs to be created
+      // once, and can be reused for multiple requests.
+      BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+      // Create struct
+      Map<String, QueryParameterValue> struct = new HashMap<>();
+      struct.put("booleanField", QueryParameterValue.bool(true));
+      struct.put("integerField", QueryParameterValue.string("test-stringField"));
+      struct.put("stringField", QueryParameterValue.int64(10));
+      QueryParameterValue recordValue = QueryParameterValue.struct(struct);
+
+      String query = "SELECT STRUCT(@recordField) AS record";
+      QueryJobConfiguration queryConfig =
+          QueryJobConfiguration.newBuilder(query)
+              .setUseLegacySql(false)
+              .addNamedParameter("recordField", recordValue)
+              .build();
+
+      TableResult results = bigquery.query(queryConfig);
+
+      results
+          .iterateAll()
+          .forEach(row -> row.forEach(val -> System.out.printf("%s", val.toString())));
+
+      System.out.println("Query with struct parameter performed successfully.");
+    } catch (BigQueryException | InterruptedException e) {
+      System.out.println("Query not performed \n" + e.toString());
+    }
+  }
+}
+// [END bigquery_query_params_structs]
