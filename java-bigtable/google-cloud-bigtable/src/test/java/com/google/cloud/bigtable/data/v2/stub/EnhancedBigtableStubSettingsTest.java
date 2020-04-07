224d402327b6d6e79f2bbe2921bff6298c9fbd1d
kolea2 <45548808+kolea2@users.noreply.github.com>
2020-04-07 12:36:35 -0400

    deps: update core dependencies for google-cloud-core and gax (#240)
    * deps: update core dependencies for google-cloud-core and gax
    
    * format

--- java-bigtable/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStubSettingsTest.java
+++ java-bigtable/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStubSettingsTest.java
@@ -548,18 +548,17 @@ public class EnhancedBigtableStubSettingsTest {
 
     assertThat(builder.checkAndMutateRowSettings().getRetryableCodes())
         .containsAtLeast(Code.ABORTED, Code.DEADLINE_EXCEEDED);
-    assertThat(builder.checkAndMutateRowSettings().getRetrySettings())
-        .isSameInstanceAs(retrySettings);
+    assertThat(builder.checkAndMutateRowSettings().getRetrySettings()).isEqualTo(retrySettings);
 
     assertThat(builder.build().checkAndMutateRowSettings().getRetryableCodes())
         .containsAtLeast(Code.ABORTED, Code.DEADLINE_EXCEEDED);
     assertThat(builder.build().checkAndMutateRowSettings().getRetrySettings())
-        .isSameInstanceAs(retrySettings);
+        .isEqualTo(retrySettings);
 
     assertThat(builder.build().toBuilder().checkAndMutateRowSettings().getRetryableCodes())
         .containsAtLeast(Code.ABORTED, Code.DEADLINE_EXCEEDED);
     assertThat(builder.build().toBuilder().checkAndMutateRowSettings().getRetrySettings())
-        .isSameInstanceAs(retrySettings);
+        .isEqualTo(retrySettings);
   }
 
   @Test
