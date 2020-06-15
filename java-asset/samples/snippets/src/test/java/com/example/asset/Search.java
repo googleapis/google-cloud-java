0e2c626915b5d4d17af802729a2dde44039df900
yuyifan-google <65194920+yuyifan-google@users.noreply.github.com>
2020-06-15 11:10:06 -0700

    [asset] feat: add samples for SearchAllResources and SearchAllIamPolicies (#3168)
    * Add samples for SearchAllResources and SearchAllIamPolicies
    
    * Update error handling to catch specific exceptions.
    
    * Fix static analysis issues for the newly added files; Remove arguments that are not required by tests.

---
+++ java-asset/samples/snippets/src/test/java/com/example/asset/Search.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.asset;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQuery.DatasetDeleteOption;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.DatasetId;
+import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.testing.RemoteBigQueryHelper;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for search samples. */
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public class Search {
+
+  private static final String projectId = System.getenv("GOOGLE_CLOUD_PROJECT");
+  private static final String datasetName = RemoteBigQueryHelper.generateDatasetName();
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+  private BigQuery bigquery;
+
+  @Before
+  public void setUp() {
+    bigquery = BigQueryOptions.getDefaultInstance().getService();
+    if (bigquery.getDataset(datasetName) == null) {
+      bigquery.create(DatasetInfo.newBuilder(datasetName).build());
+    }
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    System.setOut(null);
+    bout.reset();
+    DatasetId datasetId = DatasetId.of(bigquery.getOptions().getProjectId(), datasetName);
+    bigquery.delete(datasetId, DatasetDeleteOption.deleteContents());
+  }
+
+  @Test
+  public void testSearchAllResourcesExample() throws Exception {
+    // Wait 10 seconds to let dataset creation event go to CAI
+    Thread.sleep(10000);
+    String scope = "projects/" + projectId;
+    String query = "name:" + datasetName;
+    SearchAllResourcesExample.searchAllResources(scope, query);
+    String got = bout.toString();
+    assertThat(got).contains(datasetName);
+  }
+
+  @Test
+  public void testSearchAllIamPoliciesExample() throws Exception {
+    String scope = "projects/" + projectId;
+    String query = "policy:roles/owner";
+    SearchAllIamPoliciesExample.searchAllIamPolicies(scope, query);
+    String got = bout.toString();
+    assertThat(got).contains("roles/owner");
+  }
+}
