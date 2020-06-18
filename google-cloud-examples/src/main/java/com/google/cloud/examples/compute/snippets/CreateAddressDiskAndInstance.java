c331b80ce8aa713fc2743f268a6bca556e234365
suraj dhamecha <suraj.dhamecha@qlogic.io>
2020-06-18 18:29:25 +0530

    chore: update deprecated snippets for compute client

--- google-cloud-examples/src/main/java/com/google/cloud/examples/compute/snippets/CreateAddressDiskAndInstance.java
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/compute/snippets/CreateAddressDiskAndInstance.java
@@ -16,86 +16,97 @@
 
 package com.google.cloud.examples.compute.snippets;
 
-import com.google.cloud.compute.deprecated.Address;
-import com.google.cloud.compute.deprecated.AddressInfo;
-import com.google.cloud.compute.deprecated.AttachedDisk;
-import com.google.cloud.compute.deprecated.AttachedDisk.PersistentDiskConfiguration;
-import com.google.cloud.compute.deprecated.Compute;
-import com.google.cloud.compute.deprecated.ComputeOptions;
-import com.google.cloud.compute.deprecated.DiskId;
-import com.google.cloud.compute.deprecated.DiskInfo;
-import com.google.cloud.compute.deprecated.ImageDiskConfiguration;
-import com.google.cloud.compute.deprecated.ImageId;
-import com.google.cloud.compute.deprecated.InstanceId;
-import com.google.cloud.compute.deprecated.InstanceInfo;
-import com.google.cloud.compute.deprecated.MachineTypeId;
-import com.google.cloud.compute.deprecated.NetworkId;
-import com.google.cloud.compute.deprecated.NetworkInterface;
-import com.google.cloud.compute.deprecated.NetworkInterface.AccessConfig;
-import com.google.cloud.compute.deprecated.Operation;
-import com.google.cloud.compute.deprecated.RegionAddressId;
-import java.util.concurrent.TimeoutException;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.compute.v1.Address;
+import com.google.cloud.compute.v1.AddressClient;
+import com.google.cloud.compute.v1.AttachedDisk;
+import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
+import com.google.cloud.compute.v1.Disk;
+import com.google.cloud.compute.v1.DiskClient;
+import com.google.cloud.compute.v1.InsertDiskHttpRequest;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstanceClient;
+import com.google.cloud.compute.v1.NetworkInterface;
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.ProjectRegionName;
+import com.google.cloud.compute.v1.ProjectZoneMachineTypeName;
+import com.google.cloud.compute.v1.ProjectZoneName;
+import java.io.IOException;
 
 /**
  * A snippet for Google Cloud Compute Engine showing how to create a disk and an address. The
  * snippet also shows how to create a virtual machine instance using the created disk and address.
  */
 public class CreateAddressDiskAndInstance {
+  private static final String DEFAULT_PROJECT = ServiceOptions.getDefaultProjectId();
+  private static final String ZONE = "us-central1-a";
+  private static final String ADDRESS_NAME = "test-address";
+  private static final String DEFAULT_IMAGE =
+      "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20150710";
+  private static final String FORMATTED_ZONE = ProjectZoneName.format(DEFAULT_PROJECT, ZONE);
+  private static final String REGION = "us-central1";
 
-  public static void main(String... args) throws InterruptedException, TimeoutException {
-    // Create a service object
-    // Credentials are inferred from the environment.
-    Compute compute = ComputeOptions.getDefaultInstance().getService();
-
-    // Create an external region address
-    RegionAddressId addressId = RegionAddressId.of("us-central1", "test-address");
-    Operation operation = compute.create(AddressInfo.of(addressId));
-    // Wait for operation to complete
-    operation = operation.waitFor();
-    if (operation.getErrors() == null) {
-      System.out.println("Address " + addressId + " was successfully created");
-    } else {
-      // inspect operation.getErrors()
-      throw new RuntimeException("Address creation failed");
+  public static void main(String... args) throws IOException {
+    try (AddressClient addressClient = AddressClient.create()) {
+      ProjectRegionName projectRegionName = ProjectRegionName.of(DEFAULT_PROJECT, REGION);
+      Address address = Address.newBuilder().setName(ADDRESS_NAME).build();
+      Operation operation = addressClient.insertAddress(projectRegionName, address);
+      if (operation.getError() == null) {
+        System.out.println("Address " + ADDRESS_NAME + " was successfully created");
+      } else {
+        // inspect operation.getErrors()
+        throw new RuntimeException("Address creation failed");
+      }
     }
-
     // Create a persistent disk
-    ImageId imageId = ImageId.of("debian-cloud", "debian-8-jessie-v20160329");
-    DiskId diskId = DiskId.of("us-central1-a", "test-disk");
-    ImageDiskConfiguration diskConfiguration = ImageDiskConfiguration.of(imageId);
-    DiskInfo disk = DiskInfo.of(diskId, diskConfiguration);
-    operation = compute.create(disk);
-    // Wait for operation to complete
-    operation = operation.waitFor();
-    if (operation.getErrors() == null) {
-      System.out.println("Disk " + diskId + " was successfully created");
-    } else {
-      // inspect operation.getErrors()
-      throw new RuntimeException("Disk creation failed");
+    try (DiskClient diskClient = DiskClient.create()) {
+      Disk diskResource =
+          Disk.newBuilder()
+              .setName("test-disk")
+              .setSourceImageId("debian-8-jessie-v20160329")
+              .setSizeGb("10")
+              .build();
+      InsertDiskHttpRequest request =
+          InsertDiskHttpRequest.newBuilder()
+              .setZone(FORMATTED_ZONE)
+              .setDiskResource(diskResource)
+              .build();
+      Operation response = diskClient.insertDisk(request);
+      if (response.getError() == null) {
+        System.out.println("Disk " + diskResource.getName() + " was successfully created");
+      } else {
+        // inspect operation.getErrors()
+        throw new RuntimeException("Disk creation failed");
+      }
     }
-
     // Create a virtual machine instance
-    Address externalIp = compute.getAddress(addressId);
-    InstanceId instanceId = InstanceId.of("us-central1-a", "test-instance");
-    NetworkId networkId = NetworkId.of("default");
-    PersistentDiskConfiguration attachConfiguration =
-        PersistentDiskConfiguration.newBuilder(diskId).setBoot(true).build();
-    AttachedDisk attachedDisk = AttachedDisk.of("dev0", attachConfiguration);
-    NetworkInterface networkInterface =
-        NetworkInterface.newBuilder(networkId)
-            .setAccessConfigurations(AccessConfig.of(externalIp.getAddress()))
-            .build();
-    MachineTypeId machineTypeId = MachineTypeId.of("us-central1-a", "n1-standard-1");
-    InstanceInfo instance =
-        InstanceInfo.of(instanceId, machineTypeId, attachedDisk, networkInterface);
-    operation = compute.create(instance);
-    // Wait for operation to complete
-    operation = operation.waitFor();
-    if (operation.getErrors() == null) {
-      System.out.println("Instance " + instanceId + " was successfully created");
-    } else {
-      // inspect operation.getErrors()
-      throw new RuntimeException("Instance creation failed");
+    try (InstanceClient instanceClient = InstanceClient.create()) {
+      String machineType =
+          ProjectZoneMachineTypeName.of("n1-standard-1", DEFAULT_PROJECT, ZONE).toString();
+      AttachedDisk disk =
+          AttachedDisk.newBuilder()
+              .setBoot(true)
+              .setAutoDelete(true)
+              .setType("PERSISTENT")
+              .setDiskSizeGb("10")
+              .setInitializeParams(
+                  AttachedDiskInitializeParams.newBuilder().setSourceImage(DEFAULT_IMAGE).build())
+              .build();
+      NetworkInterface networkInterface = NetworkInterface.newBuilder().setName("default").build();
+      Instance instanceResource =
+          Instance.newBuilder()
+              .setName("test-instance")
+              .setMachineType(machineType)
+              .addDisks(disk)
+              .addNetworkInterfaces(networkInterface)
+              .build();
+      Operation response = instanceClient.insertInstance(FORMATTED_ZONE, instanceResource);
+      if (response.getError() == null) {
+        System.out.println("Instance " + instanceResource.getName() + " was successfully created");
+      } else {
+        // inspect operation.getErrors()
+        throw new RuntimeException("Instance creation failed");
+      }
     }
   }
 }
