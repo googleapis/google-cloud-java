a6d94913f74a6b4d29e9314a0652a9e3fa30aed9
Praful Makani <praful@qlogic.io>
2020-07-14 02:41:07 +0530

    docs(samples): add get table labels (#540)

---
+++ samples/snippets/src/test/java/com/example/bigquery/GetTableLabelsIT.java
@@ -0,0 +1,64 @@
+package com.example.bigquery;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import com.google.cloud.bigquery.Schema;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GetTableLabelsIT {
+
+  private String tableName;
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+
+  private static final String BIGQUERY_DATASET_NAME = requireEnvVar("BIGQUERY_DATASET_NAME");
+
+  private static String requireEnvVar(String varName) {
+    String value = System.getenv(varName);
+    assertNotNull(
+        "Environment variable " + varName + " is required to perform these tests.",
+        System.getenv(varName));
+    return value;
+  }
+
+  @BeforeClass
+  public static void checkRequirements() {
+    requireEnvVar("BIGQUERY_DATASET_NAME");
+  }
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+    // create a temporary table
+    tableName = "MY_TABLE_TEST_" + UUID.randomUUID().toString().substring(0, 8);
+    CreateTable.createTable(BIGQUERY_DATASET_NAME, tableName, Schema.of());
+    // add label on a table
+    LabelTable.labelTable(BIGQUERY_DATASET_NAME, tableName);
+
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    // delete a temporary table
+    DeleteTable.deleteTable(BIGQUERY_DATASET_NAME, tableName);
+    System.setOut(null);
+  }
+
+  @Test
+  public void testGetTableLabels() {
+    GetTableLabels.getTableLabels(BIGQUERY_DATASET_NAME, tableName);
+    assertThat(bout.toString()).contains("Retrieved labels successfully");
+  }
+}
