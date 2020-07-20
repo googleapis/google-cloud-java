259d470ecadc9f384920164c8a2f45643cff130a
Praful Makani <praful@qlogic.io>
2020-07-20 20:57:13 +0530

    docs(samples): add load json file from gcs into a table with autodetect schema (#569)

---
+++ samples/snippets/src/test/java/com/example/bigquery/LoadJsonFromGCSAutodetectIT.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquery;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertNotNull;
+
+import com.google.cloud.bigquery.Schema;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoadJsonFromGCSAutodetectIT {
+
+  private String tableName;
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+
+  private static final String BIGQUERY_DATASET_NAME = requireEnvVar("BIGQUERY_DATASET_NAME");
+
+  private static String requireEnvVar(String varName) {
+    String value = System.getenv(varName);
+    assertNotNull(
+        "Environment variable " + varName + " is required to perform these tests.",
+        System.getenv(varName));
+    return value;
+  }
+
+  @BeforeClass
+  public static void checkRequirements() {
+    requireEnvVar("BIGQUERY_DATASET_NAME");
+  }
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+
+    // Create a test table
+    tableName =
+        "MY_LOAD_JSON_TABLE_AUTODETECT_FROM_GCS_TEST_"
+            + UUID.randomUUID().toString().substring(0, 8);
+    CreateTable.createTable(BIGQUERY_DATASET_NAME, tableName, Schema.of());
+
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    // Clean up
+    DeleteTable.deleteTable(BIGQUERY_DATASET_NAME, tableName);
+    System.setOut(null);
+  }
+
+  @Test
+  public void loadLoadJsonFromGCSAutodetect() {
+    String sourceUri = "gs://cloud-samples-data/bigquery/us-states/us-states.json";
+    LoadJsonFromGCSAutodetect.loadJsonFromGCSAutodetect(
+        BIGQUERY_DATASET_NAME, tableName, sourceUri);
+    assertThat(bout.toString()).contains("Json Autodetect from GCS successfully loaded in a table");
+  }
+}
