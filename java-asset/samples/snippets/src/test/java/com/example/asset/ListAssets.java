49d9a0e8d45a919e6b1e5d2944af31332c345409
Larittic-GG <59073661+Larittic-GG@users.noreply.github.com>
2020-07-31 10:30:44 -0700

    samples: Add sample code for Cloud Assets Inventory ListAssets v1p5beta1 APIs. (#3408)
    * Add sample code for Cloud Assets Inventory ListAssets v1p5beta1 APIs.
    
    * Fix lint.
    
    * Revamped according to style guide.
    
    * Add a missing function call in listAssets().

---
+++ java-asset/samples/snippets/src/test/java/com/example/asset/ListAssets.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.asset;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.asset.v1p5beta1.ContentType;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for list assets sample. */
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public class ListAssets {
+  private ByteArrayOutputStream bout;
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bout));
+  }
+
+  @After
+  public void tearDown() {
+    System.setOut(null);
+    bout.reset();
+  }
+
+  @Test
+  public void testListAssetsExample() throws Exception {
+    // Use the default project Id (configure it by setting environment variable
+    // "GOOGLE_CLOUD_PROJECT").
+    String projectId = ServiceOptions.getDefaultProjectId();
+    String[] assetTypes = {"storage.googleapis.com/Bucket", "bigquery.googleapis.com/Table"};
+    ContentType contentType = ContentType.CONTENT_TYPE_UNSPECIFIED;
+    ListAssetsExample.listAssets(projectId, assetTypes, contentType);
+    String got = bout.toString();
+    if (!got.isEmpty()) {
+      assertThat(got).contains("asset");
+    }
+  }
+}
