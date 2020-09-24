03bda1bf79169bf0fca3ee12c1c3dfeb19c2e102
Praful Makani <praful@qlogic.io>
2020-09-24 21:04:41 +0530

    fix: nullpointer exception when executionerror is null (#754)

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/Job.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/Job.java
@@ -297,8 +297,10 @@ public class Job extends JobInfo {
       job = reload();
     }
     if (job.getStatus() != null && job.getStatus().getError() != null) {
-      throw new JobException(
-          getJobId(), ImmutableList.copyOf(job.getStatus().getExecutionErrors()));
+      throw new BigQueryException(
+          job.getStatus().getExecutionErrors() == null
+              ? ImmutableList.of(job.getStatus().getError())
+              : ImmutableList.copyOf(job.getStatus().getExecutionErrors()));
     }
 
     // If there are no rows in the result, this may have been a DDL query.
