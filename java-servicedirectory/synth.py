e21cbb0b7c2741869afcb2088ba0bad458879e28
Jeff Ching <chingor@google.com>
2020-11-16 10:43:36 -0800

    feat: add v1 client (#227)

--- java-servicedirectory/synth.py
+++ java-servicedirectory/synth.py
@@ -14,14 +14,12 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import synthtool as s
-import synthtool.gcp as gcp
 import synthtool.languages.java as java
 
 AUTOSYNTH_MULTIPLE_COMMITS = True
 
 service = 'servicedirectory'
-versions = ['v1beta1']
+versions = ['v1beta1', 'v1']
 
 for version in versions:
   java.bazel_library(
