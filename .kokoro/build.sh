0d2455cfd86716bf52793aedd2a1302792e27fc1
Justin Beckwith <justin.beckwith@gmail.com>
2021-01-28 22:14:51 -0800

    build: migrate to flakybot (#814)

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -115,8 +115,8 @@ bash .kokoro/coerce_logs.sh
 
 if [[ "${ENABLE_BUILD_COP}" == "true" ]]
 then
-    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/buildcop
-    ${KOKORO_GFILE_DIR}/linux_amd64/buildcop -repo=googleapis/java-bigquerystorage
+    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/flakybot
+    ${KOKORO_GFILE_DIR}/linux_amd64/flakybot -repo=googleapis/java-bigquerystorage
 fi
 
 echo "exiting with ${RETURN_CODE}"
