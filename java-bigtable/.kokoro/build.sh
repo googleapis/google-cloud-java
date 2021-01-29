8c4e0d8e1001eb448cc4e55d718d64d5bc1abfc4
Justin Beckwith <justin.beckwith@gmail.com>
2021-01-28 22:14:21 -0800

    build: migrate to flakybot (#603)

--- java-bigtable/.kokoro/build.sh
+++ java-bigtable/.kokoro/build.sh
@@ -110,8 +110,8 @@ bash .kokoro/coerce_logs.sh
 
 if [[ "${ENABLE_BUILD_COP}" == "true" ]]
 then
-    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/buildcop
-    ${KOKORO_GFILE_DIR}/linux_amd64/buildcop -repo=googleapis/java-bigtable
+    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/flakybot
+    ${KOKORO_GFILE_DIR}/linux_amd64/flakybot -repo=googleapis/java-bigtable
 fi
 
 echo "exiting with ${RETURN_CODE}"
