a2d9ce2b5a3b50ea814bdea47ec26b55677c0c48
Justin Beckwith <justin.beckwith@gmail.com>
2021-01-28 22:16:08 -0800

    build: migrate to flakybot (#397)

--- java-talent/.kokoro/build.sh
+++ java-talent/.kokoro/build.sh
@@ -108,8 +108,8 @@ bash .kokoro/coerce_logs.sh
 
 if [[ "${ENABLE_BUILD_COP}" == "true" ]]
 then
-    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/buildcop
-    ${KOKORO_GFILE_DIR}/linux_amd64/buildcop -repo=googleapis/java-talent
+    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/flakybot
+    ${KOKORO_GFILE_DIR}/linux_amd64/flakybot -repo=googleapis/java-talent
 fi
 
 echo "exiting with ${RETURN_CODE}"
