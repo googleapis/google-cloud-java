2aed20aa82f61786d9e21a9d01d5eefee990e01d
Justin Beckwith <justin.beckwith@gmail.com>
2021-01-28 22:18:13 -0800

    build: migrate to flakybot (#12)

--- java-networkconnectivity/.kokoro/build.sh
+++ java-networkconnectivity/.kokoro/build.sh
@@ -110,8 +110,8 @@ bash .kokoro/coerce_logs.sh
 
 if [[ "${ENABLE_BUILD_COP}" == "true" ]]
 then
-    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/buildcop
-    ${KOKORO_GFILE_DIR}/linux_amd64/buildcop -repo=googleapis/java-networkconnectivity
+    chmod +x ${KOKORO_GFILE_DIR}/linux_amd64/flakybot
+    ${KOKORO_GFILE_DIR}/linux_amd64/flakybot -repo=googleapis/java-networkconnectivity
 fi
 
 echo "exiting with ${RETURN_CODE}"
