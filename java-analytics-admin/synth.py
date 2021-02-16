b6f77da69022f778695926cfbc851491767ac758
Jeff Ching <chingor@google.com>
2021-02-16 12:12:09 -0800

    chore: switch to using pre-generated library (#157)
    Fixes #154

--- java-analytics-admin/synth.py
+++ java-analytics-admin/synth.py
@@ -14,20 +14,17 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import synthtool as s
-import synthtool.gcp as gcp
 import synthtool.languages.java as java
 
 service = 'analytics-admin'
 versions = ['v1alpha']
 
 for version in versions:
-  java.bazel_library(
-      service=service,
-      version=version,
-      proto_path=f'google/analytics/admin/{version}',
-      bazel_target=f'//google/analytics/admin/{version}:google-{service}-{version}-java',
-      cloud_api=False,
+  java.pregenerated_library(
+    service=service,
+    version=version,
+    path=f'google/analytics/admin/{version}',
+    cloud_api=False,
   )
 
 java.common_templates()
