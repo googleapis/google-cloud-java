1d0d6c3a877943b83775430d5a2bf7fcd24a1f21
shollyman <shollyman@google.com>
2021-04-07 10:48:04 -0700

    docs: update region tag for the storage quickstart (#985)
    This sample uses the wrong prefix, which associates it to BQ rather than
    BQ storage.

--- samples/snippets/src/main/java/com/example/bigquerystorage/StorageSample.java
+++ samples/snippets/src/main/java/com/example/bigquerystorage/StorageSample.java
@@ -16,7 +16,7 @@
 
 package com.example.bigquerystorage;
 
-// [START bigquery_storage_quickstart]
+// [START bigquerystorage_quickstart]
 import com.google.api.gax.rpc.ServerStream;
 import com.google.cloud.bigquery.storage.v1.AvroRows;
 import com.google.cloud.bigquery.storage.v1.BigQueryReadClient;
@@ -157,4 +157,4 @@ public class StorageSample {
     }
   }
 }
-// [END bigquery_storage_quickstart]
+// [END bigquerystorage_quickstart]
