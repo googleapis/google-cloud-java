b533d52d01874ca3267cf8180b134f5a4775d913
Igor Bernstein <igorbernstein@google.com>
2021-04-12 16:32:16 -0400

    chore: exclude autogenerated files from codecov (#725)

--- java-bigtable/synth.py
+++ java-bigtable/synth.py
@@ -41,6 +41,8 @@ def main():
     # todo remove once template is updated
     '.github/ISSUE_TEMPLATE/bug_report.md',
     'CONTRIBUTING.md',
+    # exclude autogen
+    'codecov.yaml'
   ])
 
 def generate_data_api(gapic):
