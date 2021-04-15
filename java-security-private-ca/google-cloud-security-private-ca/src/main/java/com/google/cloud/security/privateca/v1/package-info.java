8710fdfc1fb7f51219463a10a833d6369af51386
Neenu Shaji <Neenu1995@users.noreply.github.com>
2021-04-15 15:28:05 -0400

    feat: add v1 client (#155)

---
+++ java-security-private-ca/google-cloud-security-private-ca/src/main/java/com/google/cloud/security/privateca/v1/package-info.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>======================= CertificateAuthorityServiceClient =======================
+ *
+ * <p>Service Description: [Certificate Authority
+ * Service][google.cloud.security.privateca.v1.CertificateAuthorityService] manages private
+ * certificate authorities and issued certificates.
+ *
+ * <p>Sample for CertificateAuthorityServiceClient:
+ *
+ * <pre>{@code
+ * try (CertificateAuthorityServiceClient certificateAuthorityServiceClient =
+ *     CertificateAuthorityServiceClient.create()) {
+ *   CaPoolName parent = CaPoolName.of("[PROJECT]", "[LOCATION]", "[CA_POOL]");
+ *   Certificate certificate = Certificate.newBuilder().build();
+ *   String certificateId = "certificateId-644529902";
+ *   Certificate response =
+ *       certificateAuthorityServiceClient.createCertificate(parent, certificate, certificateId);
+ * }
+ * }</pre>
+ */
+@Generated("by gapic-generator-java")
+package com.google.cloud.security.privateca.v1;
+
+import javax.annotation.Generated;
