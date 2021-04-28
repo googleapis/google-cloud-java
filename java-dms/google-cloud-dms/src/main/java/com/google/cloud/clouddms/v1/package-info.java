9368f9fdf7c3740061f2b78bff08d4bfa170139f
Neenu1995 <neenushaji@google.com>
2021-04-28 17:47:35 -0400

    feat: initial generation

---
+++ java-dms/google-cloud-dms/src/main/java/com/google/cloud/clouddms/v1/package-info.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>======================= DataMigrationServiceClient =======================
+ *
+ * <p>Service Description: Database Migration service
+ *
+ * <p>Sample for DataMigrationServiceClient:
+ *
+ * <pre>{@code
+ * try (DataMigrationServiceClient dataMigrationServiceClient =
+ *     DataMigrationServiceClient.create()) {
+ *   MigrationJobName name = MigrationJobName.of("[PROJECT]", "[LOCATION]", "[MIGRATION_JOB]");
+ *   MigrationJob response = dataMigrationServiceClient.getMigrationJob(name);
+ * }
+ * }</pre>
+ */
+@Generated("by gapic-generator-java")
+package com.google.cloud.clouddms.v1;
+
+import javax.annotation.Generated;
