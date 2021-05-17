156e3dfbed326d8ebcf30bff0c27ff01a61ee8a8
Kristen O'Leary <kaoleary@google.com>
2021-05-17 12:34:39 -0400

    close input stream after each run

--- datastore-v1-proto-client/src/test/java/com/google/datastore/v1/client/ChecksumEnforcingInputStreamTest.java
+++ datastore-v1-proto-client/src/test/java/com/google/datastore/v1/client/ChecksumEnforcingInputStreamTest.java
@@ -42,6 +42,8 @@ public class ChecksumEnforcingInputStreamTest {
       }
     } catch (IOException e) {
       fail("checksum verification failed!");
+    } finally {
+      testInstance.close();
     }
   }
 
