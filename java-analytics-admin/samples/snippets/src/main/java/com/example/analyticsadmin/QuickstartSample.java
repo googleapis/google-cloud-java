f99b9301ca99fc992c9f479ef8fea74751ecc073
Neenu Shaji <Neenu1995@users.noreply.github.com>
2021-05-20 18:18:04 -0400

    chore: migrate to owlbot (#213)

--- java-analytics-admin/samples/snippets/src/main/java/com/example/analyticsadmin/QuickstartSample.java
+++ java-analytics-admin/samples/snippets/src/main/java/com/example/analyticsadmin/QuickstartSample.java
@@ -17,35 +17,35 @@
 package com.example.analytics;
 
 /* This application demonstrates the usage of the Analytics Admin API using
- service account credentials. For more information on service accounts, see
- https://cloud.google.com/iam/docs/understanding-service-accounts
+service account credentials. For more information on service accounts, see
+https://cloud.google.com/iam/docs/understanding-service-accounts
 
- The following document provides instructions on setting service account
- credentials for your application:
- https://cloud.google.com/docs/authentication/production
+The following document provides instructions on setting service account
+credentials for your application:
+https://cloud.google.com/docs/authentication/production
 
- In a nutshell, you need to:
+In a nutshell, you need to:
 
- 1. Create a service account and download the key JSON file.
- https://cloud.google.com/docs/authentication/production#creating_a_service_account
+1. Create a service account and download the key JSON file.
+https://cloud.google.com/docs/authentication/production#creating_a_service_account
 
- 2. Provide service account credentials using one of the following options:
- - set the GOOGLE_APPLICATION_CREDENTIALS environment variable, the API
- client will use the value of this variable to find the service account key
- JSON file.
+2. Provide service account credentials using one of the following options:
+- set the GOOGLE_APPLICATION_CREDENTIALS environment variable, the API
+client will use the value of this variable to find the service account key
+JSON file.
 
- https://cloud.google.com/docs/authentication/production#setting_the_environment_variable
+https://cloud.google.com/docs/authentication/production#setting_the_environment_variable
 
- OR
+OR
 
- - manually pass the path to the service account key JSON file to the API client
- by specifying the keyFilename parameter in the constructor.
- https://cloud.google.com/docs/authentication/production#passing_the_path_to_the_service_account_key_in_code
+- manually pass the path to the service account key JSON file to the API client
+by specifying the keyFilename parameter in the constructor.
+https://cloud.google.com/docs/authentication/production#passing_the_path_to_the_service_account_key_in_code
 
- To run this sample using Maven:
-   cd java-analytics-admin/samples/snippets
-   mvn exec:java -Dexec.mainClass="com.example.analytics.QuickstartSample"
- */
+To run this sample using Maven:
+  cd java-analytics-admin/samples/snippets
+  mvn exec:java -Dexec.mainClass="com.example.analytics.QuickstartSample"
+*/
 
 // [START analytics_admin_quickstart]
 
@@ -61,7 +61,6 @@ public class QuickstartSample {
     listAccounts();
   }
 
-
   // This is an example snippet that calls the Google Analytics Admin API and lists all Google
   // Analytics accounts available to the authenticated user.
   static void listAccounts() throws Exception {
@@ -71,8 +70,8 @@ public class QuickstartSample {
     try (AnalyticsAdminServiceClient analyticsAdmin = AnalyticsAdminServiceClient.create()) {
       // Calls listAccounts() method of the Google Analytics Admin API and prints
       // the response for each account.
-      ListAccountsPagedResponse response = analyticsAdmin
-          .listAccounts(ListAccountsRequest.newBuilder().build());
+      ListAccountsPagedResponse response =
+          analyticsAdmin.listAccounts(ListAccountsRequest.newBuilder().build());
       for (ListAccountsPage page : response.iteratePages()) {
         for (Account account : page.iterateAll()) {
           System.out.printf("Account name: %s%n", account.getName());
