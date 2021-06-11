2e7d685e418649cabd8fd843997c69665299baf1
Neenu Shaji <Neenu1995@users.noreply.github.com>
2021-06-11 00:34:01 -0400

    chore: migrate to owlbot (#68)
    * chore: migrate to owlbot
    
    * 🦉 Updates from OwlBot
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- java-artifact-registry/owlbot.py
+++ java-artifact-registry/owlbot.py
@@ -1,10 +1,10 @@
-# Copyright 2020 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,19 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This script is used to synthesize generated parts of this library."""
+import synthtool as s
+from synthtool.languages import java
 
-import synthtool.languages.java as java
 
-service = 'devtools-artifactregistry'
-versions = ['v1beta2']
+for library in s.get_staging_dirs():
+    # put any special-case replacements here
+    s.move(library)
 
-for version in versions:
-  java.pregenerated_library(
-    service=service,
-    version=version,
-    path=f'google/devtools/artifactregistry/{version}',
-    destination_name="artifact-registry"
-  )
-
+s.remove_staging_dirs()
 java.common_templates()
