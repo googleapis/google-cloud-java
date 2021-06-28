7b19e260b078c87fe61b64b405112a7fe413e9a1
Frank Natividad <frankyn@users.noreply.github.com>
2021-06-28 16:07:29 -0700

    feat(storage): add samples for storage public access prevention (#7423)

---
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/storage/buckets/SetPublicAccessPreventionUnspecified.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.examples.storage.buckets;
+
+// [START storage_set_public_access_prevention_unspecified]
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+public class SetPublicAccessPreventionUnspecified {
+  public static void setPublicAccessPreventionUnspecified(String projectId, String bucketName) {
+    // The ID of your GCP project
+    // String projectId = "your-project-id";
+
+    // The ID of your GCS bucket
+    // String bucketName = "your-unique-bucket-name";
+
+    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    Bucket bucket = storage.get(bucketName);
+
+    // Sets public access prevention to 'unspecified' for the bucket
+    bucket
+        .toBuilder()
+        .setIamConfiguration(
+            BucketInfo.IamConfiguration.newBuilder()
+                .setPublicAccessPrevention(BucketInfo.PublicAccessPrevention.UNSPECIFIED)
+                .build())
+        .build()
+        .update();
+
+    System.out.println("Public access prevention is set to unspecified for " + bucketName);
+  }
+}
+// [END storage_set_public_access_prevention_unspecified]
