9ed6abf4d40e1d5b2f3b12761db39b4736a299a4
Neenu1995 <neenushaji@google.com>
2021-07-02 13:46:38 -0400

    feat: initial generation

---
+++ java-data-fusion/google-cloud-data-fusion/src/main/java/com/google/cloud/datafusion/v1beta1/package-info.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>======================= DataFusionClient =======================
+ *
+ * <p>Service Description: Service for creating and managing Data Fusion instances. Data Fusion
+ * enables ETL developers to build code-free, data integration pipelines via a point-and-click UI.
+ *
+ * <p>Sample for DataFusionClient:
+ *
+ * <pre>{@code
+ * try (DataFusionClient dataFusionClient = DataFusionClient.create()) {
+ *   GetInstanceRequest request = GetInstanceRequest.newBuilder().setName("name3373707").build();
+ *   Instance response = dataFusionClient.getInstance(request);
+ * }
+ * }</pre>
+ */
+@Generated("by gapic-generator-java")
+package com.google.cloud.datafusion.v1beta1;
+
+import javax.annotation.Generated;
