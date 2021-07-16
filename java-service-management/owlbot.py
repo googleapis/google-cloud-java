5cd27080ab2498cd7ba54e409ee339e78f84e64b
Neenu Shaji <Neenu1995@users.noreply.github.com>
2021-07-16 13:44:26 -0400

    chore: migrate to owlbot (#84)

--- java-service-management/owlbot.py
+++ java-service-management/owlbot.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,20 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This script is used to synthesize generated parts of this library."""
+import synthtool as s
+from synthtool.languages import java
 
-import synthtool.languages.java as java
 
-service = 'api-servicemanagement'
-versions = ['v1']
+for library in s.get_staging_dirs():
+    # put any special-case replacements here
+    s.move(library)
 
-for version in versions:
-  java.pregenerated_library(
-    service=service,
-    version=version,
-    path=f'google/api/servicemanagement/{version}',
-    destination_name='service-management',
-  )
-
+s.remove_staging_dirs()
 java.common_templates()
-
