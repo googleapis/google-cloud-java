e78feeb046915a83abd3d98de272df86e1a74778
Neenu Shaji <Neenu1995@users.noreply.github.com>
2021-09-09 11:42:00 -0400

    chore: migrate to owlbot (#103)
    * chore: migrate to owlbot
    
    * chore: fix depndencies
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * chore: remove v1beta1
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- java-managed-identities/google-cloud-managed-identities/src/main/java/com/google/cloud/managedidentities/v1/stub/ManagedIdentitiesServiceStubSettings.java
+++ java-managed-identities/google-cloud-managed-identities/src/main/java/com/google/cloud/managedidentities/v1/stub/ManagedIdentitiesServiceStubSettings.java
@@ -320,7 +320,9 @@ public class ManagedIdentitiesServiceStubSettings
 
   /** Returns a builder for the default credentials for this service. */
   public static GoogleCredentialsProvider.Builder defaultCredentialsProviderBuilder() {
-    return GoogleCredentialsProvider.newBuilder().setScopesToApply(DEFAULT_SERVICE_SCOPES);
+    return GoogleCredentialsProvider.newBuilder()
+        .setScopesToApply(DEFAULT_SERVICE_SCOPES)
+        .setUseJwtAccessWithScope(true);
   }
 
   /** Returns a builder for the default ChannelProvider for this service. */
