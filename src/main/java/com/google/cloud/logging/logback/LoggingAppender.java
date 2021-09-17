65ab6f8dfbfe9f28538b905db58aadeae0182276
minherz <minherz@users.noreply.github.com>
2021-09-17 19:21:54 +0300

    feat: add writeSynchronicity flag to appender configuration (#542)
    * feat: add support for write synchronicity config
    
    add new adapter configuration to allow setting writeSynchronicity
    
    * chore: set default value and remove debug output
    
    remove debug prints
    add option description into class javadoc
    setup default value of the instance
    
    * chore: remove async testing from samples
    
    * chore: change type of writeSynchronicity to enum

--- src/main/java/com/google/cloud/logging/logback/LoggingAppender.java
+++ src/main/java/com/google/cloud/logging/logback/LoggingAppender.java
@@ -33,6 +33,7 @@ import com.google.cloud.logging.LoggingOptions;
 import com.google.cloud.logging.MonitoredResourceUtil;
 import com.google.cloud.logging.Payload;
 import com.google.cloud.logging.Severity;
+import com.google.cloud.logging.Synchronicity;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import java.io.FileInputStream;
@@ -63,8 +64,12 @@ import java.util.Set;
  *         &lt;!-- Optional: defaults to "ERROR" --&gt;
  *         &lt;flushLevel&gt;WARNING&lt;/flushLevel&gt;
  *
+ *         &lt;!-- Optional: defaults to ASYNC --&gt;
+ *         &lt;writeSynchronicity&gt;SYNC&lt;/writeSynchronicity&gt;
+ *
  *         &lt;!-- Optional: auto detects on App Engine Flex, Standard, GCE and GKE, defaults to "global". See <a
- *         href="https://cloud.google.com/logging/docs/api/v2/resource-list">supported resource types</a> --&gt;
+ *         href=
+ * "https://cloud.google.com/logging/docs/api/v2/resource-list">supported resource types</a> --&gt;
  *         &lt;resourceType&gt;&lt;/resourceType&gt;
  *
  *         &lt;!-- Optional: defaults to the default credentials of the environment --&gt;
@@ -96,6 +101,7 @@ public class LoggingAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
   private String log;
   private String resourceType;
   private String credentialsFile;
+  private Synchronicity writeSyncFlag = Synchronicity.ASYNC;
   private final Set<String> enhancerClassNames = new HashSet<>();
   private final Set<String> loggingEventEnhancerClassNames = new HashSet<>();
 
@@ -122,8 +128,9 @@ public class LoggingAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
   /**
    * Sets the name of the monitored resource (Optional).
    *
-   * <p>Must be a <a href="https://cloud.google.com/logging/docs/api/v2/resource-list">supported</a>
-   * resource type. gae_app, gce_instance and container are auto-detected.
+   * <p>Must be a <a href=
+   * "https://cloud.google.com/logging/docs/api/v2/resource-list">supported</a> resource type.
+   * gae_app, gce_instance and container are auto-detected.
    *
    * <p>Defaults to "global"
    *
@@ -144,6 +151,15 @@ public class LoggingAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
     this.credentialsFile = credentialsFile;
   }
 
+  /**
+   * Define synchronization mode for writing log entries.
+   *
+   * @param flag to set {@code Synchronicity} value.
+   */
+  public void setWriteSynchronicity(Synchronicity flag) {
+    this.writeSyncFlag = flag;
+  }
+
   /** Add extra labels using classes that implement {@link LoggingEnhancer}. */
   public void addEnhancer(String enhancerClassName) {
     this.enhancerClassNames.add(enhancerClassName);
@@ -159,6 +175,10 @@ public class LoggingAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
   String getLogName() {
     return (log != null) ? log : "java.log";
+  }
+
+  public Synchronicity getWriteSynchronicity() {
+    return (this.writeSyncFlag != null) ? this.writeSyncFlag : Synchronicity.ASYNC;
   }
 
   MonitoredResource getMonitoredResource(String projectId) {
@@ -253,6 +273,7 @@ public class LoggingAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
       synchronized (this) {
         if (logging == null) {
           logging = getLoggingOptions().getService();
+          logging.setWriteSynchronicity(writeSyncFlag);
         }
       }
     }
