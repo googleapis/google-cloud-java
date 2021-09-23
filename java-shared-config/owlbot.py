ee887bf74e7546cc41b5f6ffd482dc600a989169
Jeff Ching <chingor@google.com>
2021-09-23 10:00:12 -0700

    chore: convert to owlbot (#320)

--- java-shared-config/owlbot.py
+++ java-shared-config/owlbot.py
@@ -1,10 +1,10 @@
-# Copyright 2019 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,17 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This script is used to synthesize generated parts of this library."""
+import synthtool as s
+from synthtool.languages import java
 
-import synthtool.languages.java as java
 
-AUTOSYNTH_MULTIPLE_COMMITS = True
+for library in s.get_staging_dirs():
+    # put any special-case replacements here
+    s.move(library)
 
-java.common_templates(excludes=[
-  "README.md",
-  ".github/release-please.yml",
-  '.github/workflows/auto-release.yaml',
-  '.github/workflows/samples.yaml',
-  "samples/*"
-])
-
+s.remove_staging_dirs()
+java.common_templates(
+    excludes=[
+        "README.md",
+        ".github/release-please.yml",
+        ".github/workflows/auto-release.yaml",
+        ".github/workflows/samples.yaml",
+        "samples/*",
+    ]
+)
