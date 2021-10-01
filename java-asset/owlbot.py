bded0eb0ffa60e09fb9a293bafe014807179bcbb
Mike Eltsufin <meltsufin@google.com>
2021-10-01 10:52:53 -0400

    chore(deps): Use Pub/Sub BOM (#954)
    * chore(deps): Use Pub/Sub BOM
    
    Unblocks: #865.
    
    * Try just upgrading Pub/Sub
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * formatting
    
    * chore: add dependencis.sh to exclude list
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Neenu1995 <neenushaji@google.com>

--- java-asset/owlbot.py
+++ java-asset/owlbot.py
@@ -21,4 +21,8 @@ for library in s.get_staging_dirs():
     s.move(library)
 
 s.remove_staging_dirs()
-java.common_templates()
+java.common_templates(
+    excludes=[
+        ".kokoro/dependencies.sh"
+    ]
+)
