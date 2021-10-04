5dfd5231c1ffa68a1b92ba01a9608d8cee2c0596
Veronica Wasson <3992422+VeronicaWasson@users.noreply.github.com>
2021-10-04 09:26:02 -0700

    docs(samples): Add WriteAPI BUFFERED mode sample (#1338)
    * docs(samples): Add WriteAPI BUFFERED mode sample
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Verify the rows were added to the table
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Veronica Wasson <3992422+MikeWasson@users.noreply.github.com>
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ samples/snippets/src/main/java/com/example/bigquerystorage/WriteBufferedStream.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquerystorage;
+
+// [START bigquerystorage_jsonstreamwriter_buffered]
+import com.google.api.core.ApiFuture;
+import com.google.cloud.bigquery.storage.v1beta2.AppendRowsResponse;
+import com.google.cloud.bigquery.storage.v1beta2.BigQueryWriteClient;
+import com.google.cloud.bigquery.storage.v1beta2.CreateWriteStreamRequest;
+import com.google.cloud.bigquery.storage.v1beta2.FlushRowsRequest;
+import com.google.cloud.bigquery.storage.v1beta2.FlushRowsResponse;
+import com.google.cloud.bigquery.storage.v1beta2.JsonStreamWriter;
+import com.google.cloud.bigquery.storage.v1beta2.TableName;
+import com.google.cloud.bigquery.storage.v1beta2.WriteStream;
+import com.google.protobuf.Descriptors.DescriptorValidationException;
+import com.google.protobuf.Int64Value;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WriteBufferedStream {
+
+  public static void runWriteBufferedStream()
+      throws DescriptorValidationException, InterruptedException, IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String projectId = "MY_PROJECT_ID";
+    String datasetName = "MY_DATASET_NAME";
+    String tableName = "MY_TABLE_NAME";
+
+    writeBufferedStream(projectId, datasetName, tableName);
+  }
+
+  public static void writeBufferedStream(String projectId, String datasetName, String tableName)
+      throws DescriptorValidationException, InterruptedException, IOException {
+    try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+      // Initialize a write stream for the specified table.
+      // For more information on WriteStream.Type, see:
+      // https://googleapis.dev/java/google-cloud-bigquerystorage/latest/com/google/cloud/bigquery/storage/v1beta2/WriteStream.Type.html
+      WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.BUFFERED).build();
+      TableName parentTable = TableName.of(projectId, datasetName, tableName);
+      CreateWriteStreamRequest createWriteStreamRequest =
+          CreateWriteStreamRequest.newBuilder()
+              .setParent(parentTable.toString())
+              .setWriteStream(stream)
+              .build();
+      WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+      // Use the JSON stream writer to send records in JSON format.
+      // For more information about JsonStreamWriter, see:
+      // https://googleapis.dev/java/google-cloud-bigquerystorage/latest/com/google/cloud/bigquery/storage/v1beta2/JsonStreamWriter.html
+      try (JsonStreamWriter writer =
+          JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema())
+              .build()) {
+        // Write two batches to the stream, each with 10 JSON records.
+        for (int i = 0; i < 2; i++) {
+          JSONArray jsonArr = new JSONArray();
+          for (int j = 0; j < 10; j++) {
+            // Create a JSON object that is compatible with the table schema.
+            JSONObject record = new JSONObject();
+            record.put("col1", String.format("buffered-record %03d", i));
+            jsonArr.put(record);
+          }
+          ApiFuture<AppendRowsResponse> future = writer.append(jsonArr);
+          AppendRowsResponse response = future.get();
+        }
+
+        // Flush the buffer.
+        FlushRowsRequest flushRowsRequest =
+            FlushRowsRequest.newBuilder()
+                .setWriteStream(writeStream.getName())
+                .setOffset(Int64Value.of(10 * 2 - 1)) // Advance the cursor to the latest record.
+                .build();
+        FlushRowsResponse flushRowsResponse = client.flushRows(flushRowsRequest);
+        // You can continue to write to the stream after flushing the buffer.
+      }
+      System.out.println("Appended and committed records successfully.");
+    } catch (ExecutionException e) {
+      // If the wrapped exception is a StatusRuntimeException, check the state of the operation.
+      // If the state is INTERNAL, CANCELLED, or ABORTED, you can retry. For more information, see:
+      // https://grpc.github.io/grpc-java/javadoc/io/grpc/StatusRuntimeException.html
+      System.out.println(e);
+    }
+  }
+}
+// [END bigquerystorage_jsonstreamwriter_buffered]
