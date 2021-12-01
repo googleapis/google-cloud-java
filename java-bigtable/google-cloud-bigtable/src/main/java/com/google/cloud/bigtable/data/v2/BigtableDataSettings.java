b22633abfe1076afe38054b433c5a9c549367265
Igor Bernstein <igorbernstein@google.com>
2021-12-01 14:48:14 -0500

    chore: fix emulator keepalives (#1083)

--- java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/BigtableDataSettings.java
+++ java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/BigtableDataSettings.java
@@ -128,7 +128,7 @@ public final class BigtableDataSettings {
                         return input.usePlaintext();
                       }
                     })
-                .setKeepAliveTime(Duration.ofSeconds(30)) // sends ping in this interval
+                .setKeepAliveTime(Duration.ofSeconds(61)) // sends ping in this interval
                 .setKeepAliveTimeout(
                     Duration.ofSeconds(10)) // wait this long before considering the connection dead
                 .build());
