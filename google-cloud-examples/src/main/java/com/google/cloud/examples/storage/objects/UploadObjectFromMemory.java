6d3bc60b5adc0da5a0ffc7ad1c689db5a6f55e20
Sameena Shaffeeullah <shaffeeullah@google.com>
2021-12-14 14:50:35 -0800

    samples(storage): Upload file download file into memory

---
+++ google-cloud-examples/src/main/java/com/google/cloud/examples/storage/objects/UploadObjectFromMemory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.examples.storage.objects;
+
+// [START storage_file_upload_from_memory]
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class UploadObjectFromMemory {
+  public static void uploadObjectFromMemory(
+      String projectId, String bucketName, String objectName, String contents) throws IOException {
+    // The ID of your GCP project
+    // String projectId = "your-project-id";
+
+    // The ID of your GCS bucket
+    // String bucketName = "your-unique-bucket-name";
+
+    // The ID of your GCS object
+    // String objectName = "your-object-name";
+
+    // The string of contents you wish to upload
+    // String contents = "Hello world!"
+
+    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    BlobId blobId = BlobId.of(bucketName, objectName);
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+    storage.create(blobInfo, contents.getBytes(StandardCharsets.UTF_8));
+
+    System.out.println(
+        "Object " + objectName + " uploaded to bucket " + bucketName + " with contents " + contents);
+  }
+}
+// [END storage_file_upload_from_memory]
