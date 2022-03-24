5141210d05e9f22ef28b407e1f37187769ab1959
Neenu Shaji <Neenu1995@users.noreply.github.com>
2022-03-24 18:34:12 -0400

    chore: revert to using JDST for nighly integration tests (#1158)

--- java-asset/owlbot.py
+++ java-asset/owlbot.py
@@ -23,6 +23,7 @@ for library in s.get_staging_dirs():
 s.remove_staging_dirs()
 java.common_templates(
     excludes=[
-        ".kokoro/dependencies.sh"
+        ".kokoro/dependencies.sh",
+        ".kokoro/nightly/integration.cfg"
     ]
 )
