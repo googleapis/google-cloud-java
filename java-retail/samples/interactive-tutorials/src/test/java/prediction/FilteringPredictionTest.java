32dc7d59aef4e0c258f3dae0f4a9a568d109270d
Sergey Borisenko <78493601+sborisenkox@users.noreply.github.com>
2022-04-28 01:37:14 +0300

    docs(samples): Retail. Prediction samples (#412)
    * Update README
    
    * the bash scripts are added
    
    * Update README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Update user_environment_setup.sh
    
    * Update user_import_data_to_catalog.sh
    
    * Update README.md
    
    * Add predictions samples.
    
    * Add predictions samples unit-tests.
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: tetiana-karasova <tetiana.karasova@gmail.com>
    Co-authored-by: t-karasova <tkarasova@google.com>
    Co-authored-by: t-karasova <91195610+t-karasova@users.noreply.github.com>
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Karl Weinmeister <11586922+kweinmeister@users.noreply.github.com>

---
+++ java-retail/samples/interactive-tutorials/src/test/java/prediction/FilteringPredictionTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prediction;
+
+import static com.google.common.truth.Truth.assertThat;
+import static prediction.FilteringPrediction.predict;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FilteringPredictionTest {
+
+  private ByteArrayOutputStream bout;
+  private PrintStream originalPrintStream;
+
+  @Before
+  public void setUp() throws IOException, InterruptedException {
+    bout = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bout);
+    originalPrintStream = System.out;
+    System.setOut(out);
+  }
+
+  @Test
+  public void testPredict() {
+    String projectId = System.getenv("GOOGLE_CLOUD_PROJECT");
+    String placementId = System.getenv("GOOGLE_CLOUD_PLACEMENT");
+    String predictPlacement =
+        String.format(
+            "projects/%s/locations/global/catalogs/default_catalog/placements/%s",
+            projectId, placementId);
+
+    predict(predictPlacement);
+
+    String outputResult = bout.toString();
+
+    assertThat(outputResult).contains("Predict request");
+    assertThat(outputResult).contains("filter: \"filterOutOfStockItems\"");
+    assertThat(outputResult).contains("Predict response");
+  }
+
+  @After
+  public void tearDown() {
+    System.out.flush();
+    System.setOut(originalPrintStream);
+  }
+}
