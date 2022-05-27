2c5dbe3ba66d0a0690011312dcfb18ac18f12aea
Neenu1995 <Neenu1995@google.com>
2022-05-27 15:55:22 +0000

    feat: create CoverageAggregator module

---
+++ .kokoro/build.bat
@@ -0,0 +1,3 @@
+:: See documentation in type-shell-output.bat
+￼
+"C:\Program Files\Git\bin\bash.exe" github/google-cloud-java/.kokoro/build.sh
---
+++ .kokoro/build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+## Get the directory of the build script
+scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+## cd to the parent directory, i.e. the root of the git repo
+cd ${scriptDir}/..
+
+function client_has_changes() {
+  CLIENT_NAME=$1
+  if [[ ! -z $(git diff master google-cloud-clients/google-cloud-core*) ]]; then
+    echo "true"
+    return
+  fi
+
+  if [[ ! -z $(git diff master google-cloud-clients/${CLIENT_NAME}) ]]; then
+    echo "true"
+    return
+  fi
+
+  if [[ ! -z $(git diff master google-api-grpc/*${CLIENT_NAME}*) ]]; then
+    echo "true"
+    return
+  fi
+
+  echo "false"
+}
+
+# Print out Java version
+java -version
+echo $JOB_TYPE
+
+if [[ "${SKIP_INTEGRATION_TESTS_IF_NO_CHANGES}" == "true" ]] &&
+   [[ "${JOB_TYPE}" == "integration" ]]; then
+  CLIENT=$(echo ${INTEGRATION_TEST_ARGS} | cut -d' ' -f1 | cut -d'/' -f2)
+  CLIENT_HAS_CHANGES=$(client_has_changes ${CLIENT})
+  if [[ "${CLIENT_HAS_CHANGES}" == "false" ]]; then
+    echo "No difference from master, skipping tests."
+    exit 0
+  fi
+fi
+
+mvn install -B -V \
+  -DskipTests=true \
+  -Dmaven.javadoc.skip=true \
+  -Dgcloud.download.skip=true
+
+# if GOOGLE_APPLICATION_CREDIENTIALS is specified as a relative path prepend Kokoro root directory onto it
+if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS}" && "${GOOGLE_APPLICATION_CREDENTIALS}" != /* ]]; then
+    export GOOGLE_APPLICATION_CREDENTIALS=$(realpath ${KOKORO_ROOT}/src/${GOOGLE_APPLICATION_CREDENTIALS})
+fi
+
+case ${JOB_TYPE} in
+test)
+    mvn test -B
+    ;;
+lint)
+    mvn com.coveo:fmt-maven-plugin:check
+    ;;
+javadoc)
+    mvn javadoc:javadoc javadoc:test-javadoc
+    ;;
+integration)
+    mvn -B -pl ${INTEGRATION_TEST_ARGS} -DtrimStackTrace=false -fae verify
+    ;;
+*)
+    ;;
+esac
