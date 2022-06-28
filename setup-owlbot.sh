64cc22b1720fb6e223cb65725e821726b3a3774d
Neenu1995 <neenushaji@google.com>
2022-06-28 12:30:23 -0400

    chore: fix script

---
+++ setup-owlbot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+repo="$HOME/Documents/java-temp/monorepo/owlbot/google-cloud-java/repos.txt"
+
+while IFS= read -r line; do
+    pushd $line > /dev/null
+    echo "$line"
+    if [[ -f "$HOME/Documents/java-temp/monorepo/owlbot/google-cloud-java/$line/.OwlBot.yaml" ]]; then
+        sed -i '/docker/d' .OwlBot.yaml
+        sed -i '/image/d' .OwlBot.yaml
+        text=$(grep '^.*api_shortname.*' .repo-metadata.json)
+        text=$(echo "$text" | sed 's/\"//g; s/\,//g' )
+        text=${text/api_shortname/api_name}
+        echo -e "\n" $text>> .OwlBot.yaml
+
+    fi
+    popd
+done < "$repo"
