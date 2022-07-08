f496d8ac042c8b72705fd20fa369a455bfa05c79
sai-chaithu <99007515+sai-chaithu@users.noreply.github.com>
2022-07-08 23:57:48 +0530

    feat(samples): added all entity type samples (#976)
    * feat(samples): added all entity type samples
    
    * feat(samples): update all entity type samples
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ java-aiplatform/samples/snippets/src/main/java/aiplatform/GetEntityTypeSample.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Get entity type details. See
+ * https://cloud.google.com/vertex-ai/docs/featurestore/setup before running
+ * the code snippet
+ */
+
+package aiplatform;
+
+// [START aiplatform_get_entity_type_sample]
+
+import com.google.cloud.aiplatform.v1.EntityType;
+import com.google.cloud.aiplatform.v1.EntityTypeName;
+import com.google.cloud.aiplatform.v1.FeaturestoreServiceClient;
+import com.google.cloud.aiplatform.v1.FeaturestoreServiceSettings;
+import com.google.cloud.aiplatform.v1.GetEntityTypeRequest;
+import java.io.IOException;
+
+public class GetEntityTypeSample {
+
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String project = "YOUR_PROJECT_ID";
+    String featurestoreId = "YOUR_FEATURESTORE_ID";
+    String entityTypeId = "YOUR_ENTITY_TYPE_ID";
+    String location = "us-central1";
+    String endpoint = "us-central1-aiplatform.googleapis.com:443";
+    getEntityTypeSample(project, featurestoreId, entityTypeId, location, endpoint);
+  }
+
+  static void getEntityTypeSample(
+      String project, String featurestoreId, String entityTypeId, String location, String endpoint)
+      throws IOException {
+
+    FeaturestoreServiceSettings featurestoreServiceSettings =
+        FeaturestoreServiceSettings.newBuilder().setEndpoint(endpoint).build();
+
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (FeaturestoreServiceClient featurestoreServiceClient =
+        FeaturestoreServiceClient.create(featurestoreServiceSettings)) {
+
+      GetEntityTypeRequest getEntityTypeRequest =
+          GetEntityTypeRequest.newBuilder()
+              .setName(
+                  EntityTypeName.of(project, location, featurestoreId, entityTypeId).toString())
+              .build();
+
+      EntityType entityType = featurestoreServiceClient.getEntityType(getEntityTypeRequest);
+      System.out.println("Get Entity Type Response");
+      System.out.println(entityType);
+    }
+  }
+}
+// [END aiplatform_get_entity_type_sample]
