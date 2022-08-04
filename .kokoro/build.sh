ace09fc2cdcc0d08a4a99fdf52301017f232303d
Lawrence Qiu <lawrenceqiu@google.com>
2022-08-04 19:33:31 +0000

    feat: Time the installation for packages

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -27,6 +27,10 @@ source ${scriptDir}/common.sh
 mkdir -p ${HOME}/.m2
 cp settings.xml ${HOME}/.m2
 
+echo "Download the dependencies...Run 1"
+mvn clean compile -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -DskipTests=true -Dclirr.skip=true -Denforcer.skip=true
+
+echo "Compile the dependencies...Run 2"
 mvn clean install -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -DskipTests=true -Dclirr.skip=true -Denforcer.skip=true
 
 # if GOOGLE_APPLICATION_CREDENTIALS is specified as a relative path, prepend Kokoro root directory onto it
