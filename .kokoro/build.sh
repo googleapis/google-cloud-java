160a94a4a16800b5a34865dbd1f0ac85bead8dd7
Lawrence Qiu <lawrenceqiu@google.com>
2022-08-04 20:33:57 +0000

    feat: Ignore all plugins

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -27,11 +27,12 @@ source ${scriptDir}/common.sh
 mkdir -p ${HOME}/.m2
 cp settings.xml ${HOME}/.m2
 
-echo "Download and compile the dependencies...Run 1"
-mvn clean compile -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -Dclirr.skip=true -Denforcer.skip=true
+#echo "Download and compile the dependencies...Run 1"
+#mvn clean
+#mvn compile -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -Dclirr.skip=true -Denforcer.skip=true -Dcheckstyle.skip=true -Dflatten.skip=true -Djacoco.skip=true
 
-echo "Compile the dependencies...Run 2"
-mvn clean compile -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -Dclirr.skip=true -Denforcer.skip=true
+#echo "Compile the dependencies...Run 2"
+mvn clean install -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -DskipTests=true -Dclirr.skip=true -Denforcer.skip=true -Dcheckstyle.skip=true -Dflatten.skip=true -Djacoco.skip=true
 
 # if GOOGLE_APPLICATION_CREDENTIALS is specified as a relative path, prepend Kokoro root directory onto it
 if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS}" && "${GOOGLE_APPLICATION_CREDENTIALS}" != /* ]]; then
