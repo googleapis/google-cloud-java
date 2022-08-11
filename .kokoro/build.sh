f5d07d882337ea19e9713b1500b005ee22a36edc
Lawrence Qiu <lawrenceqiu@google.com>
2022-08-11 21:47:47 +0000

    feat: Add IT tests for modified modules

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -23,11 +23,46 @@ cd ${scriptDir}/..
 # include common functions
 source ${scriptDir}/common.sh
 
+# TODO: Switch monorepo_script_output to main
+# Find the root commit between the two branches
+base_commit=$(git merge-base monorepo_script_output HEAD)
+echo "Base Commit: ${base_commit}"
+
+# Find the files changed from when the PR branched to the last commit
+# Filter for java modules and get all the unique elements
+# grep returns 1 (error code) and exits the pipeline if there is no match
+# If there is no match, it will return true so the rest of the commands can run
+modified_files=$(git diff --name-only "${base_commit}" HEAD)
+echo "Modified files: ${modified_files}"
+directories=$(echo "${modified_files}" | grep -e 'java-.*' || true)
+echo "Files in java modules: ${directories}"
+if [[ -n $directories ]]; then
+  directories=$(echo "${directories}" | cut -d '/' -f1 | sort -u)
+  dir_list=()
+  for directory in $directories
+  do
+    dir_list+=($directory)
+  done
+  # Combine each entry with a comma
+  module_list=$(IFS=, ; echo "${dir_list[*]}")
+fi
+echo "Module List: ${module_list}"
+
 # Use GCP Maven Mirror
 mkdir -p ${HOME}/.m2
 cp settings.xml ${HOME}/.m2
 
-mvn install -pl '!google-cloud-gapic-bom,!CoverageAggregator' -T 1C -DskipTests=true -Dclirr.skip=true -Denforcer.skip=true -Dcheckstyle.skip=true -Dflatten.skip=true -Danimal.sniffer.skip=true -Djacoco.skip=true
+mvn -B -pl "!google-cloud-gapic-bom,!CoverageAggregator" \
+    -ntp \
+    -DskipTests=true \
+    -Dclirr.skip=true \
+    -Denforcer.skip=true \
+    -Dcheckstyle.skip=true \
+    -Dflatten.skip=true \
+    -Danimal.sniffer.skip=true \
+    -Djacoco.skip=true \
+    -T 1C \
+    install
 
 # if GOOGLE_APPLICATION_CREDENTIALS is specified as a relative path, prepend Kokoro root directory onto it
 if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS}" && "${GOOGLE_APPLICATION_CREDENTIALS}" != /* ]]; then
@@ -38,84 +73,92 @@ RETURN_CODE=0
 set +e
 
 case ${JOB_TYPE} in
-test)
-  mvn test -B -ntp -Dclirr.skip=true -Denforcer.skip=true
-  RETURN_CODE=$?
-  ;;
-lint)
-  mvn com.coveo:fmt-maven-plugin:check -B -ntp
-  RETURN_CODE=$?
-  ;;
-javadoc)
-  mvn javadoc:javadoc javadoc:test-javadoc -B -ntp
-  RETURN_CODE=$?
-  ;;
-integration)
-  if [ -f "${KOKORO_GFILE_DIR}/secret_manager/java-bigqueryconnection-samples-secrets" ]
-         then
-             source "${KOKORO_GFILE_DIR}/secret_manager/java-bigqueryconnection-samples-secrets"
-  fi
+  test)
+    mvn test -B -ntp -Dclirr.skip=true -Denforcer.skip=true
+    RETURN_CODE=$?
+    ;;
+  lint)
+    mvn com.coveo:fmt-maven-plugin:check -B -ntp
+    RETURN_CODE=$?
+    ;;
+  javadoc)
+    mvn javadoc:javadoc javadoc:test-javadoc -B -ntp
+    RETURN_CODE=$?
+    ;;
+  integration)
+    if [ -f "${KOKORO_GFILE_DIR}/secret_manager/java-bigqueryconnection-samples-secrets" ]
+           then
+               source "${KOKORO_GFILE_DIR}/secret_manager/java-bigqueryconnection-samples-secrets"
+    fi
 
-  mvn -B -pl '!google-cloud-gapic-bom,!CoverageAggregator,!java-game-servers/google-cloud-game-servers' \
-    -ntp \
-    -Penable-integration-tests \
-    -DtrimStackTrace=false \
-    -Dclirr.skip=true \
-    -Denforcer.skip=true \
-    -Dcheckstyle.skip=true \
-    -Dflatten.skip=true \
-    -Danimal.sniffer.skip=true \
-    -Djacoco.skip=true \
-    -DskipUnitTests=true \
-    -fae \
-    -T 1C \
-    verify
-  RETURN_CODE=$?
-  ;;
-graalvm)
-  # Run Unit and Integration Tests with Native Image
-  mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
-  RETURN_CODE=$?
-  ;;
-graalvm17)
-  # Run Unit and Integration Tests with Native Image
-  mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
-  RETURN_CODE=$?
-  ;;
-samples)
-  SAMPLES_DIR=samples
-  # only run ITs in snapshot/ on presubmit PRs. run ITs in all 3 samples/ subdirectories otherwise.
-  if [[ ! -z ${KOKORO_GITHUB_PULL_REQUEST_NUMBER} ]]
-  then
-    SAMPLES_DIR=samples/snapshot
-  fi
-
-  if [[ -f ${SAMPLES_DIR}/pom.xml ]]
-  then
-      for FILE in ${KOKORO_GFILE_DIR}/secret_manager/*-samples-secrets; do
-        [[ -f "$FILE" ]] || continue
-        source "$FILE"
-      done
-
-      pushd ${SAMPLES_DIR}
-      mvn -B \
-        -ntp \
-        -DtrimStackTrace=false \
-        -Dclirr.skip=true \
-        -fae \
-        verify
+    if [[ -n $module_list ]]; then
+      echo "Running Integration Tests for: ${module_list}"
+      mvn -B -pl "${module_list}" \
+          -amd \
+          -ntp \
+          -Penable-integration-tests \
+          -DtrimStackTrace=false \
+          -Dclirr.skip=true \
+          -Denforcer.skip=true \
+          -Dcheckstyle.skip=true \
+          -Dflatten.skip=true \
+          -Danimal.sniffer.skip=true \
+          -Djacoco.skip=true \
+          -DskipUnitTests=true \
+          -fae \
+          -T 1C \
+          verify
       RETURN_CODE=$?
-      popd
-  else
-      echo "no sample pom.xml found - skipping sample tests"
-  fi
-  ;;
-clirr)
-  mvn -B -ntp -Denforcer.skip=true clirr:check
-  RETURN_CODE=$?
-  ;;
-*)
-  ;;
+      echo "Finished Integration Tests for: ${module_list}"
+    else
+      echo "No integration tests to run"
+      RETURN_CODE=0
+    fi
+    ;;
+  graalvm)
+    # Run Unit and Integration Tests with Native Image
+    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
+    RETURN_CODE=$?
+    ;;
+  graalvm17)
+    # Run Unit and Integration Tests with Native Image
+    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
+    RETURN_CODE=$?
+    ;;
+  samples)
+    SAMPLES_DIR=samples
+    # only run ITs in snapshot/ on presubmit PRs. run ITs in all 3 samples/ subdirectories otherwise.
+    if [[ ! -z ${KOKORO_GITHUB_PULL_REQUEST_NUMBER} ]]
+    then
+      SAMPLES_DIR=samples/snapshot
+    fi
+
+    if [[ -f ${SAMPLES_DIR}/pom.xml ]]
+    then
+        for FILE in ${KOKORO_GFILE_DIR}/secret_manager/*-samples-secrets; do
+          [[ -f "$FILE" ]] || continue
+          source "$FILE"
+        done
+
+        pushd ${SAMPLES_DIR}
+        mvn -B \
+          -ntp \
+          -DtrimStackTrace=false \
+          -Dclirr.skip=true \
+          -fae \
+          verify
+        RETURN_CODE=$?
+        popd
+    else
+        echo "no sample pom.xml found - skipping sample tests"
+    fi
+    ;;
+  clirr)
+    mvn -B -ntp -Denforcer.skip=true clirr:check
+    RETURN_CODE=$?
+    ;;
+  *)
+    ;;
 esac
 
 if [ "${REPORT_COVERAGE}" == "true" ]
