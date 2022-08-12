b66da2a0e9ecf65428a128ce519e7b073bcb6e0e
Lawrence Qiu <lawrenceqiu@google.com>
2022-08-12 17:02:30 +0000

    feat: Fix formatting

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -28,9 +28,9 @@ source ${scriptDir}/common.sh
 # grep returns 1 (error code) and exits the pipeline if there is no match
 # If there is no match, it will return true so the rest of the commands can run
 modified_files=$(git diff --name-only $KOKORO_GITHUB_PULL_REQUEST_COMMIT $KOKORO_GITHUB_PULL_REQUEST_TARGET_BRANCH)
-printf "Modified files:\n%s" "${modified_files}"
+printf "Modified files:\n%s\n" "${modified_files}"
 directories=$(echo "${modified_files}" | grep -e 'java-.*' || true)
-printf "Files in java modules:\n%s" "${directories}"
+printf "Files in java modules:\n%s\n" "${directories}"
 if [[ -n $directories ]]; then
   directories=$(echo "${directories}" | cut -d '/' -f1 | sort -u)
   dir_list=()
@@ -41,7 +41,7 @@ if [[ -n $directories ]]; then
   # Combine each entry with a comma
   module_list=$(IFS=, ; echo "${dir_list[*]}")
 fi
-printf "Module List:\n%s" "${module_list}"
+printf "Module List:\n%s\n" "${module_list}"
 
 # Use GCP Maven Mirror
 mkdir -p ${HOME}/.m2
@@ -87,7 +87,7 @@ case ${JOB_TYPE} in
     fi
 
     if [[ -n $module_list ]]; then
-      printf "Running Integration Tests for:\n%s" "${module_list}"
+      printf "Running Integration Tests for:\n%s\n" "${module_list}"
       mvn -B -pl "${module_list}" \
           -amd \
           -ntp \
@@ -104,7 +104,7 @@ case ${JOB_TYPE} in
           -T 1C \
           verify
       RETURN_CODE=$?
-      printf "Finished Integration Tests for:\m%s" "${module_list}"
+      printf "Finished Integration Tests for:\n%s\n" "${module_list}"
     else
       echo "No integration tests to run"
       RETURN_CODE=0
