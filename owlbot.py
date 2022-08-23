b4fd72167576ad6082a7c9890783b9d35797f222
Daniel Sanche <d.sanche14@gmail.com>
2022-08-23 14:55:32 -0500

    chore: added extra variables owlbot kokoro configs (#822)

--- owlbot.py
+++ owlbot.py
@@ -14,7 +14,9 @@
 
 """This script is used to synthesize generated parts of this library."""
 
+import os
 import synthtool.languages.java as java
+import synthtool as s
 
 java.common_templates(excludes=[
     '.gitignore',
@@ -23,3 +25,25 @@ java.common_templates(excludes=[
     'CONTRIBUTING.md',
     '.github/auto-label.yaml',
 ])
+
+# --------------------------------------------------------------------------
+# Modify test configs
+# --------------------------------------------------------------------------
+
+# add shared environment variables to test configs
+s.move(
+    ".kokoro/common_env_vars.cfg",
+    ".kokoro/common.cfg",
+    merge=lambda src, dst, _, : f"{dst}\n{src}",
+)
+tracked_subdirs = ["continuous", "presubmit", "release", "nightly"]
+for subdir in tracked_subdirs:
+    for path, subdirs, files in os.walk(f".kokoro/{subdir}"):
+        for name in files:
+            if name == "common.cfg":
+                file_path = os.path.join(path, name)
+                s.move(
+                    ".kokoro/common_env_vars.cfg",
+                    file_path,
+                    merge=lambda src, dst, _, : f"{dst}\n{src}",
+                )
