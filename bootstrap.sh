d3f9049bee8a019d9a9af8bdbb91eb72b7404a0d
ddixit14 <deepankardixit@google.com>
2022-08-25 15:54:19 -0400

    fixing

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,11 +32,11 @@ do
   git filter-repo --to-subdirectory-filter ${service}
 
   # Search for <parent> tag in module pom and replace the next three lines -- groupId, artifcatId, and version
-  sed -i.bak -e '/<parent>/{N;s/com.google.cloud/com.google.api/;N;s/google-cloud-shared-config/google-cloud-java/;N;s/<version>.*<\/version>/<version>0.0.1<\/version>/}' ${service}/pom.xml && rm ${service}/pom.xml.bak
+  sed -i.bak -e '/<parent>/{N;s/com.google.cloud/com.google.api/;N;s/google-cloud-shared-config/google-cloud-java/;N;s/<version>.*<\/version>/<version>0.0.1-SNAPSHOT<\/version>/}' ${service}/pom.xml && rm ${service}/pom.xml.bak
 
   NAME=$(jq -r '.distribution_name' ${service}/.repo-metadata.json | cut -d ':' -f 2)
   # Search for <parent> tag in module bom and replace the next three lines -- groupId, artifcatId, and version
-  sed -i.bak -e '/<parent>/{N;s/com.google.cloud/com.google.api/;N;s/google-cloud-shared-config/google-cloud-java/;N;s/<version>.*<\/version>/<version>0.0.1<\/version>\n    <relativePath>..\/..\/pom.xml<\/relativePath>/}' ${service}/${NAME}-bom/pom.xml && rm ${service}/${NAME}-bom/pom.xml.bak
+  sed -i.bak -e '/<parent>/{N;s/com.google.cloud/com.google.api/;N;s/google-cloud-shared-config/google-cloud-java/;N;s/<version>.*<\/version>/<version>0.0.1-SNAPSHOT<\/version>\n    <relativePath>..\/..\/pom.xml<\/relativePath>/}' ${service}/${NAME}-bom/pom.xml && rm ${service}/${NAME}-bom/pom.xml.bak
 
   # setup owlbot files correctly to match monorepo configuration
   cp ${service}/.github/.OwlBot.yaml ${service}/.OwlBot.yaml
@@ -93,15 +93,10 @@ sh generation/print_root_pom.sh > pom.xml
 git add google-cloud-gapic-bom/pom.xml
 git commit -am 'feat: create google-cloud-gapic-bom'
 
-
-num_modules="$(wc -l < ../../repos.txt)"
-echo "{" > .release-please-manifest.json
-
-echo ""\""google-cloud-gapic-bom"\"": "\""0.0.0"\""," >> .release-please-manifest.json
 cp generation/gapic_bom_versions.txt google-cloud-gapic-bom/versions.txt
 
 # Generate Release Please configuration files
-bash generation/release_please_config_generation.sh
+bash generation/generate_release_please_config.sh
 
 git add --all
 git commit -am 'feat: create release please configuration'
