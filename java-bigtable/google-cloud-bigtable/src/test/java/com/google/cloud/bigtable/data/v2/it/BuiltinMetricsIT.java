8d263541137ff01cb91f30cb9bccc683abdad5ba
Mattie Fu <mattiefu@google.com>
2022-09-01 12:48:53 -0400

    test: disable integration test (#1375)
    * test: disable integration test
    
    * change to ignore
    
    * ignore the class

--- java-bigtable/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/it/BuiltinMetricsIT.java
+++ java-bigtable/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/it/BuiltinMetricsIT.java
@@ -37,11 +37,14 @@ import java.util.ArrayList;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+// TODO: re-enable this test
 @RunWith(JUnit4.class)
+@Ignore
 public class BuiltinMetricsIT {
   @ClassRule public static TestEnvRule testEnvRule = new TestEnvRule();
   public static MetricServiceClient metricClient;
