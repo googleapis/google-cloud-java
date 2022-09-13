83576b2ec1293f63b5d917d981786635618947e1
Tomo Suzuki <suztomo@google.com>
2022-09-13 12:57:55 +0000

    format and comment

--- generation/set_parent_pom.sh
+++ generation/set_parent_pom.sh
@@ -7,12 +7,14 @@ set -ef
 
 # Run this script at the root of google-cloud-java repository
 
-
+# First, read the values from the root pom.xml
 parent_version=$(perl -nle 'print $1 if m|<version>(.+)</version>|' pom.xml |head -1)
 parent_group_id=$(perl -nle 'print $1 if m|<groupId>(.+)</groupId>|' pom.xml |head -1)
 parent_artifact_id=$(perl -nle 'print $1 if m|<artifactId>(.+)</artifactId>|' pom.xml |head -1)
 
+# Then, apply the values as the parent pom of each module
 for module in $(find . -mindepth 2 -maxdepth 2 -name pom.xml |sort | xargs dirname); do
+  # example value of module is "./java-accessapproval"
   if [[ "${module}" = *google-cloud-gapic-bom ]] || [[ "${module}" = *CoverageAggregator ]]; then
     continue
   fi
