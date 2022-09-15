2f17a4f83c50093bb0cbf1c4f00ca2cf06967caf
Lawrence Qiu <lawrenceqiu@google.com>
2022-09-15 17:24:50 +0000

    chore: Remove the waiting for deleting instances/addresses

--- .kokoro/common.sh
+++ .kokoro/common.sh
@@ -117,8 +117,6 @@ function run_graalvm_tests() {
     -Dcheckstyle.skip=true \
     -Dflatten.skip=true \
     -Danimal.sniffer.skip=true \
-    -DskipUnitTests=true \
-    -Penable-integration-tests \
     -Pnative \
     -fae \
     test
