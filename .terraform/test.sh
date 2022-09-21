65ff848ab6b20a9d1ba52f42f48466da3b3be019
Burke Davison <burkedavison@google.com>
2022-09-21 15:28:39 -0400

    fix: Ensure 'set-quota-project' is set to the created GCP project. Despite generating an error, it works and is necessary.

--- .terraform/test.sh
+++ .terraform/test.sh
@@ -44,6 +44,7 @@ function performTest() {
   GOOGLE_CLOUD_PROJECT=$(terraform output -raw project_id)
   export GOOGLE_CLOUD_PROJECT
   gcloud config set project "$GOOGLE_CLOUD_PROJECT"
+  gcloud auth application-default set-quota-project "$GOOGLE_CLOUD_PROJECT"
 
   # Set module-specific environment variables for upcoming integration test(s)
   if [[ -f "./env.sh" ]]; then
