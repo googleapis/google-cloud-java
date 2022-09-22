cf3c219d3293b51110741d89d5560aa8f9508a95
Burke Davison <burkedavison@google.com>
2022-09-22 18:17:36 -0400

    chore: Add java-trace terraform config

---
+++ java-trace/.terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_project_service" "trace_api" {
+  service            = "cloudtrace.googleapis.com"
+  disable_on_destroy = false
+}
