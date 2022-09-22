52b48842d980173fe8542b2749e41fb04fb08cb6
Burke Davison <burkedavison@google.com>
2022-09-22 18:02:07 -0400

    chore: Add java-kms terraform config

---
+++ java-kms/.terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_project_service" "kms_api" {
+  service            = "cloudkms.googleapis.com"
+  disable_on_destroy = false
+}
