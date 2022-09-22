dad488eac267462c45d357a5b8110e0055f8ba67
Burke Davison <burkedavison@google.com>
2022-09-22 13:20:54 -0400

    chore: Add 'auto_create_subnetworks' to container network configuration.

--- java-container/.terraform/main.tf
+++ java-container/.terraform/main.tf
@@ -33,9 +33,10 @@ resource "google_project_service" "compute_api" {
   disable_on_destroy = false
 }
 resource "google_compute_network" "java_container_network" {
-  name       = "java-container-network"
-  project    = module.project.project_id
-  depends_on = [
+  name                    = "java-container-network"
+  project                 = module.project.project_id
+  auto_create_subnetworks = true
+  depends_on              = [
     module.project,
     google_project_service.compute_api,
     google_project_service.container_api
