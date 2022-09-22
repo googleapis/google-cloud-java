cbe9b98830581bdcd720f267a6cb61d4b6a9cc0e
Burke Davison <burkedavison@google.com>
2022-09-22 18:11:41 -0400

    chore: Add java-speech terraform config

---
+++ java-speech/.terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_project_service" "speech_api" {
+  service            = "speech.googleapis.com"
+  disable_on_destroy = false
+}
