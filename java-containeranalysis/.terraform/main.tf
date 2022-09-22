9eaac549fd867fabbe59a89e03693a9c7765b167
Burke Davison <burkedavison@google.com>
2022-09-22 17:42:22 -0400

    chore: Add java-containeranalysis terraform config

---
+++ java-containeranalysis/.terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
