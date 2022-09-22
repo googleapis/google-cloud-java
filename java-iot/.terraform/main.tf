57a1ef3b4c889ac2a0af6471e75bf47430e369c3
Burke Davison <burkedavison@google.com>
2022-09-22 17:59:06 -0400

    chore: Add java-iot terraform config

---
+++ java-iot/.terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_project_service" "iot_api" {
+  service            = "cloudiot.googleapis.com"
+  disable_on_destroy = false
+}
