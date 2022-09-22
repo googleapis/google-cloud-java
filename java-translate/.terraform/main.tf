f7543ae7e859f928100c8501aaf6566c0934bb13
Burke Davison <burkedavison@google.com>
2022-09-22 18:19:36 -0400

    chore: Add java-translate terraform config

---
+++ java-translate/.terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_project_service" "translate_api" {
+  service            = "translate.googleapis.com"
+  disable_on_destroy = false
+}
