2dc9964c147bfa3a2db9f30e256f9ad28983a2ee
Lawrence Qiu <lawrenceqiu@google.com>
2022-09-23 02:45:43 +0000

    chore: Call delete samples script afterwards

--- generation/generate_diff.sh
+++ generation/generate_diff.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-modules=$(mvn help:evaluate -Dexpression=project.modules | grep '<.*>.*</.*>' | sed -e 's/<.*>\(.*\)<\/.*>/\1/g' | grep 'java')
+modules=$(mvn help:evaluate -Dexpression=project.modules | grep '<.*>.*</.*>' | grep 'java' | sed -e 's/<.*>\(.*\)<\/.*>/\1/g')
 
-for module in "${modules[@]}"; do
+for module in $modules; do
+  echo "Running for ${module}"
   rm -rf "${module}"
   git clone "https://github.com/googleapis/${module}"
   rm -rf "${module}/.git"
-  ./delete_non_generated_samples.sh
+  echo "Done running for ${module}"
 done
+
+./generation/delete_non_generated_samples.sh
+
+echo "Done running script"
