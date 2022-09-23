1e47be82db2436fe5a427b727d5867e51cff57cb
Lawrence Qiu <lawrenceqiu@google.com>
2022-09-23 18:16:06 +0000

    chore: Add push to multiple branches

--- generation/generate_diff.sh
+++ generation/generate_diff.sh
@@ -13,22 +13,27 @@ for module in $modules; do
 done
 
 current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+diff_java_branch="main-diff_java"
+diff_non_java_branch="main-diff_non_java"
 
-if [[ $(git branch | grep "main-diff_java") ]]; then
-  git checkout main-diff_java
+if [[ $(git branch | grep "${diff_java_branch}") ]]; then
+  git checkout "${diff_java_branch}"
 else
-  git checkout -b main-diff_java
+  git checkout -b "${diff_java_branch}"
 fi
 git add "*.java"
 git commit -m "chore: Adding java diffs" --no-verify
+git push origin "${diff_java_branch}"
 
-if [[ $(git branch | grep "main-diff_non_java") ]]; then
-  git checkout main-diff_non_java
+if [[ $(git branch | grep "${diff_non_java_branch}") ]]; then
+  git checkout "${diff_non_java_branch}"
 else
-  git checkout -b main-diff_non_java
+  git checkout -b "${diff_non_java_branch}"
 fi
 git add .
 git commit -m "chore: Adding non-java diffs" --no-verify
+git push origin "${diff_non_java_branch}"
+
 git checkout "${current_branch}"
 
 echo "Done running script"
