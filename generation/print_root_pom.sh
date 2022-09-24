f4c4649e36f64fb1b53be4a0681656826b2c5f9c
Mike Eltsufin <meltsufin@google.com>
2022-09-23 21:46:46 -0400

    chore: update scripts and make them macOS compatible

--- generation/print_root_pom.sh
+++ generation/print_root_pom.sh
@@ -5,7 +5,7 @@ set -e
 GENERATION_DIR=$(dirname -- "$0");
 
 # Find all Maven modules (a directory that contains pom.xml)
-modules=$(find . -mindepth 2 -maxdepth 2 -name pom.xml |sort | xargs dirname \
-    |sed -e 's|./||' | xargs -I '{}' echo "    <module>{}</module>")
+find . -mindepth 2 -maxdepth 2 -name pom.xml |sort | xargs dirname \
+    |sed -e 's|./||' | xargs -I '{}' echo "    <module>{}</module>" > /tmp/repo-modules.txt
 
-awk -v MODULES="${modules}" '1;/<modules>/{print MODULES}' "${GENERATION_DIR}/parent.pom.xml"
+awk -v MODULES="`awk -v ORS='\\\\n' '1' /tmp/repo-modules.txt`" '1;/<modules>/{print MODULES}' ${GENERATION_DIR}/parent.pom.xml
