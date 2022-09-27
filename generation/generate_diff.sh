b5d9af345424c9a416c21eb3813bdb90ca6b2bc4
Lawrence Qiu <lawrenceqiu@google.com>
2022-09-27 15:51:45 +0000

    chore: Add more speciic regex for branch

--- generation/generate_diff.sh
+++ generation/generate_diff.sh
@@ -44,9 +44,9 @@ function retry_with_backoff {
 [ -z "`git config user.name`" ] && git config --global user.name "${USERNAME:-script}"
 
 current_branch="main-diff"
-diff_java_branch="main-diff_java"
-diff_java_it_branch="main-diff_java_it"
-diff_non_java_branch="main-diff_non_java"
+diff_java_branch="^main-diff_java$"
+diff_java_it_branch="^main-diff_java_it$"
+diff_non_java_branch="^main-diff_non_java$"
 
 if [[ $(git branch | grep "${current_branch}") ]]; then
   git checkout "${current_branch}"
