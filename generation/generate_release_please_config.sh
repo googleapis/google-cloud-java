dd426fa094b687db250a182a29afa96c321a605f
Lawrence Qiu <lawrenceqiu@google.com>
2022-09-28 19:01:34 +0000

    chore: Add in manual version bump

--- generation/generate_release_please_config.sh
+++ generation/generate_release_please_config.sh
@@ -13,7 +13,7 @@ release_please_config_file="release-please-config.json"
 [ -e "${release_please_config_file}" ] && rm "${release_please_config_file}"
 
 # Gapic-Bom has not been released (set it as v0.0.0)
-echo -e "{\n${tab}\"google-cloud-gapic-bom\": \"0.0.0\"," > "${release_please_manifest_file}"
+echo -e "{\n${tab}\"google-cloud-gapic-bom\": \"0.0.0\"," >"${release_please_manifest_file}"
 
 # We are manually excluding two modules -- TODO: Update this for any other exclusions
 num_modules=$(find . -mindepth 2 -maxdepth 2 -name pom.xml | wc -l)
@@ -49,13 +49,24 @@ for path in $(find . -mindepth 2 -maxdepth 2 -name pom.xml | sort | xargs dirnam
     num_modules=$((num_modules - 1))
   fi
 
+  #for multi-module libraries, sync the versions to released(non-snapshot):current(snapshot) type
+  cat ${version_file} | while read line; do
+    if [[ ${line} =~ [0-9] ]] && ! [[ ${line} == *"SNAPSHOT"* ]]; then
+      artifact_name=$(echo "${line}" | awk -F':' '{print $1}')
+      old_version=$(echo "${line}" | awk -F':' '{print $3}')
+      new_version=$(echo ${old_version} | awk -F'.' '{print $1"."$2"."$3+1}' | sed s/[.]$//)
+      new_version="${new_version}-SNAPSHOT"
+      sed -i.bak -e "s|${artifact_name}:${old_version}:${old_version}|${artifact_name}:${old_version}:${new_version}|" "${version_file}" && rm "${version_file}".bak
+    fi
+  done
+
   # Adding the line to manifest config file
-  echo -e "${rp_manifest_line}" >> "${release_please_manifest_file}"
+  echo -e "${rp_manifest_line}" >>"${release_please_manifest_file}"
 done
 
 # End the .json file with the closing bracket
-echo "}" >> "${release_please_manifest_file}"
+echo "}" >>"${release_please_manifest_file}"
 
 # Replace the package list where `ALL_PACKAGES` string is in the release_please_config_raw.json
 awk -v "packagesList=${rp_config_line}" '{gsub(/ALL_PACKAGES/,packagesList)}1' \
-  "${GENERATION_DIR}/release_please_config_raw.json" > "${release_please_config_file}"
+  "${GENERATION_DIR}/release_please_config_raw.json" >"${release_please_config_file}"
