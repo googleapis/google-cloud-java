cd1b082f956c4e544041a2e9c2bdce4ba8dbe3c9
Burke Davison <burkedavison@google.com>
2022-09-29 10:16:51 -0400

    chore: Add java-scheduler, although not yet reliable.

---
+++ java-scheduler/.terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    time = {
+      source = "hashicorp/time"
+    }
+  }
+}
+
+module "project-services" {
+  source = "terraform-google-modules/project-factory/google//modules/project_services"
+
+  project_id                  = var.inputs.project_id
+  enable_apis                 = true
+  disable_services_on_destroy = false
+  depends_on                  = [google_project.project]
+  activate_apis               = [
+    "pubusb.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "cloudtrace.googleapis.com",
+  ]
+}
+resource "time_sleep" "for_1m_allowServicesTimeToFullyEnable" {
+  depends_on      = [module.project-services]
+  create_duration = "1m"
+}
