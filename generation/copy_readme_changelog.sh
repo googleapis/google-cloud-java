64fce016b57eb6c5c6a1cba58ba155aea2b39571
Lawrence Qiu <lawrenceqiu@google.com>
2022-09-30 21:13:33 +0000

    chore: Check if readme/changelog url exists

--- generation/copy_readme_changelog.sh
+++ generation/copy_readme_changelog.sh
@@ -3,17 +3,18 @@
 set -e
 
 for path in $(find . -mindepth 2 -maxdepth 2 -name pom.xml | sort | xargs dirname); do
-  if [[ "${path}" =~ google-cloud-gapic-bom ]] || [[ "${path}" =~ CoverageAggregator ]]; then
-    continue
-  fi
   pushd "${path}"
 
   module_name="${path:2}"
   raw_readme_url="https://raw.githubusercontent.com/googleapis/${module_name}/main/README.md"
   raw_changelog_url="https://raw.githubusercontent.com/googleapis/${module_name}/main/CHANGELOG.md"
 
-  curl -s "${raw_readme_url}" > README.md
-  curl -s "${raw_changelog_url}" > CHANGELOG.md
+  if curl --output /dev/null --silent --head --fail "${raw_readme_url}"; then
+    curl -s "${raw_readme_url}" > README.md
+  fi
+  if curl --output /dev/null --silent --head --fail "${raw_changelog_url}"; then
+    curl -s "${raw_changelog_url}" > CHANGELOG.md
+  fi
 
   popd
 done
--- java-automl/README.md
+++ java-automl/README.md
@@ -50,7 +50,7 @@ If you are using Maven without BOM, add this to your dependencies:
 <dependency>
   <groupId>com.google.cloud</groupId>
   <artifactId>google-cloud-automl</artifactId>
-  <version>2.3.13</version>
+  <version>2.3.14</version>
 </dependency>
 <dependency>
   <groupId>com.google.cloud</groupId>
