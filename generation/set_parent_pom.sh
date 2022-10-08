fd77b453b0d69dc0e3fd1ccdf84ddd6c66cc664b
Mike Eltsufin <meltsufin@google.com>
2022-10-08 00:31:20 -0400

    chore: improve set_parent_pom.sh

--- generation/set_parent_pom.sh
+++ generation/set_parent_pom.sh
@@ -7,10 +7,19 @@ set -ef
 
 # Run this script at the root of google-cloud-java repository
 
-# First, read the values from the parent pom.xml
-parent_version=$(perl -nle 'print $1 if m|<version>(.+)</version>|' google-cloud-jar-parent/pom.xml |head -1)
-parent_group_id=$(perl -nle 'print $1 if m|<groupId>(.+)</groupId>|' google-cloud-jar-parent/pom.xml |head -1)
-parent_artifact_id=$(perl -nle 'print $1 if m|<artifactId>(.+)</artifactId>|' google-cloud-jar-parent/pom.xml |head -1)
+function replaceParent {
+  parent_pom=$1
+  # First, read the values from the parent pom.xml
+  parent_version=$(perl -nle 'print $1 if m|<version>(.+)</version>|' "$parent_pom"|head -1)
+  parent_group_id=$(perl -nle 'print $1 if m|<groupId>(.+)</groupId>|' "$parent_pom" |head -1)
+  parent_artifact_id=$(perl -nle 'print $1 if m|<artifactId>(.+)</artifactId>|' "$parent_pom"|head -1)
+  relativePath=$(echo "$parent_pom" | sed 's/\//\\\//g')
+
+  # Search for <parent> tag in module pom and replace the next three lines -- groupId, artifcatId, and version
+  perl_command="s/\s*<parent>.*?<\/parent>/\n\n  <parent>\n    <groupId>${parent_group_id}<\/groupId>\n    <artifactId>${parent_artifact_id}<\/artifactId>\n    <version>${parent_version}<\/version><!-- {x-version-update:${parent_artifact_id}:current} -->\n    <relativePath>${relativePath}<\/relativePath>\n  <\/parent>/s"
+  # execute the replacement in pom.xml
+  perl -0pe "$perl_command" pom.xml > pom.xml.new && rm pom.xml && mv pom.xml.new pom.xml
+}
 
 # Then, apply the values as the parent pom of each module
 for module in $(find . -mindepth 2 -maxdepth 2 -name pom.xml | sort --dictionary-order | xargs dirname); do
@@ -20,19 +29,14 @@ for module in $(find . -mindepth 2 -maxdepth 2 -name pom.xml | sort --dictionary
   fi
   echo "Processing module $module"
   pushd $module
-  # Search for <parent> tag in module pom and replace the next three lines -- groupId, artifcatId, and version
-  perl_command="s/\s*<parent>.*?<\/parent>/\n\n  <parent>\n    <groupId>${parent_group_id}<\/groupId>\n    <artifactId>${parent_artifact_id}<\/artifactId>\n    <version>${parent_version}<\/version><!-- {x-version-update:${parent_artifact_id}:current} -->\n    <relativePath>..\/google-cloud-jar-parent\/pom.xml<\/relativePath>\n  <\/parent>/s"
-  # execute the replacement in pom.xml
-  perl -0pe "$perl_command" pom.xml > pom.xml.new && rm pom.xml && mv pom.xml.new pom.xml
+
+  replaceParent ../google-cloud-jar-parent/pom.xml pom.xml
 
   # update the bom projects as well by removing parent
   if ls -1 | grep 'bom'; then
-    BOM=$(ls -1 | grep 'bom')'/pom.xml'
-    echo the bom is $BOM
-    # Search for <parent> tag in module bom and replace the next three lines -- groupId, artifcatId, and version
-    perl_command="s/\s*<parent>.*?<\/parent>/\n\n  <parent>\n    <groupId>com.google.cloud<\/groupId>\n    <artifactId>google-cloud-pom-parent<\/artifactId>\n    <version>1.0.0-SNAPSHOT<\/version>\n    <relativePath>..\/..\/pom.xml<\/relativePath>\n  <\/parent>/s"
-    # execute the replacement in pom.xml
-    perl -0pe "$perl_command" "$BOM" > "$BOM".new && rm "$BOM" && mv "$BOM".new "$BOM"
+    BOM=$(ls -1 | grep 'bom')
+    cd "$BOM"
+    replaceParent ../../pom.xml
   fi
 
   popd
