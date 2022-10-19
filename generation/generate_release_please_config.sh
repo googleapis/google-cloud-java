a1737fe4a16376d331d5af4e3ba51230d2139e5a
Deepankar Dixit <90280028+ddixit14@users.noreply.github.com>
2022-10-19 14:37:13 -0400

    chore: Fixing config generation script.
    This will handle the case for config to have snapshots in it. Till now we were generating config only with non-snapshot versions.
    
    Ideal Results for this script:
    For release:main PR, this script will generate config with no-snapshots in it (in release:main PR, the manifest file has non-snapshot versions)
    For release:snapshot PR, this script will generate config with snapshots in it (in snapshot PR, the manifest file has snapshot versions)

--- generation/generate_release_please_config.sh
+++ generation/generate_release_please_config.sh
@@ -30,7 +30,7 @@ for path in $module_list; do
   module_snapshot_version=$(echo "${module_line}" | cut -d ":" -f3)
 
   # concatenating module name and module version
-  rp_manifest_line="${tab}\"${module_name}\": \"${module_released_version}\""
+  rp_manifest_line="${tab}\"${module_name}\": \"${module_snapshot_version}\""
   # Generate the JSON block with formatting
   rp_config_line+="${tab}${tab}\"${module_name}\": {\n${tab}${tab}${tab}\"component\": \"${artifact_name}\"\n${tab}${tab}}"
 
