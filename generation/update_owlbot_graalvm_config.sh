ff4cba647e83d687b55dea08debc61eb4572c215
Lawrence Qiu <lawrenceqiu@google.com>
2022-10-20 21:27:42 +0000

    chore: Fix header formatting

--- generation/update_owlbot_graalvm_config.sh
+++ generation/update_owlbot_graalvm_config.sh
@@ -40,18 +40,18 @@ new_config=$(cat "${OWLBOT_FILE}" | yq -y "${yq_query}" --sort-keys)
 mv "${OWLBOT_FILE}" "${dir_name}/temp.yaml"
 touch "${OWLBOT_FILE}"
 printf "# Copyright 2022 Google LLC\n\
-      #\n\
-      # Licensed under the Apache License, Version 2.0 (the "License");\n\
-      # you may not use this file except in compliance with the License.\n\
-      # You may obtain a copy of the License at \
-      # \n\
-      #     http://www.apache.org/licenses/LICENSE-2.0\n\
-      # \n\
-      # Unless required by applicable law or agreed to in writing, software\n\
-      # distributed under the License is distributed on an \"AS IS\" BASIS, \n\
-      # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n\
-      # See the License for the specific language governing permissions and\n\
-      # limitations under the License." >> "${OWLBOT_FILE}"
+#\n\
+# Licensed under the Apache License, Version 2.0 (the "License");\n\
+# you may not use this file except in compliance with the License.\n\
+# You may obtain a copy of the License at \
+# \n\
+#     http://www.apache.org/licenses/LICENSE-2.0\n\
+# \n\
+# Unless required by applicable law or agreed to in writing, software\n\
+# distributed under the License is distributed on an \"AS IS\" BASIS, \n\
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n\
+# See the License for the specific language governing permissions and\n\
+# limitations under the License." >> "${OWLBOT_FILE}"
 
 printf "\n" >> "${OWLBOT_FILE}"
 
