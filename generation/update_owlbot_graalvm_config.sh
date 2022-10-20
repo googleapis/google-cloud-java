c589aeec924943b4b925a02306ee7225bbb7b62d
Lawrence Qiu <lawrenceqiu@google.com>
2022-10-20 21:26:51 +0000

    chore: Add the header value

--- generation/update_owlbot_graalvm_config.sh
+++ generation/update_owlbot_graalvm_config.sh
@@ -37,20 +37,20 @@ yq_query=".\"deep-preserve-regex\" += [\"${main_config}\",\"${test_config}\"]"
 
 new_config=$(cat "${OWLBOT_FILE}" | yq -y "${yq_query}" --sort-keys)
 
-mv "${OWLBOT_FILE}" temp.yaml
+mv "${OWLBOT_FILE}" "${dir_name}/temp.yaml"
 touch "${OWLBOT_FILE}"
-echo "# Copyright 2022 Google LLC \
-      # \
-      # Licensed under the Apache License, Version 2.0 (the "License"); \
-      # you may not use this file except in compliance with the License. \
+printf "# Copyright 2022 Google LLC\n\
+      #\n\
+      # Licensed under the Apache License, Version 2.0 (the "License");\n\
+      # you may not use this file except in compliance with the License.\n\
       # You may obtain a copy of the License at \
-      # \
-      #     http://www.apache.org/licenses/LICENSE-2.0 \
-      # \
-      # Unless required by applicable law or agreed to in writing, software \
-      # distributed under the License is distributed on an "AS IS" BASIS, \
-      # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. \
-      # See the License for the specific language governing permissions and \
+      # \n\
+      #     http://www.apache.org/licenses/LICENSE-2.0\n\
+      # \n\
+      # Unless required by applicable law or agreed to in writing, software\n\
+      # distributed under the License is distributed on an \"AS IS\" BASIS, \n\
+      # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n\
+      # See the License for the specific language governing permissions and\n\
       # limitations under the License." >> "${OWLBOT_FILE}"
 
 printf "\n" >> "${OWLBOT_FILE}"
