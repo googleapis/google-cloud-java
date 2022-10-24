4fa27ddf416174ade0429d3cc48d7f5c1c2f6396
shantstepanian <17996546+shantstepanian@users.noreply.github.com>
2022-10-24 10:06:55 -0400

    chore: Fix typo in PartialListClustersException and PartialListInstancesException message (#1473)
    * Fix typo in PartialListClustersException message
    
    * Update PartialListInstancesException.java
    
    * Update PartialListClustersException.java

--- java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/PartialListClustersException.java
+++ java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/PartialListClustersException.java
@@ -33,7 +33,7 @@ public class PartialListClustersException extends RuntimeException {
    */
   @InternalApi
   public PartialListClustersException(List<String> unavailableZones, List<Cluster> clusters) {
-    super("Failed to list all instances, some zones where unavailable");
+    super("Failed to list all clusters, some zones were unavailable");
     this.unavailableZones = ImmutableList.copyOf(unavailableZones);
     this.clusters = ImmutableList.copyOf(clusters);
   }
--- java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/PartialListInstancesException.java
+++ java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/PartialListInstancesException.java
@@ -31,7 +31,7 @@ public class PartialListInstancesException extends RuntimeException {
   @InternalApi
   public PartialListInstancesException(
       @Nonnull List<String> unavailableZones, @Nonnull List<Instance> instances) {
-    super("Failed to list all instances, some zones where unavailable");
+    super("Failed to list all instances, some zones were unavailable");
 
     this.unavailableZones = ImmutableList.copyOf(unavailableZones);
     this.instances = ImmutableList.copyOf(instances);
