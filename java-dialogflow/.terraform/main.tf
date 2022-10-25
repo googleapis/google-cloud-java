a2263a056f010f65cf290902780cbec7592c46af
Burke Davison <burkedavison@google.com>
2022-10-25 16:35:38 -0400

    fix: Remove random suffix from Dialogflow Agent label, as only one agent can exist per project per region.

--- java-dialogflow/.terraform/main.tf
+++ java-dialogflow/.terraform/main.tf
@@ -8,19 +8,13 @@ terraform {
 module "project-services" {
   source = "terraform-google-modules/project-factory/google//modules/project_services"
 
-  project_id                  = random_id.id.keepers.project_id
+  project_id                  = var.inputs.project_id
   enable_apis                 = var.inputs.should_enable_apis_on_apply
   disable_services_on_destroy = var.inputs.should_disable_apis_on_destroy
   activate_apis               = ["dialogflow.googleapis.com"]
 }
-resource "random_id" "id" {
-  byte_length = 3
-  keepers     = {
-    project_id = var.inputs.project_id
-  }
-}
 locals {
-  agent_display_name = lower("google-cloud-java-tests-${random_id.id.hex}")
+  agent_display_name = "google-cloud-java-tests"
 }
 resource "google_dialogflow_agent" "design_time_agent" {
   display_name          = local.agent_display_name
