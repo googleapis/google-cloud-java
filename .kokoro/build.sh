8ce670a487a192807ccfd58613fc271d9402ca7c
Mridula <66699525+mpeddada1@users.noreply.github.com>
2022-10-25 16:04:14 -0400

    fix(java): restore native image configurations (#1844)
    This configuration was added in https://togithub.com/googleapis/java-bigquerystorage/pull/1488 but was removed by owlbot in https://togithub.com/googleapis/java-bigquerystorage/pull/1543. The build script changes were excluded from https://togithub.com/googleapis/java-bigquerystorage/pull/1362 so this missing configuration was only caught through manual testing by running `mvn test -Pnative`. This PR restores both changes.

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -68,6 +68,16 @@ integration)
       verify
     RETURN_CODE=$?
     ;;
+graalvm)
+    # Run Unit and Integration Tests with Native Image
+    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
+    RETURN_CODE=$?
+    ;;
+graalvm17)
+    # Run Unit and Integration Tests with Native Image
+    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
+    RETURN_CODE=$?
+    ;;
 samples)
     SAMPLES_DIR=samples
     # only run ITs in snapshot/ on presubmit PRs. run ITs in all 3 samples/ subdirectories otherwise.
