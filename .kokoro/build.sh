48063daacaf6ae918e1f22da0e86fc7a02c3f016
Burke Davison <burkedavison@google.com>
2022-10-26 15:47:57 -0400

    chore: Disable -x flag to reduce scripting noise.

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -61,8 +61,6 @@ case ${JOB_TYPE} in
         echo "${modified_module_list[*]}"
       )
 
-      set -x
-
       terraform -version
       source ./.terraform/helpers/init.sh "$module_list"
       source ./.terraform/helpers/plan.sh
