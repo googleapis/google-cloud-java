08651858db6b1361d062098b525bc306c22884b6
deepankar <deepankardixit@google.com>
2022-11-14 14:57:44 +0000

    chore: Fixing some stuff

--- generation/generate_root_versions_txt.sh
+++ generation/generate_root_versions_txt.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+# This script adds the contents of a module's versions.txt to root versions.txt and then deletes it.
+# This should be run during addition of a new-module to the monorepo
+
 set -e
 
 GENERATION_DIR=$(dirname -- "$0")
 
-rm -f versions.txt
-
-echo "# Format:" >> versions.txt
-echo "# module:released-version:current-version" >> versions.txt
-
 module_list=$(find . -mindepth 2 -maxdepth 2 -name pom.xml | sort --dictionary-order |xargs dirname)
 
 for path in $module_list; do
-
-  cat ${path}/versions.txt | while read LINE; do
-    if ! [[ $LINE == *"#"* ]] && [ "$LINE" != "" ]; then
-      echo $LINE >> versions.txt
-    fi
-  done
-
+  FILE=${path}/versions.txt
+  if [ -f ${FILE} ]; then
+    cat ${path}/versions.txt | while read LINE; do
+      if ! [[ $LINE == *"#"* ]] && [ "$LINE" != "" ]; then
+        echo $LINE >> versions.txt
+      fi
+    done
+  fi
+  rm -f ${FILE}
 done
 
