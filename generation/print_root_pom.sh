fc5919fae7c88fda91a1b841e1c7f6d6e3d3d1f3
Tomo Suzuki <suztomo@google.com>
2022-11-14 11:03:07 -0500

    chore: excluding java-maps-* modules

--- generation/print_root_pom.sh
+++ generation/print_root_pom.sh
@@ -8,4 +8,6 @@ GENERATION_DIR=$(dirname -- "$0");
 find . -mindepth 2 -maxdepth 2 -name pom.xml |sort --dictionary-order | xargs dirname \
     |sed -e 's|./||' | xargs -I '{}' echo "    <module>{}</module>" > /tmp/repo-modules.txt
 
+sed -i.bak '/java-maps/d' /tmp/repo-modules.txt
+
 awk -v MODULES="`awk -v ORS='\\\\n' '1' /tmp/repo-modules.txt`" '1;/<modules>/{print MODULES}' ${GENERATION_DIR}/parent.pom.xml
