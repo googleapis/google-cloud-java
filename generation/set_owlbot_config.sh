991a8ccaf26b9ae7b7c7eeb2cd69eb9d20d218c8
Alice Li <alicejli@google.com>
2022-11-15 15:22:05 -0500

    merge owlbot config scripts

--- generation/set_owlbot_config.sh
+++ generation/set_owlbot_config.sh
@@ -42,3 +42,34 @@ sed -i.bak "s|\"/samples|\"/${module_name}/samples|" "${OWLBOT_FILE}" && rm "${O
 if grep --quiet 'owl-bot-staging/$1' "${OWLBOT_FILE}"; then
   sed -i.bak "s|owl-bot-staging|owl-bot-staging/${module_name}|" "${OWLBOT_FILE}" && rm "${OWLBOT_FILE}".bak
 fi
+
+# This section is specifically around the generated snippet directories
+# If snippets are already being copied, exit out
+if grep -q samples/snippets/generated ${OWLBOT_FILE}; then
+  echo "module ${module_name} already copies generated snippets"
+  exit 1
+  fi
+
+# Insert into `deep-remove-regex:` section
+deep_remove_regex="- \"\/${module_name}\/samples\/snippets\/generated\""
+entry_before_deep_remove_regex="${module_name}\/google-.*\/src"
+sed -i "/${entry_before_deep_remove_regex}/a ${deep_remove_regex}" ${OWLBOT_FILE}
+
+
+# Insert into `deep-copy-regex:` section
+proto_path=$(grep -oPm1 '(?<=source: ").*(?=\(v.*\))' "${OWLBOT_FILE}")
+deep_copy_regex="- source: \"${proto_path}(v.*)/.*-java/samples/snippets/generated\"\n  dest: \"/owl-bot-staging/${module_name}/\$1/samples/snippets/generated\""
+
+entry_before_deep_copy_regex="dest: \"\/owl-bot-staging\/${module_name}\/\$1\/google-"
+
+# echo ${proto_path}
+sed -i "/${entry_before_deep_copy_regex}/a ${deep_copy_regex}" ${OWLBOT_FILE}
+
+# Remove duplicate lines
+perl -i -ne 'if ( /^\s*#/ ) { print } else { print if ! $SEEN{$_}++}' ${OWLBOT_FILE}
+
+# Add back new lines between sections
+sed -i  's/deep-copy-regex/\n&/g'  ${OWLBOT_FILE}
+sed -i  's/deep-remove-regex/\n&/g'  ${OWLBOT_FILE}
+sed -i  's/deep-preserve-regex/\n&/g'  ${OWLBOT_FILE}
+sed -i  's/api-name/\n&/g'  ${OWLBOT_FILE}
