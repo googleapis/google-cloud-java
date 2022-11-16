bf8a82206512604cfd714ef71b36079c57bdbe64
Mike Eltsufin <meltsufin@google.com>
2022-11-16 00:00:24 -0500

    fix same file reading/writing

--- generation/print_root_pom.sh
+++ generation/print_root_pom.sh
@@ -8,4 +8,6 @@ GENERATION_DIR=$(dirname -- "$0");
 find . -mindepth 2 -maxdepth 2 -name pom.xml |sort --dictionary-order | xargs dirname \
     |sed -e 's|./||' | xargs -I '{}' echo "    <module>{}</module>" > /tmp/repo-modules.txt
 
-perl -0pe 's/<modules>.*<\/modules>/<modules>\n  <\/modules>/s' ${GENERATION_DIR}/../pom.xml | awk -v MODULES="`awk -v ORS='\\\\n' '1' /tmp/repo-modules.txt`" '1;/<modules>/{print MODULES}'
+perl -0pe 's/<modules>.*<\/modules>/<modules>\n  <\/modules>/s' ${GENERATION_DIR}/../pom.xml > ${GENERATION_DIR}/parent.pom.xml
+awk -v MODULES="`awk -v ORS='\\\\n' '1' /tmp/repo-modules.txt`" '1;/<modules>/{print MODULES}' ${GENERATION_DIR}/parent.pom.xml
+rm ${GENERATION_DIR}/parent.pom.xml
