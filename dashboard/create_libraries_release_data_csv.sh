83bc41e12ea8561f9f07a10966ad49998c54f612
deepankar <deepankardixit@google.com>
2022-11-30 16:08:29 +0000

    fixing

--- dashboard/create_libraries_release_data_csv.sh
+++ dashboard/create_libraries_release_data_csv.sh
@@ -8,20 +8,24 @@
 #Output:
 #cloud_java_client_library_release_dates.csv that holds the data defined below. It has artifact_id,service_name,version, and release_date columns.
 
-cat libraries.txt | while read line; do
+cat ./dashboard/libraries.txt | while read line; do
 
   group_id=${line%:*}
   artifact_id=${line#*:}
   new_group_id="${group_id//.//}"
   service_name=${artifact_id#*-cloud-}
 
+  if [[ "${artifact_id}" == *storage* ]]; then
+    service_name=bigstore
+  fi
+
   URL=https://repo1.maven.org/maven2/$new_group_id/$artifact_id
 
-  ./fetch_library_release_date.sh $URL $artifact_id $service_name
+  ./dashboard/fetch_library_release_date.sh $URL $artifact_id $service_name
 
 done
 
-sed 's/ \+/,/g' cloud_java_client_library_release_dates_tsv.txt > cloud_java_client_library_release_dates.csv
-sed -i '1s/^/version,release_date,artifact_id,service_name\n/' cloud_java_client_library_release_dates.csv
+sed 's/ \+/,/g' cloud_java_client_library_release_dates_tsv.txt > ./dashboard/cloud_java_client_library_release_dates.csv
+sed -i '1s/^/version,release_date,artifact_id,service_name\n/' ./dashboard/cloud_java_client_library_release_dates.csv
 
 rm -f cloud_java_client_library_release_dates_tsv.txt
