eebe02c7b6bf41110ae12f540ce6f8ec2389d84c
Tomo Suzuki <suztomo@google.com>
2023-01-31 22:32:24 +0000

    fixing owlbot.py to adjust monorepo

--- java-iam/owlbot.py
+++ java-iam/owlbot.py
@@ -22,8 +22,16 @@ for library in s.get_staging_dirs():
   s.move(library)
 
 s.remove_staging_dirs()
-java.common_templates(excludes=[
-  'README.md',
-  'samples/*',
-  '.github/workflows/samples.yaml',
+java.common_templates(monorepo=True, excludes=[
+    ".github/*",
+    ".kokoro/*",
+    "samples/*",
+    "CODE_OF_CONDUCT.md",
+    "CONTRIBUTING.md",
+    "LICENSE",
+    "SECURITY.md",
+    "java.header",
+    "license-checks.xml",
+    "renovate.json",
+    ".gitignore"
 ])
