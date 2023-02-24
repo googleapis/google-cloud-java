27248c8659bea478bb5c0527812742f98916e316
Tomo Suzuki <suztomo@google.com>
2023-02-24 14:00:58 -0500

    ci: release job for Maps libraries (#9152)
    * Renamed stage-google-oss-sonatype-org job
    
    * Created stage-oss-sonatype-org job
    
    * Adding group_id_check_for_maps_libraries

--- .kokoro/release/stage_oss_sonatype_org.sh
+++ .kokoro/release/stage_oss_sonatype_org.sh
@@ -15,6 +15,13 @@
 
 set -eo pipefail
 
+# This stage_oss_sonatype_org.sh publishes the artifacts to oss.sonatype.org publishing
+# host, not google.oss.sonatype.org. A group ID of Maven artifact can belong to
+# one publishing host. As of February 2023, com.google.maps group
+# ID is not part of the google.oss.sonatype.org publishing host. In future when
+# we move com.google.maps (and others) to the
+# google.oss.sonatype.org publishing host, we can stop this job separated.
+
 # Start the releasetool reporter
 requirementsFile=$(realpath $(dirname "$0")/../../)/.kokoro/requirements.txt
 python3 -m pip install --require-hashes -r $requirementsFile
@@ -27,11 +34,12 @@ pushd $(dirname "$0")/../../
 setup_environment_secrets
 create_settings_xml_file "settings.xml"
 
-echo "Deploying artifacts to staging repositories"
+echo "Deploying non-Cloud artifacts to staging repositories in oss.sonatype.org"
 
-excludedMapsModule=$(find java-maps-* -name 'pom.xml'  \
-    |sed -e 's/^/!/' -e 's|/pom.xml$||' |xargs  |sed -e 's/ /,/g')
-echo "Excluded modules: ${excludedMapsModule}"
+# Lists modules for Google Maps artifacts, including their submodules.
+IncludedNonCloudModules=$(find java-maps-* -name 'pom.xml'  \
+    |sed -e 's|/pom.xml$||' |xargs  |sed -e 's/ /,/g')
+echo "Included modules: ${IncludedNonCloudModules}"
 
 mvn clean deploy -B \
   -DskipTests=true \
@@ -40,8 +48,11 @@ mvn clean deploy -B \
   -Dgpg.executable=gpg \
   -Dgpg.passphrase=${GPG_PASSPHRASE} \
   -Dgpg.homedir=${GPG_HOMEDIR} \
-  --projects "${excludedMapsModule}" \
-  -P release
+  --projects "${IncludedNonCloudModules}" \
+  -P release \
+  -DserverId=nexus \
+  -DnexusUrl=https://oss.sonatype.org/
+
 
 # The job triggered by Release Please (release-trigger) has this AUTORELEASE_PR
 # environment variable. Fusion also lets us to specify this variable.
@@ -50,7 +61,9 @@ then
   echo "Releasing the staging repositories"
   mvn nexus-staging:release -B \
     -DperformRelease=true \
-    --settings=${MAVEN_SETTINGS_FILE}
+    --settings=${MAVEN_SETTINGS_FILE} \
+    -DserverId=nexus \
+    -DnexusUrl=https://oss.sonatype.org/
 else
   echo "AUTORELEASE_PR is not set. Not releasing."
 fi
