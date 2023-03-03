fd71308487f953b43532f2bc47ee6e22329ffa47
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2023-03-03 14:48:31 -0500

    chore: use reasonable timeout

--- java-speech/google-cloud-speech/src/test/java/com/google/cloud/speech/v1/it/ITSpeechTest.java
+++ java-speech/google-cloud-speech/src/test/java/com/google/cloud/speech/v1/it/ITSpeechTest.java
@@ -56,7 +56,7 @@ public class ITSpeechTest {
                 .setInitialRpcTimeout(Duration.ZERO)
                 .setRpcTimeoutMultiplier(1.0)
                 .setMaxRpcTimeout(Duration.ZERO)
-                .setTotalTimeout(Duration.ofDays(1))
+                .setTotalTimeout(Duration.ofHours(1))
                 .build());
     SpeechSettings.Builder settingsBuilder = SpeechSettings.newBuilder();
     settingsBuilder
