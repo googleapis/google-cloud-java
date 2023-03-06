dd6534b8f0aabb392f5e2f36fab37cca2208b2d6
Deepankar Dixit <90280028+ddixit14@users.noreply.github.com>
2023-03-06 15:09:08 -0500

    chore: ignoring the broken test

--- java-bigqueryconnection/google-cloud-bigqueryconnection/src/test/java/com/google/cloud/bigqueryconnection/v1/it/ITSystemTest.java
+++ java-bigqueryconnection/google-cloud-bigqueryconnection/src/test/java/com/google/cloud/bigqueryconnection/v1/it/ITSystemTest.java
@@ -43,7 +43,11 @@ import java.util.UUID;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+@Ignore(
+    "Excluding this test until the problem is resolved. https://github.com/googleapis/google-cloud-java/issues/9040")
 
 public class ITSystemTest {
 
