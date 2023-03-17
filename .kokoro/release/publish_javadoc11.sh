c0c341fc092fe4f96bb28d419184d8fde81bb4a2
Lawrence Qiu <lawrenceqiu@google.com>
2023-03-17 15:52:11 -0400

    chore: Add shared-deps to javadoc exclusion list

--- .kokoro/release/publish_javadoc11.sh
+++ .kokoro/release/publish_javadoc11.sh
@@ -58,7 +58,7 @@ if [[ -z "${MODULE_LIST}" ]]; then
 else
   modules=($(echo "${MODULE_LIST}" | tr ',' ' '))
 fi
-excluded_modules=('gapic-libraries-bom' 'google-cloud-jar-parent' 'google-cloud-pom-parent')
+excluded_modules=('gapic-libraries-bom' 'google-cloud-jar-parent' 'google-cloud-pom-parent' 'java-shared-dependencies')
 # TODO: Maps docs exclusion logic to be removed once we move to correct location on devsite. See b/262712184 and b/262600829
 while IFS=  read -r -d $'\0'; do
     excluded_modules+=("$REPLY")
