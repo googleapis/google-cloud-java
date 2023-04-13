bb916aa475180a042ef480c19665baa6e5d77c65
ddixit14 <deepankardixit@google.com>
2023-04-13 10:32:23 -0400

    chore: working on comments

--- generation/cleanup/repo-status.sh
+++ generation/cleanup/repo-status.sh
@@ -3,46 +3,34 @@
 set +e
 
 for repository in $(cat repositories.txt); do
+  owlbot_file_url=https://raw.githubusercontent.com/googleapis/${repository}/main/.github/.OwlBot.yaml
+  curl ${owlbot_file_url} --fail --silent >/dev/null
+  status_1=$?
 
-    owlbot_file_url=https://raw.githubusercontent.com/googleapis/${repository}/main/.github/.OwlBot.yaml
+  if [ ${status_1} = 0 ]; then
+    owlbot="1"
+  else
+    owlbot="0"
+  fi
 
-    curl ${owlbot_file_url} --fail --silent >/dev/null
+  release_please_url=https://raw.githubusercontent.com/googleapis/${repository}/main/.github/release-please.yml
+  curl ${release_please_url} --fail --silent >/dev/null
+  status_2=$?
+  if [ ${status_2} = 0 ]; then
+    release_please="1"
+  else
+    release_please="0"
+  fi
 
-    status_1=$?
+  renovate_bot_url=$(curl -s https://raw.githubusercontent.com/googleapis/java-compute/main/renovate.json)
+  if grep -q "\"enabled\":\s*false" <<<"$renovate_bot_url"; then
+    renovate="0"
+  else
+    renovate="1"
+  fi
 
-    if [ ${status_1} = 0 ] ; then
-      owlbot="1"
-    else
-      owlbot="0"
-    fi
+  open_issues_count=$(gh issue list -R googleapis/${repository} -L 100 -s open | wc -l)
+  open_pull_requests=$(gh pr list -R googleapis/${repository} -L 100 -s open | wc -l)
 
-    release_please_url=https://raw.githubusercontent.com/googleapis/${repository}/main/.github/release-please.yml
-
-    curl ${release_please_url} --fail --silent >/dev/null
-
-    status_2=$?
-
-    if [ ${status_2} = 0 ] ; then
-          release_please="1"
-        else
-          release_please="0"
-    fi
-
-    renovate_bot_url=$(curl -s https://raw.githubusercontent.com/googleapis/java-compute/main/renovate.json)
-
-    if grep -q "\"enabled\":\s*false" <<< "$renovate_bot_url"; then
-      renovate="0"
-    else
-      renovate="1"
-    fi
-
-    open_issues_count=$(gh issue list -R googleapis/${repository} -L 100 -s open | wc -l)
-
-
-    open_pull_requests=$(gh pr list -R googleapis/${repository} -L 100 -s open | wc -l)
-
-
-    echo "${repository},${owlbot},${release_please},${renovate},${open_issues_count},${open_pull_requests}">>status.csv
-
+  echo "${repository},${owlbot},${release_please},${renovate},${open_issues_count},${open_pull_requests}" >>status.csv
 done
-
