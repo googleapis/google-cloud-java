a268c8016262a7a5a13be6a9983294f83d1ecc3f
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2023-06-07 23:52:30 -0400

    feat: [migrationcenter] new module for migrationcenter (#9514)
    * feat: [migrationcenter] new module for migrationcenter
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ java-migrationcenter/samples/snippets/generated/com/google/cloud/migrationcenter/v1/migrationcenter/updatesettings/AsyncUpdateSettings.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.migrationcenter.v1.samples;
+
+// [START migrationcenter_v1_generated_MigrationCenter_UpdateSettings_async]
+import com.google.api.core.ApiFuture;
+import com.google.cloud.migrationcenter.v1.MigrationCenterClient;
+import com.google.cloud.migrationcenter.v1.Settings;
+import com.google.cloud.migrationcenter.v1.UpdateSettingsRequest;
+import com.google.longrunning.Operation;
+import com.google.protobuf.FieldMask;
+
+public class AsyncUpdateSettings {
+
+  public static void main(String[] args) throws Exception {
+    asyncUpdateSettings();
+  }
+
+  public static void asyncUpdateSettings() throws Exception {
+    // This snippet has been automatically generated and should be regarded as a code template only.
+    // It will require modifications to work:
+    // - It may require correct/in-range values for request initialization.
+    // - It may require specifying regional endpoints when creating the service client as shown in
+    // https://cloud.google.com/java/docs/setup#configure_endpoints_for_the_client_library
+    try (MigrationCenterClient migrationCenterClient = MigrationCenterClient.create()) {
+      UpdateSettingsRequest request =
+          UpdateSettingsRequest.newBuilder()
+              .setUpdateMask(FieldMask.newBuilder().build())
+              .setSettings(Settings.newBuilder().build())
+              .setRequestId("requestId693933066")
+              .build();
+      ApiFuture<Operation> future =
+          migrationCenterClient.updateSettingsCallable().futureCall(request);
+      // Do something.
+      Operation response = future.get();
+    }
+  }
+}
+// [END migrationcenter_v1_generated_MigrationCenter_UpdateSettings_async]
