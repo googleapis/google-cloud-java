be1df828d1dcf19f1deb3914d0424c6ed561ee96
JoeWang1127 <joewa@google.com>
2023-06-09 19:50:42 +0000

    parameterize compare.sh

--- library_generation/compare.sh
+++ library_generation/compare.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
+PROTO_PATH=$1
+IS_CLOUD_SDK=$2
+OUT_LAYER_FOLDER="${PROTO_PATH////-}-java"
+if [ "${IS_CLOUD_SDK}" == true ]; then
+  OUT_LAYER_FOLDER="${OUT_LAYER_FOLDER//google/google-cloud}"
+fi
 
 REPO_ROOT=$(pwd)/..
 cd "${REPO_ROOT}"
@@ -13,4 +19,4 @@ cd "${GOOGLEAPIS_GEN_ROOT}"
 git checkout fd245b5c29cad9db7e2bd65b15e875493d4d7c03
 
 # exclude gradle files since we do not use them.
-diff -r google/monitoring/v3/google-cloud-monitoring-v3-java/ ../library_gen_out/google/monitoring/v3/google-cloud-monitoring-v3-java -x "*gradle*"
+diff -r "${PROTO_PATH}"/"${OUT_LAYER_FOLDER}"/ ../library_gen_out/"${PROTO_PATH}"/"${OUT_LAYER_FOLDER}" -x "*gradle*"
