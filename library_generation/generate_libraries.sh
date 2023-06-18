085c7e6e317e18b989d2fa554513f8afe11e92f6
JoeWang1127 <joewa@google.com>
2023-06-18 14:51:56 +0000

    do not show commands

--- library_generation/generate_libraries.sh
+++ library_generation/generate_libraries.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 LIBRARY_GENERATION="$(dirname "$(readlink -f "$0")")"
 REPO_ROOT="${LIBRARY_GENERATION}"/..
 cd "${REPO_ROOT}"
@@ -8,10 +8,10 @@ cd "${REPO_ROOT}"
 git clone https://github.com/googleapis/sdk-platform-java
 SDK_PLATFORM_JAVA_ROOT=${REPO_ROOT}/sdk-platform-java
 cd "${SDK_PLATFORM_JAVA_ROOT}"
-git checkout 9aeb301191e4d24daebaf28a7c7b700dece8cdf8
+git checkout feat/library-generation-tools
 cp -r library_generation/* "${LIBRARY_GENERATION}"
 rm -rf "${SDK_PLATFORM_JAVA_ROOT}"
-# generate libraries in the prot path list.
+# generate libraries in the proto path list.
 cd "${LIBRARY_GENERATION}"
 while IFS= read -r line
 do
@@ -20,5 +20,6 @@ do
   TRANSPORT=$(echo "${line}" | awk '{print $3}')
   REST_NUMERIC_ENUMS=$(echo "${line}" | awk '{print $4}')
   INCLUDE_SAMPLES=$(echo "${line}" | awk '{print $5}')
+  echo "Generate library in ${PROTO_PATH}"
   ./generate_library_with_fixed_dependencies.sh "${PROTO_PATH}" "${CONTAINS_CLOUD}" "${TRANSPORT}" "${REST_NUMERIC_ENUMS}" "${INCLUDE_SAMPLES}"
 done < proto_path.txt
