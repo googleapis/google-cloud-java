2dce1aa944eae70c4f7945bae1c4a4dc46134fa8
ddixit14 <deepankardixit@google.com>
2023-06-27 12:13:35 -0400

    chore: Testing chaining of jobs

--- .kokoro/release/stage_google_oss_sonatype_org.sh
+++ .kokoro/release/stage_google_oss_sonatype_org.sh
@@ -13,51 +13,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+echo "running the main stage job"
 
-# Start the releasetool reporter
-requirementsFile=$(realpath $(dirname "$0")/../../)/.kokoro/requirements.txt
-python3 -m pip install --require-hashes -r $requirementsFile
-python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
-
-source $(dirname "$0")/common.sh
-MAVEN_SETTINGS_FILE=$(realpath $(dirname "$0")/../../)/settings.xml
-pushd $(dirname "$0")/../../
-
-setup_environment_secrets
-create_settings_xml_file "settings.xml"
-
-echo "Deploying artifacts to staging repositories in google.oss.sonatype.org"
-
-# Excluding modules that cannot be published to the google.oss.sonatype.org
-# publishing host.
-# https://github.com/googleapis/google-cloud-java/issues/9045
-excludedNonCloudModules=$(find java-maps-* -name 'pom.xml'  \
-    |sed -e 's/^/!/' -e 's|/pom.xml$||' |xargs  |sed -e 's/ /,/g')
-echo "Excluded modules: ${excludedNonCloudModules}"
-
-mvn clean deploy -B \
-  -DskipTests=true \
-  -Dclirr.skip=true \
-  --settings ${MAVEN_SETTINGS_FILE} \
-  -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
-  -Dgpg.executable=gpg \
-  -Dgpg.passphrase=${GPG_PASSPHRASE} \
-  -Dgpg.homedir=${GPG_HOMEDIR} \
-  --projects "${excludedNonCloudModules}" \
-  -P release
-
-# The job triggered by Release Please (release-trigger) has this AUTORELEASE_PR
-# environment variable. Fusion also lets us to specify this variable.
-if [[ -n "${AUTORELEASE_PR}" ]]
-then
-  echo "Releasing the staging repositories"
-  mvn nexus-staging:release -B \
-    -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
-    -DperformRelease=true \
-    --settings=${MAVEN_SETTINGS_FILE}
-else
-  echo "AUTORELEASE_PR is not set. Not releasing."
-fi
+#set -eo pipefail
+#
+## Start the releasetool reporter
+#requirementsFile=$(realpath $(dirname "$0")/../../)/.kokoro/requirements.txt
+#python3 -m pip install --require-hashes -r $requirementsFile
+#python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+#
+#source $(dirname "$0")/common.sh
+#MAVEN_SETTINGS_FILE=$(realpath $(dirname "$0")/../../)/settings.xml
+#pushd $(dirname "$0")/../../
+#
+#setup_environment_secrets
+#create_settings_xml_file "settings.xml"
+#
+#echo "Deploying artifacts to staging repositories in google.oss.sonatype.org"
+#
+## Excluding modules that cannot be published to the google.oss.sonatype.org
+## publishing host.
+## https://github.com/googleapis/google-cloud-java/issues/9045
+#excludedNonCloudModules=$(find java-maps-* -name 'pom.xml'  \
+#    |sed -e 's/^/!/' -e 's|/pom.xml$||' |xargs  |sed -e 's/ /,/g')
+#echo "Excluded modules: ${excludedNonCloudModules}"
+#
+#mvn clean deploy -B \
+#  -DskipTests=true \
+#  -Dclirr.skip=true \
+#  --settings ${MAVEN_SETTINGS_FILE} \
+#  -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
+#  -Dgpg.executable=gpg \
+#  -Dgpg.passphrase=${GPG_PASSPHRASE} \
+#  -Dgpg.homedir=${GPG_HOMEDIR} \
+#  --projects "${excludedNonCloudModules}" \
+#  -P release
+#
+## The job triggered by Release Please (release-trigger) has this AUTORELEASE_PR
+## environment variable. Fusion also lets us to specify this variable.
+#if [[ -n "${AUTORELEASE_PR}" ]]
+#then
+#  echo "Releasing the staging repositories"
+#  mvn nexus-staging:release -B \
+#    -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
+#    -DperformRelease=true \
+#    --settings=${MAVEN_SETTINGS_FILE}
+#else
+#  echo "AUTORELEASE_PR is not set. Not releasing."
+#fi
 
 
--- .kokoro/release/stage_oss_sonatype_org.sh
+++ .kokoro/release/stage_oss_sonatype_org.sh
@@ -13,60 +13,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+echo "running the maps job"
 
-# This stage_oss_sonatype_org.sh publishes the artifacts to oss.sonatype.org publishing
-# host, not google.oss.sonatype.org. A group ID of Maven artifact can belong to
-# one publishing host. As of February 2023, com.google.maps group
-# ID is not part of the google.oss.sonatype.org publishing host. In future when
-# we move com.google.maps (and others) to the
-# google.oss.sonatype.org publishing host, we can stop this job separated.
-
-# Start the releasetool reporter
-requirementsFile=$(realpath $(dirname "$0")/../../)/.kokoro/requirements.txt
-python3 -m pip install --require-hashes -r $requirementsFile
-python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
-
-source $(dirname "$0")/common.sh
-MAVEN_SETTINGS_FILE=$(realpath $(dirname "$0")/../../)/settings.xml
-pushd $(dirname "$0")/../../
-
-setup_environment_secrets
-create_settings_xml_file "settings.xml"
-
-echo "Deploying non-Cloud artifacts to staging repositories in oss.sonatype.org"
-
-# Lists modules for Google Maps artifacts, including their submodules.
-IncludedNonCloudModules=$(find java-maps-* -name 'pom.xml'  \
-    |sed -e 's|/pom.xml$||' |xargs  |sed -e 's/ /,/g')
-echo "Included modules: ${IncludedNonCloudModules}"
-
-mvn clean deploy -B \
-  -DskipTests=true \
-  -Dclirr.skip=true \
-  --settings ${MAVEN_SETTINGS_FILE} \
-  -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
-  -Dgpg.executable=gpg \
-  -Dgpg.passphrase=${GPG_PASSPHRASE} \
-  -Dgpg.homedir=${GPG_HOMEDIR} \
-  --projects "${IncludedNonCloudModules}" \
-  -P release \
-  -P release-non-google-oss-sonatype
- 
-
-# The job triggered by Release Please (release-trigger) has this AUTORELEASE_PR
-# environment variable. Fusion also lets us to specify this variable.
-if [[ -n "${AUTORELEASE_PR}" ]]
-then
-  echo "Releasing the staging repositories"
-  mvn nexus-staging:release -B \
-    -DperformRelease=true \
-    -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
-    --settings=${MAVEN_SETTINGS_FILE} \
-    --projects "${IncludedNonCloudModules}" \
-    -P release-non-google-oss-sonatype
-else
-  echo "AUTORELEASE_PR is not set. Not releasing."
-fi
-
+#set -eo pipefail
+#
+## This stage_oss_sonatype_org.sh publishes the artifacts to oss.sonatype.org publishing
+## host, not google.oss.sonatype.org. A group ID of Maven artifact can belong to
+## one publishing host. As of February 2023, com.google.maps group
+## ID is not part of the google.oss.sonatype.org publishing host. In future when
+## we move com.google.maps (and others) to the
+## google.oss.sonatype.org publishing host, we can stop this job separated.
+#
+## Start the releasetool reporter
+#requirementsFile=$(realpath $(dirname "$0")/../../)/.kokoro/requirements.txt
+#python3 -m pip install --require-hashes -r $requirementsFile
+#python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+#
+#source $(dirname "$0")/common.sh
+#MAVEN_SETTINGS_FILE=$(realpath $(dirname "$0")/../../)/settings.xml
+#pushd $(dirname "$0")/../../
+#
+#setup_environment_secrets
+#create_settings_xml_file "settings.xml"
+#
+#echo "Deploying non-Cloud artifacts to staging repositories in oss.sonatype.org"
+#
+## Lists modules for Google Maps artifacts, including their submodules.
+#IncludedNonCloudModules=$(find java-maps-* -name 'pom.xml'  \
+#    |sed -e 's|/pom.xml$||' |xargs  |sed -e 's/ /,/g')
+#echo "Included modules: ${IncludedNonCloudModules}"
+#
+#mvn clean deploy -B \
+#  -DskipTests=true \
+#  -Dclirr.skip=true \
+#  --settings ${MAVEN_SETTINGS_FILE} \
+#  -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
+#  -Dgpg.executable=gpg \
+#  -Dgpg.passphrase=${GPG_PASSPHRASE} \
+#  -Dgpg.homedir=${GPG_HOMEDIR} \
+#  --projects "${IncludedNonCloudModules}" \
+#  -P release \
+#  -P release-non-google-oss-sonatype
+#
+#
+## The job triggered by Release Please (release-trigger) has this AUTORELEASE_PR
+## environment variable. Fusion also lets us to specify this variable.
+#if [[ -n "${AUTORELEASE_PR}" ]]
+#then
+#  echo "Releasing the staging repositories"
+#  mvn nexus-staging:release -B \
+#    -DperformRelease=true \
+#    -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS \
+#    --settings=${MAVEN_SETTINGS_FILE} \
+#    --projects "${IncludedNonCloudModules}" \
+#    -P release-non-google-oss-sonatype
+#else
+#  echo "AUTORELEASE_PR is not set. Not releasing."
+#fi
+#
 
