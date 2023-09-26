3141dafaaa59392d09ac9b63175b79dc77b537a2
Mridula <66699525+mpeddada1@users.noreply.github.com>
2023-09-26 16:30:17 +0000

    feat: [merchantapi] new module for merchantapi inventories (#9891)
    ```
    python3.9 generation/new_client/new-client.py generate   
    --api_shortname=merchantapi 
    --proto-path=google/shopping/merchant/inventories 
    --name-pretty="Merchant API" 
    --product-docs="https://developers.google.com/merchant/api" 
    --api-description="Programmatically manage your Merchant Center accounts." 
    --distribution-name="com.google.shopping:google-shopping-merchant-inventories"
    ```

---
+++ java-merchantapi/google-shopping-merchant-inventories/src/main/java/com/google/shopping/merchant/inventories/v1beta/package-info.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A client to Merchant API
+ *
+ * <p>The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>======================= LocalInventoryServiceClient =======================
+ *
+ * <p>Service Description: Service to manage local inventory for products
+ *
+ * <p>Sample for LocalInventoryServiceClient:
+ *
+ * <pre>{@code
+ * // This snippet has been automatically generated and should be regarded as a code template only.
+ * // It will require modifications to work:
+ * // - It may require correct/in-range values for request initialization.
+ * // - It may require specifying regional endpoints when creating the service client as shown in
+ * // https://cloud.google.com/java/docs/setup#configure_endpoints_for_the_client_library
+ * try (LocalInventoryServiceClient localInventoryServiceClient =
+ *     LocalInventoryServiceClient.create()) {
+ *   InsertLocalInventoryRequest request =
+ *       InsertLocalInventoryRequest.newBuilder()
+ *           .setParent("parent-995424086")
+ *           .setLocalInventory(LocalInventory.newBuilder().build())
+ *           .build();
+ *   LocalInventory response = localInventoryServiceClient.insertLocalInventory(request);
+ * }
+ * }</pre>
+ *
+ * <p>======================= RegionalInventoryServiceClient =======================
+ *
+ * <p>Service Description: Service to manage regional inventory for products. There is also separate
+ * `regions` resource and API to manage regions definitions.
+ *
+ * <p>Sample for RegionalInventoryServiceClient:
+ *
+ * <pre>{@code
+ * // This snippet has been automatically generated and should be regarded as a code template only.
+ * // It will require modifications to work:
+ * // - It may require correct/in-range values for request initialization.
+ * // - It may require specifying regional endpoints when creating the service client as shown in
+ * // https://cloud.google.com/java/docs/setup#configure_endpoints_for_the_client_library
+ * try (RegionalInventoryServiceClient regionalInventoryServiceClient =
+ *     RegionalInventoryServiceClient.create()) {
+ *   InsertRegionalInventoryRequest request =
+ *       InsertRegionalInventoryRequest.newBuilder()
+ *           .setParent("parent-995424086")
+ *           .setRegionalInventory(RegionalInventory.newBuilder().build())
+ *           .build();
+ *   RegionalInventory response = regionalInventoryServiceClient.insertRegionalInventory(request);
+ * }
+ * }</pre>
+ */
+@Generated("by gapic-generator-java")
+package com.google.shopping.merchant.inventories.v1beta;
+
+import javax.annotation.Generated;
