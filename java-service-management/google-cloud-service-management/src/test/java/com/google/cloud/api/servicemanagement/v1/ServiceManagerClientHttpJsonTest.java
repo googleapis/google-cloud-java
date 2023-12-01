67b8ebae483814270380a4724ad50fb8c2241bef
Lawrence Qiu <lawrenceqiu@google.com>
2023-11-30 21:10:19 -0500

    chore: Add missing @Ignore import

--- java-service-management/google-cloud-service-management/src/test/java/com/google/cloud/api/servicemanagement/v1/ServiceManagerClientHttpJsonTest.java
+++ java-service-management/google-cloud-service-management/src/test/java/com/google/cloud/api/servicemanagement/v1/ServiceManagerClientHttpJsonTest.java
@@ -91,6 +91,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 @Generated("by gapic-generator-java")
