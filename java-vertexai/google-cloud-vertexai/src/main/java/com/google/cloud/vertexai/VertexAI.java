081f276ba6be9fe980d3ecdcf4ea2c3edbc24fe9
Blake Li <blakeli@google.com>
2023-12-19 16:42:38 -0500

    fix: [vertexai] Use default scopes from Prediction Service to create application default credentials if scopes are empty. (#10169)
    Fixes: #10154.

--- java-vertexai/google-cloud-vertexai/src/main/java/com/google/cloud/vertexai/VertexAI.java
+++ java-vertexai/google-cloud-vertexai/src/main/java/com/google/cloud/vertexai/VertexAI.java
@@ -20,7 +20,9 @@ import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.vertexai.api.PredictionServiceClient;
 import com.google.cloud.vertexai.api.PredictionServiceSettings;
+import com.google.cloud.vertexai.api.stub.PredictionServiceStubSettings;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -87,9 +89,11 @@ public class VertexAI implements AutoCloseable {
     Logger logger = Logger.getLogger("com.google.auth.oauth2.DefaultCredentialsProvider");
     Level previousLevel = logger.getLevel();
     logger.setLevel(Level.SEVERE);
+    List<String> defaultScopes =
+        PredictionServiceStubSettings.defaultCredentialsProviderBuilder().getScopesToApply();
     GoogleCredentials credentials =
         scopes.length == 0
-            ? GoogleCredentials.getApplicationDefault()
+            ? GoogleCredentials.getApplicationDefault().createScoped(defaultScopes)
             : GoogleCredentials.getApplicationDefault().createScoped(scopes);
     logger.setLevel(previousLevel);
 
