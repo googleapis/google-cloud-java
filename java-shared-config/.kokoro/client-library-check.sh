26ba6101495545acacc8b509a4dfacb0d371688c
Mridula <66699525+mpeddada1@users.noreply.github.com>
2024-02-14 21:40:53 +0000

    chore: use sdk-platform-java-config for java-bigquery (#754)

--- java-shared-config/.kokoro/client-library-check.sh
+++ java-shared-config/.kokoro/client-library-check.sh
@@ -124,10 +124,10 @@ pushd ${REPO}
 
 # TODO(#748): Replace the version of sdk-platform-java-config for all libraries. This logic will no longer
 #  be needed after the rest of the handwritten libraries are migrated to use this artifact.
-if [ "$REPO" == "java-spanner" ] || [ "$REPO" == "java-pubsub" ]; then
-  replace_sdk_platform_java_config_version "${SDK_PLATFORM_JAVA_CONFIG_VERSION}"
-else
+if [ "$REPO" == "java-storage" ]; then
   replace_java_shared_config_version "${JAVA_SHARED_CONFIG_VERSION}"
+else
+  replace_sdk_platform_java_config_version "${SDK_PLATFORM_JAVA_CONFIG_VERSION}"
 fi
 
 case ${JOB_TYPE} in
