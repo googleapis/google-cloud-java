eeabd195aed28f6ca268de30a076f50ec1246c29
diegomarquezp <diegomarquezp@google.com>
2024-03-07 01:43:55 +0000

    add advanced options

--- generation/new_client_hermetic_build/new-client.py
+++ generation/new_client_hermetic_build/new-client.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Script for enabling generation of a new library
 Appends a new library to `generation_config.yaml`
@@ -94,6 +108,50 @@ def main(ctx):
     help="Maven coordinates of the generated library. By default it's "
     "com.google.cloud:google-cloud-<api_shortname>",
 )
+@click.option(
+    "--release-level",
+    type=click.Choice(["stable", "preview"]),
+    default="preview",
+    show_default=True,
+    help="A label that appears in repo-metadata.json. The first library "
+         "generation is always 'preview'."
+@click.option(
+    "--api-id",
+    type=str,
+    help="The value of the apiid parameter used in README.md It has link to "
+         "https://console.cloud.google.com/flows/enableapi?apiid=<api_id>"
+)
+@click.option(
+    "--requires-billing",
+    type=bool,
+    default=True,
+    show_default=True,
+    help="Based on this value, README.md explains whether billing setup is "
+         "needed or not."
+)
+@click.option(
+    "--cloud-api",
+    type=bool,
+    default=True,
+    show_default=True,
+    help="If true, the artifact ID of the library is 'google-cloud-'; "
+         "otherwise 'google-'"
+)
+@click.option(
+    "--group-id",
+    type=str,
+    default="com.google.cloud",
+    show_default=True,
+    help="The group ID of the artifact when distribution name is not set"
+)
+@click.option(
+    "--library-type",
+    type=str,
+    default="GAPIC_AUTO",
+    show_default=True,
+    help="A label that appear in repo-metadata.json to tell how the library is "
+         "maintained or generated"
+)
 def add_new_library(
     api_shortname,
     name_pretty,
@@ -105,14 +163,22 @@ def add_new_library(
     transport,
     destination_name,
     distribution_name,
+    release_level,
+    api_id,
+    requires_billing,
+    cloud_api,
+    group_id,
+    library_type,
 ):
+    cloud_prefix = "cloud-" if cloud_api else ""
     output_name = destination_name if destination_name else api_shortname
     if distribution_name is None:
-        distribution_name = f"com.google-cloud:google-cloud-{output_name}"
+        distribution_name = f"{group_id}:google-{cloud_prefix}{output_name}"
 
     distribution_name_short = re.split(r"[:\/]", distribution_name)[-1]
 
-    api_id = f"{api_shortname}.googleapis.com"
+    if api_id is None:
+        api_id = f"{api_shortname}.googleapis.com"
 
     if not product_docs.startswith("https"):
         sys.exit(
@@ -144,15 +210,23 @@ def add_new_library(
         "name_pretty": name_pretty,
         "product_documentation": product_docs,
         "api_description": api_description,
+        "client_documentation": client_documentation,
+        "release_level": release_level,
         "transport": transport,
-        "destination_name": destination_name,
+        "language": 'java',
+        "repo": f"googleapis/java-{output_name}",
+        "repo_short": f"java-{output_name}",
+        "distribution_name": distribution_name,
+        "api_id": api_id,
+        "library_type": library_type,
         "GAPICs": [{
             "proto_path": proto_path
         }]
     }
 
-    __add_item_if_set(new_library, "rest_docs", rest_docs)
-    __add_item_if_set(new_library, "rpc_docs", rpc_docs)
+    __add_item_if_set(new_library, "requires_billing", requires_billing)
+    __add_item_if_set(new_library, "rest_documentation", rest_docs)
+    __add_item_if_set(new_library, "rpc_documentation", rpc_docs)
     __add_item_if_set(new_library, "distribution_name", distribution_name)
 
     config["libraries"].append(new_library)
