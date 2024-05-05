d36fea4fd311bc169d933784712f2708777c7ab2
Joe Wang <joewa@google.com>
2024-05-05 10:45:17 -0400

    update script or workflow

--- generation/hermetic_library_generation.sh
+++ generation/hermetic_library_generation.sh
@@ -120,18 +120,15 @@ else
 fi
 # if the last commit doesn't contain changes to generation configuration,
 # do not generate again as the result will be the same.
-echo "line 123"
 change_of_last_commit="$(git diff-tree --no-commit-id --name-only HEAD~1..HEAD -r)"
 if [[ ! ("${change_of_last_commit}" == *"${generation_config}"*) ]]; then
     echo "The last commit doesn't contain any changes to the generation_config.yaml, skipping the whole generation process." || true
     exit 0
 fi
-echo "line 129"
 # copy generation configuration from target branch to current branch.
 git show "${base_ref}":"${generation_config}" > "${baseline_generation_config}"
-echo "line 132"
-config_diff=$(diff "${generation_config}" "${baseline_generation_config}" 2>/dev/null)
-echo "line 134"
+config_diff=$(diff "${generation_config}" "${baseline_generation_config}" || true)
+echo "line 131"
 # run hermetic code generation docker image.
 docker run \
   --rm \
