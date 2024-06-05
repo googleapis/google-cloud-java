3c868a2352adedffeaf98c494f4fbb5a84b8d1ff
Tomo Suzuki <suztomo@google.com>
2024-06-05 14:17:29 -0400

    library_name and fixed the regex

--- .github/ga_version_bump/bump_module_to_ga.sh
+++ .github/ga_version_bump/bump_module_to_ga.sh
@@ -6,21 +6,26 @@
 # otherwise the changes to the versions are overridden by the Release Please.
 
 set -eo pipefail
-set -x
 
-# Pattern to match the lines in versions.txt. Usually it's the API shortname for a module.
+# Pattern to match the lines in versions.txt. Usually it's the library_name for a module,
+# as in the generation_config.yaml.
 # For example "securityposture" for the "java-securityposture" module.
-shortname=$1
+library_name=$1
 
+if [ -z "${library_name}" ]; then
+  echo "Please specify library_name that matches the pattern in versions.txt."
+  echo "Example: 'securityposture'"
+  exit 1
+fi
 versions_file=versions.txt
 
-sed -i -E "s/^(.*${shortname}(-v.+)?):0.+:.+/\1:1.0.0:1.0.0/g" "${versions_file}"
+sed -i -E "s/^(.*${library_name}(-v.+)?):0.+:.+/\1:1.0.0:1.0.0/g" "${versions_file}"
 
 # Guardrail for bad pattern
 set +e
 diff_lines_count=$(git diff --unified=0 versions.txt |grep -c "^[+-]\w")
 
-if ! grep --quiet -E "${shortname}-v[0-9]:" versions.txt; then
+if ! grep --quiet -E "${library_name}-v[1-9]:" versions.txt; then
   echo "The module does not seem to have a GA server API version (>= v1)."
   echo "A library should not become 1.0.0 before the server API releases v1 (go/client-quality)"
   exit 1
@@ -28,22 +33,22 @@ fi
 
 set -e
 if [[ "${diff_lines_count}" -eq 0 ]]; then
-  echo "There's no matching lines in versions.txt for ${shortname}."
+  echo "There's no matching lines in versions.txt for ${library_name}."
   echo "Check the hyphenation in the versions.txt."
   exit 1
 fi
 if [[ "${diff_lines_count}" -gt 10 ]]; then
-  echo "There are more than 5 lines changed. The pattern (${shortname}) is too generic. Failing this script for safe side."
+  echo "There are more than 5 lines changed. The pattern (${library_name}) is too generic. Failing this script for safe side."
   exit 1
 fi
 
 SED_OPTIONS=""
-for KV in $(cut -f1,3 -d: "${versions_file}" |grep -v "#" | grep "${shortname}"); do
+for KV in $(cut -f1,3 -d: "${versions_file}" |grep -v "#" | grep "${library_name}"); do
   K=${KV%:*}; V=${KV#*:}
   echo Key:$K, Value:$V;
   SED_OPTIONS="$SED_OPTIONS -e /x-version-update:$K:current/{s|<version>.*<\/version>|<version>$V<\/version>|;}"
 done
 
 find . -maxdepth 3 -name pom.xml |sort --dictionary-order |xargs sed -i.bak $SED_OPTIONS
+# Clean up unnecessary backup files
 find . -maxdepth 3 -name pom.xml.bak |xargs rm
-
