66513b6fa0ffe30dad2146b9e50f99211899268c
Joe Wang <joewa@google.com>
2024-09-06 16:39:04 -0400

    start the workflow whenever there's a change

--- .github/scripts/hermetic_library_generation.sh
+++ .github/scripts/hermetic_library_generation.sh
@@ -77,13 +77,6 @@ message="chore: generate libraries at $(date)"
 
 git checkout "${target_branch}"
 git checkout "${current_branch}"
-# if the last commit doesn't contain changes to generation configuration,
-# do not generate again as the result will be the same.
-change_of_last_commit="$(git diff-tree --no-commit-id --name-only HEAD~1..HEAD -r)"
-if [[ ! ("${change_of_last_commit}" == *"${generation_config}"*) ]]; then
-    echo "The last commit doesn't contain any changes to the generation_config.yaml, skipping the whole generation process." || true
-    exit 0
-fi
 
 # copy generation configuration from target branch to current branch.
 git show "${target_branch}":"${generation_config}" > "${baseline_generation_config}"
