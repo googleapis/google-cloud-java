0600b77abf86cb6c4c6075b4b876a7c91a7b7fb6
Joe Wang <joewa@google.com>
2024-10-14 12:58:44 -0400

    add unit test

--- .github/release/release_unit_tests.py
+++ .github/release/release_unit_tests.py
@@ -1,6 +1,60 @@
+from click.testing import CliRunner
+import contextlib
+import os
+import shutil
+import tempfile
 import unittest
+from partial_release import bump_released_version
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+GOLDEN = os.path.join(SCRIPT_DIR, "testdata")
+FIXTURES = os.path.join(SCRIPT_DIR, "fixture")
 
 
 class TestCase(unittest.TestCase):
     def test_bump_version_success(self):
-        pass
+        golden = f"{GOLDEN}/asset/versions-asset-golden.txt"
+        runner = CliRunner()
+        with copied_fixtures_dir(f"{FIXTURES}/asset"):
+            runner.invoke(
+                bump_released_version,
+                [
+                    "--artifact_ids=google-cloud-asset",
+                    "--versions=versions-asset.txt",
+                ],
+            )
+            with open(golden) as g:
+                expected = g.read()
+            with open("./versions-asset.txt") as f:
+                actual = f.read()
+            self.assertEqual(expected, actual)
+
+
+@contextlib.contextmanager
+def change_dir_to(path: str) -> str:
+    """
+    Context Manager to change the current working directory and restore the
+    previous working directory after completing the context.
+
+    """
+    old_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield path
+    finally:
+        os.chdir(old_cwd)
+
+
+@contextlib.contextmanager
+def copied_fixtures_dir(source: str) -> str:
+    """
+    Context Manager to copy from a fixtures directory into a new temporary
+    directory and change the current working directory to that copy.
+    Restores the original current working directory after completing the
+    context.
+
+    """
+    with tempfile.TemporaryDirectory() as tempdir:
+        workdir = shutil.copytree(source, f"{tempdir}/workspace")
+        with change_dir_to(workdir):
+            yield workdir
