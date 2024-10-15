a9a85b1faec6244b1b7b3d7d3900d74148c1221a
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2024-10-15 15:34:00 +0000

    chore: add partial release scripts (#11250)
    * chore: add partial release scripts
    
    * bring back apply_current_version.sh
    
    * change impl
    
    * add unit test
    
    * add unit test
    
    * setup ci

---
+++ .github/release/partial_release.py
@@ -0,0 +1,123 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# GitHub action job to test core java library features on
+# downstream client libraries before they are released.
+import re
+from enum import Enum
+import click as click
+
+VERSION_PREFIX = r"^(grpc|proto)-"
+VERSION_SUFFIX = r"-v[1-9].*$"
+
+
+class VersionType(Enum):
+    MAJOR = (1,)
+    MINOR = (2,)
+    PATCH = 3
+
+
+@click.group(invoke_without_command=False)
+@click.pass_context
+@click.version_option(message="%(version)s")
+def main(ctx):
+    pass
+
+
+@main.command()
+@click.option(
+    "--artifact_ids",
+    required=True,
+    type=str,
+    help="""
+    Artifact IDs whose version needs to update, separated by comma.
+    """,
+)
+@click.option(
+    "--version-type",
+    required=False,
+    default="patch",
+    type=str,
+    help="""
+    The type of version bump, one of major, minor or patch.
+    """,
+)
+@click.option(
+    "--versions",
+    required=False,
+    default="./versions.txt",
+    type=str,
+    help="""
+    The path to the versions.txt.
+    """,
+)
+def bump_released_version(artifact_ids: str, version_type: str, versions: str) -> None:
+    target_artifact_ids = set(artifact_ids.split(","))
+    version_enum = _parse_type_or_raise(version_type)
+    newlines = []
+    with open(versions) as versions_file:
+        for num, line in enumerate(versions_file):
+            striped_line = line.strip()
+            # case 1: skip an empty line.
+            if striped_line == "":
+                newlines.append("")
+                continue
+            # case 2: keep a comment.
+            if striped_line.startswith("#"):
+                newlines.append(f"{striped_line}")
+                continue
+            values = striped_line.split(":")
+            artifact_id = values[0]
+            released_version = values[1]
+            sanitized_artifact_id = _sanitize(artifact_id)
+            # case 3: keep the line if the artifact id is not matched.
+            if sanitized_artifact_id not in target_artifact_ids:
+                newlines.append(f"{striped_line}")
+                continue
+            # case 4: bump version according to version type.
+            major, minor, patch = [
+                int(ver_num) for ver_num in released_version.split(".")
+            ]
+            match version_enum:
+                case VersionType.MAJOR:
+                    major += 1
+                case VersionType.MINOR:
+                    minor += 1
+                case VersionType.PATCH:
+                    patch += 1
+            newlines.append(
+                f"{artifact_id}:{major}.{minor}.{patch}:{major}.{minor}.{patch}"
+            )
+    with open(versions, "w") as versions_file:
+        versions_file.writelines("\n".join(newlines))
+
+
+def _parse_type_or_raise(version_type: str) -> VersionType:
+    try:
+        version_enum = VersionType[version_type.upper()]
+    except KeyError:
+        raise KeyError(
+            f"Version type {version_type} is not supported.\n"
+            f"Supported types are MAJOR, MINOR AND PATCH."
+        )
+    return version_enum
+
+
+def _sanitize(artifact_id: str) -> str:
+    # Remove the matching regex.
+    temp = re.sub(VERSION_PREFIX, "", artifact_id)
+    return re.sub(VERSION_SUFFIX, "", temp)
+
+
+if __name__ == "__main__":
+    main()
---
+++ .github/release/release_unit_tests.py
@@ -0,0 +1,76 @@
+from click.testing import CliRunner
+import contextlib
+import os
+import shutil
+import tempfile
+import unittest
+from partial_release import bump_released_version
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+GOLDEN = os.path.join(SCRIPT_DIR, "testdata")
+FIXTURES = os.path.join(SCRIPT_DIR, "fixture")
+runner = CliRunner()
+
+
+class TestCase(unittest.TestCase):
+    def test_bump_single_version_success(self):
+        golden = f"{GOLDEN}/asset/versions-asset-golden.txt"
+        with copied_fixtures_dir(f"{FIXTURES}/asset"):
+            runner.invoke(
+                bump_released_version,
+                [
+                    "--artifact_ids=google-cloud-asset",
+                    "--versions=versions-asset.txt",
+                ],
+            )
+            with open(golden) as g:
+                expected = g.read()
+            with open("./versions-asset.txt") as f:
+                actual = f.read()
+            self.assertEqual(expected, actual)
+
+    def test_bump_multiple_versions_success(self):
+        golden = f"{GOLDEN}/multiple_modules/versions-multiple-golden.txt"
+        with copied_fixtures_dir(f"{FIXTURES}/multiple_modules"):
+            runner.invoke(
+                bump_released_version,
+                [
+                    "--artifact_ids=google-cloud-bigqueryconnection,google-cloud-java",
+                    "--versions=versions-multiple.txt",
+                ],
+            )
+            with open(golden) as g:
+                expected = g.read()
+            with open("./versions-multiple.txt") as f:
+                actual = f.read()
+            self.assertEqual(expected, actual)
+
+
+@contextlib.contextmanager
+def change_dir_to(path: str) -> str:
+    """
+    Context Manager to change the current working directory and restore the
+    previous working directory after completing the context.
+
+    """
+    old_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield path
+    finally:
+        os.chdir(old_cwd)
+
+
+@contextlib.contextmanager
+def copied_fixtures_dir(source: str) -> str:
+    """
+    Context Manager to copy from a fixtures directory into a new temporary
+    directory and change the current working directory to that copy.
+    Restores the original current working directory after completing the
+    context.
+
+    """
+    with tempfile.TemporaryDirectory() as tempdir:
+        workdir = shutil.copytree(source, f"{tempdir}/workspace")
+        with change_dir_to(workdir):
+            yield workdir
