20826f1b08a3cc5bdcce5637b7ea21d467b2bce2
Phong Chuong <147636638+PhongChuong@users.noreply.github.com>
2024-10-31 13:06:59 -0400

    fix: Update experimental methods documentation to @internalapi (#3552)
    * docs: Update experimental methods documentation to @InternalApi
    
    * docs: Update experimental methods documentation to @InternalApi

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/LoadConfiguration.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/LoadConfiguration.java
@@ -16,6 +16,7 @@
 
 package com.google.cloud.bigquery;
 
+import com.google.api.core.InternalApi;
 import com.google.cloud.bigquery.JobInfo.CreateDisposition;
 import com.google.cloud.bigquery.JobInfo.SchemaUpdateOption;
 import com.google.cloud.bigquery.JobInfo.WriteDisposition;
@@ -99,17 +100,17 @@ public interface LoadConfiguration {
     Builder setIgnoreUnknownValues(Boolean ignoreUnknownValues);
 
     /**
-     * [Experimental] Sets options allowing the schema of the destination table to be updated as a
-     * side effect of the load job. Schema update options are supported in two cases: when
-     * writeDisposition is WRITE_APPEND; when writeDisposition is WRITE_TRUNCATE and the destination
-     * table is a partition of a table, specified by partition decorators. For normal tables,
-     * WRITE_TRUNCATE will always overwrite the schema.
+     * Sets options allowing the schema of the destination table to be updated as a side effect of
+     * the load job. Schema update options are supported in two cases: when writeDisposition is
+     * WRITE_APPEND; when writeDisposition is WRITE_TRUNCATE and the destination table is a
+     * partition of a table, specified by partition decorators. For normal tables, WRITE_TRUNCATE
+     * will always overwrite the schema.
      */
+    @InternalApi
     Builder setSchemaUpdateOptions(List<SchemaUpdateOption> schemaUpdateOptions);
 
-    /**
-     * [Experimental] Sets automatic inference of the options and schema for CSV and JSON sources.
-     */
+    /** Sets automatic inference of the options and schema for CSV and JSON sources. */
+    @InternalApi
     Builder setAutodetect(Boolean autodetect);
 
     /** Sets the time partitioning specification for the destination table. */
@@ -202,18 +203,19 @@ public interface LoadConfiguration {
   DatastoreBackupOptions getDatastoreBackupOptions();
 
   /**
-   * [Experimental] Returns options allowing the schema of the destination table to be updated as a
-   * side effect of the load job. Schema update options are supported in two cases: when
-   * writeDisposition is WRITE_APPEND; when writeDisposition is WRITE_TRUNCATE and the destination
-   * table is a partition of a table, specified by partition decorators. For normal tables,
-   * WRITE_TRUNCATE will always overwrite the schema.
+   * Returns options allowing the schema of the destination table to be updated as a side effect of
+   * the load job. Schema update options are supported in two cases: when writeDisposition is
+   * WRITE_APPEND; when writeDisposition is WRITE_TRUNCATE and the destination table is a partition
+   * of a table, specified by partition decorators. For normal tables, WRITE_TRUNCATE will always
+   * overwrite the schema.
    */
+  @InternalApi
   List<SchemaUpdateOption> getSchemaUpdateOptions();
 
   /**
-   * [Experimental] Returns whether automatic inference of the options and schema for CSV and JSON
-   * sources is set.
+   * Returns whether automatic inference of the options and schema for CSV and JSON sources is set.
    */
+  @InternalApi
   Boolean getAutodetect();
 
   /** Returns the time partitioning specification defined for the destination table. */
