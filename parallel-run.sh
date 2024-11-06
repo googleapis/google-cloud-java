e6985fd00a32c35f3e7c58066e0ad3ee7355ef6c
Joe Wang <joewa@google.com>
2024-11-06 16:58:04 -0500

    chore: parallel generation

---
+++ parallel-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+workspace_name="/workspace"
+for num in "1" "2" "3"; do
+  # Remove previous log.
+ if [[ -e "run-${num}.log" ]]; then
+   rm "run-${num}.log"
+ fi
+ # Run docker in the background.
+ docker run \
+   --rm \
+   -u "$(id -u):$(id -g)" \
+   -v "$(pwd):${workspace_name}" \
+   -v "$(pwd)/../googleapis:${workspace_name}/apis" \
+   -e GENERATOR_VERSION="2.49.0-SNAPSHOT" \
+   us-docker.pkg.dev/java-hermetic-build-prod/private-resources/java-library-generation:test-secure-base-image-public-repo \
+   --generation-config-path="${workspace_name}/generation_config-${num}.yaml" \
+   --api-definitions-path="${workspace_name}/apis" > run-${num}.log 2>&1 &
+done
