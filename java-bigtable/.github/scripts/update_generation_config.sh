7fb85c2eac69a43b7deea3f8743c38cf04ee8964
Diego Marquez <diegomarquezp@google.com>
2025-03-15 23:33:06 -0400

    chore: update update_generation_config.sh (#2524)

--- java-bigtable/.github/scripts/update_generation_config.sh
+++ java-bigtable/.github/scripts/update_generation_config.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 set -e
 # This script should be run at the root of the repository.
-# This script is used to update googleapis commit to latest in generation
-# configuration at the time of running and create a pull request.
+# This script is used to update googleapis_commitish, gapic_generator_version,
+# and libraries_bom_version in generation configuration at the time of running
+# and create a pull request.
 
 # The following commands need to be installed before running the script:
 # 1. git
 # 2. gh
+# 3. jq
+
+# Utility functions
+# Get the latest released version of a Maven artifact.
+function get_latest_released_version() {
+    local group_id=$1
+    local artifact_id=$2
+    latest=$(curl -s "https://search.maven.org/solrsearch/select?q=g:${group_id}+AND+a:${artifact_id}&core=gav&rows=500&wt=json" | jq -r '.response.docs[] | select(.v | test("^[0-9]+(\\.[0-9]+)*$")) | .v' | sort -V | tail -n 1)
+    echo "${latest}"
+}
+
+# Update a key to a new value in the generation config.
+function update_config() {
+    local key_word=$1
+    local new_value=$2
+    local file=$3
+    echo "Update ${key_word} to ${new_value} in ${file}"
+    sed -i -e "s/^${key_word}.*$/${key_word}: ${new_value}/" "${file}"
+}
+
+# Update an action to a new version in GitHub action.
+function update_action() {
+    local key_word=$1
+    local new_value=$2
+    local file=$3
+    echo "Update ${key_word} to ${new_value} in ${file}"
+    # use a different delimiter because the key_word contains "/".
+    sed -i -e "s|${key_word}@v.*$|${key_word}@v${new_value}|" "${file}"
+}
 
 # The parameters of this script is:
 # 1. base_branch, the base branch of the result pull request.
 # 2. repo, organization/repo-name, e.g., googleapis/google-cloud-java
 # 3. [optional] generation_config, the path to the generation configuration,
 # the default value is generation_config.yaml in the repository root.
+# 4. [optional] workflow, the library generation workflow file,
+# the default value is .github/workflows/hermetic_library_generation.yaml.
 while [[ $# -gt 0 ]]; do
 key="$1"
 case "${key}" in
@@ -26,6 +58,10 @@ case "${key}" in
     ;;
   --generation_config)
     generation_config="$2"
+    shift
+    ;;
+  --workflow)
+    workflow="$2"
     shift
     ;;
   *)
@@ -51,8 +87,13 @@ if [ -z "${generation_config}" ]; then
   echo "Use default generation config: ${generation_config}"
 fi
 
+if [ -z "${workflow}" ]; then
+    workflow=".github/workflows/hermetic_library_generation.yaml"
+    echo "Use default library generation workflow file: ${workflow}"
+fi
+
 current_branch="generate-libraries-${base_branch}"
-title="chore: update googleapis commit at $(date)"
+title="chore: Update generation configuration at $(date)"
 
 git checkout "${base_branch}"
 # Try to find a open pull request associated with the branch
@@ -80,12 +121,25 @@ git pull
 latest_commit=$(git rev-parse HEAD)
 popd
 rm -rf tmp-googleapis
-sed -i -e "s/^googleapis_commitish.*$/googleapis_commitish: ${latest_commit}/" "${generation_config}"
+update_config "googleapis_commitish" "${latest_commit}" "${generation_config}"
 
-git add "${generation_config}"
+# Update gapic-generator-java version to the latest
+latest_version=$(get_latest_released_version "com.google.api" "gapic-generator-java")
+update_config "gapic_generator_version" "${latest_version}" "${generation_config}"
+
+# Update composite action version to latest gapic-generator-java version
+update_action "googleapis/sdk-platform-java/.github/scripts" \
+  "${latest_version}" \
+  "${workflow}"
+
+# Update libraries-bom version to the latest
+latest_version=$(get_latest_released_version "com.google.cloud" "libraries-bom")
+update_config "libraries_bom_version" "${latest_version}" "${generation_config}"
+
+git add "${generation_config}" "${workflow}"
 changed_files=$(git diff --cached --name-only)
 if [[ "${changed_files}" == "" ]]; then
-    echo "The latest googleapis commit is not changed."
+    echo "The latest generation config is not changed."
     echo "Skip committing to the pull request."
 else
     git commit -m "${title}"
@@ -110,4 +164,5 @@ if [ -z "${pr_num}" ]; then
   gh pr create --title "${title}" --head "${current_branch}" --body "${title}" --base "${base_branch}"
 else
   git push
+  gh pr edit "${pr_num}" --title "${title}" --body "${title}"
 fi
