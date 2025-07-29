069be0f5be641b786e947f372b1f9ef463f2038e
Diego Marquez <diegomarquezp@google.com>
2025-07-29 18:55:11 -0400

    feat: add v2 for bigquerydatapolicy (#11690)
    b/432507596
    
    https://github.com/googleapis/google-cloud-java/pull/11687 didn't work
    because it was made with poms pointing to released versions
    
    ---------
    
    Co-authored-by: cloud-java-bot <cloud-java-bot@google.com>

---
+++ java-bigquerydatapolicy/google-cloud-bigquerydatapolicy/src/main/java/com/google/cloud/bigquery/datapolicies/v2/stub/DataPolicyServiceStub.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.bigquery.datapolicies.v2.stub;
+
+import static com.google.cloud.bigquery.datapolicies.v2.DataPolicyServiceClient.ListDataPoliciesPagedResponse;
+
+import com.google.api.gax.core.BackgroundResource;
+import com.google.api.gax.rpc.UnaryCallable;
+import com.google.cloud.bigquery.datapolicies.v2.AddGranteesRequest;
+import com.google.cloud.bigquery.datapolicies.v2.CreateDataPolicyRequest;
+import com.google.cloud.bigquery.datapolicies.v2.DataPolicy;
+import com.google.cloud.bigquery.datapolicies.v2.DeleteDataPolicyRequest;
+import com.google.cloud.bigquery.datapolicies.v2.GetDataPolicyRequest;
+import com.google.cloud.bigquery.datapolicies.v2.ListDataPoliciesRequest;
+import com.google.cloud.bigquery.datapolicies.v2.ListDataPoliciesResponse;
+import com.google.cloud.bigquery.datapolicies.v2.RemoveGranteesRequest;
+import com.google.cloud.bigquery.datapolicies.v2.UpdateDataPolicyRequest;
+import com.google.iam.v1.GetIamPolicyRequest;
+import com.google.iam.v1.Policy;
+import com.google.iam.v1.SetIamPolicyRequest;
+import com.google.iam.v1.TestIamPermissionsRequest;
+import com.google.iam.v1.TestIamPermissionsResponse;
+import com.google.protobuf.Empty;
+import javax.annotation.Generated;
+
+// AUTO-GENERATED DOCUMENTATION AND CLASS.
+/**
+ * Base stub class for the DataPolicyService service API.
+ *
+ * <p>This class is for advanced usage and reflects the underlying API directly.
+ */
+@Generated("by gapic-generator-java")
+public abstract class DataPolicyServiceStub implements BackgroundResource {
+
+  public UnaryCallable<CreateDataPolicyRequest, DataPolicy> createDataPolicyCallable() {
+    throw new UnsupportedOperationException("Not implemented: createDataPolicyCallable()");
+  }
+
+  public UnaryCallable<AddGranteesRequest, DataPolicy> addGranteesCallable() {
+    throw new UnsupportedOperationException("Not implemented: addGranteesCallable()");
+  }
+
+  public UnaryCallable<RemoveGranteesRequest, DataPolicy> removeGranteesCallable() {
+    throw new UnsupportedOperationException("Not implemented: removeGranteesCallable()");
+  }
+
+  public UnaryCallable<UpdateDataPolicyRequest, DataPolicy> updateDataPolicyCallable() {
+    throw new UnsupportedOperationException("Not implemented: updateDataPolicyCallable()");
+  }
+
+  public UnaryCallable<DeleteDataPolicyRequest, Empty> deleteDataPolicyCallable() {
+    throw new UnsupportedOperationException("Not implemented: deleteDataPolicyCallable()");
+  }
+
+  public UnaryCallable<GetDataPolicyRequest, DataPolicy> getDataPolicyCallable() {
+    throw new UnsupportedOperationException("Not implemented: getDataPolicyCallable()");
+  }
+
+  public UnaryCallable<ListDataPoliciesRequest, ListDataPoliciesPagedResponse>
+      listDataPoliciesPagedCallable() {
+    throw new UnsupportedOperationException("Not implemented: listDataPoliciesPagedCallable()");
+  }
+
+  public UnaryCallable<ListDataPoliciesRequest, ListDataPoliciesResponse>
+      listDataPoliciesCallable() {
+    throw new UnsupportedOperationException("Not implemented: listDataPoliciesCallable()");
+  }
+
+  public UnaryCallable<GetIamPolicyRequest, Policy> getIamPolicyCallable() {
+    throw new UnsupportedOperationException("Not implemented: getIamPolicyCallable()");
+  }
+
+  public UnaryCallable<SetIamPolicyRequest, Policy> setIamPolicyCallable() {
+    throw new UnsupportedOperationException("Not implemented: setIamPolicyCallable()");
+  }
+
+  public UnaryCallable<TestIamPermissionsRequest, TestIamPermissionsResponse>
+      testIamPermissionsCallable() {
+    throw new UnsupportedOperationException("Not implemented: testIamPermissionsCallable()");
+  }
+
+  @Override
+  public abstract void close();
+}
