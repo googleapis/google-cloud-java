29d4dd6839a3899eca129d1ffafcaff7a24be1c2
diegomarquezp <diegomarquezp@google.com>
2025-12-15 21:01:55 +0000

    feat(vertexai): upgrade to protobuf 4.33.0

--- java-vertexai/google-cloud-vertexai/src/test/java/com/google/cloud/vertexai/api/MockEndpointServiceImpl.java
+++ java-vertexai/google-cloud-vertexai/src/test/java/com/google/cloud/vertexai/api/MockEndpointServiceImpl.java
@@ -156,7 +156,8 @@ public class MockEndpointServiceImpl extends EndpointServiceImplBase {
       responseObserver.onError(
           new IllegalArgumentException(
               String.format(
-                  "Unrecognized response type %s for method UpdateEndpointLongRunning, expected %s or %s",
+                  "Unrecognized response type %s for method UpdateEndpointLongRunning, expected %s"
+                      + " or %s",
                   response == null ? "null" : response.getClass().getName(),
                   Operation.class.getName(),
                   Exception.class.getName())));
