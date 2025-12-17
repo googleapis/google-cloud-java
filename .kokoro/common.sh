4721b29012a829b4f3dbcab203c590a4b515f978
diegomarquezp <diegomarquezp@google.com>
2025-12-17 06:18:30 +0000

    chore: correct git fetch usage

--- .kokoro/common.sh
+++ .kokoro/common.sh
@@ -161,7 +161,7 @@ function generate_modified_modules_list() {
   # grep returns 1 (error code) and exits the pipeline if there is no match
   # If there is no match, it will return true so the rest of the commands can run
   git config --global --add safe.directory $(realpath .)
-  git fetch "${KOKORO_GITHUB_PULL_REQUEST_TARGET_BRANCH}"
+  git fetch --all
   modified_files=$(git diff --name-only "${KOKORO_GITHUB_PULL_REQUEST_TARGET_BRANCH}...${KOKORO_GITHUB_PULL_REQUEST_COMMIT}")
   printf "Modified files:\n%s\n" "${modified_files}"
 
