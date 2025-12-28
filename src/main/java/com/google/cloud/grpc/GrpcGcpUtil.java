4511def1a68754f06709f465dca2dd496d27fd88
Yuri Golobokov <golobokov@google.com>
2025-12-27 19:59:28 -0800

    Bump to 1.9.2-SNAPSHOT (#230)

--- src/main/java/com/google/cloud/grpc/GrpcGcpUtil.java
+++ src/main/java/com/google/cloud/grpc/GrpcGcpUtil.java
@@ -1,5 +1,5 @@
 package com.google.cloud.grpc;
 
 public class GrpcGcpUtil {
-  public static final String IMPLEMENTATION_VERSION = "1.9.1";
+  public static final String IMPLEMENTATION_VERSION = "1.9.2-SNAPSHOT";
 }
