2c8d9b0f9f0821b852df24355345c4907748defc
chingor13 <chingor@google.com>
2026-01-08 18:02:18 +0000

    refactor: extract inline python to a .py file

--- monorepo-migration/migrate.sh
+++ monorepo-migration/migrate.sh
@@ -34,6 +34,7 @@ TARGET_DIR="$WORKING_DIR/$MONOREPO_NAME-target"
 TRANSFORM_SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 TRANSFORM_SCRIPT="$TRANSFORM_SCRIPT_DIR/transform_workflow.py"
 MODERNIZE_POM_SCRIPT="$TRANSFORM_SCRIPT_DIR/modernize_pom.py"
+UPDATE_ROOT_POM_SCRIPT="$TRANSFORM_SCRIPT_DIR/update_root_pom.py"
 
 echo "Starting migration using git read-tree with isolated clones..."
 
@@ -140,38 +141,7 @@ fi
 
 # 7.2 Update root pom.xml modules
 echo "Updating root pom.xml modules..."
-python3 -c "
-import sys
-pom_path = sys.argv[1]
-module_name = sys.argv[2]
-new_module = f'    <module>{module_name}</module>\n'
-with open(pom_path, 'r') as f:
-    content = f.read()
-start_tag = '<modules>'
-end_tag = '</modules>'
-start_idx = content.find(start_tag)
-end_idx = content.find(end_tag)
-if start_idx != -1 and end_idx != -1:
-    modules_section = content[start_idx + len(start_tag):end_idx]
-    lines = [l for l in modules_section.splitlines(keepends=True) if l.strip()]
-
-    java_indices = [i for i, l in enumerate(lines) if '<module>java-' in l]
-    if java_indices:
-        start_java = java_indices[0]
-        end_java = java_indices[-1] + 1
-        java_lines = lines[start_java:end_java]
-        if not any(f'<module>{module_name}</module>' in l for l in java_lines):
-            java_lines.append(new_module)
-            java_lines.sort()
-            lines = lines[:start_java] + java_lines + lines[end_java:]
-    else:
-        if not any(f'<module>{module_name}</module>' in l for l in lines):
-            lines.append(new_module)
-
-    new_content = content[:start_idx + len(start_tag)] + '\n' + ''.join(lines) + '  ' + content[end_idx:]
-    with open(pom_path, 'w') as f:
-        f.write(new_content)
-" "pom.xml" "$SOURCE_REPO_NAME"
+python3 "$UPDATE_ROOT_POM_SCRIPT" "pom.xml" "$SOURCE_REPO_NAME"
 
 echo "Committing root pom.xml modules update..."
 git add pom.xml
