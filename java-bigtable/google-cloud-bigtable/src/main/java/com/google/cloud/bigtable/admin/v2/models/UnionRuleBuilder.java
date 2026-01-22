1342607007dbdf15d041ed9e2a06e098241337bd
Jin Seop Kim <jinseop@google.com>
2026-01-22 09:24:40 -0500

    feat: Add GcRuleBuilder for safe GC rule construction (#2758)
    * feat: Add GcRuleBuilder for safe GC rule construction
    
    * chore: generate libraries at Tue Jan 20 22:09:26 UTC 2026
    
    * address feedback
    
    * chore: generate libraries at Wed Jan 21 18:00:03 UTC 2026
    
    ---------
    
    Co-authored-by: cloud-java-bot <cloud-java-bot@google.com>

---
+++ java-bigtable/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/UnionRuleBuilder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.bigtable.admin.v2.models;
+
+import com.google.bigtable.admin.v2.GcRule;
+import com.google.bigtable.admin.v2.GcRule.Union;
+
+/**
+ * Builder for creating a Union (OR) GC Rule.
+ *
+ * <p>This class ensures type safety when constructing composite rules, preventing the misuse of the
+ * standard builder's ambiguous setters.
+ */
+public final class UnionRuleBuilder {
+  private final Union.Builder unionBuilder = Union.newBuilder();
+
+  /**
+   * Adds a rule to the union.
+   *
+   * @param rule The rule to add to the union.
+   * @return The builder instance for chaining.
+   */
+  public UnionRuleBuilder add(GcRule rule) {
+    unionBuilder.addRules(rule);
+    return this;
+  }
+
+  /**
+   * Builds the final GcRule proto.
+   *
+   * @return The constructed GcRule proto.
+   */
+  public GcRule build() {
+    return GcRule.newBuilder().setUnion(unionBuilder).build();
+  }
+}
