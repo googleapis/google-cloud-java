21bfc98ba7bc56b6ca6dba3216e517818e24d8ee
Jeff Ching <chingor@google.com>
2026-02-03 09:00:22 -0800

    build: add monorepo migration script (#11880)
    Usage:
    
    ```
    MIGRATION_HEAD_BRANCH=main \
      SOURCE_REPO_URL=https://github.com/googleapis/java-datastore \
      SQUASH_COMMITS=true \
      ./monorepo-migration/migrate.sh
    ```
    
    options:
    * `MIGRATION_HEAD_BRANCH` - the code to branch from (e.g. if you need
    additional commits before the migration - defaults to `main`
    * `SOURCE_REPO_URL` - the split repository to migration - defaults to
    `https://github.com/googleapis/java-logging`
    * `SQUASH_COMMITS` - if set to `true`, then squash all migration fixes
    applied into a single commit
    
    ---------
    
    Co-authored-by: Mike Eltsufin <meltsufin@google.com>

--- .kokoro/dependencies.sh
+++ .kokoro/dependencies.sh
@@ -49,6 +49,12 @@ function determineMavenOpts() {
 
 export MAVEN_OPTS=$(determineMavenOpts)
 
+if [[ -n "${BUILD_SUBDIR}" ]]
+then
+  echo "Running in subdir: ${BUILD_SUBDIR}"
+  pushd "${BUILD_SUBDIR}"
+fi
+
 # this should run maven enforcer
 retry_with_backoff 3 10 \
   mvn install -B -V -ntp \
@@ -57,3 +63,9 @@ retry_with_backoff 3 10 \
     -Dclirr.skip=true
 
 mvn -B dependency:analyze -DfailOnWarning=true
+
+if [[ -n "${BUILD_SUBDIR}" ]]
+then
+  echo "Leaving subdir: ${BUILD_SUBDIR}"
+  popd
+fi
