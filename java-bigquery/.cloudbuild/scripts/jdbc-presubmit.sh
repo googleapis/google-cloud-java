09283a8073d4897087167a9ecacf85ad0a11bc1b
Kirill Logachev <kirl@google.com>
2026-03-10 00:58:42 +0000

    chore(bq jdbc): BQ JDBC presubmit & nightly

---
+++ java-bigquery/.cloudbuild/scripts/jdbc-presubmit.sh
@@ -0,0 +1,8 @@
+ROOT_FOLDER=$(git rev-parse --show-toplevel)
+
+cd ${ROOT_FOLDER}
+source .kokoro/common.sh
+install_modules java-bigquery
+
+cd ${ROOT_FOLDER}/java-bigquery/google-cloud-bigquery-jdbc
+make integration-test test=ITBigQueryJDBCTest
