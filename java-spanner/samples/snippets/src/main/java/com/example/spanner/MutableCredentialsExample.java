b0d350490a4ab12bce374e3a597625fe98dc4ef0
ldetmer <laura.detmer@gmail.com>
2026-03-27 13:30:37 -0400

    format fix
    # Conflicts:
    #	java-spanner/samples/snippets/src/main/java/com/example/spanner/MutableCredentialsExample.java

--- java-spanner/samples/snippets/src/main/java/com/example/spanner/MutableCredentialsExample.java
+++ java-spanner/samples/snippets/src/main/java/com/example/spanner/MutableCredentialsExample.java
@@ -36,63 +36,64 @@ import java.util.concurrent.TimeUnit;
 
 public class MutableCredentialsExample {
 
-    static void createClientWithMutableCredentials(String credentialsPath) throws IOException {
-        Path path = Paths.get(credentialsPath);
-        // Use an AtomicReference to hold the mutable lastModifiedTime so it can be accessed in the lambda
-        final AtomicReference<FileTime> lastModifiedTime =
-            new AtomicReference<>(Files.getLastModifiedTime(path));
+  static void createClientWithMutableCredentials(String credentialsPath) throws IOException {
+    Path path = Paths.get(credentialsPath);
+    // Use an AtomicReference to hold the mutable lastModifiedTime so it can be accessed in the
+    // lambda
+    final AtomicReference<FileTime> lastModifiedTime =
+        new AtomicReference<>(Files.getLastModifiedTime(path));
 
-        // 1 - create service account credentials
-        ServiceAccountCredentials serviceAccountCredentials;
-        try (FileInputStream is = new FileInputStream(credentialsPath)) {
-            serviceAccountCredentials = ServiceAccountCredentials.fromStream(is);
-        }
-
-        // 2 - wrap credentials from step 1 in a MutableCredentials instance
-        MutableCredentials mutableCredentials = new MutableCredentials(serviceAccountCredentials);
-
-        // 3 - set credentials on your SpannerOptions builder to your mutableCredentials
-        SpannerOptions options = SpannerOptions.newBuilder().setCredentials(mutableCredentials).build();
-
-        // 4 - include logic for when/how to update your mutableCredentials
-        // In this example we'll use a SchedulerExecutorService to periodically check for updates
-        ThreadFactory daemonThreadFactory =
-                runnable -> {
-                    Thread thread = new Thread(runnable, "spanner-mutable-credentials-rotator");
-                    thread.setDaemon(true);
-                    return thread;
-                };
-        ScheduledExecutorService executorService =
-                Executors.newSingleThreadScheduledExecutor(daemonThreadFactory);
-        executorService.scheduleAtFixedRate(
-                () -> {
-                    try {
-                        FileTime currentModifiedTime = Files.getLastModifiedTime(path);
-                        if (currentModifiedTime.compareTo(lastModifiedTime.get()) > 0) {
-                            lastModifiedTime.set(currentModifiedTime);
-                            try (FileInputStream is = new FileInputStream(credentialsPath)) {
-                                ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(is);
-                                mutableCredentials.updateCredentials(credentials);
-                            }
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Failed to check or update credentials: " + e.getMessage());
-                    }
-                },
-                15,
-                15,
-                TimeUnit.MINUTES);
-
-        // 5. Use the client
-        try (Spanner spanner = options.getService();
-             DatabaseAdminClient databaseAdminClient = spanner.createDatabaseAdminClient()) {
-            // Perform operations...
-            // long running client operations will always use the latest credentials wrapped in
-            // mutableCredentials
-        } finally {
-            // Ensure the executor is shut down when the application exits or the client is closed
-            executorService.shutdown();
-        }
+    // 1 - create service account credentials
+    ServiceAccountCredentials serviceAccountCredentials;
+    try (FileInputStream is = new FileInputStream(credentialsPath)) {
+      serviceAccountCredentials = ServiceAccountCredentials.fromStream(is);
     }
+
+    // 2 - wrap credentials from step 1 in a MutableCredentials instance
+    MutableCredentials mutableCredentials = new MutableCredentials(serviceAccountCredentials);
+
+    // 3 - set credentials on your SpannerOptions builder to your mutableCredentials
+    SpannerOptions options = SpannerOptions.newBuilder().setCredentials(mutableCredentials).build();
+
+    // 4 - include logic for when/how to update your mutableCredentials
+    // In this example we'll use a SchedulerExecutorService to periodically check for updates
+    ThreadFactory daemonThreadFactory =
+        runnable -> {
+          Thread thread = new Thread(runnable, "spanner-mutable-credentials-rotator");
+          thread.setDaemon(true);
+          return thread;
+        };
+    ScheduledExecutorService executorService =
+        Executors.newSingleThreadScheduledExecutor(daemonThreadFactory);
+    executorService.scheduleAtFixedRate(
+        () -> {
+          try {
+            FileTime currentModifiedTime = Files.getLastModifiedTime(path);
+            if (currentModifiedTime.compareTo(lastModifiedTime.get()) > 0) {
+              lastModifiedTime.set(currentModifiedTime);
+              try (FileInputStream is = new FileInputStream(credentialsPath)) {
+                ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(is);
+                mutableCredentials.updateCredentials(credentials);
+              }
+            }
+          } catch (IOException e) {
+            System.err.println("Failed to check or update credentials: " + e.getMessage());
+          }
+        },
+        15,
+        15,
+        TimeUnit.MINUTES);
+
+    // 5. Use the client
+    try (Spanner spanner = options.getService();
+        DatabaseAdminClient databaseAdminClient = spanner.createDatabaseAdminClient()) {
+      // Perform operations...
+      // long running client operations will always use the latest credentials wrapped in
+      // mutableCredentials
+    } finally {
+      // Ensure the executor is shut down when the application exits or the client is closed
+      executorService.shutdown();
+    }
+  }
 }
 // [END spanner_mutable_credentials]
