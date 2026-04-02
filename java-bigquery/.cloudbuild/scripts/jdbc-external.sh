5025013947d7a3f702aa04656afb0cfd200725b7
Neenu1995 <neenushaji@google.com>
2026-04-02 10:55:38 -0400

    update script

--- java-bigquery/.cloudbuild/scripts/jdbc-external.sh
+++ java-bigquery/.cloudbuild/scripts/jdbc-external.sh
@@ -5,4 +5,16 @@ source .kokoro/common.sh
 install_modules java-bigquery
 
 cd ${ROOT_FOLDER}/java-bigquery/google-cloud-bigquery-jdbc/bigquery-external-jdbc-tests
-make integration-test
+
+# This pom.xml is using export vars for install
+export JDBC_VERSION=$1;
+JDBC_FOLDER=jdbc-driver
+JDBC_ZIP_NAME="SimbaJDBCDriverforGoogleBigQuery42_${JDBC_VERSION}.zip"
+JDBC_JAR_NAME=GoogleBigQueryJDBC42.jar
+export JDBC_JAR_PATH="$(pwd)/${JDBC_FOLDER}/${JDBC_JAR_NAME}"
+
+mkdir -p ${JDBC_FOLDER}
+gsutil -m cp gs://bq-dev-tools-simba-drivers-testing/simba-jdbc/${JDBC_ZIP_NAME} ${JDBC_FOLDER}
+unzip -p ./${JDBC_FOLDER}/${JDBC_ZIP_NAME} ${JDBC_JAR_NAME} > ${JDBC_JAR_PATH}
+
+mvn -B -V -fae -q install
