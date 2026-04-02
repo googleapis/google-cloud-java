f62c41a78993712542100ad4c668c556c961cced
Diego <diegomarquezp@google.com>
2026-04-02 09:28:20 -0400

    docs: update javadoc

--- sdk-platform-java/gax-java/gax/src/main/java/com/google/api/gax/tracing/ErrorTypeUtil.java
+++ sdk-platform-java/gax-java/gax/src/main/java/com/google/api/gax/tracing/ErrorTypeUtil.java
@@ -112,13 +112,13 @@ public class ErrorTypeUtil {
    * </ol>
    *
    * @param error the Throwable from which to extract the error type string.
-   * @return a low-cardinality string representing the specific error type, or {@code
-   *     ErrorType.INTERNAL.toString()} if the provided error is {@code null} or non-determined.
+   * @return a low-cardinality string representing the specific error type, or {@code null} if the
+   *     provided error is {@code null} or non-determined.
    */
   // Requirement source: go/clo:product-requirements-v1
   public static String extractErrorType(@Nullable Throwable error) {
     if (error == null) {
-      // No information about the error; we default to INTERNAL.
+      // No information about the error; we default to null.
       return null;
     }
 
