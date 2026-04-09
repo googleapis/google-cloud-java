c50bb2ed84d3cd079eff74ae4d30296b40bfc8e9
Knut Olav Løite <koloite@gmail.com>
2026-04-09 15:02:52 +0200

    chore(spanner): add LatencyTracker interface and default implementation
    Adds an internal LatencyTracker interface and a default implementation that allows the
    client to track the latency of requests. This can be used for automatic replica
    selection and load balancing.

---
+++ java-spanner/google-cloud-spanner/src/main/java/com/google/cloud/spanner/spi/v1/LatencyTracker.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spanner.spi.v1;
+
+import com.google.api.core.BetaApi;
+import com.google.api.core.InternalApi;
+
+/**
+ * Interface for tracking latency scores of Spanner servers.
+ *
+ * <p>Implementations must be thread-safe as instances may be shared across multiple concurrent
+ * operations.
+ */
+@InternalApi
+@BetaApi
+public interface LatencyTracker {
+
+  /**
+   * Returns the current latency score.
+   *
+   * @return the latency score, where lower is better.
+   */
+  double getScore();
+
+  /**
+   * Updates the latency score with a new observation.
+   *
+   * @param latencyMillis the observed latency in milliseconds.
+   */
+  void update(long latencyMillis);
+
+  /**
+   * Records an error and applies a latency penalty.
+   *
+   * @param penaltyMillis the penalty in milliseconds to apply.
+   */
+  void recordError(long penaltyMillis);
+}
