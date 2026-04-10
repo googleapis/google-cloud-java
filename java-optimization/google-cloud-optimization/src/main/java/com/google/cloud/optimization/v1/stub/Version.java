875e0b723362aa84e098ccc8afdbe07725d3a6a2
chingor13 <chingor@google.com>
2026-04-10 20:29:03 +0000

    chore(main): release 1.84.0-SNAPSHOT

--- java-optimization/google-cloud-optimization/src/main/java/com/google/cloud/optimization/v1/stub/Version.java
+++ java-optimization/google-cloud-optimization/src/main/java/com/google/cloud/optimization/v1/stub/Version.java
@@ -21,7 +21,7 @@ import com.google.api.core.InternalApi;
 @InternalApi("For internal use only")
 final class Version {
   // {x-version-update-start:google-cloud-optimization:current}
-  static final String VERSION = "1.87.0";
+  static final String VERSION = "1.88.0-SNAPSHOT";
   // {x-version-update-end}
 
 }
