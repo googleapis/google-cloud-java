15b09e582a01ce4c9eaed586224dba2b64459964
chingor13 <chingor@google.com>
2026-04-20 21:41:51 +0000

    fix test paths

--- generation/check_non_release_please_versions.sh
+++ generation/check_non_release_please_versions.sh
@@ -12,6 +12,7 @@ for pomFile in $(find . -mindepth 2 -name pom.xml | sort ); do
       [[ "${pomFile}" =~ .*java-datastore.* ]] || \
       [[ "${pomFile}" =~ .*java-logging-logback.* ]] || \
       [[ "${pomFile}" =~ .*java-bigquery.* ]] || \
+      [[ "${pomFile}" =~ .*sdk-platform-java.* ]] || \
       [[ "${pomFile}" =~ .*java-common-protos.* ]] || \
       [[ "${pomFile}" =~ .*java-showcase.* ]] || \
       [[ "${pomFile}" =~ .*java-common-protos.* ]] || \
