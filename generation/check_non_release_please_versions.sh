83ea75a3743b73aa8cd5a75d53f8d5d825a6d6ee
Diego <diegomarquezp@google.com>
2026-04-21 12:41:04 -0400

    build: move logback dependencies to jar parent

--- generation/check_non_release_please_versions.sh
+++ generation/check_non_release_please_versions.sh
@@ -19,7 +19,6 @@ for pomFile in $(find . -mindepth 2 -name pom.xml | sort ); do
       [[ "${pomFile}" =~ .*java-storage.* ]] || \
       [[ "${pomFile}" =~ .*java-storage-nio.* ]] || \
       [[ "${pomFile}" =~ .*java-vertexai.* ]] || \
-      [[ "${pomFile}" =~ .*java-compute.* ]] || \  # Skip compute due to dependency on logback
       [[ "${pomFile}" =~ .*.github*. ]]; then
     continue
   fi
