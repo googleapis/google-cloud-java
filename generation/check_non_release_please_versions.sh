4f21ce4ee40864d9f2d3df10d89bcddeae4c0dfe
chingor13 <chingor@google.com>
2026-04-23 15:43:04 +0000

    remove duplication of java-common-protos

--- generation/check_non_release_please_versions.sh
+++ generation/check_non_release_please_versions.sh
@@ -15,7 +15,6 @@ for pomFile in $(find . -mindepth 2 -name pom.xml | sort ); do
       [[ "${pomFile}" =~ .*sdk-platform-java.* ]] || \
       [[ "${pomFile}" =~ .*java-common-protos.* ]] || \
       [[ "${pomFile}" =~ .*java-showcase.* ]] || \
-      [[ "${pomFile}" =~ .*java-common-protos.* ]] || \
       [[ "${pomFile}" =~ .*java-iam.* ]] || \
       [[ "${pomFile}" =~ .*java-spanner.* ]] || \
       [[ "${pomFile}" =~ .*java-spanner-jdbc.* ]] || \
