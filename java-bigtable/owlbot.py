eaa845b64299ec7080c930f6a5657113ae64f1c0
Mike Eltsufin <meltsufin@google.com>
2026-05-08 10:49:46 -0400

    chore(java-bigtable): align versions and format owlbot configurations

--- java-bigtable/owlbot.py
+++ java-bigtable/owlbot.py
@@ -44,16 +44,18 @@ for library in s.get_staging_dirs():
     s.replace(f'{library}/**/BaseBigtable*AdminClient.java', 'public static final BaseBigtable(.*)AdminClient create\\(', 'protected static BaseBigtable\\1AdminClient create(')
     s.move(library)
 s.remove_staging_dirs()
-java.common_templates(monorepo=True, excludes=[
-    ".github/*",
-    ".kokoro/*",
-    "samples/*",
-    "CODE_OF_CONDUCT.md",
-    "CONTRIBUTING.md",
-    "LICENSE",
-    "SECURITY.md",
-    "java.header",
-    "license-checks.xml",
-    "renovate.json",
-    ".gitignore"
+java.common_templates(
+    monorepo=True,
+    excludes=[
+        ".github/*",
+        ".kokoro/*",
+        "samples/*",
+        "CODE_OF_CONDUCT.md",
+        "CONTRIBUTING.md",
+        "LICENSE",
+        "SECURITY.md",
+        "java.header",
+        "license-checks.xml",
+        "renovate.json",
+        ".gitignore"
 ])
