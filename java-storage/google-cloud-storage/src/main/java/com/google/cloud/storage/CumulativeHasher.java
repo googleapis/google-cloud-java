351729c281b051401fa3e3d84531f0135c4d0ad3
Dhriti Chopra <dhritichopra@google.com>
2026-05-25 14:53:36 +0000

    Fixing lint and small updates

--- java-storage/google-cloud-storage/src/main/java/com/google/cloud/storage/CumulativeHasher.java
+++ java-storage/google-cloud-storage/src/main/java/com/google/cloud/storage/CumulativeHasher.java
@@ -19,15 +19,16 @@ package com.google.cloud.storage;
 import com.google.api.gax.grpc.GrpcStatusCode;
 import com.google.cloud.storage.Crc32cValue.Crc32cLengthKnown;
 import com.google.protobuf.ByteString;
-import io.grpc.Status.Code;
 import com.google.storage.v2.Object;
+import io.grpc.Status.Code;
 import java.nio.ByteBuffer;
+import java.util.Locale;
 import java.util.OptionalLong;
 import java.util.function.Supplier;
 
 /**
- * A wrapper around hasher that accumulates checksums and validates them at the
- * end of the read if it was a full object read.
+ * A wrapper around hasher that accumulates checksums and validates them at the end of the read if
+ * it was a full object read.
  */
 final class CumulativeHasher implements Hasher {
   private final Hasher delegate;
@@ -53,7 +54,8 @@ final class CumulativeHasher implements Hasher {
   }
 
   @Override
-  public void validate(Crc32cValue<?> expected, Supplier<ByteBuffer> b) throws ChecksumMismatchException {
+  public void validate(Crc32cValue<?> expected, Supplier<ByteBuffer> b)
+      throws ChecksumMismatchException {
     ByteBuffer byteBuffer = b.get();
     Crc32cLengthKnown actual = delegate.hash(byteBuffer);
     if (actual != null) {
@@ -65,7 +67,8 @@ final class CumulativeHasher implements Hasher {
   }
 
   @Override
-  public void validate(Crc32cValue<?> expected, ByteString byteString) throws ChecksumMismatchException {
+  public void validate(Crc32cValue<?> expected, ByteString byteString)
+      throws ChecksumMismatchException {
     Crc32cLengthKnown actual = delegate.hash(byteString);
     if (actual != null) {
       if (expected != null && !actual.eqValue(expected)) {
@@ -106,7 +109,8 @@ final class CumulativeHasher implements Hasher {
         && (!limit.isPresent() || limit.getAsLong() >= metadata.getSize());
   }
 
-  void validateCumulativeChecksum(Object metadata) throws UncheckedCumulativeChecksumMismatchException {
+  void validateCumulativeChecksum(Object metadata)
+      throws UncheckedCumulativeChecksumMismatchException {
     if (qualifiesForVerification(metadata)) {
       Crc32cValue<?> expected = Crc32cValue.of(metadata.getChecksums().getCrc32C());
       Crc32cLengthKnown actual = getCumulativeHash();
@@ -125,7 +129,8 @@ final class CumulativeHasher implements Hasher {
   }
 }
 
-class UncheckedCumulativeChecksumMismatchException extends com.google.api.gax.rpc.DataLossException {
+final class UncheckedCumulativeChecksumMismatchException
+    extends com.google.api.gax.rpc.DataLossException {
   private static final GrpcStatusCode STATUS_CODE = GrpcStatusCode.of(Code.DATA_LOSS);
   private final Crc32cValue<?> expected;
   private final Crc32cLengthKnown actual;
@@ -133,8 +138,10 @@ class UncheckedCumulativeChecksumMismatchException extends com.google.api.gax.rp
   UncheckedCumulativeChecksumMismatchException(Crc32cValue<?> expected, Crc32cLengthKnown actual) {
     super(
         String.format(
+            Locale.US,
             "Mismatch cumulative checksum value. Expected %s actual %s",
-            expected.debugString(), actual.debugString()),
+            expected.debugString(),
+            actual.debugString()),
         /* cause= */ null,
         STATUS_CODE,
         /* retryable= */ false);
--- java-storage/google-cloud-storage/src/main/java/com/google/cloud/storage/Hasher.java
+++ java-storage/google-cloud-storage/src/main/java/com/google/cloud/storage/Hasher.java
@@ -258,4 +258,3 @@ interface Hasher {
     }
   }
 }
-
