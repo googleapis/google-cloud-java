553a4d39c1351d8b7aae1057765e7bddad021a30
Neenu1995 <neenushaji@google.com>
2026-06-17 11:32:14 -0400

    remove code duplication

--- java-bigquery-jdbc/src/main/java/com/google/cloud/bigquery/jdbc/BigQueryDatabaseMetaData.java
+++ java-bigquery-jdbc/src/main/java/com/google/cloud/bigquery/jdbc/BigQueryDatabaseMetaData.java
@@ -5301,20 +5301,11 @@ class BigQueryDatabaseMetaData implements DatabaseMetaData {
 
         @Override
         public Object get() throws InterruptedException, CancellationException {
-          if (isCancelled()) {
-            throw new CancellationException();
+          try {
+            return get(365, TimeUnit.DAYS);
+          } catch (TimeoutException e) {
+            throw new RuntimeException(e);
           }
-          while (thread.getState() != Thread.State.TERMINATED) {
-            if (isCancelled()) {
-              throw new CancellationException();
-            }
-            if (thread.getState() == Thread.State.NEW) {
-              Thread.sleep(50);
-            } else {
-              thread.join(50);
-            }
-          }
-          return null;
         }
 
         @Override
