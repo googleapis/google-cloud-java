e7809ecd2ad5f7216a2e05ac87d99b08d850907d
Lawrence Qiu <lawrenceqiu@google.com>
2026-07-08 02:05:23 +0000

    chore: optimize kokoro test job to use mvn test
    Instead of using mvn install to execute unit tests, use mvn test to
    avoid the packaging phase and skip building JAR files, speeding up
    the test lifecycle.
    Handwritten libraries share the same execution path under JOB_TYPE=test,
    and since install_modules compiles and installs their external
    dependencies to .m2 beforehand, running mvn test is sufficient and correct
    for both handwritten and generated libraries.
    
    TAG=agy
    CONV=2e2339aa-553a-429d-a821-1c9ae142d816

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -50,7 +50,7 @@ case ${JOB_TYPE} in
     fi
     echo "SUREFIRE_JVM_OPT: ${SUREFIRE_JVM_OPT}"
     retry_with_backoff 3 10 \
-      mvn install \
+      mvn test \
         -B -ntp \
         -Pquick-build \
         -Dorg.slf4j.simpleLogger.showDateTime=true \
