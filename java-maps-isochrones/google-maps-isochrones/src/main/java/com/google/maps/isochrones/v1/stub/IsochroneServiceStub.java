2ee054f75b5eaf922ee62369538efb988ef7c284
Noah Dietz <ndietz@google.com>
2026-07-17 20:25:42 +0000

    feat(maps-isochrones): onboard v1 API

---
+++ java-maps-isochrones/google-maps-isochrones/src/main/java/com/google/maps/isochrones/v1/stub/IsochroneServiceStub.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.maps.isochrones.v1.stub;
+
+import com.google.api.gax.core.BackgroundResource;
+import com.google.api.gax.rpc.UnaryCallable;
+import com.google.maps.isochrones.v1.GenerateIsochroneRequest;
+import com.google.maps.isochrones.v1.GenerateIsochroneResponse;
+import javax.annotation.Generated;
+import org.jspecify.annotations.NullMarked;
+
+// AUTO-GENERATED DOCUMENTATION AND CLASS.
+/**
+ * Base stub class for the IsochroneService service API.
+ *
+ * <p>This class is for advanced usage and reflects the underlying API directly.
+ */
+@NullMarked
+@Generated("by gapic-generator-java")
+public abstract class IsochroneServiceStub implements BackgroundResource {
+
+  public UnaryCallable<GenerateIsochroneRequest, GenerateIsochroneResponse>
+      generateIsochroneCallable() {
+    throw new UnsupportedOperationException("Not implemented: generateIsochroneCallable()");
+  }
+
+  @Override
+  public abstract void close();
+}
