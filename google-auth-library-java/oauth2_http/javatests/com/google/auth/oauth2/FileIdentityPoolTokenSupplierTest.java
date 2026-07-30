cbcce28c1339f4cb04f2aa65380c6954a8d77543
Matt Castelaz <mcastelaz@google.com>
2026-07-30 03:09:00 +0000

    chore(oauth2): update copyright year to 2026 for new files

--- google-auth-library-java/oauth2_http/javatests/com/google/auth/oauth2/FileIdentityPoolTokenSupplierTest.java
+++ google-auth-library-java/oauth2_http/javatests/com/google/auth/oauth2/FileIdentityPoolTokenSupplierTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Google LLC
+ * Copyright 2026 Google LLC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
