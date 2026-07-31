d733f1604cbb3750682ef298974ddf9a0bd90f20
Matt Castelaz <mcastelaz@google.com>
2026-07-31 13:16:01 +0000

    chore: remove remaining scratch scripts and format jars

--- get_reviews.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import json
-req = urllib.request.Request('https://api.github.com/repos/googleapis/google-cloud-java/pulls/13901/comments')
-with urllib.request.urlopen(req) as response:
-    comments = json.loads(response.read().decode())
-    for c in comments:
-        print(f"[{c['user']['login']}] {c['path']}:{c.get('line')}\n{c['body']}\n")
