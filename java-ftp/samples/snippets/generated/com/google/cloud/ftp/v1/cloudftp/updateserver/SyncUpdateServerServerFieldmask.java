f41b2d9becf930eb38842e75765d8d800eac4692
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-08-13 17:25:40 +0000

    feat(ftp): onboard a new library (#14068)
    b/543712622
    
    PiperOrigin-RevId: 960764304

---
+++ java-ftp/samples/snippets/generated/com/google/cloud/ftp/v1/cloudftp/updateserver/SyncUpdateServerServerFieldmask.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.ftp.v1.samples;
+
+// [START ftp_v1_generated_CloudFtp_UpdateServer_ServerFieldmask_sync]
+import com.google.cloud.ftp.v1.CloudFtpClient;
+import com.google.cloud.ftp.v1.Server;
+import com.google.protobuf.FieldMask;
+
+public class SyncUpdateServerServerFieldmask {
+
+  public static void main(String[] args) throws Exception {
+    syncUpdateServerServerFieldmask();
+  }
+
+  public static void syncUpdateServerServerFieldmask() throws Exception {
+    // This snippet has been automatically generated and should be regarded as a code template only.
+    // It will require modifications to work:
+    // - It may require correct/in-range values for request initialization.
+    // - It may require specifying regional endpoints when creating the service client as shown in
+    // https://cloud.google.com/java/docs/setup#configure_endpoints_for_the_client_library
+    try (CloudFtpClient cloudFtpClient = CloudFtpClient.create()) {
+      Server server = Server.newBuilder().build();
+      FieldMask updateMask = FieldMask.newBuilder().build();
+      Server response = cloudFtpClient.updateServerAsync(server, updateMask).get();
+    }
+  }
+}
+// [END ftp_v1_generated_CloudFtp_UpdateServer_ServerFieldmask_sync]
