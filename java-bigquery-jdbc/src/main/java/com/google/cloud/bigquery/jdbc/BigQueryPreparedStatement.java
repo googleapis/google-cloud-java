73d0dd7662438e4522d59e3409126ef7024b752b
Neenu1995 <neenushaji@google.com>
2026-09-04 14:05:14 -0400

    resolve imports

--- java-bigquery-jdbc/src/main/java/com/google/cloud/bigquery/jdbc/BigQueryPreparedStatement.java
+++ java-bigquery-jdbc/src/main/java/com/google/cloud/bigquery/jdbc/BigQueryPreparedStatement.java
@@ -68,6 +68,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class BigQueryPreparedStatement extends BigQueryStatement implements PreparedStatement {
