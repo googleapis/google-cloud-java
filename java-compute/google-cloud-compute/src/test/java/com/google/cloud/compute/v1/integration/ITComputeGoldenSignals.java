a0011a153b99f10e162d4543597b58df596fc54d
Blake Li <blakeli@google.com>
2026-09-08 21:26:19 +0000

    fix(compute): consolidate Awaitility exception ignorer to match GAX and gRPC exceptions

--- java-compute/google-cloud-compute/src/test/java/com/google/cloud/compute/v1/integration/ITComputeGoldenSignals.java
+++ java-compute/google-cloud-compute/src/test/java/com/google/cloud/compute/v1/integration/ITComputeGoldenSignals.java
@@ -24,6 +24,7 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import com.google.api.gax.rpc.NotFoundException;
+import com.google.api.gax.rpc.ResourceExhaustedException;
 import com.google.api.gax.tracing.ApiTracerFactory;
 import com.google.api.gax.tracing.BaseApiTracerFactory;
 import com.google.api.gax.tracing.CompositeTracerFactory;
@@ -296,14 +297,15 @@ public class ITComputeGoldenSignals extends BaseTest {
             .atMost(Duration.ofMinutes(2))
             .pollDelay(Duration.ofSeconds(3))
             .pollInterval(Duration.ofSeconds(3))
-            .ignoreExceptionsInstanceOf(NotFoundException.class)
             .ignoreExceptionsMatching(
                 e ->
-                    e instanceof StatusRuntimeException
-                        && (((StatusRuntimeException) e).getStatus().getCode()
-                                == Status.Code.NOT_FOUND
-                            || ((StatusRuntimeException) e).getStatus().getCode()
-                                == Status.Code.RESOURCE_EXHAUSTED))
+                    e instanceof NotFoundException
+                        || e instanceof ResourceExhaustedException
+                        || (e instanceof StatusRuntimeException
+                            && (((StatusRuntimeException) e).getStatus().getCode()
+                                    == Status.Code.NOT_FOUND
+                                || ((StatusRuntimeException) e).getStatus().getCode()
+                                    == Status.Code.RESOURCE_EXHAUSTED)))
             .until(
                 () -> traceClient.getTrace(DEFAULT_PROJECT, traceId),
                 t -> t != null && t.getSpansCount() > 0);
