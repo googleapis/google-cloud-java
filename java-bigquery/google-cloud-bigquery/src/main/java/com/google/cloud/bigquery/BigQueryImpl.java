4aca5676b1028667f2fe771dd7fb97e106f150d0
Jin Seop Kim <jinseop@google.com>
2026-09-11 11:37:17 -0400

    refactor(bigquery): remove redundant Schema cast in queryRpc

--- java-bigquery/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryImpl.java
+++ java-bigquery/google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryImpl.java
@@ -2293,9 +2293,7 @@ final class BigQueryImpl extends BaseService<BigQueryOptions> implements BigQuer
       if (isArrow) {
         arrowSchemaBytes = results.getArrowSchema().decodeSerializedSchema();
         try {
-          arrowSchemaPojo =
-              (org.apache.arrow.vector.types.pojo.Schema)
-                  ArrowDeserializer.deserializeSchema(arrowSchemaBytes);
+          arrowSchemaPojo = ArrowDeserializer.deserializeSchema(arrowSchemaBytes);
         } catch (IOException e) {
           throw new BigQueryException(0, "Failed to deserialize Arrow schema from response", e);
         }
