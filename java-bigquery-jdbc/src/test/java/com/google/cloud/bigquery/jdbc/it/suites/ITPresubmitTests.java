d192a4d901902231f8464e58419d805bdf1ec757
Keshav Dandeva <keshavdandeva@google.com>
2026-09-15 19:21:39 +0000

    test(bigquery-jdbc): add picosecond timestamp integration tests

--- java-bigquery-jdbc/src/test/java/com/google/cloud/bigquery/jdbc/it/suites/ITPresubmitTests.java
+++ java-bigquery-jdbc/src/test/java/com/google/cloud/bigquery/jdbc/it/suites/ITPresubmitTests.java
@@ -23,6 +23,7 @@ import com.google.cloud.bigquery.jdbc.it.ITConnectionPoolingTest;
 import com.google.cloud.bigquery.jdbc.it.ITConnectionTest;
 import com.google.cloud.bigquery.jdbc.it.ITDatabaseMetadataTest;
 import com.google.cloud.bigquery.jdbc.it.ITDriverTest;
+import com.google.cloud.bigquery.jdbc.it.ITJdbcTimestampPicosTest;
 import com.google.cloud.bigquery.jdbc.it.ITLocalSslValidationTest;
 import com.google.cloud.bigquery.jdbc.it.ITResultSetMetadataTest;
 import com.google.cloud.bigquery.jdbc.it.ITStatementTest;
@@ -38,6 +39,7 @@ import org.junit.platform.suite.api.Suite;
   ITConnectionPoolingTest.class,
   ITDatabaseMetadataTest.class,
   ITDriverTest.class,
+  ITJdbcTimestampPicosTest.class,
   ITLocalSslValidationTest.class,
   ITResultSetMetadataTest.class,
   ITStatementTest.class
