21e6dc8cd20242c7b7be5e1fc81f33be0d084a7c
Kirill Logachev <kirl@google.com>
2026-09-18 12:03:02 -0700

    fix(bigquery-jdbc): fix htapi fallback due to permission logic (#14418)

--- java-bigquery-jdbc/src/test/java/com/google/cloud/bigquery/jdbc/it/ITBase.java
+++ java-bigquery-jdbc/src/test/java/com/google/cloud/bigquery/jdbc/it/ITBase.java
@@ -284,7 +284,7 @@ public class ITBase extends BigQueryJdbcBaseTest {
   public static final BigQuery bigQuery = BigQueryJdbcBaseTest.getBigQuery(connectionUrl);
 
   public static final String FORCE_READ_API_PROPERTIES =
-      "EnableHighThroughputAPI=1;HighThroughputActivationRatio=0;HighThroughputMinTableSize=0;MaxResults=1;";
+      ";EnableHighThroughputAPI=1;HighThroughputActivationRatio=0;HighThroughputMinTableSize=0;";
 
   public static final String createDatasetQuery =
       "CREATE SCHEMA IF NOT EXISTS `%s.%s` OPTIONS(default_table_expiration_days = 5)";
@@ -460,10 +460,16 @@ public class ITBase extends BigQueryJdbcBaseTest {
   }
 
   public static void validateStatement(Statement stmt, int expectedRows) throws SQLException {
+    validateStatement(stmt, expectedRows, "ResultSet");
+  }
+
+  public static <T> void validateStatement(Statement stmt, int expectedRows, String clazz)
+      throws SQLException {
     String query = "SELECT * FROM UNNEST(GENERATE_ARRAY(1, " + expectedRows + "))";
     assertTrue(stmt.execute(query));
     int count = 0;
     try (ResultSet rs = stmt.getResultSet()) {
+      assertTrue(rs.getClass().getName().contains(clazz));
       while (rs.next()) {
         count++;
       }
