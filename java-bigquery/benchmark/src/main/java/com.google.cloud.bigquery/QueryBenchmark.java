f5a0da3e6687ad13e4694ab838e496a2fb8b1048
Jin Seop Kim <jinseop@google.com>
2026-09-21 16:45:52 -0400

    chore(bigquery): remove simple-path benchmarks from QueryBenchmark

--- java-bigquery/benchmark/src/main/java/com.google.cloud.bigquery/QueryBenchmark.java
+++ java-bigquery/benchmark/src/main/java/com.google.cloud.bigquery/QueryBenchmark.java
@@ -16,7 +16,6 @@
 
 package com.google.cloud.bigquery;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.apache.arrow.vector.VectorSchemaRoot;
@@ -121,46 +120,6 @@ public class QueryBenchmark {
     try (ArrowQueryResult result = bigquery.queryArrow(config)) {
       for (VectorSchemaRoot root : result) {
         blackhole.consume(root);
-      }
-    }
-  }
-
-  @Benchmark
-  public void querySimplePath(QueryParams queryParams, Blackhole blackhole) throws Exception {
-    TableResult result =
-        bigquery.query(
-            QueryJobConfiguration.newBuilder(queryParams.queries).setUseLegacySql(false).build());
-    for (FieldValueList row : result.getValues()) {
-      blackhole.consume(row);
-    }
-  }
-
-  @Benchmark
-  public void queryWithArrowRowBasedSimplePath(QueryParams queryParams, Blackhole blackhole)
-      throws Exception {
-    QueryJobConfiguration config =
-        QueryJobConfiguration.newBuilder(queryParams.queries)
-            .setUseLegacySql(false)
-            .setQueryResultsFormat(QueryResultsFormat.ARROW)
-            .build();
-    TableResult result = bigquery.query(config);
-    for (FieldValueList row : result.getValues()) {
-      blackhole.consume(row);
-    }
-  }
-
-  @Benchmark
-  public void queryWithArrowZeroCopySimplePath(QueryParams queryParams, Blackhole blackhole)
-      throws Exception {
-    QueryJobConfiguration config =
-        QueryJobConfiguration.newBuilder(queryParams.queries)
-            .setUseLegacySql(false)
-            .setQueryResultsFormat(QueryResultsFormat.ARROW)
-            .build();
-    try (ArrowQueryResult result = bigquery.queryArrow(config)) {
-      Iterator<VectorSchemaRoot> it = result.iterator();
-      if (it.hasNext()) {
-        blackhole.consume(it.next());
       }
     }
   }
