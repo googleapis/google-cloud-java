e72365ea11701876f6d6c3a257bf2b7fa229ea4f
Will Howes <whowes@google.com>
2026-09-25 16:04:31 -0700

    feat(gax): retry chunk upload on transient errors (#14422)
    Wraps chunk uploads in a `RetryingCallable` to retry transient errors.
    
    Note: the retry settings used here are intentionally different from
    those that will be used for starting the upload; our requirement for
    this milestone is that "the retry policy [specified by the user] is only
    applicable to the initial request."

--- sdk-platform-java/gax-java/gax/src/test/java/com/google/api/gax/rpc/CallableTest.java
+++ sdk-platform-java/gax-java/gax/src/test/java/com/google/api/gax/rpc/CallableTest.java
@@ -214,6 +214,8 @@ class CallableTest {
   void testResumableUploadCallable() {
     ResumableUploadClient<String, String> uploadClient =
         mock(ResumableUploadClient.class, Mockito.withSettings().withoutAnnotations());
+    when(uploadClient.uploadChunkCallable())
+        .thenReturn(mock(UnaryCallable.class, Mockito.withSettings().withoutAnnotations()));
     ResumableUploadCallSettings settings =
         ResumableUploadCallSettings.newBuilder().setChunkSize(1024).build();
 
